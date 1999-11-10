842830330eb8bedc624c09df2013b3bfb0fcff90
livesey <>
1999-11-10 01:52:01 +0000

    Transfer to home

--- sandbox/livesey/test.f90
+++ sandbox/livesey/test.f90
@@ -1,40 +1,54 @@
 PROGRAM Test
-USE SignalsFile
+USE MLSSignalNomenclature
 IMPLICIT NONE
 
-TYPE (MLS_SignalsDatabase_T) :: database
+TYPE (MLSSignalsDatabase_T) :: database
+TYPE (MLSSignal_T), POINTER, DIMENSION(:) :: signal
 INTEGER :: i
+CHARACTER (LEN=120) :: request
 
-PRINT*,"Hello"
 OPEN(UNIT=9,FILE="../../tables/emls-signals.dat",STATUS="OLD",ACTION="READ")
 CALL ReadSignalsDatabase(9,database)
 CLOSE(UNIT=9)
-DO i=0,database%noRadiometers-1
-   PRINT*,'--------------------'
-   PRINT*,database%radiometerInfo(i)%name
-   PRINT*,database%radiometerInfo(i)%lo
-   PRINT*,database%radiometerInfo(i)%prefix
-   PRINT*,database%radiometerInfo(i)%suffix
-   PRINT*,database%radiometerInfo(i)%number
-ENDDO
+! DO i=0,database%noRadiometers-1
+!    PRINT*,'--------------------'
+!    PRINT*,database%radiometerInfo(i)%name
+!    PRINT*,database%radiometerInfo(i)%lo
+!    PRINT*,database%radiometerInfo(i)%prefix
+!    PRINT*,database%radiometerInfo(i)%suffix
+!    PRINT*,database%radiometerInfo(i)%number
+! ENDDO
 
-DO i=0,database%noBands-1
-   PRINT*,'--------------------'
-   PRINT*,database%bandInfo(i)%name
-   PRINT*,database%bandInfo(i)%suffix
-   PRINT*,database%bandInfo(i)%number
-   PRINT*,database%bandInfo(i)%centerFreqIF
-   PRINT*,database%bandInfo(i)%spectrometerFamily
-   PRINT*,database%bandInfo(i)%spectrometerFamilyIndex
-ENDDO
+! DO i=0,database%noBands-1
+!    PRINT*,'--------------------'
+!    PRINT*,database%bandInfo(i)%name
+!    PRINT*,database%bandInfo(i)%suffix
+!    PRINT*,database%bandInfo(i)%number
+!    PRINT*,database%bandInfo(i)%centerFreqIF
+!    PRINT*,database%bandInfo(i)%spectrometerFamily
+!    PRINT*,database%bandInfo(i)%spectrometerFamilyIndex
+! ENDDO
 
-DO i=0,database%noSpectrometers-1
-   PRINT*,'--------------------'
-   PRINT*,database%spectrometerInfo(i)%name
-   PRINT*,database%spectrometerInfo(i)%fullFamily
-   PRINT*,database%spectrometerInfo(i)%number
-   PRINT*,database%spectrometerInfo(i)%familyIndex
-ENDDO   
+! DO i=0,database%noSpectrometers-1
+!    PRINT*,'--------------------'
+!    PRINT*,database%spectrometerInfo(i)%name
+!    PRINT*,database%spectrometerInfo(i)%fullFamily
+!    PRINT*,database%spectrometerInfo(i)%number
+!    PRINT*,database%spectrometerInfo(i)%familyIndex
+! ENDDO   
+
+loop: DO
+   READ*,request
+   IF (TRIM(request)=="DONE") EXIT loop
+   CALL ParseMLSSignalRequest(request,database,signal)
+   DO i=LBOUND(signal,1),UBOUND(signal,1)
+      CALL GetFullMLSSignalName(signal(i),request)
+      PRINT*,'You mean: '//TRIM(request)
+      PRINT*,'Position: ',PACK(signal(i)%channelPosition+signal(i)%lo, &
+           & signal(i)%channelIncluded)
+      signal(i)%channelPosition=0.0
+   ENDDO
+END DO loop
 END PROGRAM Test
 
 
