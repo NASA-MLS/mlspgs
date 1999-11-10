deff0dfe04bc445b76827403b99038cdc1d903ab
livesey <>
1999-11-10 03:39:31 +0000

    Fixed some bugs and made to work under absoft f90

--- sandbox/livesey/test.f90
+++ sandbox/livesey/test.f90
@@ -10,32 +10,6 @@ CHARACTER (LEN=120) :: request
 OPEN(UNIT=9,FILE="../../tables/emls-signals.dat",STATUS="OLD",ACTION="READ")
 CALL ReadSignalsDatabase(9,database)
 CLOSE(UNIT=9)
-! DO i=0,database%noRadiometers-1
-!    PRINT*,'--------------------'
-!    PRINT*,database%radiometerInfo(i)%name
-!    PRINT*,database%radiometerInfo(i)%lo
-!    PRINT*,database%radiometerInfo(i)%prefix
-!    PRINT*,database%radiometerInfo(i)%suffix
-!    PRINT*,database%radiometerInfo(i)%number
-! ENDDO
-
-! DO i=0,database%noBands-1
-!    PRINT*,'--------------------'
-!    PRINT*,database%bandInfo(i)%name
-!    PRINT*,database%bandInfo(i)%suffix
-!    PRINT*,database%bandInfo(i)%number
-!    PRINT*,database%bandInfo(i)%centerFreqIF
-!    PRINT*,database%bandInfo(i)%spectrometerFamily
-!    PRINT*,database%bandInfo(i)%spectrometerFamilyIndex
-! ENDDO
-
-! DO i=0,database%noSpectrometers-1
-!    PRINT*,'--------------------'
-!    PRINT*,database%spectrometerInfo(i)%name
-!    PRINT*,database%spectrometerInfo(i)%fullFamily
-!    PRINT*,database%spectrometerInfo(i)%number
-!    PRINT*,database%spectrometerInfo(i)%familyIndex
-! ENDDO   
 
 loop: DO
    READ*,request
@@ -44,9 +18,7 @@ loop: DO
    DO i=LBOUND(signal,1),UBOUND(signal,1)
       CALL GetFullMLSSignalName(signal(i),request)
       PRINT*,'You mean: '//TRIM(request)
-      PRINT*,'Position: ',PACK(signal(i)%channelPosition+signal(i)%lo, &
-           & signal(i)%channelIncluded)
-      signal(i)%channelPosition=0.0
+      PRINT*,signal(i)%channelIncluded
    ENDDO
 END DO loop
 END PROGRAM Test
