0d23219abf02a0c6c22c8a7b6910256c7aa757d4
pumphrey <>
1999-12-06 14:38:50 +0000

    Added a few text and shell files to mlspgs tree

---
+++ sandbox/pumphrey/fvs.sh
@@ -0,0 +1,8 @@
+## source this to set up for FMKMF
+
+export FMKMF_F90="f90"
+export FMKMF_SPATH=.:../../lib
+
+
+
+
---
+++ sandbox/pumphrey/plpvs.sh
@@ -0,0 +1,9 @@
+## source this to set up for FMKMF with plplot.
+export FMKMF_F90="f90 -B108 -YEXT_NAMES=LCS"
+export FMKMF_SPATH=.:../../lib:./plp
+export FMKMF_LINKOPTS="-lplplotdX -lf2c " 
+
+
+
+
+
