5f122d276b658be2c4e70b6ed5d3867cdd3b87af
livesey <>
1999-12-18 03:00:46 +0000

    First version

---
+++ lib/ConstructVectorTemplates.f90
@@ -0,0 +1,86 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
+!=============================================================================
+MODULE ConstructVectorTemplates ! Construct a template for a vector
+!=============================================================================
+
+  USE MLSCommon
+  USE MLSMessageModule
+  USE VectorsModule
+  USE QuantityTemplates
+  USE Temporary_Types
+
+  IMPLICIT NONE
+  PUBLIC
+  
+  PRIVATE :: Id, ModuleName
+  !---------------------------- RCS Ident Info -------------------------------
+  CHARACTER (LEN=256) :: Id = &
+       "$Id$"
+  CHARACTER (LEN=*), PARAMETER :: ModuleName= "$RCSfile$"
+  !---------------------------------------------------------------------------
+
+  ! This module performs the vector template aspects of the construct task
+
+CONTAINS
+
+  ! This is the main routine for this module
+
+  SUBROUTINE CreateVecTemplateFromMLSCfInfo(cfInfo,vectorTemplate,&
+       & quantityTemplates)
+
+    ! Dummy arguments
+    TYPE(l2cfEntry), INTENT(IN) :: cfInfo
+    TYPE (VectorTemplate_T), INTENT(OUT) :: vectorTemplate
+    TYPE (QuantityTemplate_T), DIMENSION(:) :: quantityTemplates
+
+    ! Local variables
+    INTEGER :: keyNo,status,quantityNo
+    CHARACTER (LEN=NameLen) :: name=""
+    LOGICAL, DIMENSION(:), ALLOCATABLE :: selected
+    TYPE (L2CFCell) :: cell
+
+    ! Executable code
+
+    ALLOCATE(selected(SIZE(quantityTemplates)),STAT=status)
+    IF (status/=0) CALL MLSMessage(MLSMSG_Error,ModuleName,MLSMSG_Allocate//&
+         & "selected")
+    selected=.FALSE.
+
+    ! Simply loop through the l2cf information supplied.
+    !  If it's a NAME= then copy it to name, else it's a
+    ! quantityTemplate name.
+
+    DO keyNo=1,cfInfo%l2cfEntryNoKeys
+       cell=cfInfo%cells(keyNo)
+       IF (TRIM(cell%keyword)=="NAME") THEN
+          name=cell%charValue
+       ELSE
+          quantityNo=LinearSearchStringArray(quantityTemplates%name,&
+               & cell%charValue,caseInsensitive=.TRUE.)
+          IF (quantityNo==0) CALL MLSMessage(MLSMSG_Error,ModuleName,&
+               & "No such quantityTemplate: "//cell%charValue)
+          selected(quantityNo)=.TRUE.
+       ENDIF
+    END DO
+
+    IF (LEN_TRIM(name)==0) CALL MLSMessage(MLSMSG_Error,ModuleName,&
+         & "No name given for vector")
+    IF (COUNT(selected)==0) CALL MLSMessage(MLSMSG_Error,ModuleName,&
+         & "No quantities chosen for vector: "//name)
+
+    ! Now finally construct the vector template
+
+    CALL ConstructVectorTemplate(name,PACK(quantityTemplates,selected),&
+         & vectorTemplate)
+
+  END SUBROUTINE CreateVecTemplateFromMLSCfInfo
+
+!=============================================================================
+END MODULE ConstructVectorTemplates
+!=============================================================================
+
+!
+! $Log$
+!
---
+++ lib/MLSPCF.f90
@@ -0,0 +1,28 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! NOTE: This module is automatically created by the makemlspcfdatamodule
+!       perl script.  Do *NOT* attempt to modify this by hand.
+!
+MODULE MLSPCF
+   INTEGER, PARAMETER :: mlspcf_l1b_oa_start = 20000                 
+   INTEGER, PARAMETER :: mlspcf_l1b_oa_end = 20039
+   !
+   INTEGER, PARAMETER :: mlspcf_l1b_rad_start = 20040
+   INTEGER, PARAMETER :: mlspcf_l1b_rad_end = 20079
+   !
+   INTEGER, PARAMETER :: mlspcf_l1b_log_start = 20080
+   INTEGER, PARAMETER :: mlspcf_l1b_log_end = 20080
+   !
+   INTEGER, PARAMETER :: mlspcf_l1b_eng_start = 20081
+   INTEGER, PARAMETER :: mlspcf_l1b_eng_end = 20081
+   !
+   INTEGER, PARAMETER :: mlspcf_l2gp_start = 21000                 
+   INTEGER, PARAMETER :: mlspcf_l2gp_end = 21439
+   !
+   INTEGER, PARAMETER :: mlspcf_l2aux_start = 21440
+   INTEGER, PARAMETER :: mlspcf_l2aux_end = 21479
+   !
+   INTEGER, PARAMETER :: mlspcf_l2fwm_start = 21480
+   INTEGER, PARAMETER :: mlspcf_l2fwm_end = 21480
+   !
+END MODULE MLSPCF
