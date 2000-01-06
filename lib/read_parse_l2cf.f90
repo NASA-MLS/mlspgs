a81cd9c51be32983f985074000ea68e630df95f8
lungu <>
2000-01-06 02:37:22 +0000

    Made a module out of read_parse_l2cf, strip_blanks and data structures.

--- lib/read_parse_l2cf.f90
+++ lib/read_parse_l2cf.f90
@@ -2,14 +2,14 @@
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
 
-!===================================
-SUBROUTINE read_parse_l2cf (L2cf_data, returnStatus) 
-!===================================
-
-USE l2cf
-USE MLSMessageModule
-
+!=============================================================================
+MODULE ReadParseL2cf         ! Read and Parse the l2 Configuration File
+!=============================================================================
+  
 IMPLICIT NONE
+PUBLIC
+
+PRIVATE :: Id, ModuleName
 
 
 !------------------- RCS Ident Info -----------------------
@@ -17,20 +17,178 @@ CHARACTER(LEN=130) :: Id = &
 "$Id$"
 CHARACTER (LEN=*), PARAMETER :: ModuleName="$RCSfile$"
 !----------------------------------------------------------
+ 
+integer, parameter :: MaxKeyLen = 25
+integer, parameter :: MaxTypeLen = 6
+integer, parameter :: MaxNOL2cfKeys = 20
+integer, parameter :: MaxCharValueLen =25
+integer, parameter :: UnitsLen = 10
+integer, parameter :: L2cfEntryLen = 25
+integer, parameter :: MaxNoKeysPerEntry = 10
+integer, parameter :: MaxNoEntriesPerSection = 15
 
-! Brief description of program
+type l2cfKey
+  character (len=MaxKeyLen) :: Keyword
+  character (len=MaxTypeLen):: Type
+  integer :: Keylen
+end type l2cfKey
+
+
+
+TYPE L2cfCell
+
+   ! This datatype defines a L2cf "cell", i.e. a contruction of type:
+   ! "keyword=value" or "keyword=LowerBound..UpperBound[units]"
+
+   ! First the "keyword" holder:
+
+   CHARACTER (len=MaxKeyLen) :: Keyword
+   ! Value holder for "real" type
+
+   DOUBLE PRECISION :: RealValue
+
+   ! Value holder for integer type
+
+   INTEGER :: IntValue
+
+   ! Value holder for "character" type
+
+   CHARACTER (len= MaxCharValueLen) :: CharValue
+
+   ! Value holder for "units" field
+
+   CHARACTER (len=UnitsLen) :: Units
+
+   ! Value holder for upper and lower bounds fields
+
+   DOUBLE PRECISION :: RangeLowerBound
+   DOUBLE PRECISION :: RangeUpperBound
+
+
+END TYPE L2cfCell
+
+
+TYPE L2cfEntry
+
+   ! This datatype defines am L2cf entry, i.e. a name followed by
+   ! a list of "keyword=value" cells
+ 
+   ! Entry name, e.g. aprioriTemp
+
+   CHARACTER (len=L2cfEntryLen) L2cfEntryName
+
+   ! Number of "keyword=value" cells
+
+   INTEGER L2cfEntryNoKeys
+
+   !Cells holder 
+
+   TYPE (L2cfCell), DIMENSION(MaxNoKeysPerEntry) :: Cells
+
+END TYPE L2cfEntry
+
+
+TYPE L2cfSection
+
+
+   ! For each section:
+   ! Name, Number of entries within section
+   CHARACTER (len=L2cfEntryLen) L2cfSectionName
+   INTEGER NoSectionEntries
+
+   ! The actual entries:
+
+   TYPE (L2cfEntry), DIMENSION(MaxNoEntriesPerSection) :: Entries ! (NoSectionEntries)
+
+
+END TYPE L2cfSection
+
+
+
+
+TYPE L2cf
+
+! This datatype defines the l2cf as an array of L2cfSections
+
+  INTEGER NoSections
+
+  TYPE (L2cfSection), POINTER, DIMENSION (:) :: Sections ! (NoSections) 
+
+END TYPE l2cf
+
+
+
+! --------------------------------------------------------------------------
+
+CONTAINS
+
+
+!========================================
+SUBROUTINE Strip_Blanks (line, nc, ncnb)
+!========================================
+ 
+
+CHARACTER (LEN=*) :: line
+INTEGER :: nc, ncnb
+INTEGER :: i, ncc
+
+LOGICAL :: done = .FALSE.
+
+ncc = nc
+
+! check for blank line
+CALL TRIM(line)
+IF (LEN(line) .eq.  0)THEN
+  ncnb = 0
+  RETURN
+END IF
+
+
+! ignore trailing comments
+
+i= INDEX(line, ';')
+IF(  i /= 0)THEN
+  ncc = i-1
+END IF
+
+! replace double blanks by single ones
+
+DO WHILE (.NOT. done)
+  i = INDEX(line(1:ncc), '  ')
+  IF (i .eq. 0)THEN
+    done =.TRUE.
+    ncnb = ncc
+  ELSE 
+    line(i:ncc-1)= line (i+1:ncc)
+    ncc = ncc-1
+  END IF
+END DO
+
+RETURN
+END SUBROUTINE Strip_Blanks
+
+
+!===================================
+SUBROUTINE read_parse_l2cf (L2cf_data, returnStatus) 
+!===================================
+
 ! This subroutine reads and parses L2CF and does some preliminary checking on 
 ! tokens.
 
 ! Arguments
 
 TYPE (l2cf), INTENT (OUT) :: L2cf_data
+
+type (l2cfKey), dimension(MaxNoL2cfKeys) :: L2cfTable
+
+
+
 ! Parameters
 
 
 CHARACTER (LEN=*), PARAMETER :: lineFmt = "(q,<nc>a1)"
 
-INTEGER, PARAMETER :: L2CF = 3000
+INTEGER, PARAMETER :: L2CFUnit = 3000
 
 ! Functions
 
@@ -49,6 +207,67 @@ INTEGER :: nc, returnStatus, version, isection, igs
 
 LOGICAL :: eof = .FALSE.
 
+L2cfTable(1)%Keyword = 'InputVersionString       '
+L2cfTable(1)%Type = 'string'
+L2cfTable(1)%Keylen =  18
+L2cfTable(2)%Keyword = 'OutputVersionString      '
+L2cfTable(2)%Type = 'string'
+L2cfTable(2)%Keylen =  19
+L2cfTable(3)%Keyword = 'AllowClimatologyOverloads'
+L2cfTable(3)%Type = 'string'
+L2cfTable(3)%Keylen =  25
+L2cfTable(4)%Keyword = 'source                   '
+L2cfTable(4)%Type = 'string'
+L2cfTable(4)%Keylen =  6
+L2cfTable(5)%Keyword = 'length                   '
+L2cfTable(5)%Type = 'real  '
+L2cfTable(5)%Keylen =  6
+L2cfTable(6)%Keyword = 'versionRange             '
+L2cfTable(6)%Type = 'string'
+L2cfTable(6)%Keylen =  12
+L2cfTable(7)%Keyword = 'species                  '
+L2cfTable(7)%Type = 'string'
+L2cfTable(7)%Keylen =  7
+L2cfTable(8)%Keyword = 'range                    '
+L2cfTable(8)%Type = 'range '
+L2cfTable(8)%Keylen =  5
+L2cfTable(9)%Keyword = 'method                   '
+L2cfTable(9)%Type = 'string'
+L2cfTable(9)%Keylen =  6
+L2cfTable(10)%Keyword = 'scale                    '
+L2cfTable(10)%Type = 'real  '
+L2cfTable(10)%Keylen =  5
+L2cfTable(11)%Keyword ='height                   '
+L2cfTable(11)%Type ='real  '
+L2cfTable(11)%Keylen = 6
+L2cfTable(12)%Keyword ='Overlap                  '
+L2cfTable(12)%Type ='int   '
+L2cfTable(12)%Keylen = 7
+L2cfTable(13)%Keyword ='IdealLength              '
+L2cfTable(13)%Type ='real  '
+L2cfTable(13)%Keylen = 11
+L2cfTable(14)%Keyword ='HomeLatitude             '
+L2cfTable(14)%Type ='real  '
+L2cfTable(14)%Keylen = 12
+L2cfTable(15)%Keyword ='CriticalScanningModules  '
+L2cfTable(15)%Type ='string'
+L2cfTable(15)%Keylen = 23
+L2cfTable(16)%Keyword ='CriticalBands            '
+L2cfTable(16)%Type = 'string'
+L2cfTable(16)%Keylen = 13
+L2cfTable(17)%Keyword ='MaxGap                   '
+L2cfTable(17)%Type = 'real  '
+L2cfTable(17)%Keylen = 6
+L2cfTable(18)%Keyword = 'type                    '
+L2cfTable(18)%Type ='string' 
+L2cfTable(18)%Keylen = 4
+L2cfTable(19)%Keyword = 'fraction                 '
+L2cfTable(19)%Type ='real  '
+L2cfTable(19)%Keylen = 8
+L2cfTable(20)%Keyword = 'interpolationfactor      '
+L2cfTable(20)%Type ='real  '
+L2cfTable(20)%Keylen = 19
+
 version = 1
 isection = 0
 igs=0
@@ -60,8 +279,8 @@ returnStatus = Pgs_io_gen_openF (L2CF, PGSd_IO_Gen_RSeqFrm, 0, &
 IF (returnStatus /= PGS_S_SUCCESS) THEN
 
   CALL Pgs_smf_getMsg(returnStatus, mnemonic, msg)
-  CALL MSMessage (MLSMSG_Error,
-         & ModuleName, "Error opening L2CF:  "//mnemonic//" "//msg)
+  CALL MSMessage (MLSMSG_Error, &
+                  ModuleName, "Error opening L2CF:  "//mnemonic//" "//msg)
 
 ENDIF
 
@@ -238,7 +457,7 @@ DO WHILE (.NOT. eof)
      END IF
 
    END IF
-
+  END IF
  END IF
 
 END DO
@@ -256,8 +475,15 @@ ENDIF
 
 
 
-!===================
-END subroutine  read_parse_l2cf
-!===================
+!==============================
+END SUBROUTINE  read_parse_l2cf
+!==============================
+
+!=======================
+END MODULE ReadParseL2cf
+!=======================
 
 ! $Log$
+! Revision 1.4  2000/01/04 19:46:23  lungu
+! Changed error handling using MSMessage
+!
