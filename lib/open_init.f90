415a074654fa6da5ab1caf157a504b57f53024d9
lungu <>
2000-01-07 19:50:13 +0000

    "Clean" version, ready to handle read_parse_l2cf.

--- lib/open_init.f90
+++ lib/open_init.f90
@@ -4,14 +4,16 @@
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
 !=============================================================================
-MODULE OpenInit ! Opens and reads l2cf info, NCEP, DAO and climatology(ies) file
-s
+MODULE OpenInit ! Opens and reads l2cf info, NCEP, DAO and climatology files
 !=============================================================================
 
 
 USE MLSCommon
+USE MLSL2Common
 USE MLSMessageModule
-USE GriddedData
+USE ReadParseL2cf
+!USE GriddedData
+USE MLSPCF
 
 IMPLICIT NONE
 PUBLIC
@@ -23,43 +25,56 @@ CHARACTER(LEN=130) :: id = &
 CHARACTER(LEN=*), PARAMETER :: ModuleName="$RCSfile$"
 !-----------------------------------------------------------------------------
 CONTAINS
+SUBROUTINE OpenAndInitialize(processingRange, l1bInfo, l2cf_data, aprioriData)
 
-Subroutine Open_Init (returnStatus )
+! Arguments
+
+
+TYPE (DataProcessingRange_T) :: processingRange ! Data processing range
+TYPE (L1BInfo_T) :: l1bInfo   ! File handles etc. for L1B dataset
+TYPE (L2CF) :: l2cf_data        ! The information from the l2cf file
+!TYPE (GridDatabase_T) :: aprioriData ! Input a priori database
+
+
 INTEGER ::  returnStatus
 Integer :: L1_Version, sd_id
-CHARACTER (LEN=135) :: L1physicalFilename
+CHARACTER (LEN=132) :: L1physicalFilename
 CHARACTER (LEN=32) :: mnemonic
-CHARACTER (LEN=480) :: msg
+CHARACTER (LEN=256) :: msg
+
+! Open, read and parse L2cf file
+
+Call read_parse_l2cf(mlspcf_l2cf_start, l2cf_data)
 
 
 !Open L1 file
 ! Get the l1 file name from the PCF
 
 
-returnStatus = Pgs_pc_getReference(L1FileHandle, L1_Version, L1physicalFilename)
+!returnStatus = Pgs_pc_getReference(L1FileHandle, L1_Version, L1physicalFilename)
 
-IF(returnStatus /= PGS_S_SUCCESS) THEN
-  call Pgs_smf_getMsg(returnStatus, mnemonic, msg)
-  CALL MLSMessage (MLSMSG_Error, &
-                  ModuleName, "Error opening L1:  "//mnemonic//" "//msg)
-ENDIF
+!IF(returnStatus /= PGS_S_SUCCESS) THEN
+!  call Pgs_smf_getMsg(returnStatus, mnemonic, msg)
+!  CALL MLSMessage (MLSMSG_Error, &
+!                  ModuleName, "Error opening L1:  "//mnemonic//" "//msg)
+!ENDIF
 
 ! Open the HDF file and initialize the SD interface
 
 
-sd_id = sfstart(physicalFilename, DFACC_READ)
-IF (sd_id == -1) THEN
-  CALL MLSMessage (MLSMSG_Error, &
-                  ModuleName, "Error opening L1 file"//physicalFilename)
-ENDIF
+!sd_id = sfstart(physicalFilename, DFACC_READ)
+!IF (sd_id == -1) THEN
+!  CALL MLSMessage (MLSMSG_Error, &
+!                  ModuleName, "Error opening L1 file"//physicalFilename)
+!ENDIF
 
-CALL Obtain_NCEP()
-CALL Obtain_DAO()
-Call Obtain_Clim ()
+!CALL Obtain_NCEP()
+!CALL Obtain_DAO()
+!Call Obtain_Clim ()
 
 RETURN
 
-END Subroutine Open_Init
+END Subroutine OpenAndInitialize
 
 !=============================================================================
 END MODULE OpenInit
@@ -67,6 +82,9 @@ END MODULE OpenInit
 
 !
 ! $Log$
+! Revision 1.2  2000/01/07 01:55:07  lungu
+! Release containing only open l1 and obtain a priori subroutines.
+!
 ! Revision 1.1  2000/01/05 02:12:39  lungu
 ! Added subroutines to handle GriddedData
 !
