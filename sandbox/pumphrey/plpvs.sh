ae97604c45d8e265b5a2e4d23b5c0226d0b1b9a1
pumphrey <>
2000-02-04 10:52:01 +0000

    OK for Fortran 90. Uses files in lib where possible

--- sandbox/pumphrey/plpvs.sh
+++ sandbox/pumphrey/plpvs.sh
@@ -1,13 +1,13 @@
-## source this to set up for FMKMF with plplot.
-export FMKMF_F90="f90 -B108 -YEXT_NAMES=LCS"
-export FMKMF_SPATH=.:../../lib:./plp
-
+export FMKMF_F90="f90 -g -B108 -YEXT_NAMES=LCS"
 
 echo Rebuilding Dummy SDP toolkit with ${FMKMF_F90}
-${FMKMF_F90} -c nomod_SDPToolkit.f90
-ar -rcs libSDPToolkit.a nomod_SDPToolkit.o
+${FMKMF_F90} -c ../../lib/SDPToolkitSubstitute.f90
+ar -rcs libSDPToolkitSubstitute.a SDPToolkitSubstitute.o
 
-export FMKMF_LINKOPTS="-L. -lplplotdX -lf2c -lSDPToolkit " 
+export FMKMF_F90="f90 -B108 -YEXT_NAMES=LCS"
+export FMKMF_SPATH=../../lib:.:./plp
+
+export FMKMF_LINKOPTS="-L. -lplplotdX -lf2c -lSDPToolkitSubstitute "
 
 
 
