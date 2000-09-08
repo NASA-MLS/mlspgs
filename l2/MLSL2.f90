bd683af1cd45df2eedb89de061b493795ce9bbc6
vsnyder <>
2000-09-08 22:55:56 +0000

    rev 2.1

--- l2/MLSL2.f90
+++ l2/MLSL2.f90
@@ -1,156 +1,100 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+program MLSL2
+  use DECLARATION_TABLE, only: ALLOCATE_DECL, DUMP_DECL
+  use INIT_TABLES_MODULE, only: INIT_TABLES
+  use LEXER_CORE, only: INIT_LEXER
+  use MACHINE ! At least HP for command lines, and maybe GETARG, too
+  use OUTPUT_M, only: OUTPUT
+! use Open_Init, only: CloseMLSCF, OpenMLSCF
+  use PARSER, only: CONFIGURATION
+  use STRING_TABLE, only: DO_LISTING
+  use TOGGLES, only: CON, GEN, LEVELS, LEX, PAR, SYN, TAB, TOGGLE
+  use TREE, only: ALLOCATE_TREE, PRINT_SUBTREE
+  use TREE_CHECKER, only: CHECK_TREE
+  use TREE_WALKER, only: WALK_TREE_TO_DO_MLS_L2
+  use UNITS, only: INIT_UNITS
 
-!============================================================================
-PROGRAM MLSL2                   ! MLS Level 2 software
-!============================================================================
+  implicit NONE
 
-  ! External modules
+  logical :: DO_DUMP = .false.     ! Dump declaration table
+  logical :: DUMP_TREE = .false.   ! Dump tree after parsing
+  integer :: ERROR                 ! Error flag from check_tree
+  integer :: FIRST_SECTION         ! Index of son of root of first n_cf node
+  integer :: I                     ! counter for command line arguments
+  integer :: J                     ! index within option
+  character(len=80) :: LINE        ! Into which is read the command args
+  integer :: ROOT                  ! of the abstract syntax tree
 
-  USE MLSCommon
-  USE MLSMessageModule
-  USE L2GPData
-  USE L2AUXData
-  USE QuantityTemplates
-  USE VectorsModule
-  USE MLSCF
-  USE OpenInit
-  USE ScanDivide
-  USE Construct
-  USE Fill
-  USE Join
-  USE OutputAndClose
+! Initialize the lexer, symbol table, and tree checker's tables:
+  call init_lexer ( n_chars=10000, n_symbols=1000, hash_table_size=1003 )
+  call allocate_decl ( ndecls=1000 )
+  call allocate_tree ( n_tree=10000 )
+  call init_tables
+  call init_units
 
-  IMPLICIT NONE
+  i = 1+hp
+  do ! Process the command line options to set toggles
+    call getarg ( i, line )
+    if ( line(1:1) == '-' ) then
+      j = 1
+      do while ( j < len(line) )
+        j = j + 1
+        if ( line(j:j) == 'c' ) then
+          toggle(con) = .true.
+        else if ( line(j:j) == 'g' ) then
+          toggle(gen) = .true.
+          if ( j < len(line) ) then
+            if ( line(j+1:j+1) >= '0' .and. line(j+1:j+1) <= '9' ) then
+              j = j + 1
+              levels(gen) = ichar(line(j:j)) - ichar('0')
+            end if
+          end if
+        else if ( line(j:j) == 'l' ) then
+          toggle(lex) = .true.
+        else if ( line(j:j) == 'p' ) then
+          toggle(par) = .true.
+        else if ( line(j:j) == 'a' ) then
+          toggle(syn) = .true.
+        else if ( line(j:j) == 'A' ) then
+          dump_tree = .true.
+        else if ( line(j:j) == 'd' ) then
+          do_dump = .true.
+        else if ( line(j:j) == 't' ) then
+          toggle(tab) = .true.
+        else if ( line(j:j) == 'v' ) then
+          do_listing = .true.
+        end if
+      end do
+    else    
+  exit
+    end if
+    i = i + 1
+  end do
 
-  !--------------------------- RCS Ident Info -------------------------------
-  CHARACTER (LEN=130) :: Id = &
-       "$Id$"
-  CHARACTER (LEN=*), PARAMETER :: ModuleName="$RCSfile$"
-  !--------------------------------------------------------------------------
+! Parse the L2CF, producing an abstract syntax tree
+! call openMLSCF
+  call configuration ( root )
+! call closeMLSCF
+  if ( root <= 0 ) then
+    call output ( &
+      'A syntax error occurred -- there is no abstract syntax tree', &
+      advance='yes' )
+  else
+    if ( dump_tree ) call print_subtree ( root, 0 )
 
-  ! This is the Level 2 processing software for the EOS Microwave Limb Sounder.
-  ! For more information see:
+    ! Check that supra-syntactic conditions are met, e.g. correct
+    ! types for fields of commands, correct command order, etc.
+    call check_tree ( root, error, first_section )
+    if ( do_dump ) call dump_decl
+    if ( toggle(syn) ) then
+      call output ( 'Begin abstract syntax tree:', advance='yes' )
+      call print_subtree ( root, 0 )
+      call output ( 'End abstract syntax tree', advance='yes' )
+    end if
+    if ( error == 0 .and. first_section /= 0 ) then
+      if ( do_dump ) call dump_decl
 
-  ! EOS MLS Retrieval Processes Algorithm Theoretical Basis Document
-  !   JPL D-16159
-  ! Functional Requirements for the EOS MLS Level 2 software
-  !   JPL D-18027
-  ! EOS MLS Level 2 file description document
-  !   JPL D-18028
-  ! EOS MLS Level 2 software users' guide
-  !   JPL D-18029
-  ! Design of the EOS MLS Level 2 software for version 0.1
-  !   JPL D-18030.
-
-  !--------------------------------------------------------------------------
-
-  ! Local parameters
-
-  !--------------------------------------------------------------------------
-
-  ! Local variables roughly ordered increasing triviality
-
-  TYPE (TAI93_Range_T) :: processingRange ! Data processing range
-  TYPE (L1BInfo_T) :: l1bInfo   ! File handles etc. for L1B dataset
-  TYPE (MLSCF_T) :: l2cf        ! The information from the l2cf file
-  TYPE (GriddedData_T), DIMENSION(:), POINTER :: aprioriData 
-  ! Input a priori database
-  TYPE (MLSChunk_T), DIMENSION(:), POINTER :: chunks ! Chunks for data
-
-  TYPE (QuantityTemplate_T), DIMENSION(:), POINTER :: qtyTemplates
-  TYPE (QuantityTemplate_T), DIMENSION(:), POINTER :: mifGeolocation
-  TYPE (VectorTemplate_T), DIMENSION(:), POINTER :: vectorTemplates
-  TYPE (Vector_T), DIMENSION(:), POINTER :: vectors
-
-  TYPE (L2GPData_T), DIMENSION(:), POINTER :: l2gpDatabase ! L2GP products
-  TYPE (L2AUXData_T), DIMENSION(:), POINTER :: l2auxDatabase ! L2AUX products
-
-  INTEGER :: chunkNo,instrumentModule, n            ! Loop counter
-  CHARACTER (LEN=132) :: message ! Line of message output
-
-  !--------------------------------------------------------------------------
-
-  ! Executable code
-
-
-  CALL MLSMessage(MLSMSG_Info,ModuleName, &
-       & "EOS MLS Level 2 data processing commenced")
-
-  ! Before we do anything we nullify the database pointers.  This is needed
-  ! because apparently f90 does not set pointers to .not. associated when
-  ! they are first created.
-
-  NULLIFY (aprioriData,chunks,qtyTemplates,mifGeolocation,vectorTemplates,&
-       & vectors,l2gpDatabase,l2auxDatabase)
-
-  ! The first thing to do is to open the input Level 1 files, l2cf files and do
-  ! other initialization stuff.
-
-
-  CALL OpenAndInitialize(processingRange, l1bInfo, l2cf, &
-       & aprioriData)
-
-  ! The next stage is to divide the input L1B dataset into chunks.
-
-  CALL ScanAndDivide(processingRange, l1bInfo, l2cf, chunks)
-  ! Now we loop over the chunks and do the data processing
-
-  n= SIZE(chunks)
-  DO chunkNo=1,n
-     WRITE (UNIT=message,FMT=*) "Processing chunk #",chunkNo
-     CALL MLSMessage(MLSMSG_Info,ModuleName,TRIM(message))
-     ! This will need to change a lot in 0.5 and above
-     ! First construct the vector quantity templates and vector templates
-
-     CALL MLSL2Construct(l2cf,l1bInfo,chunks(chunkNo),qtyTemplates, &
-          & vectorTemplates,mifGeolocation)
-
-     ! Now fill those templates with the a priori and/or l1b data
-     CALL MLSL2Fill(l2cf,l1bInfo,aprioriData, vectorTemplates, vectors)
-
-     ! In later versions there will be retrieve etc. and also repeats, but for
-     ! the moment we'll leave it at this.
-
-     CALL MLSL2Join(l2cf,vectors,l2gpDatabase,l2auxDatabase,chunks,chunkNo)
-     ! At the end of each chunk, destroy the vectorTemplate and vector
-     ! information.
-
-     CALL DestroyVectorDatabase(vectors)
-     CALL DestroyVectorTemplateDatabase(vectorTemplates)
-     CALL DestroyQuantityTemplateDatabase(qtyTemplates)
-     CALL DestroyQuantityTemplateDatabase(mifGeolocation)
-     CALL DestroyGridTemplateDatabase(aprioriData)
-  END DO
-  ! Now we write out the data
- CALL Ouput_Close(l2cf,l2gpDatabase,l2auxDatabase)
-  ! Now we tidy up any remaining `pointer' data, do this in order of variable
-  ! declaration above.
-
-  ! processingRange needs no deallocation
-  DEALLOCATE(l1bInfo%L1BRADIDs)
-  ! CALL DestroyMLSCFInfo(l2cf) MLSCF is currently fixed arrays. 
-  CALL DestroyGridTemplateDatabase(aprioriData)
-  DEALLOCATE(chunks)
-  ! vectors, vectorTemplates and qtyTemplates destroyed at the 
-  ! end of each chunk
-  CALL DestroyL2GPDatabase(l2gpDatabase)
-  CALL DestroyL2AUXDatabase(l2auxDatabase)
-  CALL MLSMessage(MLSMSG_Info,ModuleName, &
-       & "EOS MLS Level 2 data processing ended")
-   STOP
-!=============================================================================
-END PROGRAM MLSL2
-!=============================================================================
-
-!
-! $Log$
-! Revision 2.0  2000/09/05 18:57:03  ahanzel
-! Changing file revision to 2.0.
-!
-! Revision 1.16  2000/06/29 23:55:35  lungu
-! Uncommented:
-!   CALL DestroyL2GPDatabase(l2gpDatabase)
-!   CALL DestroyL2AUXDatabase(l2auxDatabase).
-!
-
+      ! Now do the L2 processing.
+      call walk_tree_to_do_MLS_L2 ( root, error, first_section )
+    end if
+  end if
+end program MLSL2
