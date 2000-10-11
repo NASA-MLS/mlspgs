816acb343b0fb19ba98c1d0dc97d1ea09c954a85
vsnyder <>
2000-10-11 19:10:35 +0000

    rev 2.3

---
+++ lib/machine.f90
@@ -0,0 +1,18 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
+module MACHINE
+
+  ! Do not use this one.  Do not even compile it.  Use the one in the
+  ! appropriate platform-dependent next-lower-level directory.  This one
+  ! is here only to cause automatic makefile dependency generation tools
+  ! to put it into lists of dependencies for programs that USE MACHINE.
+
+  ! $Id$
+
+end module MACHINE
+
+! $Log$
+! Revision 2.3  2000/10/11 19:09:30  vsnyder
+! Initial entry
+!
