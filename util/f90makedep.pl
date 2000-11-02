a056da53c74f7e0500fa2f6a9bc8644e88563e4f
pwagner <>
2000-11-02 23:27:38 +0000

    Dependencies may cross directories

--- util/f90makedep.pl
+++ util/f90makedep.pl
@@ -3,24 +3,65 @@
 # Usage: f90makedep.pl
 #
 # Generate a Makefile-ready dependency listing from the f90 sources 
-# in the current directory. 
+# in the current directory. If there are any command line arguments,
+# and   $ARG_DEPENDS   is set (which see)
+# they will be interpreted as additional directories in which to
+# find dependencies. In particular, if when a source file USEs a
+# module, the file containing that USEd module will be printed among
+# the list of dependencies if it is found in the current directory or
+# else found among the directories named in the command line. If it is not
+# found among any of these, it will be excluded from the dependencies.
+# In addition, if  $SELF_DEPENDS  is set (which see) the source file
+# will itself be included among the list of dependencies
+#
+# Usage:
+# 	f90makedep.pl [arg1 arg2 ..]
+# Output:
+# (printed to stdout)
+# source1.o: source1.f90 source2.f90 .. $(d1)s1.f90 $(d1)s2.f90 .. $(d2)t1.f90 ..
+# source2.o: source2.f90 source3.f90 .. $(d1)s1.f90 $(d1)s2.f90 .. $(d2)t1.f90 ..
+# source3.o: source3.f90 .. $(d1)s1.f90 $(d1)s2.f90 .. $(d2)t1.f90 ..
+# ...
+# where the contents of the current directory (cwd), arg1, arg2 are
+#    cwd         arg1       arg2
+# source1.f90   s1.f90     t1.f90
+# source2.f90   s2.f90     t2.f90
+# source3.f90   s3.f90     t3.f90
+#
+# The theory behind the $(d1), $(d2) stuff is that you will define them
+# with lines like 
+# d1 = ../up_one/elsewhere/ 
+# and similarly for d2 in the same Makefile where you plan to paste the output
+#
 # To run this, it will be necessary to modify the first line of this script
 # to point to the actual location of Perl on your system.
 #
-# Based on f90mkmf from H. Pumphrey
+# P.A. Wagner (October 31 2000)
+# (see mlsconfigure.sh for copyright statement)
+#
+# Based on f90mkmf from H. Pumphrey (check that this is true in fact)
 #
 # Based on Michael Wester <wester@math.unm.edu> February 16, 1995
 # Cotopaxi (Consulting), Albuquerque, New Mexico
 #
 # "$Id$"
 
+#         Settings which affect how the script operates
+#         * * * * * * * * * * * * * * * * * * * * * * * 
 # if TRUE, include source file in list of dependencies
 $SELF_DEPENDS = TRUE;
 
+# if TRUE, include source files in directories named in arg list
+# in list of dependencies (if arguments not empty)
+$ARG_DEPENDS = TRUE;
+
+# if TRUE, for source files in directories named in arg list
+# prepend $(d1), $(d2), .. stuff; else leave bare
+$dir1_PREPENDS = TRUE;
+
+#         * * * * * * * * * * * * * * * * * * * * * * * 
 # Fool the script into writing to stdout instead of an actual file
 open(MAKEFILE, ">-");
-# print "Why do I quit prematurely?";
-# exit;
 # Dependency listings
 #
 &MakeDependsf90($ARGV[1]);
@@ -144,6 +185,8 @@ sub MakeDependsf90 {
    local(@incs);
    local(@modules);
    local($objfile);
+   local($arg_number);
+   local($prependant);
    #
    # Associate each module with the name of the file that contains it
    #
@@ -154,6 +197,45 @@ sub MakeDependsf90 {
             ($filename{&toLower($1)} = $file) =~ s/\.f90$/.o/;
          }
       }
+   #
+   # Do the same in every directory named in arg list
+   #
+   if ($ARG_DEPENDS) {
+   #
+   # chop($cwd = \`pwd\`);
+   # The above line, although found among man pages, won't pass muster
+   #
+   # The following is a poor way to fill $cwd with the current working directory
+   # Ask a more perl-savvy programmer how to do this w/o creating temp file
+     system "pwd > temp";
+     open (TEMPFILE, temp);
+     while (<TEMPFILE>) {
+       chop;
+       $cwd = $_;
+       }
+     unlink temp;
+ #   Now loop over list of directories which are command-line arguments
+    $arg_number = 0;
+    while ($#ARGV >= $[) {
+   	die "Can't cd to $ARGV[$[]:$!\n"
+        	unless chdir $ARGV[$[] ;
+        shift @ARGV;
+        $arg_number++;
+        if ($dir1_PREPENDS) {
+           $prependant = '$(dir' . "$arg_number" . ')';
+        } else {        
+            $prependant = '';
+       }
+       foreach $file (<*.f90>) {
+          open(FILE, $file) || warn "Cannot open $file: $!\n";
+           while (<FILE>) {
+             /^\s*module\s+([^\s!]+)/i &&
+                ($filename{&toLower($1)} = "$prependant" . "$file") =~ s/\.f90$/.o/;
+             }
+          }
+       chdir $cwd;
+      }
+    }
    #
    # Print the dependencies of each file that has one or more include's or
    # references one or more modules
@@ -187,6 +269,6 @@ sub MakeDependsf90 {
       }
    }
 # $Log$
-# Revision 1.2  2000/03/11 00:41:59  vsnyder
-# Inserted CVS id and log
+# Revision 1.1  2000/10/27 23:21:26  pwagner
+# First commit
 #
--- util/makemakedep.sh
+++ util/makemakedep.sh
@@ -2,6 +2,37 @@
     #makemakedep.sh
 #Creates file Makefile.dep of dependencies to be included by Makefile
 #to compile a Fortran 9x program or library
+#In case of (2) (see below) any args are passed through to
+#f90makedep.pl where they will be interpreted as directories
+#in which to search for additional modules that may be among
+#the lists of dependencies (please see f90makedep.pl for details)
+#
+#Usage:
+#makemakedep.sh [arg1 arg2 ..]
+#
+#Result:
+#creates Makefile.dep in current working directory
+#Makefile.dep may then be inserted in a generic Makefile which
+#thereby is able to keep current with a "moving target" in case
+#the source files are changed in a way that alters their dependencies
+#E.g. examine the following mock session
+#  mlspgs/l2%ls
+#source1.f90  source2.f90  source3.f90
+#  mlspgs/l2%makemakedep.sh
+#  mlspgs/l2%ls
+#Makefile.dep  source1.f90  source2.f90  source3.f90
+#  mlspgs/l2%cat Makefile.dep
+#Makefile.dep -- a file to be included by a Makefile
+#to compile a Fortran 9x program or library
+#OBJS = \
+#source1.o               source2.o          source3.o  \
+#
+#source1.o: source1.f90 source2.o source3.o
+#source2.o: source2.f90 source3.o
+#source3.o: source3.f90
+# 
+#End of Makefile.dep
+# 
 #this version uses either of the two depmakers
 #to trace dependencies based on USEs and INCLUDEs:
 #(1)  makedepf90 (a compiled program)
@@ -23,28 +54,50 @@ DEPMAKER=2
 # 	makedepf90 | sed ...
 # below with
 # 	/dir1/dir2/../dirn/makedepf90 | sed ...
-#where /dir1.. is directory where you created makedpf90 (use 'pwd')
+#where /dir1.. is directory where you created makedepf90 (use 'pwd')
 #
 #if you use f90makedep.pl, you may have a problem if the path
 #your copy of perl is different from the one in its 1st line
 #compare 'which perl' with 'sed -n "1 p" f90makedep.pl
 #
+#makemakedep.sh may act courteously in the following sense:
+#if makemakedep.sh finds there is already a file named Makefile.dep
+#it will attempt to rename the older file rather than deleting it
+#
+# P.A. Wagner (October 31 2000)
+# (see mlsconfigure.sh for copyright statement)
+#
+ACT_COURTEOUS=1
+#             ^  -- set this to 1 to rename older Makefile.dep, 0 deletes
 #
 # "$Id$"
 if [ -f Makefile.dep ]
 then
-	echo "Renaming older Makefile.dep Make.dep.n"
-	name=Make.dep.`ls -1 Make*.dep* | wc -l`
-	cname=`echo $name | sed 's/ //'g`
-	mv Makefile.dep $cname
+	if [ $ACT_COURTEOUS = "1" ]
+	then
+		echo "Renaming older Makefile.dep Make.dep.n"
+		name=Make.dep.`ls -1 Make*.dep* | wc -l`
+		cname=`echo $name | sed 's/ //'g`
+		mv Makefile.dep $cname
+        else
+                chmod u+w Makefile.dep
+                rm -f Makefile.dep
+	fi
 fi
 echo "#Makefile.dep -- a file to be included by a Makefile" > Makefile.dep
 echo "#to compile a Fortran 9x program or library" >> Makefile.dep
+#
+#The following may be useful in rare circumstances
+#but violates assertion that Makefile.dep includes only dependency info
+#so for purity we should avoid relying on it
+#e.g., the mlspgs software does not include any file "Makefile.mac"
+#
 if [ -f Makefile.mac ]
 then
-	echo "Including macro definitions in Makefile.mac"
+	echo "Including special macro definitions from Makefile.mac"
 	echo "include ../Makefile.mac"  >> Makefile.dep
 fi
+#
 echo "OBJS = \\"  >> Makefile.dep
 (ls -C *.f90 | sed 's/.f90/.o  /g; s/$/\\/') >> Makefile.dep
 echo " "  >> Makefile.dep
@@ -72,13 +125,16 @@ else
 	echo " Your perl is `which perl` "
 	echo " f90makedep.pl is looking for it at `sed -n '1 p' $the_DEPMAKER`"
 #	f90makedep.pl >> Makefile.dep
-	$the_DEPMAKER >> Makefile.dep
+	$the_DEPMAKER "$@" >> Makefile.dep
 fi
 echo " "  >> Makefile.dep
 echo "#End of Makefile.dep" >> Makefile.dep
 echo " "  >> Makefile.dep
 
 # $Log$
+# Revision 1.3  2000/10/27 23:22:37  pwagner
+# works with f90makedep.pl
+#
 # Revision 1.2  2000/10/18 20:31:58  pwagner
 # Fix to overcome apparent bug in makedepf90
 #
