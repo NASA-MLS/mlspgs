fd8747a2e38fefc96c33524715a40e40807a6de1
pumphrey <>
2000-11-08 10:21:04 +0000

    rev 2.2

--- lib/SDPToolkitSubstitute.f90
+++ lib/SDPToolkitSubstitute.f90
@@ -18,6 +18,14 @@
   ! This module gives substitutes for the essential toolkit routines used by
   ! library code used in both the toolkit and non toolkit environment.
 
+  ! Some of the functions don't work. Code that calls them will need
+  ! to be linked to the _real_ SDP Toolkit library PGSTK.a. They are in here
+  ! to satisfy some F90 compilers which check that a routine exists if it
+  ! has an interface block defined in another module, even if it is not
+  ! called in a particular case of USEing that module. There shoulld be a 
+  ! routine in here for every SDP routine that has an interface in 
+  ! SDPToolkit.f90
+
 !CONTAINS
 
        INTEGER FUNCTION PGS_SMF_GenerateStatusReport(msg)
@@ -35,11 +43,14 @@
         INTEGER, INTENT(IN) :: record_length
         INTEGER, INTENT(OUT) :: file_handle
         INTEGER, INTENT(IN) :: file_version
+        
+        PGS_IO_Gen_OpenF=-99
       END FUNCTION PGS_IO_Gen_OpenF
 
 
       INTEGER FUNCTION PGS_IO_Gen_CloseF(file_handle)
         INTEGER, INTENT(IN) :: file_handle
+        PGS_IO_Gen_CloseF=-99
       END FUNCTION PGS_IO_Gen_CloseF
 
       INTEGER FUNCTION Pgs_pc_getReference(file_handle, &
@@ -47,6 +58,7 @@
         INTEGER, INTENT(IN) :: file_handle
         INTEGER, INTENT(INOUT) :: file_version
         character (LEN=*), INTENT(OUT) :: physicalfilename
+        Pgs_pc_getReference=-99
       END FUNCTION Pgs_pc_getReference
 
 
@@ -59,6 +71,8 @@
       INTEGER FUNCTION PGS_TD_TAItoUTC(sectai93,asciiutc)
         DOUBLE PRECISION, INTENT(IN) :: sectai93
         CHARACTER(LEN=27), INTENT(OUT) :: asciiutc
+        asciiutc="Use the Real PGSTK"
+        PGS_TD_TAItoUTC=-99
       END FUNCTION PGS_TD_TAItoUTC
 
 !=============================================================================
@@ -67,6 +81,9 @@
 
 !
 ! $Log$
+! Revision 2.2  2000/11/08 10:21:04  pumphrey
+! Added return values to functions to prevent compile errors
+!
 ! Revision 2.1  2000/11/06 18:52:22  pumphrey
 ! Added routines so that every routine in SDPToolkit.f90 is represented.
 ! The routines do nothing, they are just to satisfy the link stage with
