9f9ad48fe0145d959d9d6ce24da838f8abd67092
vsnyder <>
2000-11-15 02:37:18 +0000

    Moved generic array dumping to lib/dump_0.f90

--- l2/dumper.f90
+++ l2/dumper.f90
@@ -2,6 +2,7 @@ module DUMPER
 
 ! Dump various stuff so we can look at it.
 
+  use DUMP_0, only: AfterSub, DUMP
   use HGRID, only: HGRID_T
   use INIT_TABLES_MODULE, only: LIT_INDICES, PHYQ_INDICES
   use MLSCommon, only: MLSCHUNK_T
@@ -26,14 +27,8 @@ module DUMPER
     module procedure DUMP_HGRIDS
     module procedure DUMP_QUANTITY_TEMPLATES
     module procedure DUMP_VGRIDS
-    module procedure DUMP_1D_DOUBLE
-    module procedure DUMP_1D_INTEGER
-    module procedure DUMP_2D_DOUBLE
-    module procedure DUMP_2D_INTEGER
-    module procedure DUMP_3D_DOUBLE
   end interface
 
-  character, parameter :: AfterSub = '#'
 contains ! =====     Private Procedures     ============================
   ! ------------------------------------------------  DUMP_CHUNKS  -----
   subroutine DUMP_CHUNKS ( CHUNKS )
@@ -191,126 +186,6 @@ contains ! =====     Private Procedures     ============================
       call dump ( vgrids(i)%surfs, ' Surfs = ' )
     end do
   end subroutine DUMP_VGRIDS
-
-  ! ---------------------------------------------  DUMP_1D_DOUBLE  -----
-  subroutine DUMP_1D_DOUBLE ( ARRAY, NAME )
-    double precision, intent(in) :: ARRAY(:)
-    character(len=*), intent(in), optional :: NAME
-    integer :: J, K
-    if ( size(array) == 1 ) then
-      if ( present(name) ) call output ( name )
-      call output ( array(1), '(1x,1pg13.6)', advance='yes' )
-    else
-      if ( present(name) ) call output ( name, advance='yes' )
-      do j = 1, size(array), 5
-        call output ( j, 4 ); call output ( afterSub )
-        do k = j, min(j+4, size(array))
-          call output ( array(k), '(1x,1pg13.6)' )
-        end do
-        call output ( '', advance='yes' )
-      end do
-    end if
-  end subroutine DUMP_1D_DOUBLE
-
-  ! --------------------------------------------  DUMP_1D_INTEGER  -----
-  subroutine DUMP_1D_INTEGER ( ARRAY, NAME )
-    integer, intent(in) :: ARRAY(:)
-    character(len=*), intent(in), optional :: NAME
-    integer :: J, K
-    if ( size(array) == 1 ) then
-      if ( present(name) ) call output ( name )
-      call output ( array(1), advance='yes' )
-    else
-      if ( present(name) ) call output ( name, advance='yes' )
-      do j = 1, size(array), 10
-        call output ( j, 4 ); call output ( afterSub )
-        do k = j, min(j+9, size(array))
-          call output ( array(k), 6 )
-        end do
-        call output ( '', advance='yes' )
-      end do
-    end if
-  end subroutine DUMP_1D_INTEGER
-
-  ! ---------------------------------------------  DUMP_2D_DOUBLE  -----
-  subroutine DUMP_2D_DOUBLE ( ARRAY, NAME )
-    double precision, intent(in) :: ARRAY(:,:)
-    character(len=*), intent(in), optional :: NAME
-    integer :: I, J, K
-    if ( size(array) == 1 ) then
-      if ( present(name) ) call output ( name )
-      call output ( array(1,1), '(1x,1pg13.6)', advance='yes' )
-    else if ( size(array,2) == 1 ) then
-      call dump ( array(:,1), name )
-    else
-      if ( present(name) ) call output ( name, advance='yes' )
-      do i = 1, size(array,1)
-        do j = 1, size(array,2), 5
-          call output ( i, 4 )
-          call output ( j, 4 ); call output ( afterSub )
-          do k = j, min(j+4, size(array,2))
-            call output ( array(i,k), '(1x,1pg13.6)' )
-          end do
-          call output ( '', advance='yes' )
-        end do
-      end do
-    end if
-  end subroutine DUMP_2D_DOUBLE
-
-  ! --------------------------------------------  DUMP_2D_INTEGER  -----
-  subroutine DUMP_2D_INTEGER ( ARRAY, NAME )
-    integer, intent(in) :: ARRAY(:,:)
-    character(len=*), intent(in), optional :: NAME
-    integer :: I, J, K
-    if ( size(array) == 1 ) then
-      if ( present(name) ) call output ( name )
-      call output ( array(1,1), advance='yes' )
-    else if ( size(array,2) == 1 ) then
-      call dump ( array(:,1), name )
-    else
-      if ( present(name) ) call output ( name, advance='yes' )
-      do i = 1, size(array,1)
-        do j = 1, size(array,2), 10
-          call output ( i, 4 )
-          call output ( j, 4 ); call output ( afterSub )
-          do k = j, min(j+9, size(array,2))
-            call output ( array(i,k), 6 )
-          end do
-          call output ( '', advance='yes' )
-        end do
-      end do
-    end if
-  end subroutine DUMP_2D_INTEGER
-
-  ! ---------------------------------------------  DUMP_3D_DOUBLE  -----
-  subroutine DUMP_3D_DOUBLE ( ARRAY, NAME )
-    double precision, intent(in) :: ARRAY(:,:,:)
-    character(len=*), intent(in), optional :: NAME
-    integer :: I, J, K, L
-    if ( size(array) == 1 ) then
-      if ( present(name) ) call output ( name )
-      call output ( array(1,1,1), '(1x,1pg13.6)', advance='yes' )
-    else if ( size(array,2) == 1 .and. size(array,3) == 1 ) then
-      call dump ( array(:,1,1), name )
-    else if ( size(array,3) == 1 ) then
-      call dump ( array(:,:,1), name )
-    else
-      if ( present(name) ) call output ( name, advance='yes' )
-      do i = 1, size(array,1)
-        do j = 1, size(array,2)
-          do k = 1, size(array,3), 5
-            call output ( i, 4 )
-            call output ( j, 4 )
-            call output ( k, 4 ); call output ( afterSub )
-            do l = k, min(k+4, size(array,3))
-              call output ( array(i,j,l), '(1x,1pg13.6)' )
-            end do
-            call output ( '', advance='yes' )
-          end do
-        end do
-      end do
-    end if
-  end subroutine DUMP_3D_DOUBLE
 end module DUMPER
 
 ! $Log,v $
