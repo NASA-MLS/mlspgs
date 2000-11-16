d1a334f6786aea354ab0c643d42a33c4777d9af5
vsnyder <>
2000-11-16 01:45:25 +0000

    Implement timing.

--- l2/global_settings.f90
+++ l2/global_settings.f90
@@ -1,7 +1,9 @@
 module GLOBAL_SETTINGS
 
   use INIT_TABLES_MODULE, only: L_TRUE, P_ALLOW_CLIMATOLOGY_OVERLOADS, &
-    & P_INPUT_VERSION_STRING, P_OUTPUT_VERSION_STRING, P_VERSION_COMMENT
+    & P_INPUT_VERSION_STRING, P_OUTPUT_VERSION_STRING, P_VERSION_COMMENT, &
+    & S_TIME
+  use OUTPUT_M, only: OUTPUT
   use TOGGLES, only: GEN, TOGGLE
   use TRACE_M, only: TRACE_BEGIN, TRACE_END
   use TREE, only: DECORATION, NSONS, SUB_ROSA, SUBTREE
@@ -29,9 +31,12 @@ contains
 
     integer :: I         ! Index of son of root
     integer :: SON       ! Son of root
+    double precision :: T1, T2     ! for timing
+    logical :: TIMING
 
     if ( toggle(gen) ) call trace_begin ( 'SET_GLOBAL_SETTINGS', root )
 
+    timing = .false.     ! timing isn't on.
     do i = 2, nsons(root)-1 ! Skip names at beginning and end of section
       son = subtree(i,root)
       select case ( decoration(subtree(1,son)) )
@@ -43,8 +48,23 @@ contains
         output_version_string = sub_rosa(subtree(2,son))
       case ( p_version_comment )
         version_comment = sub_rosa(subtree(2,son))
+      case ( s_time )
+        if ( timing ) then
+          call cpu_time ( t2 )
+          call output ( "Timing for Set_Global_Settings = " )
+          call output ( t2-t1, advance = 'yes' )
+          timing = .false.
+        else
+          call cpu_time ( t1 )
+          timing = .true.
+        end if
       end select
     end do
+    if ( timing ) then
+      call cpu_time ( t2 )
+      call output ( "Timing for Set_Global_Settings = " )
+      call output ( t2-t1, advance = 'yes' )
+    end if
 
   if ( toggle(gen) ) call trace_end ( 'SET_GLOBAL_SETTINGS' )
 
@@ -53,6 +73,9 @@ contains
 end module GLOBAL_SETTINGS
 
 ! $Log$
+! Revision 2.0  2000/09/05 18:57:05  ahanzel
+! Changing file revision to 2.0.
+!
 ! Revision 1.1  2000/09/02 02:05:04  vsnyder
 ! Initial entry
 !
