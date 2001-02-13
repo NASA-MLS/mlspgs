17c5fbc47a390f8aa7baf7da058af5210a7d5031
nakamura <>
2001-02-13 19:47:31 +0000

    Module containing PCF numbers for L3.

---
+++ l3/MLSPCF3.f90
@@ -0,0 +1,51 @@
+! Copyright (c) 2001, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! NOTE: This module is automatically created by the makemlspcfmodule
+!       perl script.  Do *NOT* attempt to modify this by hand.
+!
+MODULE MLSPCF3
+   INTEGER, PARAMETER :: mlspcf_l3_param_OutputVersion = 3000
+   INTEGER, PARAMETER :: mlspcf_l3_param_Cycle = 3001
+   INTEGER, PARAMETER :: mlspcf_l3_param_L2DayRange = 3002
+   INTEGER, PARAMETER :: mlspcf_l3_param_RangDays = 3003
+   !
+   INTEGER, PARAMETER :: mlspcf_pcf_start = 900
+   INTEGER, PARAMETER :: mlspcf_pcf_end = 900
+   !
+   INTEGER, PARAMETER :: mlspcf_l3cf_start = 901
+   INTEGER, PARAMETER :: mlspcf_l3cf_end = 901
+   !
+   INTEGER, PARAMETER :: mlspcf_l2gp_start = 22000
+   INTEGER, PARAMETER :: mlspcf_l2gp_end = 22299
+   !
+   INTEGER, PARAMETER :: mlspcf_l3dm_start = 30000
+   INTEGER, PARAMETER :: mlspcf_l3dm_end = 30099
+   !
+   INTEGER, PARAMETER :: mlspcf_l3sp_start = 31000
+   INTEGER, PARAMETER :: mlspcf_l3sp_end = 31009
+   !
+   INTEGER, PARAMETER :: mlspcf_l3dz_start = 31100
+   INTEGER, PARAMETER :: mlspcf_l3dz_end = 31119
+   !
+   INTEGER, PARAMETER :: mlspcf_l3mm_start = 31200
+   INTEGER, PARAMETER :: mlspcf_l3mm_end = 31200
+   !
+   INTEGER, PARAMETER :: mlspcf_l3mz_start = 31300
+   INTEGER, PARAMETER :: mlspcf_l3mz_end = 31301
+   !
+   INTEGER, PARAMETER :: mlspcf_mcf_l3log_start = 4000
+   INTEGER, PARAMETER :: mlspcf_mcf_l3log_end = 4000
+   !
+   INTEGER, PARAMETER :: mlspcf_mcf_l3dm_start = 4001
+   INTEGER, PARAMETER :: mlspcf_mcf_l3dm_end = 4010
+   !
+   INTEGER, PARAMETER :: mlspcf_mcf_l3dz_start = 4011
+   INTEGER, PARAMETER :: mlspcf_mcf_l3dz_end = 4012
+   !
+   INTEGER, PARAMETER :: mlspcf_mcf_l3mm_start = 4013
+   INTEGER, PARAMETER :: mlspcf_mcf_l3mm_end = 4013
+   !
+   INTEGER, PARAMETER :: mlspcf_mcf_l3mz_start = 4014
+   INTEGER, PARAMETER :: mlspcf_mcf_l3mz_end = 4015
+   !
+END MODULE MLSPCF3
