f381a7d26522a2625aa705c76e2a76f0187ff2c7
vsnyder <>
2001-03-14 02:06:31 +0000

    rev 2.10

--- lib/lexer_m.f90
+++ lib/lexer_m.f90
@@ -2,7 +2,8 @@
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
 module LEXER_M
-! Lexer for SMALL
+
+! Lexers for MLS.
 
   use ERROR_HANDLER, only: ERROR_WALKBACK
   use LEXER_CORE, only: NEED, TOKEN
@@ -15,36 +16,75 @@ module LEXER_M
                           SET_SYMBOL, SYMBOL
   use SYMBOL_TYPES ! everything
   use TOGGLES, only: CON, EMIT, GEN, LEX, PAR, SYN, TAB, TOGGLE ! (everything)
-  public
+  private
+
+  public :: Lexer, Lex_Signal
+
+  ! Parameters for character classes
+  integer, parameter :: LETTER = 1
+  integer, parameter :: DIGIT = 2
+  integer, parameter :: PUNCT = 3
+  integer, parameter :: OP_CHAR = 4
+  integer, parameter :: DOT = 5
+  integer, parameter :: SPACES = 6
+  integer, parameter :: EOL_IN = 7
+  integer, parameter :: EOF_IN = 8
+  integer, parameter :: CMT = 9
+  integer, parameter :: MORE = 10
+  integer, parameter :: UNDER = 11
+  integer, parameter :: CONT = 12
+  integer, parameter :: QUOTE = 13
+  integer, parameter :: APOST = 14
+  integer, parameter :: TOG_CH = 15
+
+  ! Parameter for classifying ASCII characters
+  integer, parameter :: CLASSES(0:127) = (/ &
+! NUL     SOH     STX     ETX     EOT=^D  ENQ     ACK     BEL
+  more,   more,   more,   more,   eof_in, more,   more,   more,   & ! 000
+! BS      HT      LF      VT      FF      CR      SO      SI
+  more,   spaces, eol_in, more,   more,   spaces, more,   more,   & ! 010
+! DLE     DC1     DC2     DC3     DC4     NAK     SYN     ETB
+  more,   more,   more,   more,   more,   more,   more,   more,   & ! 020
+! CAN     EM      SUB     ESC     FS      GS      RS      US
+  more,   more,   more,   more,   more,   more,   more,   more,   & ! 030
+! SPACE   !       "       #       $       %       &       '
+  spaces, more,   quote,  more,   cont,   more,   more,   apost,  & ! 040
+! (       )       *       +       ,       -       .       /
+  punct,  punct,  op_char,op_char,punct,  op_char,dot,    op_char,& ! 050
+! 0       1       2       3       4       5       6       7
+  digit,  digit,  digit,  digit,  digit,  digit,  digit,  digit,  & ! 060
+! 8       9       :       ;       <       =       >       ?
+  digit,  digit,  op_char,cmt,    op_char,op_char,more,   more,   & ! 070
+! @       A       B       C       D       E       F       G
+  tog_ch, letter, letter, letter, letter, letter, letter, letter, & ! 100
+! H       I       J       K       L       M       N       O
+  letter, letter, letter, letter, letter, letter, letter, letter, & ! 110
+! P       Q       R       S       T       U       V       W
+  letter, letter, letter, letter, letter, letter, letter, letter, & ! 120
+! X       Y       Z       [       \       ]       ^       _
+  letter, letter, letter, punct,  more,   punct,  more,   under,  & ! 130
+! `       a       b       c       d       e       f       g
+  more,   letter, letter, letter, letter, letter, letter, letter, & ! 140
+! h       i       j       k       l       m       n       o
+  letter, letter, letter, letter, letter, letter, letter, letter, & ! 150
+! p       q       r       s       t       u       v       w
+  letter, letter, letter, letter, letter, letter, letter, letter, & ! 160
+! x       y       z       {       |       }       ~       DEL
+  letter, letter, letter, more,   cont,   more,   more,   more   /) ! 170
 
 !---------------------------- RCS Ident Info -------------------------------
-  character (len=256), private :: Id = &
+  character (len=*), private, parameter :: IdParm = &
        "$Id$"
+  character (len=len(idParm)), private :: Id = idParm
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
 !---------------------------------------------------------------------------
 
 contains
 
+  ! ------------------------------------------------------  Lexer  -----
   subroutine LEXER ( THE_TOKEN )
     type(token), intent(out) :: THE_TOKEN
-
-    ! Parameters for character classes
-    integer, parameter :: LETTER = 1
-    integer, parameter :: DIGIT = 2
-    integer, parameter :: PUNCT = 3
-    integer, parameter :: OP_CHAR = 4
-    integer, parameter :: DOT = 5
-    integer, parameter :: SPACES = 6
-    integer, parameter :: EOL_IN = 7
-    integer, parameter :: EOF_IN = 8
-    integer, parameter :: CMT = 9
-    integer, parameter :: MORE = 10
-    integer, parameter :: UNDER = 11
-    integer, parameter :: CONT = 12
-    integer, parameter :: QUOTE = 13
-    integer, parameter :: APOST = 14
-    integer, parameter :: TOG_CH = 15
 
     ! Parameters for ERROR routine's CODE argument
     integer, parameter :: DOUBLE_UNDER = 1
@@ -73,39 +113,6 @@ contains
 
     character, save :: CH          ! Current character
     integer :: CLASS               ! Classification of current character
-    integer, parameter :: CLASSES(0:127) = (/ &
-!   NUL     SOH     STX     ETX     EOT=^D  ENQ     ACK     BEL
-    more,   more,   more,   more,   eof_in, more,   more,   more,   & ! 000
-!   BS      HT      LF      VT      FF      CR      SO      SI
-    more,   spaces, eol_in, more,   more,   spaces, more,   more,   & ! 010
-!   DLE     DC1     DC2     DC3     DC4     NAK     SYN     ETB
-    more,   more,   more,   more,   more,   more,   more,   more,   & ! 020
-!   CAN     EM      SUB     ESC     FS      GS      RS      US
-    more,   more,   more,   more,   more,   more,   more,   more,   & ! 030
-!   SPACE   !       "       #       $       %       &       '
-    spaces, more,   quote,  more,   cont,   more,   more,   apost,  & ! 040
-!   (       )       *       +       ,       -       .       /
-    punct,  punct,  op_char,op_char,punct,  op_char,dot,    op_char,& ! 050
-!   0       1       2       3       4       5       6       7
-    digit,  digit,  digit,  digit,  digit,  digit,  digit,  digit,  & ! 060
-!   8       9       :       ;       <       =       >       ?
-    digit,  digit,  op_char,cmt,    op_char,op_char,more,   more,   & ! 070
-!   @       A       B       C       D       E       F       G
-    tog_ch, letter, letter, letter, letter, letter, letter, letter, & ! 100
-!   H       I       J       K       L       M       N       O
-    letter, letter, letter, letter, letter, letter, letter, letter, & ! 110
-!   P       Q       R       S       T       U       V       W
-    letter, letter, letter, letter, letter, letter, letter, letter, & ! 120
-!   X       Y       Z       [       \       ]       ^       _
-    letter, letter, letter, punct,  more,   punct,  more,   under,  & ! 130
-!   `       a       b       c       d       e       f       g
-    more,   letter, letter, letter, letter, letter, letter, letter, & ! 140
-!   h       i       j       k       l       m       n       o
-    letter, letter, letter, letter, letter, letter, letter, letter, & ! 150
-!   p       q       r       s       t       u       v       w
-    letter, letter, letter, letter, letter, letter, letter, letter, & ! 160
-!   x       y       z       {       |       }       ~       DEL
-    letter, letter, letter, more,   cont,   more,   more,   more   /) ! 170
     
     integer :: COLUMN              ! Character index of start of token in
                                    ! CHAR_TABLE
@@ -194,7 +201,7 @@ contains
           call lookup_and_insert ( string_index, found ) ! will be found
                                                          ! if tables OK
           the_token = token( symbol(string_index), string_index, .false., &
-                               source_start )
+                             source_start )
     exit ! main lexer loop
         case ( cmt )
           call new_line
@@ -498,9 +505,104 @@ contains
 
   end subroutine LEXER
 
+  ! -------------------------------------------------  Lex_Signal  -----
+  subroutine Lex_Signal ( Buf, Where, The_Token )
+
+  ! Get the next piece of a signal string from Buf.  Blank or end-of-string
+  ! terminates the signal string.
+
+    character(len=*), intent(in) :: Buf      ! The signal string, sans quotes
+    integer, intent(inout) :: Where          ! Last analyzed character in Buf
+    type(token), intent(out) :: The_Token    ! The token
+
+    character :: Ch                          ! Next character from Buf
+    integer :: Class                         ! Class of next character
+    logical :: Found                         ! "Found the symbol by lookup"
+    integer :: Source_Start                  ! Column of beginning of token
+    integer :: State                         ! State of Lexer
+    integer :: String_Index                  ! Where in the string table?
+
+    ! Parameters for states
+    integer, parameter :: Start = 1
+    integer, parameter :: Id = 2
+    integer, parameter :: Number = 3
+
+    state = start
+    do
+      where = where + 1
+      if ( where > len(buf) ) then
+        ch = eof
+        class = eof_in
+      else
+        ch = buf(where:where)
+        class = classes(iachar(ch))
+      end if
+      select case ( state )
+      case ( start )
+        select case ( class )
+        case ( eof_in, spaces )
+          string_index = enter_terminal ( '<eof>', t_end_of_input )
+          the_token = token( t_end_of_input, string_index, .false., where )
+          call set_symbol ( string_index, t_end_of_input )
+    exit
+        case ( digit )
+          call add_char ( ch )
+          source_start = where
+          state = number
+        case ( letter )
+          call add_char ( ch )
+          source_start = where
+          state = id
+        case ( dot, punct, op_char )
+          call add_char ( ch )
+          call lookup_and_insert ( string_index, found )
+          if ( .not. found ) then
+            call set_symbol ( string_index, t_unk_op )
+          else
+            the_token = token( symbol(string_index), string_index, .false., &
+                               where )
+          end if
+    exit ! main lexer loop
+        case default
+          call lookup_and_insert ( string_index, found )
+          call set_symbol ( string_index, t_unk_op )
+          the_token = token( t_unk_op, string_index, .false., where )
+    exit ! main lexer loop
+        end select
+      case ( id )
+        if ( class == letter .or. class == digit ) then
+          call add_char ( ch )
+        else
+          where = where - 1
+          string_index = add_terminal ( t_identifier )
+          the_token = token( t_identifier, string_index, .true., source_start )
+    exit ! main lexer loop
+        end if
+      case ( number )
+        if ( class == digit ) then
+          call add_char ( ch )
+        else
+          where = where - 1
+          string_index = add_terminal ( t_number )
+          the_token = token( t_number, string_index, .true., source_start )
+    exit ! main lexer loop
+        end if
+      end select
+    end do
+
+    if ( toggle(lex) ) then
+      call dump_1_symbol ( the_token%string_index, advance='yes' )
+    end if
+    return
+
+  end subroutine Lex_Signal
+
 end module LEXER_M
 
 ! $Log$
+! Revision 2.10  2001/03/14 02:06:31  vsnyder
+! Added lex_signal
+!
 ! Revision 2.9  2001/03/06 03:02:25  vsnyder
 ! Allow comments between continuations -- maybe got it right this time
 !
