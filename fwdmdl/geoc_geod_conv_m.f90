cf410bfdaaa7b9a6f6c629a2bfebb956bfcca0a8
vsnyder <>
2001-03-27 19:48:00 +0000

    Get Deg2Rad from Units, add some comments, make everything but geoc_geod_conv private, make constants _r8 instead of d0, cosmetic changes

--- fwdmdl/geoc_geod_conv_m.f90
+++ fwdmdl/geoc_geod_conv_m.f90
@@ -1,80 +1,88 @@
 module GEOC_GEOD_CONV_M
   use MLSCommon, only: R8
-  use Geometry  !  , only: GeodToGeocLat
+  use Geometry, only: GeodToGeocLat
   use STRINGS, only: STRLWR
-  use L2PC_FILE_PARAMETERS, only: DEG2RAD
+  use UNITS, only: DEG2RAD
   use L2PC_PFA_STRUCTURES, only: EARTH_MAJOR, EARTH_MINOR
   use ELLIPSE, only: A2, C2, C2OA2, CPT, SPT, CPS, SPS, CPTS, SPTS, &
                      NPHI_TAN, ROC, XOC, YOC
+
   implicit NONE
+  private
+  public :: GEOC_GEOD_CONV
+
 !---------------------------- RCS Ident Info -------------------------------
-  CHARACTER (LEN=256) :: Id = &
+  character (len=*), parameter :: IdParm = &
        "$Id$"
-  CHARACTER (LEN=*), PARAMETER :: ModuleName= &
+  character (len=len(idParm)) :: Id = idParm
+  character (len=*), parameter :: ModuleName= &
        "$RCSfile$"
 !---------------------------------------------------------------------------
 contains
 
-!---------------------------------------------------------------------
+  ! ---------------------------------------------  GEOC_GEOD_CONV  -----
+  subroutine GEOC_GEOD_CONV ( BETA_INC, PHI_TAN, GEOD_LAT, GEOC_LAT, Rp)
 
-SUBROUTINE geoc_geod_conv(beta_inc,phi_tan,geod_lat,geoc_lat,Rp)
+    real(r8), intent(in) :: BETA_INC    ! In Degrees
+    real(r8), intent(in) :: PHI_TAN     ! In Radians
+    real(r8), intent(in) :: GEOD_LAT    ! In Radians
 
-Real(r8), INTENT(IN) :: beta_inc                ! In Degrees
-Real(r8), INTENT(IN) :: phi_tan                 ! In Radians
-Real(r8), INTENT(IN) :: geod_lat                ! In Radians
+    real(r8), intent(out) :: GEOC_LAT   ! In Radians
 
-Real(r8), INTENT(OUT) :: geoc_lat               ! In Radians
+    real(r8), intent(out) :: RP         ! In Kilometers
 
-Real(r8), INTENT(OUT) :: rp
+    real(r8) :: q, r, incl, cw, sw, b2
 
-Real(r8) :: q,r,incl,cw,sw,b2
+    q = real(earth_major,r8)
+    a2 = q * q
 
-  q = Real(earth_major,r8)
-  a2 = q * q
+    r = real(earth_minor,r8)
+    b2 = r * r
 
-  r = Real(earth_minor,r8)
-  b2 = r * r
-
-  incl = (beta_inc - 90.0D0) * deg2rad
-  q = TAN(incl)
-  r = q * q
+    incl = (beta_inc - 90.0_r8) * deg2rad
+    q = tan(incl)
+    r = q * q
 
 ! c is the Minor axis for 2D ellipse, c2 = c * c
 
-  c2 = (1.0D0+r)*a2*b2/(a2+b2*r)
+    c2 = (1.0_r8+r)*a2*b2/(a2+b2*r)
 
-  c2oa2 = c2 / a2
+    c2oa2 = c2 / a2
 !
 ! Get the Geocentric Lat. (geoc_lat) from the Geodetic Lat.
 
-  geoc_lat = GeodToGeocLat(geod_lat)
+    geoc_lat = GeodToGeocLat(geod_lat)
 !
-  spt = SIN(Phi_tan)
-  cpt = COS(Phi_tan)
-  sw = spt * spt
-  cw = 1.0d0 - sw
+    spt = sin(Phi_tan)
+    cpt = cos(Phi_tan)
+    sw = spt * spt
+    cw = 1.0_r8 - sw
 
-  nphi_tan = a2 / SQRT(c2-(c2-a2)*cw)
+    nphi_tan = a2 / sqrt(c2-(c2-a2)*cw)
 
 !  Compute Radius of Curvature Circle (RoC) and its center coordinates:
 
-  r = c2 * cpt / a2
-  q = spt * spt + r * r
-  RoC = nphi_tan * SQRT(q)
-  XoC = (nphi_tan - RoC) * cpt
-  YoC = (c2oa2 * nphi_tan - RoC) * spt
+    r = c2 * cpt / a2
+    q = spt * spt + r * r
+    RoC = nphi_tan * sqrt(q)
+    XoC = (nphi_tan - RoC) * cpt
+    YoC = (c2oa2 * nphi_tan - RoC) * spt
 
-!  Compure Earth Radius (Elliptical)
+!  Compute Earth Radius (Elliptical)
 
-  q = ((a2*a2)*cw+(b2*b2)*sw)/(a2*cw+b2*sw)
-  Rp = SQRT(q)
+    q = ((a2*a2)*cw+(b2*b2)*sw)/(a2*cw+b2*sw)
+    Rp = sqrt(q)
 
-  Return
+    return
 
- END SUBROUTINE geoc_geod_conv
+  end subroutine GEOC_GEOD_CONV
 
-End module GEOC_GEOD_CONV_M
+end module GEOC_GEOD_CONV_M
+
 ! $Log$
+! Revision 1.3  2001/03/20 23:22:40  zvi
+! Change to new geoc_geod routine..
+!
 ! Revision 1.2  2001/03/20 11:03:15  zvi
 ! Fixing code for "real" data run, increase dim. etc.
 !
