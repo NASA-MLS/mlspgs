4c47101b35795778498854f07d1d7151951e0f51
pwagner <>
2001-03-30 00:27:38 +0000

    rev 2.14

--- l2/read_apriori.f90
+++ l2/read_apriori.f90
@@ -13,27 +13,31 @@ module ReadAPriori
   use L2AUXData, only: L2AUXData_T, AddL2AUXToDatabase, &
     &                  ReadL2AUXData!, SetupNewL2AUXRecord
   use L2GPData, only: L2GPData_T, AddL2GPToDatabase, ReadL2GPData
+  use LEXER_CORE, only: PRINT_SOURCE
   use MLSCommon, only: FileNameLen, L1BInfo_T, TAI93_Range_T
-  use MLSMessageModule, only: MLSMessage, MLSMSG_Allocate, MLSMSG_DeAllocate, &
-    &                         MLSMSG_Error, MLSMSG_FileOpen!, MLSMSG_Info
+!  use MLSMessageModule, only: MLSMessage, MLSMSG_Allocate, MLSMSG_DeAllocate, &
+!    &                         MLSMSG_Error, MLSMSG_FileOpen!, MLSMSG_Info
 !  use OBTAINCLIMATOLOGY, only: READ_CLIMATOLOGY
 !  use OBTAINDAO, only: READ_DAO
 !  use OBTAINNCEP, only: READ_NCEP
   use MLSPCF2, only: mlspcf_l2clim_start, mlspcf_l2clim_end
   use MoreTree, only: Get_Spec_ID
   use ncep_dao, only: AddGridTemplateToDatabase, &
-  & READ_CLIMATOLOGY, ReadGriddedData
+  & READ_CLIMATOLOGY, ReadGriddedData, source_file_already_read
+  use OUTPUT_M, only: OUTPUT
+  use SDPToolkit, only: Pgs_pc_getReference, PGS_S_SUCCESS
   use String_Table, only: GET_STRING, DISPLAY_STRING
   use TOGGLES, only: GEN, TOGGLE
   use TRACE_M, only: TRACE_BEGIN, TRACE_END
   use TREE, only: DECORATE, DECORATION, NODE_ID, NSONS, &
-    &             SUB_ROSA, SUBTREE
+    &             SUB_ROSA, SUBTREE, DUMP_TREE_NODE, SOURCE_REF
   use TREE_TYPES, only: N_NAMED!, N_DOT
-  use OUTPUT_M, only: OUTPUT
 
   implicit none
   private
   public ::  read_apriori
+  private ::  announce_error
+  integer, private :: ERROR
 
   ! -----     Private declarations     ---------------------------------
 
@@ -50,13 +54,13 @@ contains ! =====     Public Procedures     =============================
   ! Read a priori data from data files, be they l2gp, l2aux, climatology,
   ! NCEP, DAO etc.
 
-  subroutine read_apriori ( root, L2GPDatabase, l2auxDatabase, aprioriData)
+  subroutine read_apriori ( root, L2GPDatabase, l2auxDatabase, GriddedDatabase)
 
     ! Dummy arguments
     integer, intent(in) :: ROOT    ! Of the Read a priori section in the AST
     type (l2gpdata_t), dimension(:), pointer :: L2GPDatabase
     type (L2AUXData_T), dimension(:), pointer :: l2auxDatabase
-    type (GriddedData_T), dimension(:), pointer :: aprioriData 
+    type (GriddedData_T), dimension(:), pointer :: GriddedDatabase 
 
     !Local Variables
     integer :: FIELD               ! Son of KEY, must be n_assign
@@ -65,14 +69,17 @@ contains ! =====     Public Procedures     =============================
     integer :: fileName            ! Sub-rosa index of name in file='name'
     character(len=FileNameLen) :: FileNameString   ! actual literal file name
     integer :: FileType            ! either s_l2gp or s_l2aux
+    integer :: gridIndex           ! In the griddeddata database
     integer :: I, J                ! Loop indices for section, spec
     integer :: KEY                 ! Index of n_spec_args in the AST
+    integer :: lastClimPCF
     type (L2AUXData_T) :: L2AUX
     type (L2GPData_T) :: L2GP
     TYPE (GriddedData_T) :: GriddedData
     integer :: l2Index             ! In the l2gp or l2aux database
     integer :: L2Name              ! Sub-rosa index of L2[aux/gp] label
     character (LEN=480) :: msr     ! Error message if can't find file
+    integer :: pcf                 ! loop index of climatology pcf numbers
 
     integer :: sd_id
     integer :: SON              ! Of root, an n_spec_args or a n_named
@@ -80,13 +87,19 @@ contains ! =====     Public Procedures     =============================
     integer :: sdName        ! sub-rosa index of name in sdName='name'
     integer :: fieldName        ! sub-rosa index of name in field='name'
     integer :: griddedOrigin            ! From tree
+    character(len=FileNameLen) :: FIELDNAMESTRING ! actual literal clim. field
     character(len=FileNameLen) :: SWATHNAMESTRING ! actual literal swath name
     character(len=FileNameLen) :: SDNAMESTRING ! actual literal sdName
-    character(len=FileNameLen) :: FIELDNAMESTRING ! actual literal fieldName
+    integer :: version
+	integer :: returnStatus
 
     logical, dimension(field_first:field_last) :: got
 
     if ( toggle (gen) ) call trace_begin( "read_apriori", root )
+
+    error = 0
+	 version = 1
+	lastClimPCF = mlspcf_l2clim_start - 1
 
     do i = 2, nsons(root)-1 ! Skip the section name at begin and end
       got=.false.
@@ -124,7 +137,9 @@ contains ! =====     Public Procedures     =============================
         end select
       end do
 
-      if ( fileName == 0 ) call MLSMessage(MLSMSG_Error, ModuleName, &
+!      if ( fileName == 0 ) call MLSMessage(MLSMSG_Error, ModuleName, &
+!        & 'File name not specified in read a priori')
+      if ( fileName == 0 ) call announce_error(son, &
         & 'File name not specified in read a priori')
       
       call get_string ( FileName, fileNameString )
@@ -132,8 +147,11 @@ contains ! =====     Public Procedures     =============================
       
       select case( FileType )
       case ( s_l2gp )
+!        if ( .not. all(got((/f_swath, f_file/)))) &
+!          & call MLSMessage(MLSMSG_Error, ModuleName, &
+!          & 'Swath/filename name not specified in read a priori')
         if ( .not. all(got((/f_swath, f_file/)))) &
-          & call MLSMessage(MLSMSG_Error, ModuleName, &
+          & call announce_error(son, &
           & 'Swath/filename name not specified in read a priori')
         
         call get_string ( swathName, swathNameString )
@@ -141,8 +159,9 @@ contains ! =====     Public Procedures     =============================
         ! Open the l2gp file
         fileHandle = swopen(FileNameString, DFACC_READ)
         if (fileHandle == -1) then
-          msr = MLSMSG_Fileopen // FileNameString
-          call MLSMessage ( MLSMSG_Error, ModuleName, trim(msr) )
+!          msr = MLSMSG_Fileopen // FileNameString
+!          call MLSMessage ( MLSMSG_Error, ModuleName, trim(msr) )
+          call announce_error ( son, 'Failed to open swath file ' // FileNameString)
         end if
 
         ! Read the swath
@@ -151,8 +170,9 @@ contains ! =====     Public Procedures     =============================
         ! Close the file
         fileHandle = swclose(fileHandle)
         if (fileHandle == -1) THEN
-          msr = 'Failed to close file ' // FileNameString
-          call MLSMessage(MLSMSG_Error, ModuleName, trim(msr))
+!          msr = 'Failed to close file ' // FileNameString
+!          call MLSMessage(MLSMSG_Error, ModuleName, trim(msr))
+          call announce_error(son, 'Failed to close swath file ' // FileNameString)
         end if
 
         ! Add this l2gp to the database, decorate this key with index
@@ -161,8 +181,11 @@ contains ! =====     Public Procedures     =============================
         ! copy.
       case ( s_l2aux )
 
+!        if ( .not. all(got((/f_sdName, f_file/)))) &
+!          & call MLSMessage(MLSMSG_Error, ModuleName, &
+!          & 'file/sd name not specified in read a priori')
         if ( .not. all(got((/f_sdName, f_file/)))) &
-          & call MLSMessage(MLSMSG_Error, ModuleName, &
+          & call announce_error(son, &
           & 'file/sd name not specified in read a priori')
         
         call get_string ( sdName, sdNameString )
@@ -170,8 +193,9 @@ contains ! =====     Public Procedures     =============================
         ! create SD interface identifier for l2aux
         sd_id = sfstart(FilenameString, DFACC_READ)
         IF (sd_id == -1) THEN
-          msr = MLSMSG_Fileopen // FileNameString
-          CALL MLSMessage(MLSMSG_Error, ModuleName, msr)
+!          msr = MLSMSG_Fileopen // FileNameString
+!          CALL MLSMessage(MLSMSG_Error, ModuleName, msr)
+          CALL announce_error(son, 'Failed to open l2aux ' // sdNameString)
         ENDIF
         ! ??? subtree(1,key) is l2aux or l2gp.  It doesn't have a subtree ???
         !       vectorIndex = decoration(decoration(subtree(2,subtree(1,key))))
@@ -197,8 +221,11 @@ contains ! =====     Public Procedures     =============================
 
       case ( s_gridded )
 
+!        if ( .not. all(got((/f_origin, f_field, f_file/)))) &
+!          & call MLSMessage(MLSMSG_Error, ModuleName, &
+!          & 'Incomplete gridded data information')
         if ( .not. all(got((/f_origin, f_field, f_file/)))) &
-          & call MLSMessage(MLSMSG_Error, ModuleName, &
+          & call announce_error(son, &
           & 'Incomplete gridded data information')
 
         call get_string ( fieldName, fieldNameString )
@@ -207,32 +234,60 @@ contains ! =====     Public Procedures     =============================
         select case ( griddedOrigin )
         case ( l_ncep )
           
-          l2Index = AddGridTemplateToDatabase( aprioriData, GriddedData )
-          call decorate ( key, l2Index )
+          gridIndex = AddGridTemplateToDatabase( GriddedDatabase, GriddedData )
+          call decorate ( key, gridIndex )
           CALL ReadGriddedData(FileNameString, son, 'ncep', v_is_pressure, &
-            & aprioriData(l2Index), &
+            & GriddedDatabase(gridIndex), &
 				& 'XDim,YDim,Height,TIME', TRIM(fieldNameString))
           
         case ( l_dao )
           
-          l2Index = AddGridTemplateToDatabase( aprioriData, GriddedData )
-          call decorate ( key, l2Index )
+          gridIndex = AddGridTemplateToDatabase( GriddedDatabase, GriddedData )
+          call decorate ( key, gridIndex )
           CALL ReadGriddedData(FileNameString, son, 'dao', v_is_pressure, &
-            & aprioriData(l2Index), fieldName = TRIM(fieldNameString))
+            & GriddedDatabase(gridIndex), fieldName = TRIM(fieldNameString))
           
         case ( l_climatology )
          
           ! Identify file (maybe from PCF if no name given)
 
+				if(.NOT. got(f_file)) then
+					
+					do pcf=lastClimPCF+1, mlspcf_l2clim_end
+           		 returnStatus = Pgs_pc_getReference(i, version, &
+              & fileNameString)
+				  	if(returnStatus == PGS_S_SUCCESS) exit
+				  
+				  enddo
+				  
+				  if(returnStatus /= PGS_S_SUCCESS) then
+						call announce_error(son, &
+						& 'PCF number not found to supply missing Climatology file name')
+              endif
+				endif
+
           ! Have we read this already?
-
+				if(source_file_already_read(GriddedDatabase, trim(fileNameString), &
+				& trim(fieldNameString))) then
+				
+			! Excellent, no need to read it again--if it's already in the database
+				else
           ! No, well read it then, add its entire contents to the database
-          
-          ! Locate requested grid by name, store index in gridIndex
-          ! call decorate ( key, gridIndex )
-
           CALL READ_CLIMATOLOGY(FileNameString, son, &
-            & aprioriData, mlspcf_l2clim_start, mlspcf_l2clim_end)
+            & GriddedDatabase, mlspcf_l2clim_start, mlspcf_l2clim_end)
+          
+				endif
+          ! Locate requested grid by name, store index in gridIndex
+
+			! Check that field name is among those added by the source field
+				if(source_file_already_read(GriddedDatabase, trim(fileNameString), &
+				& trim(fieldNameString))) then
+		          call decorate ( key, gridIndex )
+				else
+	          CALL announce_error(son, 'Field ' // trim(fieldNameString) // &
+				 & ' not found in clim. file ' // trim(fileNameString))
+				endif
+
           
         case default ! Can't get here if tree_checker worked correctly
         end select
@@ -245,12 +300,77 @@ contains ! =====     Public Procedures     =============================
   
   end subroutine read_apriori
 
+  ! ------------------------------------------------  announce_error  -----
+  subroutine announce_error ( lcf_where, full_message, use_toolkit, &
+  & error_number )
+  
+   ! Arguments
+	
+	integer, intent(in)    :: lcf_where
+	character(LEN=*), intent(in)    :: full_message
+	logical, intent(in), optional :: use_toolkit
+	integer, intent(in), optional    :: error_number
+	! Local
+!  character (len=80) :: msg, mnemonic
+!  integer :: status
+  logical :: just_print_it
+  logical, parameter :: default_output_by_toolkit = .true.
+	
+	if(present(use_toolkit)) then
+		just_print_it = use_toolkit
+	elseif(default_output_by_toolkit) then
+		just_print_it = .false.
+	else
+		just_print_it = .true.
+	endif
+	
+	if(.not. just_print_it) then
+    error = max(error,1)
+    call output ( '***** At ' )
+
+	if(lcf_where > 0) then
+	    call print_source ( source_ref(lcf_where) )
+		else
+    call output ( '(no lcf node available)' )
+		endif
+
+    call output ( ': ' )
+    call output ( "The " );
+	if(lcf_where > 0) then
+    call dump_tree_node ( lcf_where, 0 )
+		else
+    call output ( '(no lcf tree available)' )
+		endif
+
+		CALL output("Caused the following error:", advance='yes', &
+		& from_where=ModuleName)
+		CALL output(trim(full_message), advance='yes', &
+		& from_where=ModuleName)
+		if(present(error_number)) then
+			CALL output('error number ', advance='no')
+			CALL output(error_number, places=9, advance='yes')
+		endif
+	else
+		print*, '***Error in module ', ModuleName
+		print*, trim(full_message)
+		if(present(error_number)) then
+			print*, 'error number ', error_number
+		endif
+	endif
+
+!===========================
+  end subroutine announce_error
+!===========================
+
 end module ReadAPriori
 
 !=============================================================================
 
 !
 ! $Log$
+! Revision 2.14  2001/03/30 00:27:38  pwagner
+! Fleshed out njl outline for reading clim. files
+!
 ! Revision 2.13  2001/03/29 19:13:41  livesey
 ! Added some comment guidelines for read climatology
 !
