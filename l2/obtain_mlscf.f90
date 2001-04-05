60016ad03850136315fed516264d2a530a176f5a
pwagner <>
2001-04-05 23:42:10 +0000

    Added announce_error, deleted all MLSMessages

--- l2/obtain_mlscf.f90
+++ l2/obtain_mlscf.f90
@@ -4,9 +4,12 @@
 module OBTAIN_MLSCF
 
 ! Open and close the MLSCF
-  use MLSMessageModule, only: MLSMessage, MLSMSG_Error
+  use LEXER_CORE, only: PRINT_SOURCE
+!  use MLSMessageModule, only: MLSMessage, MLSMSG_Error
+  USE output_m, only: output
   use SDPToolkit, only: Pgs_io_gen_closeF, Pgs_io_gen_openF, PGS_S_SUCCESS, &
     & PGSd_IO_Gen_RSeqFrm
+  use TREE, only: DUMP_TREE_NODE, SOURCE_REF
 
   implicit NONE
 
@@ -21,6 +24,8 @@ module OBTAIN_MLSCF
   character(len=*), parameter :: ModuleName="$RCSfile$"
   !-----------------------------------------------------------------------------
 
+  integer, private :: ERROR
+
 contains ! =====     Public Procedures     =============================
 
   ! ------------------------------------------------  CLOSE_MLSCF  -----
@@ -32,13 +37,16 @@ contains ! =====     Public Procedures     =============================
     character (LEN=256) :: MSG
     integer :: RETURN_STATUS
 
+    error = 0
     return_Status = Pgs_io_gen_closeF ( CF_Unit )
 
     if ( return_Status /= PGS_S_SUCCESS ) then
-      call Pgs_smf_getMsg ( return_Status, mnemonic, msg )
-      call MLSMessage ( MLSMSG_Error, ModuleName, &
-        & 'Error closing L2CF:  '//mnemonic//' '//msg)
+!      call Pgs_smf_getMsg ( returnStatus, mnemonic, msg )
+!      call MLSMessage ( MLSMSG_Error, ModuleName, &
+!        & 'Error closing L2CF:  '//mnemonic//' '//msg)
+		call announce_error(0, 'Error closing L2CF')
     end if
+
   end subroutine CLOSE_MLSCF
 
   ! -------------------------------------------------  OPEN_MLSCF  -----
@@ -52,22 +60,88 @@ contains ! =====     Public Procedures     =============================
     character (LEN=256) :: MSG
     integer :: RETURN_STATUS
 
+    error = 0
     L2CF_Version = 1
     return_Status = Pgs_io_gen_openF ( MLSPCF_Start, PGSd_IO_Gen_RSeqFrm, &
                                       0, CF_Unit, L2CF_Version)
 
     if ( return_Status /= PGS_S_SUCCESS ) then
-
-      call Pgs_smf_getMsg ( return_Status, mnemonic, msg )
-      call MLSMessage ( MLSMSG_Error, ModuleName, &
-        & "Error opening MLSCF:  "//mnemonic//"  "//msg)
+!      call Pgs_smf_getMsg ( returnStatus, mnemonic, msg )
+!      call MLSMessage ( MLSMSG_Error, ModuleName, &
+!        & "Error opening MLSCF:  "//mnemonic//"  "//msg)
+		call announce_error(0, "Error opening MLSCF")
     end if
 
   end subroutine OPEN_MLSCF
 
+  ! ------------------------------------------------  announce_error  -----
+  subroutine announce_error ( lcf_where, full_message, use_toolkit, &
+  & error_number )
+  
+   ! Arguments
+	
+	integer, intent(in)    :: lcf_where
+	character(LEN=*), intent(in)    :: full_message
+	logical, intent(in), optional :: use_toolkit
+	integer, intent(in), optional    :: error_number
+
+	! Local
+  logical :: just_print_it
+  logical, parameter :: default_output_by_toolkit = .true.
+	
+    error = 0
+	if(present(use_toolkit)) then
+		just_print_it = use_toolkit
+	elseif(default_output_by_toolkit) then
+		just_print_it = .false.
+	else
+		just_print_it = .true.
+	endif
+	
+	if(.not. just_print_it) then
+    error = max(error,1)
+    call output ( '***** At ' )
+
+	if(lcf_where > 0) then
+	    call print_source ( source_ref(lcf_where) )
+		else
+    call output ( '(no lcf node available)' )
+		endif
+
+    call output ( ': ' )
+    call output ( "The " );
+	if(lcf_where > 0) then
+    call dump_tree_node ( lcf_where, 0 )
+		else
+    call output ( '(no lcf tree available)' )
+		endif
+
+		CALL output("Caused the following error:", advance='yes', &
+		& from_where=ModuleName)
+		CALL output(trim(full_message), advance='yes', &
+		& from_where=ModuleName)
+		if(present(error_number)) then
+			CALL output('error number ', advance='no')
+			CALL output(error_number, places=9, advance='yes')
+		endif
+	else
+		print*, '***Error in module ', ModuleName
+		print*, trim(full_message)
+		if(present(error_number)) then
+			print*, 'error number ', error_number
+		endif
+	endif
+
+!===========================
+  end subroutine announce_error
+!===========================
+
 end module OBTAIN_MLSCF
 
 ! $Log$
+! Revision 2.3  2001/02/28 02:02:21  vsnyder
+! Remove unused reference to MLSPCF2
+!
 ! Revision 2.2  2001/02/28 01:58:07  vsnyder
 ! Get pcf # and unit # from arguments
 !
