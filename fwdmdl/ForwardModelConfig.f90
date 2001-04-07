fba39b5208ce26599cb565c85a1d7e4404443ed2
vsnyder <>
2001-04-07 01:56:25 +0000

    Initial commit

---
+++ fwdmdl/ForwardModelConfig.f90
@@ -0,0 +1,166 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
+!=============================================================================
+module ForwardModelConfig
+!=============================================================================
+
+! Set up the forward model configuration, except for actually processing
+! the command.
+
+
+  use Allocate_Deallocate, only: Allocate_Test, Deallocate_Test
+  use Dump_0, only: DUMP
+  use MLSCommon, only: R8
+  use MLSMessageModule, only: MLSMessage, MLSMSG_Allocate, MLSMSG_Deallocate,&
+    & MLSMSG_Error
+  use MLSSignals_M, only: GetSignalName, MaxSigLen
+  use Output_M, only: Output
+  use String_Table, only: Display_String
+  use VGridsDatabase, only: VGrid_T
+
+  implicit NONE
+  private
+
+  ! Public procedures:
+  interface Dump
+    module procedure Dump_ForwardModelConfigs
+  end interface Dump
+
+  public :: AddForwardModelConfigToDatabase, DestroyFWMConfigDatabase, Dump
+
+  ! Public Types:
+
+  type, public :: ForwardModelSignalInfo_T
+    integer :: signal                   ! The signal we're considering
+    logical, dimension(:), pointer :: channelIncluded=>null() ! Which channels to use
+  end type ForwardModelSignalInfo_T
+
+  type, public :: ForwardModelConfig_T
+    integer :: fwmType        ! l_linear, l_full or l_scan
+    logical :: Atmos_Der      ! Do atmospheric derivatives
+    logical :: Do_Conv        ! Do convolution
+    logical :: Do_Freq_Avg    ! Do Frequency averaging
+    integer, dimension(:), pointer :: molecules=>NULL() ! Which molecules to consider
+    logical, dimension(:), pointer :: moleculeDerivatives=>NULL() ! Want jacobians
+    real(r8) :: The_Freq      ! Frequency to use if .not. do_freq_avg
+    type (ForwardModelSignalInfo_T), dimension(:), pointer :: siginfo=>NULL()
+    logical :: Spect_Der      ! Do spectroscopy derivatives
+    logical :: Temp_Der       ! Do temperature derivatives
+    type(vGrid_T), pointer :: integrationGrid ! Zeta grid for integration
+    type(vGrid_T), pointer :: tangentGrid     ! Zeta grid for integration
+    integer :: surfaceTangentIndex  ! Index in Tangentgrid of Earth's surface
+    integer :: phiWindow            ! Window size for examining stuff
+  end type ForwardModelConfig_T
+
+  !---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter, private :: IdParm = &
+    & "$Id$"
+  character (len=len(idParm)) :: Id = IdParm
+  character (len=*), parameter, private :: ModuleName= &
+    & "$RCSfile$"
+  !---------------------------------------------------------------------------
+
+contains
+
+  ! ----------------------------  AddForwardModelConfigToDatabase  -----
+  integer function AddForwardModelConfigToDatabase ( Database, Item )
+
+    ! Add a quantity template to a database, or create the database if it
+    ! doesn't yet exist
+
+    ! Dummy arguments
+    type (ForwardModelConfig_T), dimension(:), pointer :: Database
+    type (ForwardModelConfig_T), intent(in) :: Item
+
+    ! Local variables
+    type (ForwardModelConfig_T), dimension(:), pointer :: tempDatabase
+
+    include "addItemToDatabase.f9h"
+
+    AddForwardModelConfigToDatabase = newSize
+  end function AddForwardModelConfigToDatabase
+
+  ! --------------------------  DestroyForwardModelConfigDatabase  -----
+  subroutine DestroyFWMConfigDatabase ( Database )
+    ! Dummy arguments
+    type (ForwardModelConfig_T), dimension(:), pointer :: Database
+
+    ! Local variables
+    integer :: Config                   ! Loop counter
+    integer :: Signal                   ! Loop counter
+    integer :: Status                   ! Flag
+
+    if ( associated(database) ) then
+      do config = 1, size(database)
+        do signal = 1, size(database(config)%sigInfo)
+          deallocate ( database(config)%sigInfo(signal)%channelIncluded, &
+            & stat=status )
+          if ( status /= 0 ) call MLSMessage ( MLSMSG_Error, ModuleName, &
+            & MLSMSG_Deallocate // "database%signals%channelIncluded" )
+        end do
+        deallocate ( database(config)%sigInfo, stat=status )
+        if ( status /= 0 ) call MLSMessage ( MLSMSG_Error, ModuleName, &
+          & MLSMSG_Deallocate // "database%signals" )
+        deallocate ( database(config)%molecules, stat=status )
+        if ( status /= 0 ) call MLSMessage ( MLSMSG_Error, ModuleName, &
+          & MLSMSG_Deallocate // "database%molecules" )
+      end do
+
+      deallocate ( database, stat=status )
+      if ( status /= 0 ) call MLSMessage ( MLSMSG_Error, ModuleName, &
+        & MLSMSG_Deallocate // "Database" )
+    end if
+  end subroutine DestroyFWMConfigDatabase
+
+  ! =====     Private Procedures     =====================================
+  ! ------------------------------------  DUMP_FOWARDMODELCONFIGS  -----
+  subroutine Dump_ForwardModelConfigs ( Database, Lit_Indices )
+    type (ForwardModelConfig_T), pointer, dimension(:) :: Database
+    integer, intent(in), dimension(:) :: Lit_Indices
+
+    ! Local variables
+    integer :: I, J                          ! Loop counters
+    character (len=MaxSigLen) :: SignalName  ! A line of text
+
+    ! executable code
+    if ( associated(database) ) then
+      do i = 1, size(database)
+        call output ( 'FowardModelConfig: ' )
+        call output ( i, advance = 'yes' )
+        call output ( '  Atmos_der:' )
+        call output ( database(i)%atmos_der, advance='yes' )
+        call output ( '  Do_conv:' )
+        call output ( database(i)%do_conv, advance='yes' )
+        call output ( '  Do_freq_avg:' )
+        call output ( database(i)%do_freq_avg, advance='yes' )
+        call output ( '  Spect_der:' )
+        call output ( database(i)%spect_der, advance='yes' )
+        call output ( '  Temp_der:' )
+        call output ( database(i)%temp_der, advance='yes' )
+        call output ( '  The_freq:' )
+        call output ( database(i)%the_freq, advance='yes' )
+        call output ( '  Molecules: ', advance='yes' )
+        do j = 1, size(database(i)%molecules)
+          call output ( '    ' )
+          call display_string(lit_indices(database(i)%molecules(j)))
+          if (database(i)%moleculeDerivatives(j)) then
+            call output (' compute derivatives', advance='yes')
+          else
+            call output (' no derivatives', advance='yes')
+          end if
+        end do
+        call output ( '  Signals:', advance='yes')
+        do j = 1, size(database(i)%sigInfo)
+          call output ( '    ' )
+          call GetSignalName( database(i)%sigInfo(j)%signal, signalName)
+          call output ( signalName//' channelIncluded:', advance='yes')
+          call dump ( database(i)%sigInfo(j)%channelIncluded )
+        end do
+      end do
+    end if
+  end subroutine Dump_ForwardModelConfigs
+
+end module ForwardModelConfig
+
+! $Log$
