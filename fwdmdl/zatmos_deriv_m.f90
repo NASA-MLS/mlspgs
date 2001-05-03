b2bb4707e0d8deaf463a4058576d39b27a78e009
vsnyder <>
2001-05-03 22:30:21 +0000

    Insert copyright notice, CVS log line, some cosmetic changes

--- fwdmdl/zatmos_deriv_m.f90
+++ fwdmdl/zatmos_deriv_m.f90
@@ -1,3 +1,6 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
 module ZATMOS_DERIV_M
   use MLSCommon, only: I4, R8
   use ForwardModelConfig, only: ForwardModelConfig_T
@@ -9,17 +12,17 @@ module ZATMOS_DERIV_M
   private
   public :: ZATMOS_DERIV
 !---------------------------- RCS Ident Info -------------------------------
-  private ID, ModuleName
-  CHARACTER (LEN=256) :: Id = &
+  character (len=*), private, parameter :: IdParm = &
        "$Id$"
-  CHARACTER (LEN=*), PARAMETER :: ModuleName= &
+  character (len=len(idParm)), private :: Id = idParm
+  character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
 !---------------------------------------------------------------------------
 contains
 !---------------------------------------------------------------------------
-  Subroutine ZATMOS_DERIV(ForwardModelConfig, FwdModelExtra, FwdModelIn, &
-             frq_i, mid, delta, t_script, tau, ilo, ihi, k_atmos,Ier)
-!
+  Subroutine ZATMOS_DERIV ( ForwardModelConfig, FwdModelExtra, FwdModelIn, &
+             frq_i, mid, delta, t_script, tau, ilo, ihi, k_atmos, Ier )
+
     type(forwardModelConfig_T), intent(in) :: forwardModelConfig
     type (Vector_T), intent(in) :: fwdModelIn, fwdModelExtra
 
@@ -31,37 +34,39 @@ contains
     integer(i4), intent(out) :: Ier
 
     Type(path_derivative), intent(in out) :: k_atmos(:)
-!
-! --- Local variables --------------------------------------------
-!
+
+    ! --- Local variables --------------------------------------------
+
     Real(r8) :: r
     Integer(i4) :: J, IZ, IP
     type (VectorValue_T), pointer :: f
-!
+
     Ier = 0
     do j = 1, size(forwardModelConfig%molecules)
-!
-      IF (forwardModelConfig%moleculeDerivatives(j)) THEN
+
+      if ( forwardModelConfig%moleculeDerivatives(j) ) then
 
         f => GetVectorQuantityByType ( fwdModelIn, fwdModelExtra, &
      &       quantityType=l_vmr, molecule=forwardModelConfig%molecules(j))
-!
+
         do ip = lbound(k_atmos(j)%values,3), ubound(k_atmos(j)%values,3)
 
           do iz = 1, f%template%noSurfs
-!
+
             Call get_drad_notder (delta(1:,iz,ip,j), t_script, tau, &
    &                              mid, ilo, ihi, r)
             k_atmos(j)%values(frq_i,iz,ip) = r
-!
+
           end do
-!
+
         end do
-!
-      ENDIF
-!
+
+      end if
+
     end do
-!
+
     Return
   End Subroutine ZATMOS_DERIV
 end module ZATMOS_DERIV_M
+
+! $Log$
