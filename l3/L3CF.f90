0ead56d58f4c27d5d0799008c9fe9eeb4f2695f9
nakamura <>
2001-05-04 18:29:16 +0000

    Added DZ stuff.

--- l3/L3CF.f90
+++ l3/L3CF.f90
@@ -41,6 +41,10 @@ MODULE L3CF
 
    TYPE L3CFDef_T
 
+     INTEGER :: nNom		! number of points in the L2 nominal latitude grid
+
+     REAL(r8), DIMENSION(maxGridPoints) :: l2nomLats		! dimensioned (nNom)
+
      CHARACTER (LEN=FileNameLen) :: logType
 	! template for the log file name
 
@@ -70,30 +74,42 @@ MODULE L3CF
      CHARACTER (LEN=GridNameLen) :: l3prodNameD
 	! name of product processed at l3
 
-     INTEGER :: nDays		! number of days in the l3 processing window
+     INTEGER :: nDays			! number of days in the l3 processing window
 
      REAL(r8), DIMENSION(maxWindow) :: timeD		! dimensioned (nDays)
 	! array of TAI93 synoptic times for each day in the processing window
 
-     CHARACTER (LEN=3) :: intpMethod	! method of interpolation (lin/csp)
+     CHARACTER (LEN=3) :: mode					! asc/des/com/all
 
      INTEGER :: nLats 			! number of points in latitude grid
 
-     REAL(r8), DIMENSION(maxGridPoints) :: latGridMap      ! dimensioned (nLats)
+     REAL(r8), DIMENSION(maxGridPoints) :: latGridMap	! dimensioned (nLats)
 
      INTEGER :: nLons			! number of points in longitude grid
 
-     REAL(r8), DIMENSION(maxGridPoints) :: longGrid        ! dimensioned (nLons)
+     REAL(r8), DIMENSION(maxGridPoints) :: longGrid	! dimensioned (nLons)
 
-     INTEGER :: nFreqs			! number of frequencies in range
+     REAL(r8), DIMENSION(2) :: l3presLvl       ! min, max combined pressure levels
 
-     REAL(r8), DIMENSION(maxGridPoints) :: rangFrequency ! dimensioned (nFreqs)
+     REAL(r8), DIMENSION(2) :: ascPresLvl      ! min, max ascending pressure levels
 
-     REAL(r8), DIMENSION(2) :: l3presLvl	! min, max pressure levels
+     REAL(r8), DIMENSION(2) :: desPresLvl      ! min, max descending pressure levels
+
+     REAL(r8), DIMENSION(2) :: rangFrequency	! min, max of frequency range
 
      INTEGER, DIMENSION(2) :: rangWavenumber	! min, max of wave # range
 
-     CHARACTER (LEN=3) :: mode			! asc/des/com/all
+     CHARACTER (LEN=3) :: intpMethod	! method of interpolation (lin/csp)
+
+     ! Daily Zonal Mean specifications
+
+     CHARACTER (LEN=2) :: calMethod			! calculation method (l2/l3)
+
+     REAL(r8), DIMENSION(2) :: zComLvl		! min, max com pressure levels
+
+     REAL(r8), DIMENSION(2) :: zAscLvl		! min, max asc pressure levels
+
+     REAL(r8), DIMENSION(2) :: zDesLvl		! min, max des pressure levels
 
      ! Output section
 
@@ -204,6 +220,21 @@ CONTAINS
       IF (l3Ver /= pcfL3Ver) CALL MLSMessage(MLSMSG_Error, ModuleName, &
                                   'Output versions in the CF and PCF differ.')
 
+! Find the L2 nominal latitude grid in the GlobalSettings section of the cf;
+! save it in L3CFDef_T
+
+      indx = LinearSearchStringArray(cf%Sections(iGlob)%Cells%Keyword, &
+                                     'l2nomLats')
+      IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'No entry in &
+                                     &the CF for l2nomLats.')
+
+      DO i = 1, maxGridPoints
+         cfDef%l2nomLats(i) = cf%Sections(iGlob)%Cells(indx)%RealValue
+         IF (cf%Sections(iGlob)%Cells(indx)%More == 0) EXIT
+         indx = indx + 1
+      ENDDO
+      cfDef%nNom = i
+ 
 ! Find/save the log type from the GlobalSettings section of the cf
 
       indx = LinearSearchStringArray(cf%Sections(iGlob)%Cells%Keyword,'LogType')
@@ -211,7 +242,7 @@ CONTAINS
                                     &the CF for LogType.')
       cfDef%logType = cf%Sections(iGlob)%Cells(indx)%CharValue
 
-! Find/save minDays from the GlobalSettings section of the cf
+! Find/save minDays from GlobalSettings
 
       indx = LinearSearchStringArray(cf%Sections(iGlob)%Cells%Keyword,'MinDays')
       IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'No entry in &
@@ -337,13 +368,13 @@ CONTAINS
                             ModuleName, 'Error converting from CCSDSB to TAI.')
        ENDDO
 
-! Find/save the interpolation method
+! Find/save mode
 
        indx = LinearSearchStringArray( &
-                     cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'intpMethod')
+                           cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'mode')
        IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
-                     &keyword INTPMETHOD in the DailyMap section of the l3cf.')
-       l3cf(i)%intpMethod = cf%Sections(iMap)%Entries(i)%Cells(indx)%CharValue
+                           &keyword MODE in the DailyMap section of the l3cf.')
+       l3cf(i)%mode = cf%Sections(iMap)%Entries(i)%Cells(indx)%CharValue
 
 ! Find the boundaries of the latitude grid
 
@@ -388,37 +419,56 @@ CONTAINS
 
        CALL CalculateArray(start, end, delta, l3cf(i)%longGrid, l3cf(i)%nLons)
 
-! Find the boundaries of the frequency range
+! Find/save the min & max of the pressure levels for the desired modes
+
+       IF ( (l3cf(i)%mode == 'com') .OR. (l3cf(i)%mode == 'all') ) THEN
+          indx = LinearSearchStringArray( &
+                        cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'l3presLvl')
+          IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
+                         &keyword L3PRESLVL in the DailyMap section of the l3cf.')
+          l3cf(i)%l3presLvl(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
+          l3cf(i)%l3presLvl(2) = &
+                         cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
+       ELSE
+          l3cf(i)%l3presLvl = 0.0
+       ENDIF
+
+       IF ( (l3cf(i)%mode == 'asc') .OR. (l3cf(i)%mode == 'all') ) THEN
+
+          indx = LinearSearchStringArray( &
+                        cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'ascPresLvl')
+          IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
+                         &keyword ASCPRESLVL in the DailyMap section of the l3cf.')
+          l3cf(i)%ascPresLvl(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
+          l3cf(i)%ascPresLvl(2) = &
+                         cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
+       ELSE
+          l3cf(i)%ascPresLvl = 0.0
+       ENDIF
+
+      IF ( (l3cf(i)%mode == 'des') .OR. (l3cf(i)%mode == 'all') ) THEN
+
+          indx = LinearSearchStringArray( &
+                        cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'desPresLvl')
+          IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
+                         &keyword DESPRESLVL in the DailyMap section of the l3cf.')
+          l3cf(i)%desPresLvl(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
+          l3cf(i)%desPresLvl(2) = &
+                         cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
+
+      ELSE
+          l3cf(i)%desPresLvl = 0.0
+      ENDIF
+
+! Find/save the min & max for the ranges of frequencies, wavenumbers
 
        indx = LinearSearchStringArray( &
                    cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'rangFrequency')
        IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
                   &keyword RANGFREQUENCY in the DailyMap section of the l3cf.')
-       start = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
-       end = cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
-
-! Find the increment of the frequency range
-
-       indx = LinearSearchStringArray( &
-                           cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'dF')
-       IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
-                             &keyword DF in the DailyMap section of the l3cf.')
-       delta = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
-
-! Calculate the array containing the frequency range
-
-       CALL CalculateArray(start, end, delta, l3cf(i)%rangFrequency, &
-                           l3cf(i)%nFreqs)
-
-! Find/save the min & max for the pressure levels, range of wavenumbers
-
-       indx = LinearSearchStringArray( &
-                     cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'l3presLvl')
-       IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
-                      &keyword L3PRESLVL in the DailyMap section of the l3cf.')
-       l3cf(i)%l3presLvl(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
-       l3cf(i)%l3presLvl(2) = &
-                      cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
+       l3cf(i)%rangFrequency(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
+       l3cf(i)%rangFrequency(2) = &
+                            cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
 
        indx = LinearSearchStringArray( &
                cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'rangWavenumber')
@@ -429,13 +479,13 @@ CONTAINS
        l3cf(i)%rangWavenumber(2) = &
                        cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
 
-! Find/save mode
+! Find/save the interpolation method
 
        indx = LinearSearchStringArray( &
-                           cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'mode')
+                     cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'intpMethod')
        IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
-                           &keyword MODE in the DailyMap section of the l3cf.')
-       l3cf(i)%mode = cf%Sections(iMap)%Entries(i)%Cells(indx)%CharValue
+                     &keyword INTPMETHOD in the DailyMap section of the l3cf.')
+       l3cf(i)%intpMethod = cf%Sections(iMap)%Entries(i)%Cells(indx)%CharValue
 
 ! Find the label in the DailyMap section
 
@@ -484,6 +534,40 @@ CONTAINS
        l3cf(i)%fileTemplate = &
                           cf%Sections(iOut)%Entries(iLab)%Cells(indx)%CharValue
 
+! Daily Zonal Mean specifications -- find/save the DZ calculation method
+
+       indx = LinearSearchStringArray( &
+                     cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'calMethod')
+       IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
+                     &keyword CALMETHOD in the DailyMap section of the l3cf.')
+       l3cf(i)%calMethod = cf%Sections(iMap)%Entries(i)%Cells(indx)%CharValue
+
+! Find/save the DZ min:max pressure levels
+
+       indx = LinearSearchStringArray( &
+                     cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'zComLvl')
+       IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
+                      &keyword ZCOMLVL in the DailyMap section of the l3cf.')
+       l3cf(i)%zComLvl(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
+       l3cf(i)%zComLvl(2) = &
+                      cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
+
+       indx = LinearSearchStringArray( &
+                     cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'zAscLvl')
+       IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
+                      &keyword ZASCLVL in the DailyMap section of the l3cf.')
+       l3cf(i)%zAscLvl(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
+       l3cf(i)%zAscLvl(2) = &
+                      cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
+
+       indx = LinearSearchStringArray( &
+                     cf%Sections(iMap)%Entries(i)%Cells%Keyword, 'zDesLvl')
+       IF (indx == 0) CALL MLSMessage(MLSMSG_Error, ModuleName, 'Missing &
+                      &keyword ZDESLVL in the DailyMap section of the l3cf.')
+       l3cf(i)%zDesLvl(1) = cf%Sections(iMap)%Entries(i)%Cells(indx)%RealValue
+       l3cf(i)%zDesLvl(2) = &
+                      cf%Sections(iMap)%Entries(i)%Cells(indx)%RangeUpperBound
+
     ENDDO
 
 !-------------------------
@@ -495,6 +579,9 @@ END MODULE L3CF
 !==============
 
 ! $Log$
+! Revision 1.10  2001/03/27 17:26:11  nakamura
+! Moved maxGridPoints to MLSL3Common.
+!
 ! Revision 1.9  2001/02/21 20:43:43  nakamura
 ! Changed MLSPCF to MLSPCF3; removed InputVersion & check; added L3DZ stuff.
 !
