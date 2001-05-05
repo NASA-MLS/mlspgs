ee62f1a2862506a1a42b83918a8cc1828b43e01f
livesey <>
2001-05-05 00:02:27 +0000

    rev 2.29

--- l2/SidsModule.f90
+++ l2/SidsModule.f90
@@ -15,17 +15,21 @@ module SidsModule
   use ForwardModelIntermediate, only: ForwardModelIntermediate_T,&
     & ForwardModelStatus_T, DestroyForwardModelIntermediate
   use Init_Tables_Module, only: f_forwardModel, f_fwdModelIn, f_fwdModelExtra, &
-    f_fwdModelOut, f_jacobian
+    f_fwdModelOut, f_jacobian, f_perturbation
   use Lexer_Core, only: Print_Source
+  use MLSCommon, only: R8
   use MLSMessageModule, only: MLSMessage, MLSMSG_Error, MLSMSG_Allocate
+  use MatrixModule_0, only: M_Absent, M_Full, M_Banded, M_Column_Sparse, &
+    & MatrixElement_T
   use MatrixModule_1, only: AddToMatrixDatabase, CreateEmptyMatrix, &
-    GetFromMatrixDatabase, Matrix_Database_T, Matrix_T, DestroyBlock
+    GetFromMatrixDatabase, Matrix_Database_T, Matrix_T, DestroyBlock, CreateBlock, &
+    & FindBlock
   use MoreTree, only: Get_Field_Id
   use Output_M, only: Output
   use Toggles, only: Gen, Toggle
   use Trace_M, only: Trace_begin, Trace_end
   use Tree, only: Decoration, Node_ID, Nsons, Source_Ref, Sub_Rosa, Subtree
-  use VectorsModule, only: Vector_T
+  use VectorsModule, only: CopyVector, DestroyVectorInfo, Vector_T, operator(-)
 
   implicit none
 
@@ -56,22 +60,39 @@ contains
     type(vector_T), pointer :: FwdModelIn
     type(vector_T), pointer :: FwdModelExtra
     type(vector_T), pointer :: FwdModelOut
+    type(vector_T), pointer :: Perturbation
+    type(vector_T) :: SaveState         ! For numerical derivatives
+    type(vector_T) :: Deviation         ! Result of perturbation
+    type(vector_T) :: SaveResult        ! Result of forward model out
     integer :: I                        ! Subscript, loop inductor
     integer :: IxJacobian               ! Index of Jacobian in matrix database
     type(matrix_T), pointer :: Jacobian ! The Jacobian matrix
     integer :: Son                      ! Of ROOT
+    integer :: QUANTITY                 ! Index
+    integer :: INSTANCE                 ! Index
+    integer :: ELEMENT                  ! Index
     integer :: STATUS                   ! Flag
+    logical :: DOTHISONE                ! Flag
+    integer :: LOOPEND                  ! Loop limit
+    integer :: COL                      ! Column in jacobian
+    integer :: ROW                      ! Row in jacobian
+    integer :: ROWINSTANCE              ! From jacobian
+    integer :: ROWQUANTITY              ! From jacobian
+    type (MatrixElement_T), pointer :: M0 ! A block from the jacobian
 
     type (ForwardModelIntermediate_T) :: ifm ! Work space for forward model
     type (ForwardModelStatus_T) :: fmStat ! Status for forward model
 
+    real (r8) :: THISPTB                ! A scalar perturbation
+
     ! Error message codes
     integer, parameter :: NeedJacobian = 1   ! Needed if derivatives requested
     integer, parameter :: NotPlain = needJacobian + 1  ! Not a "plain" matrix
+    integer, parameter :: PerturbationNotState = NotPlain + 1 ! Ptb. not same as state
 
     if ( toggle(gen) ) call trace_begin ( "SIDS", root )
 
-    nullify ( configs )
+    nullify ( configs, perturbation )
 
     ! Process the fields of the "sids" specification
     error = 0
@@ -87,6 +108,8 @@ contains
         fwdModelExtra => vectorDatabase(decoration(decoration(subtree(2,son))))
       case ( f_fwdModelOut )
         fwdModelOut => vectorDatabase(decoration(decoration(subtree(2,son))))
+      case ( f_perturbation )
+        perturbation => vectorDatabase(decoration(decoration(subtree(2,son))))
       case ( f_jacobian )
         ixJacobian = decoration(subtree(2,son)) ! jacobian: matrix vertex
       case ( f_forwardModel )
@@ -97,6 +120,7 @@ contains
       end select
     end do ! i = 2, nsons(root)
 
+    ! Check that we have a Jacobian if we need one
     if ( ixJacobian > 0 ) then
       i = decoration(ixJacobian)
       call getFromMatrixDatabase ( matrixDatabase(i), jacobian )
@@ -109,36 +133,140 @@ contains
       endif
     end if
 
-    fmStat%newHydros = .true.
-    fmStat%maf = 0
-    fmStat%finished = .false.
-    if ( ixJacobian > 0 ) then
-      call allocate_test ( fmStat%rows, jacobian%row%nb, 'fmStat%rows', &
-        & ModuleName )
-    else ! because it's not optional in many places in the forward model
-      call allocate_test ( fmStat%rows, 0, 'fmStat%rows', ModuleName )
-    end if
+    ! Setup some stuff for the case where we're doing numerical derivatives.
+    if ( associated ( perturbation ) ) then
+      if ( perturbation%template%id /= fwdModelIn%template%id ) then
+        call AnnounceError ( perturbationNotState )
+        stop
+      endif
+      quantity = 1
+      instance = 1
+      element = 0
+      loopEnd = perturbation%template%totalElements
+    else
+      ! Alternatively do simple one shot run.
+      loopEnd = 1
+      doThisOne = .true.
+    endif
 
-    ! Loop over mafs
-    do while ( .not. fmStat%finished )
-      ! What if one config set finished but others still had more to do?
-      ! Ermmm, think of this next time.
-      fmStat%maf = fmStat%maf + 1
-      do config = 1, size(configs)
-        if ( ixJacobian > 0 ) then
-          call forwardModel ( configDatabase(configs(config)), &
-            & FwdModelIn, FwdModelExtra, &
-            & FwdModelOut, ifm, fmStat, Jacobian )
+    ! Now have a possible loop over state vector elements, applying corrections.
+    do i = 1, loopEnd
+
+      ! Deal with the case where this is a perturbation.
+      if ( associated ( perturbation ) ) then
+        if ( element == 0 ) then
+          ! Do a first unperturbed case
+          call CopyVector ( saveState, fwdModelIn, clone=.true. )
+          doThisOne=.true.
         else
-          call forwardModel ( configDatabase(configs(config)), &
-            & FwdModelIn, FwdModelExtra, &
-            & FwdModelOut, ifm, fmStat )
+          thisPtb =perturbation%quantities(quantity)%values(element,instance)
+          if ( thisPtb /= 0.0 ) then
+            call CopyVector ( fwdModelIn, saveState )
+            fwdModelIn%quantities(quantity)%values(element,instance) = &
+              & fwdModelIn%quantities(quantity)%values(element,instance) + &
+              & thisPtb
+            doThisOne=.true.
+          else
+            doThisOne=.false.
+          endif
         end if
-      end do
-    end do
-    call deallocate_test ( fmStat%rows, 'FmStat%rows', moduleName )
+        print*,'Doing perturbation:', i, element, instance, quantity
+      end if
 
-    call DestroyForwardModelIntermediate ( ifm )
+      ! Now loop over the MAFs / forward model configs and run the models
+      if ( doThisOne ) then
+        fmStat%newHydros = .true.
+        fmStat%newScanHydros = .true.
+        fmStat%maf = 0
+        fmStat%finished = .false.
+        if ( ixJacobian > 0 ) then
+          call allocate_test ( fmStat%rows, jacobian%row%nb, 'fmStat%rows', &
+            & ModuleName )
+        else ! because it's not optional in many places in the forward model
+          call allocate_test ( fmStat%rows, 0, 'fmStat%rows', ModuleName )
+        end if
+        
+        ! Loop over mafs
+        do while ( .not. fmStat%finished )
+          ! What if one config set finished but others still had more to do?
+          ! Ermmm, think of this next time.
+          fmStat%maf = fmStat%maf + 1
+          do config = 1, size(configs)
+            if ( ixJacobian > 0 .and. .not. associated(perturbation)) then
+              call forwardModel ( configDatabase(configs(config)), &
+                & FwdModelIn, FwdModelExtra, &
+                & FwdModelOut, ifm, fmStat, Jacobian )
+            else
+              call forwardModel ( configDatabase(configs(config)), &
+                & FwdModelIn, FwdModelExtra, &
+                & FwdModelOut, ifm, fmStat )
+            end if
+          end do
+        end do
+
+        ! Tidy up the intermediate/status stuff from the model
+        call deallocate_test ( fmStat%rows, 'FmStat%rows', moduleName )
+        call DestroyForwardModelIntermediate ( ifm )
+        
+        ! Place the numerical derivative result into Jacobian if needed
+        if ( associated ( perturbation ) ) then
+          if ( element == 0 ) then
+            call CopyVector (saveResult, fwdModelOut, clone=.true.)
+          else
+            deviation = fwdModelOut - saveResult
+            ! Store the deviation in the jacobian
+            col = FindBlock ( jacobian%col, quantity, instance )
+            ! Loop over rows in the jacobian
+            do row = 1, jacobian%row%nb
+              rowQuantity = jacobian%row%quant(row)
+              rowInstance = jacobian%row%inst(row)
+              print*,'Possibly filling:',row,col
+              ! Is there anydeviaiton to store?
+              if ( maxval ( abs ( &
+                & deviation%quantities(rowQuantity)%values(:,rowInstance))) /= 0.0 ) then
+                print*,'Yes'
+                
+                ! If so, this column of the block (creating if necessary)
+                m0 => jacobian%block(row,col)
+                if ( m0%kind == M_Absent ) &
+                  & call CreateBlock ( jacobian, row, col, m_full )
+                if ( any (m0%kind == (/ m_banded, m_column_sparse /) ) ) &
+                  & call MLSMessage(MLSMSG_Error, ModuleName, &
+                  & 'Unable to fill banded/column sparse blocks numerically')
+                m0%values(:,element) = &
+                  & deviation%quantities(rowQuantity)%values(:,rowInstance)/thisPtb
+
+              end if                    ! Anything to place?
+            end do                      ! Loop over rows
+          endif                         ! Not very first run
+        endif                           ! Doing perturbation.
+
+      end if                            ! Do this element
+
+      ! Work out which element is next if we're perturbing
+      if ( associated ( perturbation ) ) then
+        element = element + 1
+        if ( element == &
+          & perturbation%quantities(quantity)%template%instanceLen + 1) then
+          element = 1
+          instance = instance + 1
+          if ( instance == &
+            & perturbation%quantities(quantity)%template%noInstances + 1 ) then
+            instance = 1
+            quantity = quantity + 1
+          end if
+        end if
+      end if
+
+    end do                              ! Possible loop over perturbation elements
+
+    if ( associated ( perturbation ) ) then
+      call CopyVector ( fwdModelIn, saveState )
+      call CopyVector ( fwdModelOut, saveResult )
+      call DestroyVectorInfo ( saveState )
+      call DestroyVectorInfo ( saveResult )
+      call DestroyVectorInfo ( deviation )
+    end if
 
     if ( toggle(gen) ) call trace_end ( "SIDS" )
 
@@ -160,6 +288,9 @@ contains
       case ( notPlain )
         call output ( 'The Jacobian matrix is not a "plain" matrix.', &
           & advance='yes' )
+      case ( perturbationNotState )
+        call output ( 'The perturbation vector is not the same type as fwdModelIn.', &
+          & advance='yes' )
       end select
     end subroutine AnnounceError
 
@@ -168,6 +299,9 @@ contains
 end module SidsModule
 
 ! $Log$
+! Revision 2.29  2001/05/05 00:02:27  livesey
+! Got a numerical derivative code working.
+!
 ! Revision 2.28  2001/05/03 23:09:09  livesey
 ! Removed temporary code to delete jacobian
 !
