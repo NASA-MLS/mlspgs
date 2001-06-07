37e90a20d75f5edbc98a9e1db206c271896a43b8
pwagner <>
2001-06-07 21:58:28 +0000

    rev 2.4

--- l2/dnwt_type.f90
+++ l2/dnwt_type.f90
@@ -1,3 +1,6 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
 module DNWT_TYPE
 
 ! This is a separate module only because the parameter RK is needed in
@@ -15,6 +18,9 @@ module DNWT_TYPE
 end module DNWT_TYPE
 
 ! $Log$
+! Revision 2.4  2001/06/07 21:58:28  pwagner
+! Added Copyright statement
+!
 ! Revision 2.3  2001/02/06 23:25:29  vsnyder
 ! Initial commit
 !
