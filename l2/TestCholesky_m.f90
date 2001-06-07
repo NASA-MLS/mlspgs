680555ff63cc8e415ec479a0ac46e68af58d3a0e
pwagner <>
2001-06-07 21:58:30 +0000

    rev 2.2

--- l2/TestCholesky_m.f90
+++ l2/TestCholesky_m.f90
@@ -1,3 +1,6 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
 module TestCholesky_M
 
   use Biggify_M, only: Biggify
