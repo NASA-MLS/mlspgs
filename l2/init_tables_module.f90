f9b3cd842e8e3f75f5eed8f0d54aa8f49ac3e67d
vsnyder <>
2001-06-22 01:26:25 +0000

    rev 2.120

--- l2/init_tables_module.f90
+++ l2/init_tables_module.f90
@@ -135,7 +135,9 @@ module INIT_TABLES_MODULE
   integer, parameter :: F_RADIANCEQUANTITY    = f_range + 1
   integer, parameter :: F_RATIOQUANTITY       = f_radianceQuantity + 1
   integer, parameter :: F_REFGPHQUANTITY      = f_ratioQuantity + 1
-  integer, parameter :: F_ROWCHANNELS         = f_refGPHQuantity + 1
+  integer, parameter :: F_REGORDERS           = f_refGPHQuantity + 1
+  integer, parameter :: F_REGWEIGHT           = f_regOrders + 1
+  integer, parameter :: F_ROWCHANNELS         = f_regWeight + 1
   integer, parameter :: F_ROWINSTANCES        = f_rowChannels + 1 
   integer, parameter :: F_ROWQUANTITY         = f_rowInstances + 1
   integer, parameter :: F_ROWS                = f_rowQuantity + 1
@@ -472,6 +474,8 @@ contains ! =====     Public procedures     =============================
     field_indices(f_range) =               add_ident ( 'range' )
     field_indices(f_radianceQuantity ) =   add_ident ( 'radianceQuantity' )
     field_indices(f_ratioQuantity) =       add_ident ( 'ratioQuantity' )
+    field_indices(f_regOrders) =           add_ident ( 'regOrders' )
+    field_indices(f_regWeight) =           add_ident ( 'regWeight' )
     field_indices(f_refGPHQuantity) =      add_ident ( 'refGPHquantity' )
     field_indices(f_rowChannels) =         add_ident ( 'rowChannels' )
     field_indices(f_rowInstances) =        add_ident ( 'rowInstances' )
@@ -881,6 +885,8 @@ contains ! =====     Public procedures     =============================
              begin, f+f_method, t+t_method, n+n_field_type, &
              begin, f+f_outputCovariance, s+s_matrix, n+n_field_spec, &
              begin, f+f_outputSD, s+s_vector, n+n_field_spec, &
+             begin, f+f_regOrders, t+t_numeric, n+n_field_type, &
+             begin, f+f_regWeight, t+t_numeric, n+n_field_type, &
              begin, f+f_state, s+s_vector, nr+n_field_spec, &
              begin, f+f_toleranceA, t+t_numeric, n+n_field_type, &
              begin, f+f_toleranceF, t+t_numeric, n+n_field_type, &
@@ -971,6 +977,9 @@ contains ! =====     Public procedures     =============================
 end module INIT_TABLES_MODULE
 
 ! $Log$
+! Revision 2.120  2001/06/22 01:26:25  vsnyder
+! Add fields for regularization
+!
 ! Revision 2.119  2001/06/21 20:06:42  vsnyder
 ! Make the tolerance field of the ForwardModel spec optional
 !
