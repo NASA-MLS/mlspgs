24160d48631c29ac562b740666e438b681f1e777
livesey <>
2001-06-22 05:09:04 +0000

    rev 2.1

--- l2/MLSL2Common.f90
+++ l2/MLSL2Common.f90
@@ -32,12 +32,36 @@ MODULE MLSL2Common              ! Common data types for the MLSL2 program
 
   ! --------------------------------------------------------------------------
 
+CONTAINS
+
+  ! -------------------------------------------- FindFirst --------------
+  integer function FindFirst ( condition )
+    ! Find the first logical in the array that is true
+    logical, dimension(:), intent(in) :: CONDITION
+
+    ! Local variables
+    integer :: I                        ! Loop counter
+
+    ! Executable code
+    FindFirst = -1
+    do i = 1, size(condition)
+      if ( condition(i) ) then
+        FindFirst = i
+        return
+      end if
+    end do
+  end function FindFirst
+
+!=============================================================================
 !=============================================================================
 END MODULE MLSL2Common
 !=============================================================================
 
 !
 ! $Log$
+! Revision 2.1  2001/06/22 05:09:04  livesey
+! Moved FindFirst in from L2Parallel
+!
 ! Revision 2.0  2000/09/05 18:57:03  ahanzel
 ! Changing file revision to 2.0.
 !
