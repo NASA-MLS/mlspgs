1a495e43f324c378ca35b2facab5132a3df16163
pumphrey <>
2001-06-27 16:54:01 +0000

    Added command line args.

--- util/h5subset.c
+++ util/h5subset.c
@@ -22,12 +22,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <hdf5.h>
+#include <unistd.h> /* required for use of getopt() */
 
 /* Global as I can't be arsed to pass them to the iterator */
 /* hid_t outfile_id; */
 char **global_varlist;
 char **global_grouplist;
 int n_vars,n_groups;
+int global_nattrs, global_attrcnt,verbose;
 
 /* Function prototype for operator (function is later on in this file) */
 herr_t operator(hid_t group_id, const char *member_name, void
@@ -51,24 +53,67 @@ int main(int argc , char *argv[] ){
   int error, nmembers,*idx,obj_type, stringlen, i;
   char obj_name[80],rootname[80];
   char *varstring, *dummy,*groupstring;
+  char *invarstring,*ingroupstring;
+  
   void *operator_data;
   opdat_t opdat; 
   hid_t *newloc_idp,newloc_id;
+  /* Stuff for getopt */
+  char optstring[]="vd:g:a:";
+  int optval;
+  char optvalc;
+  extern char *optarg;
+  extern int optind,opterr,optopt;
+
+  /* end of getopt vars */
  
   /*------------------Executable----------------------------*/
   strcpy(rootname,"/\0");
-  if (argc < 4){
-    printf("Usage: subset <infile> <outfile> <comma separated ");
-    printf("list of datasets> <opt.comma-sep. list of groups to copy all of>");
+  if (argc < 3){
+    printf("Usage: h5subset -a nattr -d dsets -g groups <infile> <outfile>\n");
+    printf("where dsets is a comma separated list of datasets to copy\n"); 
+    printf("groups is a comma-sep. list of groups to copy all of \n");
+    printf("nattr is the number of attributes to copy for each dataset\n");
+    printf(" -v : verbose \n");
     exit(3);
   }
-  strcpy(infile,argv[1]);
-  strcpy(outfile,argv[2]);
-  stringlen=strlen(argv[3]);
-  varstring=(char *) malloc(stringlen+3);
-  strcpy(varstring,argv[3]);
   
+  /* Loop through options */
+  invarstring=NULL;
+  ingroupstring=NULL;
+  optval=0;
+  verbose=0;
+  while(optval >= 0){
+    optval=getopt(argc,argv,optstring);
+    optvalc=(char)optval;
+    if(optvalc=='v'){
+      verbose=1;
+      printf("Ohhh! Verbosity on! Now I can spout lots of rubbish at you\n");
+    }
+    else if(optvalc=='d'){
+      stringlen=strlen(optarg);
+      invarstring=(char *) malloc(stringlen+3);
+      varstring=(char *) malloc(stringlen+3);
+      strcpy(invarstring,optarg);
+      strcpy(varstring,optarg);
+    }else if(optvalc=='g'){
+      if(verbose) printf("arg to -g was%s\n",optarg);
+      stringlen=strlen(optarg);
+      ingroupstring=(char *) malloc(stringlen+3);
+      groupstring=(char *) malloc(stringlen+3);
+      strcpy(groupstring,optarg); 
+      strcpy(ingroupstring,optarg); 
+    }else if(optvalc=='a'){
+      sscanf(optarg,"%d",&global_nattrs);
+      if(verbose)
+	printf("Doing %d attrs for each dataset \n",global_nattrs);
+    }
+  }/* End of getopt loop */
   
+  strcpy(infile,argv[argc-2]);
+  strcpy(outfile,argv[argc-1]);
+
+  if(verbose)printf("copying from %s to %s \n ",infile,outfile);
   /* split varstring into an array of variable names. */
   /* First, count the variables */
   n_vars=0;
@@ -78,27 +123,23 @@ int main(int argc , char *argv[] ){
     dummy=strtok(NULL,",");
   }
   /* Now allocate an array of strings for them and put them in it */
-  printf("That is %d variables\n ",n_vars);
+  if(verbose)printf("That is %d variables\n ",n_vars);
   global_varlist=(char **) malloc(sizeof(char *)*n_vars);
-  strcpy(varstring,argv[3]);
+  strcpy(varstring,invarstring);
   
   global_varlist[0]=strtok(varstring,",");
-  printf("Will search for variables X%sX  ",global_varlist[0]);
+  if(verbose)printf("Will search for variables X%sX  ",global_varlist[0]);
   for(i=1;i<n_vars;i++){
     global_varlist[i]=strtok(NULL,",");
-    printf(" X%sX ",global_varlist[i]);
+    if(verbose) printf(" X%sX ",global_varlist[i]);
   }
-  printf("\n");
+  if(verbose) printf("\n");
 
   /* Done making string array of  variable names */
   
-  if(argc>4){
+  if(ingroupstring!=NULL){
     /* split groupstring into an array of variable names. */
     /* First, count the variables */
-    stringlen=strlen(argv[4]);
-    groupstring=(char *) malloc(stringlen+3);
-    strcpy(groupstring,argv[4]);
-
     n_groups=0;
     dummy=strtok(groupstring,",");
     while(dummy !=NULL){
@@ -106,17 +147,18 @@ int main(int argc , char *argv[] ){
       dummy=strtok(NULL,",");
     }
     /* Now allocate an array of strings for them and put them in it */
-    printf("That is %d groups\n ",n_groups);
+    
+    if(verbose) printf("That is %d groups\n ",n_groups);
     global_grouplist=(char **) malloc(sizeof(char *)*n_groups);
-    strcpy(groupstring,argv[4]);
+    strcpy(groupstring,ingroupstring);
   
     global_grouplist[0]=strtok(groupstring,",");
-    printf("Will search for groups X%sX  ",global_grouplist[0]);
+    if(verbose) printf("Will search for groups X%sX  ",global_grouplist[0]);
     for(i=1;i<n_groups;i++){
       global_grouplist[i]=strtok(NULL,",");
-      printf(" X%sX ",global_grouplist[i]);
+      if(verbose) printf(" X%sX ",global_grouplist[i]);
     }
-    printf("\n");
+    if(verbose) printf("\n");
   }
 
   /* printf("opening file: %s id  was",infile);*/
@@ -155,12 +197,12 @@ herr_t operator(hid_t group_id, const char *member_name, void
  
   newloc_id=opdatp->newloc_id;
   if(opdatp->getall==1){
-    printf("getting %s whatever\n",member_name);
+    if(verbose) printf("getting %s whatever\n",member_name);
     newopdat.getall=1;
     is_wanted=1;
   }
   else{
-    printf("operating on %s ... ",member_name);
+    if(verbose) printf("operating on %s ... ",member_name);
     is_wanted=0;
     newopdat.getall=0;
       for(i=0;i<n_vars;i++){
@@ -176,7 +218,7 @@ herr_t operator(hid_t group_id, const char *member_name, void
     }
   }
   if(is_wanted==0){
-    printf("not on wanted list\n");
+    if(verbose) printf("not on wanted list\n");
   }
   else {
     
@@ -193,7 +235,7 @@ herr_t operator(hid_t group_id, const char *member_name, void
       int *idx;
       opdat_t newsubgroup;
       size_t size_hint;
-      printf("It's a group: Looking into it\n");
+      if(verbose) printf("It's a group: Looking into it\n");
       /*oldsubgroup_id=H5Gopen(group_id,member_name );*/
       size_hint=40;
       newsubgroup.newloc_id=H5Gcreate(newloc_id, member_name, size_hint );
@@ -204,7 +246,7 @@ herr_t operator(hid_t group_id, const char *member_name, void
 
       /*error=H5Gclose(oldsubgroup_id); */
       error=H5Gclose(newsubgroup.newloc_id); 
-      printf("Finished with group %s\n",member_name);
+      if(verbose) printf("Finished with group %s\n",member_name);
     }
     else if(statbuf.type ==  H5G_DATASET){
       /* OK, it's a dataset. Now we need to process it to write it out */
@@ -216,7 +258,7 @@ herr_t operator(hid_t group_id, const char *member_name, void
       void *aopdata;
       hid_t *new_dsetp;
       /* executable for this bock */
-      printf("It's a dataset: \n");
+      if(verbose) printf("It's a dataset: \n");
       old_dset=H5Dopen(group_id, member_name );
       /*printf("Opened dataset with id=%d\n",old_dset); */
       old_dspace=H5Dget_space(old_dset) ;
@@ -243,7 +285,9 @@ herr_t operator(hid_t group_id, const char *member_name, void
       aidx=NULL;
       new_dsetp=&new_dset;
       aopdata=(void *) new_dsetp;
-      error=H5Aiterate(old_dset, aidx, attr_op, aopdata ) ;
+      global_attrcnt=0;
+      if(global_nattrs > 0)
+	error=H5Aiterate(old_dset, aidx, attr_op, aopdata ) ;
 
 
       free(databuf);
@@ -253,14 +297,14 @@ herr_t operator(hid_t group_id, const char *member_name, void
       error=H5Dclose(new_dset); 
     }
     else if(statbuf.type ==  H5G_LINK){
-      printf("It's a link: ignore\n");
+      if(verbose) printf("It's a link: ignore\n");
     }
     else if(statbuf.type ==  H5G_TYPE){
-      printf("It's a type: ignore\n");
+      if(verbose) printf("It's a type: ignore\n");
     }
     else {
-      printf("I really don't know what the fsck this is: it has type:%d\n",
-	     statbuf.type);
+      printf("I really don't know what the fsck %s is: it has type:%d\n",
+	     member_name,statbuf.type);
     }
     /* End of main if block  to process each of the possible item types */  
   }
@@ -295,8 +339,11 @@ herr_t attr_op(hid_t loc_id, const char *attr_name, void *operator_data){
   
   error=H5Aclose(attr_id);
 
-  /* "return 1;" here causes only the first attribute to be processed
-     "return 0" causes all of them to be processed. 
-  */
-  return 1;
+
+  global_attrcnt++;
+  if(global_attrcnt < global_nattrs){
+    return 0; /* next attribute will be processed */
+  }else{
+    return 1; /* No more attributes will be processed */
+  }
 }
