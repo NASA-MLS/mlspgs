1a9aebcac4458d62f8abb19777e2f99a0ed19597
vsnyder <>
2001-07-19 17:44:02 +0000

    rev 2.2

--- lib/Biggify_M.f90
+++ lib/Biggify_M.f90
@@ -4,7 +4,7 @@
 module Biggify_M
 
   use Allocate_Deallocate, only: Allocate_Test
-  use MatrixModule_0, only: Densify
+  use MatrixModule_0, only: Densify, M_Absent
   use MatrixModule_1, only: Matrix_T, RC_Info
   use MLSCommon, only: R8
   use VectorsModule, only: Vector_T
@@ -24,31 +24,39 @@ module Biggify_M
 contains
 
   ! ---------------------------------------------  Biggify_Matrix  -----
-  subroutine Biggify_Matrix ( Matrix, Array )
+  subroutine Biggify_Matrix ( Matrix, Array, Update )
 
   ! Convert Matrix from a Matrix_T to Array, a Fortran array.
   ! The primary purpose of this routine is for debugging, but it
-  ! could also be useful for output.
+  ! could also be useful for output.  If UPDATE is present and true,
+  ! don't allocate the array, and don't fill in absent blocks with
+  ! zeroes.  This is useful if you're trying to make a Jacobian, but
+  ! it's being constructed little by little.
 
     type(matrix_T), intent(in) :: Matrix
     real(r8), dimension(:,:), pointer :: Array
+    logical, intent(in), optional :: Update
 
     integer :: I, J           ! Subscripts, loop inductors
     integer :: II, JJ         ! Subscripts of block(i,j) in Array
     integer :: M, N           ! Dimensions of Array
     integer :: MM, NN         ! Dimensions of a block of Matrix
+    logical :: MY_UPD
 
+    my_upd = .false.
+    if ( present(update) ) my_upd = update
     m = sum(matrix%row%nelts)
     n = sum(matrix%col%nelts)
-    call allocate_test ( array, m, n, 'Array', moduleName )
+    if ( .not. my_upd ) call allocate_test ( array, m, n, 'Array', moduleName )
     jj = 1
     do j = 1, matrix%col%nb
       nn = matrix%block(1,j)%ncols
       ii = 1
       do i = 1, matrix%row%nb
         mm = matrix%block(i,1)%nrows
-        call densify ( Array(ii:ii+mm-1,jj:jj+nn-1), &
-          & matrix%block(i,j) )
+        if ( .not. my_upd .or. matrix%block(i,j)%kind /= m_absent ) &
+          & call densify ( Array(ii:ii+mm-1,jj:jj+nn-1), &
+            & matrix%block(i,j) )
         ii = ii + mm
       end do ! i
       jj = jj + nn
@@ -90,6 +98,9 @@ contains
 end module Biggify_M
 
 ! $Log$
+! Revision 2.2  2001/07/19 17:44:02  vsnyder
+! Add 'update' option
+!
 ! Revision 2.1  2001/06/01 01:14:27  vsnyder
 ! For debugging only
 !
