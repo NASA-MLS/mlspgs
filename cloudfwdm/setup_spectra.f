f5b6b8d5ca5ea310ef6e03e120e153b13d89824d
jonathan <>
2001-07-23 23:20:46 +0000

    set spectra data block, jonathan

--- cloudfwdm/setup_spectra.f
+++ cloudfwdm/setup_spectra.f
@@ -3,54 +3,206 @@
 
       IMPLICIT NONE
       INCLUDE 'spectra.h'
-      CHARACTER*80 TITLE
+      INCLUDE 'data.h'
       REAL QQ(3)
-      INTEGER NV                          !NO OF SPECTRAL LINES
-      INTEGER I, ICNT
+      INTEGER I, J
 
-      OPEN(31,FILE='spectra.dat', FORM='FORMATTED',STATUS='OLD')
-      NMOL=0
-      ICNT=0
+C -------------------------------------------------------------
 
-c      DO WHILE(1)
-
-      DO WHILE(.TRUE.)
-
-         READ(31,4,ERR=100)TITLE
-         NMOL=NMOL+1
-         NCNT(NMOL)=0
-         READ(31,4)TITLE
-         READ(31,2)MOL,(QQ(I),I=1,3)
-         READ(31,3)NV
-
-         NCNT(NMOL)=NV
-         DO I=1,NV
-            QLG(1,ICNT+I)=QQ(1)
-            QLG(2,ICNT+I)=QQ(2)
-            QLG(3,ICNT+I)=QQ(3)
-         ENDDO
-
-         READ(31,4)TITLE
-         DO I=1,NV
-            ICNT=ICNT+1
-            READ(31,1)V0(ICNT),GSE(ICNT),IST(ICNT),WTH(ICNT),NTH(ICNT),
-     +                DELTA(ICNT),N1(ICNT),GAMMA(ICNT),N2(ICNT)
-         ENDDO
-
+      DO I=1, 
+     >     N_O2_LINES
+            QLG(1,I) = QQ_O2(1)
+            QLG(2,I) = QQ_O2(2)
+            QLG(3,I) = QQ_O2(3)
+            V0(I)    = V0_O2(I)
+            GSE(I)   = GSE_O2(I)
+            IST(I)   = IST_O2(I)
+            WTH(I)   = WTH_O2(I)
+            NTH(I)   = NTH_O2(I)
+            DELTA(I) = DELTA_O2(I)
+            N1(I)    = N1_O2(I)
+            GAMMA(I) = GAMMA_O2(I)
+            N2(I)    = N2_O2(I)
       ENDDO
 
- 100  CONTINUE
-      CLOSE(31)
+      J=N_O2_LINES
+      DO I=J+1, J+N_H2O_LINES
+            QLG(1,I)=QQ_H2O(1)
+            QLG(2,I)=QQ_H2O(2)
+            QLG(3,I)=QQ_H2O(3)
+            V0(I)    = V0_H2O(I-J)
+            GSE(I)   = GSE_H2O(I-J)
+            IST(I)   = IST_H2O(I-J)
+            WTH(I)   = WTH_H2O(I-J)
+            NTH(I)   = NTH_H2O(I-J)
+            DELTA(I) = DELTA_H2O(I-J)
+            N1(I)    = N1_H2O(I-J)
+            GAMMA(I) = GAMMA_H2O(I-J)
+            N2(I)    = N2_H2O(I-J)
+      ENDDO
 
- 1    FORMAT(1X,F12.4,1X,F10.4,1X,F8.4,1X,F6.3,1X,F5.2,1X,E10.3,1X,
-     +       F4.2,1X,E10.3,1X,F5.2)
- 2    FORMAT(18X,I4,3X,3(F10.4))
- 3    FORMAT(1X,I5)
- 4    FORMAT(1X,A80)
+      J=N_O2_LINES+N_H2O_LINES
+      DO I=J+1, J+N_O18O_LINES
+            QLG(1,I)=QQ_O_18_O(1)
+            QLG(2,I)=QQ_O_18_O(2)
+            QLG(3,I)=QQ_O_18_O(3)
+            V0(I)    = V0_O_18_O(I-J)
+            GSE(I)   = GSE_O_18_O(I-J)
+            IST(I)   = IST_O_18_O(I-J)
+            WTH(I)   = WTH_O_18_O(I-J)
+            NTH(I)   = NTH_O_18_O(I-J)
+            DELTA(I) = DELTA_O_18_O(I-J)
+            N1(I)    = N1_O_18_O(I-J)
+            GAMMA(I) = GAMMA_O_18_O(I-J)
+            N2(I)    = N2_O_18_O(I-J)
+      ENDDO
+
+      J=N_O2_LINES+N_H2O_LINES+N_O18O_LINES
+      DO I=J+1, J+N_H2O18_LINES
+            QLG(1,I)=QQ_H2O_18(1)
+            QLG(2,I)=QQ_H2O_18(2)
+            QLG(3,I)=QQ_H2O_18(3)
+            V0(I)    = V0_H2O_18(I-J)
+            GSE(I)   = GSE_H2O_18(I-J)
+            IST(I)   = IST_H2O_18(I-J)
+            WTH(I)   = WTH_H2O_18(I-J)
+            NTH(I)   = NTH_H2O_18(I-J)
+            DELTA(I) = DELTA_H2O_18(I-J)
+            N1(I)    = N1_H2O_18(I-J)
+            GAMMA(I) = GAMMA_H2O_18(I-J)
+            N2(I)    = N2_H2O_18(I-J)
+      ENDDO
+
+      J=N_O2_LINES+N_H2O_LINES+N_O18O_LINES+N_H2O18_LINES
+      DO I=J+1, J+N_O3_LINES
+            QLG(1,I)=QQ_O3(1)
+            QLG(2,I)=QQ_O3(2)
+            QLG(3,I)=QQ_O3(3)
+      ENDDO
+
+      J=N_O2_LINES+N_H2O_LINES+N_O18O_LINES+N_H2O18_LINES
+      DO I=J+1, J+NL
+            V0(I)    = V0_O3_PT1(I-J)
+            GSE(I)   = GSE_O3_PT1(I-J)
+            IST(I)   = IST_O3_PT1(I-J)
+            WTH(I)   = WTH_O3_PT1(I-J)
+            NTH(I)   = NTH_O3_PT1(I-J)
+            DELTA(I) = DELTA_O3_PT1(I-J)
+            N1(I)    = N1_O3_PT1(I-J)
+            GAMMA(I) = GAMMA_O3_PT1(I-J)
+            N2(I)    = N2_O3_PT1(I-J)
+     
+            V0(I+NL)    = V0_O3_PT2(I-J)
+            GSE(I+NL)   = GSE_O3_PT2(I-J)
+            IST(I+NL)   = IST_O3_PT2(I-J)
+            WTH(I+NL)   = WTH_O3_PT2(I-J)
+            NTH(I+NL)   = NTH_O3_PT2(I-J)
+            DELTA(I+NL) = DELTA_O3_PT2(I-J)
+            N1(I+NL)    = N1_O3_PT2(I-J)
+            GAMMA(I+NL) = GAMMA_O3_PT2(I-J)
+            N2(I+NL)    = N2_O3_PT2(I-J)
+
+            V0(I+2*NL)    = V0_O3_PT3(I-J)
+            GSE(I+2*NL)   = GSE_O3_PT3(I-J)
+            IST(I+2*NL)   = IST_O3_PT3(I-J)
+            WTH(I+2*NL)   = WTH_O3_PT3(I-J)
+            NTH(I+2*NL)   = NTH_O3_PT3(I-J)
+            DELTA(I+2*NL) = DELTA_O3_PT3(I-J)
+            N1(I+2*NL)    = N1_O3_PT3(I-J)
+            GAMMA(I+2*NL) = GAMMA_O3_PT3(I-J)
+            N2(I+2*NL)    = N2_O3_PT3(I-J)
+
+            V0(I+3*NL)    = V0_O3_PT4(I-J)
+            GSE(I+3*NL)   = GSE_O3_PT4(I-J)
+            IST(I+3*NL)   = IST_O3_PT4(I-J)
+            WTH(I+3*NL)   = WTH_O3_PT4(I-J)
+            NTH(I+3*NL)   = NTH_O3_PT4(I-J)
+            DELTA(I+3*NL) = DELTA_O3_PT4(I-J)
+            N1(I+3*NL)    = N1_O3_PT4(I-J)
+            GAMMA(I+3*NL) = GAMMA_O3_PT4(I-J)
+            N2(I+3*NL)    = N2_O3_PT4(I-J)
+
+            V0(I+4*NL)    = V0_O3_PT5(I-J)
+            GSE(I+4*NL)   = GSE_O3_PT5(I-J)
+            IST(I+4*NL)   = IST_O3_PT5(I-J)
+            WTH(I+4*NL)   = WTH_O3_PT5(I-J)
+            NTH(I+4*NL)   = NTH_O3_PT5(I-J)
+            DELTA(I+4*NL) = DELTA_O3_PT5(I-J)
+            N1(I+4*NL)    = N1_O3_PT5(I-J)
+            GAMMA(I+4*NL) = GAMMA_O3_PT5(I-J)
+            N2(I+4*NL)    = N2_O3_PT5(I-J)
+
+            V0(I+5*NL)    = V0_O3_PT6(I-J)
+            GSE(I+5*NL)   = GSE_O3_PT6(I-J)
+            IST(I+5*NL)   = IST_O3_PT6(I-J)
+            WTH(I+5*NL)   = WTH_O3_PT6(I-J)
+            NTH(I+5*NL)   = NTH_O3_PT6(I-J)
+            DELTA(I+5*NL) = DELTA_O3_PT6(I-J)
+            N1(I+5*NL)    = N1_O3_PT6(I-J)
+            GAMMA(I+5*NL) = GAMMA_O3_PT6(I-J)
+            N2(I+5*NL)    = N2_O3_PT6(I-J)
+
+            V0(I+6*NL)    = V0_O3_PT7(I-J)
+            GSE(I+6*NL)   = GSE_O3_PT7(I-J)
+            IST(I+6*NL)   = IST_O3_PT7(I-J)
+            WTH(I+6*NL)   = WTH_O3_PT7(I-J)
+            NTH(I+6*NL)   = NTH_O3_PT7(I-J)
+            DELTA(I+6*NL) = DELTA_O3_PT7(I-J)
+            N1(I+6*NL)    = N1_O3_PT7(I-J)
+            GAMMA(I+6*NL) = GAMMA_O3_PT7(I-J)
+            N2(I+6*NL)    = N2_O3_PT7(I-J)
+      ENDDO
+
+      J=N_O2_LINES+N_H2O_LINES+N_O18O_LINES+N_H2O18_LINES+7*NL
+      DO I=J+1, J+NLL
+             V0(I)    = V0_O3_PT8(I-J)
+            GSE(I)   = GSE_O3_PT8(I-J)
+            IST(I)   = IST_O3_PT8(I-J)
+            WTH(I)   = WTH_O3_PT8(I-J)
+            NTH(I)   = NTH_O3_PT8(I-J)
+            DELTA(I) = DELTA_O3_PT8(I-J)
+            N1(I)    = N1_O3_PT8(I-J)
+            GAMMA(I) = GAMMA_O3_PT8(I-J)
+            N2(I)    = N2_O3_PT8(I-J)        
+      ENDDO
+
+      NCNT(1)=N_O2_LINES
+      NCNT(2)=N_H2O_LINES
+      NCNT(3)=N_O18O_LINES
+      NCNT(4)=N_H2O18_LINES
+      NCNT(5)=N_O3_LINES
+      NMOL=5
+
+      PRINT*,NMOL
+      PRINT*,NCNT
+      
+
+      write(9,'(4x,5f6.2)') (n2(i), 
+     >   i=1, N_O2_LINES)
+
+      write(9,'(4x,5f6.2)') (n2(i), 
+     >   i=N_O2_LINES+1, N_O2_LINES+N_H2O_LINES)
+
+      write(9,'(4x,5f6.2)') (n2(i), 
+     >   i=N_O2_LINES+N_H2O_LINES+1, 
+     >     N_O2_LINES+N_H2O_LINES+N_O18O_LINES )
+
+      write(9,'(4x,5f6.2)') (n2(i), 
+     >   i=N_O2_LINES+N_H2O_LINES+N_O18O_LINES+1,
+     >     N_O2_LINES+N_H2O_LINES+N_O18O_LINES+N_H2O18_LINES)
+
+      write(9,'(4x,5f6.2)') (n2(i), 
+     >   i=N_O2_LINES+N_H2O_LINES+N_O18O_LINES+N_H2O18_LINES+1,
+     >     N_O2_LINES+N_H2O_LINES+N_O18O_LINES+N_H2O18_LINES+N_O3_LINES)
+
 
       RETURN
       END
 
-! $Log: setup_spectra.f,v      
+
+
+
+
+
 
 
