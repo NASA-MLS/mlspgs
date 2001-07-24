31ec19963a00d143d0c31f9835b09ea23877ec5a
jonathan <>
2001-07-24 20:31:51 +0000

    f90 version, jonathan

---
+++ cloudfwdm/Radxfer.f90
@@ -0,0 +1,385 @@
+
+      SUBROUTINE RADXFER(L,NU,NUA,U,DU,PHH,NT,ZT,W0,TAU,RS,TS,FREQ, &
+      &          YZ,TEMP_AIR,N,THETA,THETAI,PHI,UI,UA,TT,MNT,ICON,RE)
+
+!===================================================================
+!     >>>>>>ITERATIVE RADIATIVE TRANSFER CALCULATION<<<<<<
+!
+! METHOD: 1. GET SCATTERING TB IN A PARALLEL PLANE ASSUMING THAT
+!            SCATTERING IS LOCALIZED AND PARALLEL PLANE APPROXI-
+!            MATION IS OK;
+!         2. EACH ITERATION INVOLVES INTERPOLATIONS OF TB BETWEEN 
+!            NT-TANGENT HEIGHT GRIDS ON THE SPHERICAL PLANE AND
+!            N-STREAMS ON THE PARALLEL PLANE;
+!         3. OUTPUT TB FOR ALL TANGENT HEIGHTS
+!
+! LAST UPDATE: 18/05/2001, J.JIANG
+!===================================================================
+
+      IMPLICIT NONE
+      REAL PI
+      REAL RE
+      PARAMETER (PI=3.1415926)
+
+      INTEGER L,NT,NU,NU0,NUA,N0,N,MNT
+      INTEGER NZ                       ! MAXIMUM NO. OF INTEGRAL LAYERS
+      REAL FIRST                       ! FIRST GUESS OF UPWELLING TB
+      PARAMETER (NZ=1600,FIRST=250.,NU0=256,N0=2)
+
+      REAL U(NU),DU(NU),THETA(NU),PHI(NUA),      &
+      &     THETAI(NU,NU,NUA),UI(NU,NU,NUA),UA(NUA) 
+      REAL PHH(N,NU,L),RS(NU/2),W0(N,L),TAU(L)
+
+      REAL TEMP_AIR(L)                 ! MEAN AIR TEMPERATURE AT L
+      REAL YZ(L+1)                     ! PRESSURE HEIGHT (km)
+      REAL ZT(NT)                      ! TANGENT HEIGHT (m)
+      REAL TT(MNT+1,L+1)                ! TB AT ZT, LAST INDEX IS ZENITH LOOKING
+      REAL FREQ                        ! FREQUENCY (GHz)
+      REAL TSPACE                      ! COSMIC BACKGROUND RADIANCE
+      REAL TS                          ! SURFACE TEMPERATURE (K)
+      REAL TEMP(NZ)                    ! BRIGHTNESS TEMPERATURE FROM TEMP_AIR
+      REAL TB0(NU0)                    ! TB AT THE SURFACE
+      REAL TAVG                        ! TB AVERAGED OVER PHI AT A GIVEN U
+      REAL TB(NU0,NZ)                  ! TB IN FLAT PLANE GEOMETRY
+                                       ! 1->NU/2 UPWELLING, NU/2->NU DOWNWELLING
+      REAL TSCAT(N0,NU0,NZ)            ! TB FROM SCATTERING PHASE FUNCTION
+
+      INTEGER LMIN(NZ)                 ! LOWEST LAYER REACHED BY A TANGENT HT
+
+      INTEGER ICON                             ! CONTROL SWITCH
+                                               ! 0 = CLEAR-SKY
+                                               ! 1 = CLEAR-SKY, 100% R.H. 
+                                               !     BELOW 100hPa
+                                               ! 2 = DEFAULT
+                                               ! 3 = NEAR SIDE CLOUD ONLY
+
+      REAL UAVE(NZ,NZ)                 ! INCIDENT ANGLES FOR EACH TANGENT HT
+      REAL UEFF                        ! EFFECTIVE U BETWEEN K AND K+1
+
+      REAL ITS0                        ! NO. OF MAXIMUM ITERATIONS
+      REAL DELTA                       ! DELTA TB FOR CONVERGENCE CHECK
+
+      INTEGER I,J,K,K1,ITS,ITT,IH,IP,JM,IND,ISPI
+      REAL D1,D2,DY,TGT, jj0
+      REAL WK,WK1,U1(NU0),UU(NZ),X2,RSAVG,XTB(NZ),WW0,CHK(NZ)
+      REAL tsource, wwk, wwk1,www0
+
+!------------------------------------------------
+!     FIND BRIGHTNESS TEMPERATURE AT EACH LAYER
+!------------------------------------------------
+
+      DO K=1,L
+         CALL PLANCK(TEMP_AIR(K),FREQ,TEMP(K))
+      ENDDO
+      CALL PLANCK(2.7,FREQ,TSPACE)
+
+!------------------------------------------------
+!     DETERMINE NO. OF ITERATIONS
+!------------------------------------------------
+      ITS0=1
+      DELTA=0.01
+      jj0=1.e-9
+
+      DO K=1,L
+         CHK(K)=0.
+         DO ISPI=1,N
+            CHK(K)=CHK(K)+W0(ISPI,K)
+         ENDDO
+         IF(CHK(K).NE.0.) ITS0=20
+      ENDDO
+      
+!-----------------------------------------------------
+!     CALCULATE EQUIVALENT U FOR EACH TANGENT HEIGHT
+!----------------------------------------------------
+
+
+!      PRINT*,(ZT(I),I=1,NT)
+!      print*,L
+!      stop
+
+      DO I=1,NT
+         LMIN(I)=1
+         DO K=1,L
+            IF(YZ(K) .LE. ZT(I)) LMIN(I)=K
+         ENDDO
+ 
+         DO K=LMIN(I),L
+            TGT= YZ(K)
+            if(tgt .lt. zt(i)) tgt=zt(i)
+            IF(ZT(I) .LT. 0.) THEN
+               D2=SQRT((YZ(K+1)+RE)**2-(ZT(I)+RE)**2)
+               D1=SQRT((TGT+RE)**2-(ZT(I)+RE)**2)
+               UAVE(I,K)=(YZ(K+1)-YZ(K))/(D2-D1)          !
+            ELSE
+!              print*,'YZ(K+1), K+1', YZ(K+1), K+1
+!              print*,'ZT(I), I', ZT(I),I
+
+               DY=SQRT(YZ(K+1)-ZT(I))-SQRT(TGT-ZT(I))
+               UAVE(I,K)=(YZ(K+1)-YZ(K))/SQRT(2*RE+TGT+ZT(I))/DY
+
+            ENDIF
+
+         ENDDO
+      ENDDO
+
+!      print*,'here again'
+!------------------------------------------------
+!     INITIAL GUESS OF TB, L=0 IS THE SURFACE
+!------------------------------------------------
+      DO K=1,L+1
+         DO I=1,NU/2
+            TB(I,K)=FIRST
+            XTB(I)=0.
+         ENDDO
+         DO I=NU/2+1,NU
+            TB(I,K)=TSPACE
+         ENDDO
+      ENDDO
+
+!------------------------------------------------
+!     START ITERATION
+!------------------------------------------------
+      ITS=1
+ 1000 CONTINUE
+
+      DO J=1,NU/2
+         TB(J+NU/2,L+1)=TSPACE         ! BOUNDARY CONDITION AT TOP OF ATMOS
+      ENDDO
+
+      DO J=1,NT
+         TT(J,L+1)=TSPACE              ! BOUNDARY CONDITION AT TOP OF ATMOS
+         TT(J,L)=TSPACE                ! INITIALIZATION
+      ENDDO
+
+!------------------------------------------------
+!     COMPUTING TSCAT FOR EACH ANGLE AND LAYER
+!------------------------------------------------
+
+      DO ISPI=1,N                         
+         DO 1009 IP=1,NU
+            U1(IP)=-U(IP)
+            DO 1008 K=1,L
+               TSCAT(ISPI,IP,K)=0.0
+               IF(PHH(ISPI,1,K).NE.0.)THEN 
+                  DO IH=1,NU
+                     TAVG=0.
+                     DO J=1,NUA
+                        K1=K
+                        IF(THETAI(IP,IH,J).LT.PI/2) K1=K-1
+                        IF(THETAI(IP,IH,J).GT.PI/2) K1=K+1
+                        CALL LOCATE(THETA,NU,NU,THETAI(IP,IH,J),JM)
+                        WK=(TB(JM+1,K1)*(THETAI(IP,IH,J)-THETA(JM))+  &
+     &                     TB(JM,K1)*(THETA(JM+1)-THETAI(IP,IH,J)))/  &
+     &                     (THETA(JM+1)-THETA(JM))
+                        TAVG = TAVG + WK*(PHI(2)-PHI(1))/2./PI
+                     ENDDO
+                     TSCAT(ISPI,IP,K)=TSCAT(ISPI,IP,K)+               &
+     &                                2.*PHH(ISPI,IH,K)*TAVG*DU(IH)
+                  ENDDO
+               ENDIF
+ 1008       CONTINUE
+ 1009    CONTINUE
+      ENDDO
+
+!------------------------------------
+!     COMPUTE TB AT ANGLES U
+!     INTEGRATION FROM TOP TO BOTTOM
+!------------------------------------
+      DO 1100 I=NU/2+1,NU
+         X2 = U(I)*U(I)
+         UEFF = ABS(U(I))
+         DO 1100 K=L,1,-1
+            WK=0.
+            WW0=0.
+            DO ISPI=1,N
+               WK=WK+TSCAT(ISPI,I,K)*W0(ISPI,K)
+               WW0=WW0+W0(ISPI,K)
+            ENDDO
+
+            wwk=0.
+            www0=0.
+            DO ISPI=1,N
+               wwk=wwk+TSCAT(ISPI,I,K+1)*W0(ISPI,K+1)
+               www0=www0+W0(ISPI,K+1)
+            ENDDO
+
+         tsource=( ((1-WW0)*TEMP(K)+WK)+((1-www0)*TEMP(K+1)+wwk) )*0.5
+
+            TB(I,K)=TB(I,K+1)*EXP(-TAU(K)/UEFF)+           &
+     &              (1.-EXP(-TAU(K)/UEFF))*tsource
+
+ 1100 CONTINUE
+
+!-----------------------------------------------------------------------
+!     DETERMINE SURFACE REFLECTION 
+!-----------------------------------------------------------------------
+      DO J=1,NU/2
+        TB0(J)=TS*(1.-RS(J))+TB(J+NU/2,1)*RS(J)
+	TB(J,1) = TB0(J)
+      ENDDO
+    
+!------------------------------------
+!     COMPUTE TB AT ANGLES U
+!     INTEGRATION FROM TOP TO BOTTOM
+!------------------------------------
+      DO 1200 I=1,NU/2
+        X2 = U(I)*U(I)
+        UEFF = ABS(U(I))
+        DO 1200 K=1,L
+           WK=0.
+           WW0=0.
+           DO ISPI=1,N
+              WK=WK+TSCAT(ISPI,I,K)*W0(ISPI,K)
+              WW0=WW0+W0(ISPI,K)
+           ENDDO
+
+           wwk=0.
+           www0=0.
+           DO ISPI=1,N
+              wwk=wwk+TSCAT(ISPI,I,K+1)*W0(ISPI,K+1)
+              www0=www0+W0(ISPI,K+1)
+           ENDDO
+
+           tsource=(((1-WW0)*TEMP(K)+WK)+((1-www0)*TEMP(K+1)+wwk))*0.5
+
+           TB(I,K+1)=TB(I,K)*EXP(-TAU(K)/UEFF)+      &
+     &               (1.-EXP(-TAU(K)/UEFF))*tsource
+
+ 1200 CONTINUE
+
+!------------------------------------
+!     CHECK CONVERGENCE
+!------------------------------------
+      IND=0
+      X2=0.
+      DO J=1,NU/2
+         X2=X2+(XTB(J)-TB(J,L))**2/NU
+         IF (ABS(XTB(J)-TB(J,L)) .GT. DELTA) IND=1
+      ENDDO
+ 
+      IF (IND.NE.0 .AND. ITS.LT.ITS0) THEN
+      DO J=1,NT
+         XTB(J)=TB(J,L)
+      ENDDO
+         IF (ITS .GT. 1) THEN
+            WRITE(*,*)'ITERATION=',ITS-1,'  EPS. AT TOP OF ATMOS=',X2
+         ELSE
+            WRITE(*,*)'ITERATION=',ITS-1 
+         ENDIF
+         ITS=ITS+1
+         GOTO 1000
+      ENDIF
+
+!-----------------------------------------------------------
+!     AFTER TB CONVERGENCE IS FOUND, DO TT CALCULATIONS
+!     BY PROJECTING TB AT EACH TANGENT HEIGHT ZT
+!     INTEGRATION FROM TOP TO BOTTOM
+!-----------------------------------------------------------
+      DO 2000 ITT=1,NT
+      DO 2000 K=1,L-LMIN(ITT)+1
+         K1=L+1-K
+         UU(ITT)=UAVE(ITT,K1)                 ! INCIDENT ANGLE AT ZT(ITT) 
+         CALL LOCATE(U1,NU/2,NU0,-UU(ITT),JM) ! INTERPOLATE TSCAT ONTO UU(ITT)
+         WK=0.
+         WW0=0.
+         DO ISPI=1,N
+            WK1=W0(ISPI,K1)*( (TSCAT(ISPI,JM+NU/2+1,K1)*(UU(ITT)-U(JM))+  &
+     &          TSCAT(ISPI,JM+NU/2,K1)*(U(JM+1)-UU(ITT)))/                &
+     &          (U(JM+1)-U(JM)) )
+            WK=WK+WK1
+            WW0=WW0+W0(ISPI,K1)
+         END DO
+
+         UU(ITT)=UAVE(ITT,K1+1) 
+         wwk=0.
+         www0=0. 
+         DO ISPI=1,N
+            wwk1=W0(ISPI,K1+1)*                                     &
+     &          ( (TSCAT(ISPI,JM+NU/2+1,K1+1)*(UU(ITT)-U(JM))+      &
+     &          TSCAT(ISPI,JM+NU/2,K1+1)*(U(JM+1)-UU(ITT)))/        &
+     &          (U(JM+1)-U(JM)) )
+            wwk=wwk+wwk1
+            www0=www0+W0(ISPI,K1+1)
+         END DO
+
+         tsource=(((1-WW0)*TEMP(K1)+WK)+((1-www0)*TEMP(K1+1)+wwk))/2.
+
+         IF (ICON .eq. 3) THEN
+            tsource=( TEMP(K1)+TEMP(K1+1) )/2. ! NO CLOUD AFTER TANGENT POINT
+         ENDIF
+
+         UU(ITT)=(UAVE(ITT,K1)+UAVE(ITT,K1+1))*0.5
+
+         TT(ITT,K1)=TT(ITT,K1+1)*EXP(-TAU(K1)/UU(ITT))+  &
+     &              (1.-EXP(-TAU(K1)/UU(ITT)))*tsource
+
+ 2000 CONTINUE
+
+!---------------------------------------------
+!     DETERMINE SURFACE REFLECTION 
+!     IF TANGENT HEIGHT IS ABOVE THE SURFACE, 
+!     TT(ITT,LMIN(ITT)) IS PASSED ALONG
+!----------------------------------------------
+
+      DO 2500 ITT=1,NT
+         IF(LMIN(ITT).EQ.1) THEN
+            CALL LOCATE(U1,NU/2,NU0,-UAVE(ITT,1),JM) 
+            RSAVG=(RS(JM+1)*(UAVE(ITT,1)-U(JM)) +                 &
+     &           RS(JM)*(U(JM+1)-UAVE(ITT,1)))/(U(JM+1)-U(JM))
+            TT(ITT,1)=(TB0(JM+1)*(UAVE(ITT,1)-U(JM)) +            &
+     &               TB0(JM)*(U(JM+1)-UAVE(ITT,1)))/(U(JM+1)-U(JM))
+         ENDIF
+ 2500 CONTINUE
+
+!-----------------------------------------
+!     NOW INTEGRATION TT FROM BOTTOM UP
+!-----------------------------------------
+      DO 3000 ITT=1,NT
+      DO 3000 K=LMIN(ITT),L
+         UU(ITT)=uave(ITT,K)
+         CALL LOCATE(U1,NU/2,NU0,UU(ITT),JM)      ! INTERPOLATE TSCAT ONTO UU
+         WK=0.
+         WW0=0.
+         DO ISPI=1,N
+            WK1=W0(ISPI,K)*(TSCAT(ISPI,JM+1,K)*(UU(ITT)-U(JM))+     &
+     &          TSCAT(ISPI,JM,K)*(U(JM+1)-UU(ITT)))/(U(JM+1)-U(JM)) 
+            WK=WK+WK1
+            WW0=WW0+W0(ISPI,K)
+         END DO
+
+         UU(ITT)=uave(ITT,K+1)
+         wwk=0.
+         www0=0.
+         DO ISPI=1,N
+            wwk1=W0(ISPI,K+1)*(TSCAT(ISPI,JM+1,K+1)*(UU(ITT)-U(JM))+ &
+     &          TSCAT(ISPI,JM,K+1)*(U(JM+1)-UU(ITT)))/(U(JM+1)-U(JM)) 
+            wwk=wwk+wwk1
+            www0=www0+W0(ISPI,K+1)
+         END DO
+
+         tsource=( ((1-WW0)*TEMP(K)+WK)+((1-www0)*TEMP(K+1)+wwk) )*0.5
+
+         UU(ITT)=(UAVE(ITT,K)+UAVE(ITT,K+1))*0.5
+
+         TT(ITT,K+1)=TT(ITT,K)*EXP(-TAU(K)/UU(ITT))+          &
+     &                (1.-EXP(-TAU(K)/UU(ITT)))*tsource
+
+ 3000 CONTINUE
+  
+!---------------------------------------------------------------------
+      DO K=1,L
+	TT(NT+1,K)=TB(NU,K)              ! OUTPUT ZENITH LOOKING TB 
+      ENDDO
+!---------------------------------------------------------------------
+ 
+      RETURN
+      END
+
+! $Log: Radxfer.f,v      
+
+
+
+
+
+
+
---
+++ cloudfwdm/get_beta.f90
@@ -0,0 +1,238 @@
+
+      SUBROUTINE GET_BETA(QLG,V0,GSE,IST,WTH,NTH,DELTA,N1,GAMMA,N2, &
+                 &        MOL,NMOL,NCNT,T,PB,F,RH,VMR,ABSC)
+
+!==============================================================
+!      CALCULATE CLEAR-SKY ABSORPTION COEFFICIENT AT F AND T
+!      LATEST UPDATE: J.JIANG, MAY 18, 2001
+!==============================================================
+
+      IMPLICIT NONE
+!      INCLUDE 'spectra.h' !-----------------------------------
+!... spectral header file 
+	integer no_line		! max no. of lines for all species
+	integer no_mol		! max no. of molecules
+	parameter (no_line= 907, no_mol = 20)
+	real v0(no_line)
+	real gse(no_line)
+	real ist(no_line)
+	real wth(no_line)
+	real nth(no_line)
+	real qlg(3,no_line)
+	real delta(no_line)
+	real n1(no_line)
+	real gamma(no_line)
+	real n2(no_line)
+	integer mol		! molecule mass number
+
+	integer nmol		! no. of molecules
+	integer ncnt(no_mol)	! no. of lines used for each molecule
+! -----------------------------------------------------------------
+
+      REAL QTP(3)                      ! TEMPERATURE ON WHICH QLG ARE GIVEN
+      DATA QTP /300.0,225.0,150.0/
+
+      REAL QRAT                        ! INTERPOLATED QLG RATIO AT A GIVEN TEMP
+      REAL F                           ! FREQUENCY IN GHz
+      REAL T                           ! TEMPERATURE (K)
+      REAL P                           ! DRY AIR PARTIAL PRESSURE (hPa)
+      REAL PB                          ! TOTAL AIR PRESSURE (hPa)
+      REAL VP                          ! VAPOR PARTIAL PRESSURE (hPa)
+      REAL VMR(5)                      ! MINOR SPECIES 1-O3
+      REAL VMR_H2O                     ! H2O VOLUME MIXING RATIO
+      REAL VMR_O2                      ! O2 VOLUME MIXING RATIO
+      REAL B                           ! BETA (1/m/ppv)
+      REAL RH                          ! Relative Humidity
+      REAL ABSC                        ! ABSORPTION COEFFICIENT (1/m)
+                                       ! 1-O2
+                                       ! 2-H2O
+                                       ! 3-O_18_O
+                                       ! 4-H2O_18
+
+      REAL CONT_1,CONT_2,CONT_3        ! CONTINUUM ABSORPTION COEFFICIENTS
+      REAL SC_CONST
+      REAL SD,G0                       ! DEBY CONTRIBUTION (LIEBE 1989)
+
+      REAL PI
+      PARAMETER (PI=3.1415926)
+      REAL ZP,YY,TT,TWTH0,DWTH0        ! WORKING SPACE
+      INTEGER I,J,IMOL                
+
+      REAL MYSHAPE,FF
+      EXTERNAL MYSHAPE
+
+!------------------------------------------------------------------------
+
+      IF(RH .NE. 100.) THEN
+         VMR_H2O = RH                  ! PH IS WATER VAPOR MIXING RATIO
+         VP=VMR_H2O*PB                 ! VP IS VAPOR PRESSURE, PB IS TOTAL
+         P=PB-VP                       ! PRESSURE, P IS DRY-AIR PRESSURE
+      ELSE IF (RH .EQ. 100.) THEN
+         CALL RHtoEV(PB,T,100.,VP)     ! RH HERE IS 100% RELATIVE HUMIDITY 
+         P = PB-VP
+         VMR_H2O = VP/(max(1.e-9, P))
+      END IF
+
+      VMR_O2 = 0.209476
+      FF     = F*1000.                 ! CONVERT F TO MHz
+      ZP     = -ALOG10( max(1.e-9,P) ) ! CONVERT P(hPa) TO ZP
+      TT     = 300./T
+
+      ABSC=0.
+      I=0
+
+!      print*,'check'
+
+!-------------------------------------
+!     LINE EMISSION
+!-------------------------------------
+      DO IMOL=1,NMOL
+         B=0.
+         DO J=1,NCNT(IMOL)
+            I=I+1
+
+            IF(IMOL.GE.3.AND.ABS(V0(i)-FF).GT.10000.) GOTO 100 ! SAVE CPU
+
+            IF(T .LE. QTP(2)) THEN
+!               QRAT = (QLG(3,I)-QLG(1,I))*(T-QTP(2))/(QTP(3)-QTP(2))
+               QRAT = (QLG(2,I)-QLG(1,I))+(QLG(3,I)-QLG(2,I))*(T-QTP(2))/(QTP(3)-QTP(2))
+            ELSE
+               QRAT = (QLG(2,I)-QLG(1,I))*(T-QTP(1))/(QTP(2)-QTP(1))
+            ENDIF
+
+            YY=10.**(-ZP)*(DELTA(I)*TT**N1(I)+GAMMA(I)*TT**N2(I))
+
+!-------------------------------------
+!           PRESSURE WIDTH
+!-------------------------------------
+
+            TWTH0=WTH(I)*10.**(-ZP)*TT**NTH(I)
+
+!-------------------------------------
+!           DOPPLER WIDTH
+!-------------------------------------
+
+            IF(IMOL.EQ.1) DWTH0 = 3.58e-7*SQRT(T/32.)*FF
+            IF(IMOL.EQ.2) DWTH0 = 3.58e-7*SQRT(T/18.)*FF
+            IF(IMOL.EQ.3) DWTH0 = 3.58e-7*SQRT(T/34.)*FF
+            IF(IMOL.EQ.4) DWTH0 = 3.58e-7*SQRT(T/20.)*FF
+            IF(IMOL.EQ.5) DWTH0 = 3.58e-7*SQRT(T/48.)*FF
+            
+!--------------------------------------------------
+!           WHITING'S APPROXIMATION FOR THE VOIGT
+!--------------------------------------------------
+
+            TWTH0 = TWTH0/2+SQRT(TWTH0**2/4+DWTH0**2)
+
+!---------------------------------------------------------------
+!	TO SAVE COMPUTING TIME CHECK IF FREQ IS CLOSE TO O3 LINES
+!----------------------------------------------------------------
+!            print*,IST(I),GSE(I),V0(I), FF, YY, TWTH0, T
+!            print*,ZP, MYSHAPE(V0(i),FF,YY,TWTH0)   
+
+            B = B+2.30549e09*10.0**(IST(I)-QRAT+GSE(I)*   &
+     &		(1./300-1./T)/1.600386 - ZP) * PI *       &
+     &		 MYSHAPE(V0(i),FF,YY,TWTH0) * FF/V0(I) *  &
+     &		(1. - EXP(-V0(i)/(20836.7*T)))/           &
+     &		(1. - EXP(-V0(i)/(20836.7*300.0)))/T 
+
+ 100     CONTINUE
+
+         ENDDO   
+
+         B=AMAX1(0.,B)                 ! AVOID NEGATIVE LINE SHAPE     
+
+         IF(IMOL .EQ. 1) ABSC = ABSC + B*VMR_O2                   ! O2      
+         IF(IMOL .EQ. 2) ABSC = ABSC + B*VMR_H2O                  ! H2O
+         IF(IMOL .EQ. 3) ABSC = ABSC + B*VMR_O2*0.00409524        ! O_18_O
+         IF(IMOL .EQ. 4) ABSC = ABSC + B*VMR_H2O*0.00204          ! H2O_18
+         IF(IMOL .EQ. 5) ABSC = ABSC + B*VMR(1)*1e-6              ! O3
+
+      ENDDO  
+
+!----------------------------------------
+!     DRY CONTINUUM (ATBD APPENDIX F)
+!----------------------------------------
+
+      B=(7.7e-10*EXP(-1.5e-3*(F/30)**2)+1.e-13*(60**2+(F/30)**2)  &
+     &     *EXP(-1.e-4*(F/30)**2))*TT**1.7
+
+      ABSC = ABSC + B*0.65*(P/1013.)**2*TT**2*(F/30)**2*1.e5
+
+!     CONT_1=1.4e-10*(1-1.2e-5*F**1.5)     ! LIEBE 1989
+!     CONT_1 = 1.4e-12/(1+1.93e-5*F**1.5)  ! LIEBE 1993
+!
+!      CONT_1 = 1.4e-12/(1+1.93e-5*F**1.5)*1.28*1.17  ! MATCH THE UARS FIT
+!
+!      B=CONT_1 * F * (P/10.)**2*TT**3.5
+!      ABSC=ABSC + B*.182*F/4.343 
+!------------------------------------------
+!     DEBYE TERM FROM LIEBE
+!------------------------------------------
+      SD = 6.14e-4*P/10.*TT**2
+      G0 = 5.6e-3*(1+1.1*VMR_H2O)*P/10.*TT
+      B = SD*F/(G0 * (1.+ (F/G0)**2))		! B is Npp in LIEBE
+      ABSC = ABSC + B*.182*F/4.343              ! CONVERTED TO 1/km 
+
+!----------------------------------------
+!     WET CONTINUUM 
+!----------------------------------------
+!     CONT_1 = 1.28e-15 	! BILL'S VALIDATION PAPER
+      CONT_1 = 7.53e-16 
+      CONT_2 = 4.20
+      CONT_3 = 0.00
+      SC_CONST = CONT_1 * FF**2 * EXP(-CONT_3 * FF**2)
+      B = SC_CONST * 10.**(-2.0*ZP)* TT**CONT_2
+!--------------------------------------------------------------
+!     THE SECOND TERM FOR H2O-H2O COLLISION (GODON ET AL 1991)
+!--------------------------------------------------------------
+      B = B+SC_CONST*1.595e-8/8.592e-10*TT**(6.18)*10.**(-2*ZP)*VMR_H2O
+      ABSC = ABSC + B * VMR_H2O
+      ABSC = ABSC * 1.e-3       ! converted from 1/km to 1/m
+
+      RETURN
+      END
+
+!--------------------------------------------------------------
+!     DEFINE LINE-SHAPE FUNCTIONS
+!--------------------------------------------------------------
+	real function myshape(v0,ff,yy,twth0)
+	implicit none
+        real pi
+        parameter (pi=3.1415926)
+	real voffm		! frequency difference
+	real voffp		! frequency difference
+	real ff		! frequency in MHz
+	real v0		! line frequency in MHz
+	real twth0		! line width in MHz/mb
+	real yy 		! interference coeff.
+	real twthm, twthp
+!
+	  voffm = v0 - ff
+	  voffp = v0 + ff
+	  twthm = twth0 - voffm*yy*1.
+	  twthp = twth0 - voffp*yy*1.
+!... Gross function
+!	myshape = 4.*ff*v0*twth0/pi/((voffm*voffp)**2+4*(ff*twthm)**2)
+!	return
+
+!... Van Vleck-Weisskopf
+ 	myshape = twthm/(voffm**2 + twth0**2) + &
+     &      twthp/(voffp**2 + twth0**2)
+ 	myshape = myshape * (ff/v0)/pi
+!...
+	return
+	end
+
+! $Log: get_beta.f,v      
+
+
+
+
+
+
+
+
+
+
+
