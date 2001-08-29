c7880869b9c7e1c703b96e068a85505c387005a4
pwagner <>
2001-08-29 18:09:33 +0000

    Added C, CC, FC options

--- util/build_f90_in_misc.sh
+++ util/build_f90_in_misc.sh
@@ -18,6 +18,9 @@
 # -M MYMAKE          use MYMAKE for make command instead of make
 # -t test_dir_path   use test_dir_path/misc instead of tests/misc
 # -c MLSCONFG        use arg for MLSCONFG instead of any current setting
+# -C MLSCFILE        use arg for MLSCFILE instead of .configure
+# -CC compiler       use arg for c compiler instead of cc
+# -FC compiler       use arg for Fortran compiler instead of MLSCONFG
 # -i INC_PATHS       let arg override usual value for INC_PATHS
 #                     used for search paths when compiling
 #                     e.g., -i "../ /software/SRC..."
@@ -145,7 +148,7 @@ else
 fi
 
 # Initialize settings to defaults
-DEEBUG=off
+DEEBUG=on
 BUILD=on
 prog_name=test
 test_dir_name=misc
@@ -157,6 +160,7 @@ override_INC_PATHS="no"
 INC_PATHS=""
 EXTRA_PATHS=""
 MYMAKE=make
+MYMAKEOPTS=
 
 me="$0"
 my_name=build_f90_in_misc.sh
@@ -197,6 +201,18 @@ while [ "$1" != "" ] ; do
        NEED_MLSCONFG="no"
 	    shift
 	;;
+	-C )
+	    MYMAKEOPTS="MLSCFILE=$2 $MYMAKEOPTS"
+	    shift
+	;;
+	-CC )
+	    MYMAKEOPTS="CC=$2 $MYMAKEOPTS"
+	    shift
+	;;
+	-FC )
+	    MYMAKEOPTS="FC=$2 $MYMAKEOPTS"
+	    shift
+	;;
 	-I )
 	    EXTRA_PATHS="-I $2 $EXTRA_PATHS"
 	    shift
@@ -232,10 +248,11 @@ then
    echo "override_INC_PATHS: $override_INC_PATHS"
    echo "INC_PATHS: $INC_PATHS"
    echo "EXTRA_PATHS: $EXTRA_PATHS"
+   echo "extra make options: $MYMAKEOPTS"
    echo "arglist: $arglist"
 fi
 
-# Check on args--need any more? ARe they self-consistent?
+# Check on args--need any more? Are they self-consistent?
 if [ "$NEED_MLSCONFG" = "yes" ]
 then
    echo "Sorry--MLSCONFG must be defined"
@@ -338,12 +355,12 @@ then
       cd $test_dir_path/$test_dir_name
 #      make depends ghostbuster
 #      make EXTRA_PATHS="$EXTRA_PATHS"
-      $MYMAKE depends ghostbuster
-      $MYMAKE EXTRA_PATHS="$EXTRA_PATHS"
+      $MYMAKE depends ghostbuster $MYMAKEOPTS
+      $MYMAKE EXTRA_PATHS="$EXTRA_PATHS" $MYMAKEOPTS
    fi
    if [ "$DEEBUG" = "on" ]
    then
-      echo $MYMAKE EXTRA_PATHS="$EXTRA_PATHS"
+      echo $MYMAKE EXTRA_PATHS="$EXTRA_PATHS" $MYMAKEOPTS
    fi
 elif [ "$override_INC_PATHS" = "no" ]
 then
@@ -352,8 +369,8 @@ then
       cd $test_dir_path/$test_dir_name
 #      make depends ghostbuster
 #      make
-      $MYMAKE depends ghostbuster
-      $MYMAKE
+      $MYMAKE depends ghostbuster $MYMAKEOPTS
+      $MYMAKE $MYMAKEOPTS
    fi
 else
    repair_subpaths $INC_PATHS
@@ -363,12 +380,12 @@ else
       cd $test_dir_path/$test_dir_name
 #      make depends ghostbuster
 #      make INC_PATHS="$INC_PATHS"
-      $MYMAKE depends ghostbuster
-      $MYMAKE INC_PATHS="$INC_PATHS"
+      $MYMAKE depends ghostbuster $MYMAKEOPTS
+      $MYMAKE INC_PATHS="$INC_PATHS" $MYMAKEOPTS
    fi
    if [ "$DEEBUG" = "on" ]
    then
-      echo $MYMAKE INC_PATHS="$INC_PATHS"
+      echo $MYMAKE INC_PATHS="$INC_PATHS" $MYMAKEOPTS
    fi
 fi
 
@@ -393,6 +410,9 @@ fi
 exit 0
 
 # $Log$
+# Revision 1.8  2001/08/28 18:26:32  pwagner
+# Added -M option; no more bare make
+#
 # Revision 1.7  2001/08/17 23:15:26  pwagner
 # EXTRA_PATHS now implemented; relative paths w.r.t. cwd
 #
