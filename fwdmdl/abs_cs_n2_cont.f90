36aa63a0ca2e5ab6473f090c6ff888bcd42248dd
zvi <>
2001-10-17 17:03:17 +0000

    rev 2.2

--- fwdmdl/abs_cs_n2_cont.f90
+++ fwdmdl/abs_cs_n2_cont.f90
@@ -9,8 +9,10 @@ module ABS_CS_N2_CONT_M
        "$RCSfile$"
 !---------------------------------------------------------------------------
   integer, private, parameter :: RK = kind(0.0d0)
+
 contains
-! This function computes the water continuum contribution to the dry air
+
+! This function computes the N2 continuum contribution
 
   real(rk) Function ABS_CS_N2_CONT (cont, temperature, pressure, frequency )
 
@@ -24,15 +26,18 @@ contains
     theta = 300.0_rk / temperature
     fsqr = frequency * frequency
     fsxt = fsqr * theta
-    abs_cs_n2_cont = pressure * pressure * fsqr * (theta ** cont(2)) * &
-                   & ( cont(1) * exp(cont(3) * fsxt) +                 &
-                   &   cont(4) * exp(cont(5) * fsxt) * (cont(6) + fsqr))
+    abs_cs_n2_cont = pressure * pressure * fsqr * (theta**cont(2)) * &
+                   & ( cont(1) * exp(cont(3) * fsxt) +               &
+                   &   cont(4) * exp(cont(5) * fsxt) * (cont(6)**2 + fsqr))
 !
   End function abs_cs_n2_cont
 end module ABS_CS_N2_CONT_M
 ! $Log$
+! Revision 2.2  2001/10/17 17:03:17  zvi
+! Fix a bug
+!
 ! Revision 2.1  2001/10/16 14:45:02  zvi
-! New Dry air function (N2) with continuum parameters passed in as part of the Catalog
+! New Dry air function (N2) with continuum parameters passed in
 !
 ! Revision 1.1  2000/05/04 18:12:04  vsnyder
 ! Initial conversion to Fortran 90
