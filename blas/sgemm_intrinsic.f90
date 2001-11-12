9c72d55252df34bb51f88b448bf04451742360f8
vsnyder <>
2001-11-12 18:59:20 +0000

    Initial commit

---
+++ blas/sgemm_intrinsic.f90
@@ -0,0 +1,16 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
+! This is NOT a module so that we can link it in place of ATLAS or LAPACK
+! without causing a cascade.
+
+! $RCSfile$
+
+  subroutine SGEMM ( TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, &
+    &                BETA, C, LDC )
+    use MLSMessageModule, only: MLSMessage, MLSMSG_Error
+    integer, parameter :: RK = kind(0.0e0)
+    include "gemm.f9h"
+  end subroutine SGEMM
+
+! $Log$
