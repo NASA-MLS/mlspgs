58fe4208edb5f37d4b3d500bfbd7de0c36272f72
vsnyder <>
2001-11-13 21:16:11 +0000

    Get rid of dependence on MLSMessage

--- blas/dgemm_intrinsic.f90
+++ blas/dgemm_intrinsic.f90
@@ -8,9 +8,11 @@
 
   subroutine DGEMM ( TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, &
     &                BETA, C, LDC )
-    use MLSMessageModule, only: MLSMessage, MLSMSG_Error
     integer, parameter :: RK = kind(0.0d0)
     include "gemm.f9h"
   end subroutine DGEMM
 
 ! $Log$
+! Revision 1.1  2001/11/12 18:59:20  vsnyder
+! Initial commit
+!
--- blas/sgemm_intrinsic.f90
+++ blas/sgemm_intrinsic.f90
@@ -8,9 +8,11 @@
 
   subroutine SGEMM ( TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, &
     &                BETA, C, LDC )
-    use MLSMessageModule, only: MLSMessage, MLSMSG_Error
     integer, parameter :: RK = kind(0.0e0)
     include "gemm.f9h"
   end subroutine SGEMM
 
 ! $Log$
+! Revision 1.1  2001/11/12 18:59:20  vsnyder
+! Initial commit
+!
