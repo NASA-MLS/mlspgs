ea6da014b2133a8cc65d6163f29bcc657ab0d169
vsnyder <>
2002-02-13 20:52:26 +0000

    rev 2.3

--- lib/BitStuff.f90
+++ lib/BitStuff.f90
@@ -80,37 +80,46 @@ contains
   end function CountBits_2
 
   ! --------------------------------------------  CountCharBits_0  -----
-  integer function CountCharBits_0 ( Char )
+  integer function CountCharBits_0 ( Char, What )
+    ! Count the number of nonzero bits.
     character(len=1), intent(in) :: Char
-    ! Count the number of nonzero bits in all of the elements of Word.
-    countCharBits_0 = countBits(ichar(char))
+    integer, intent(in), optional :: What ! mask of which bits to count
+    integer :: myWhat
+    myWhat = not(0)
+    if ( present(what) ) myWhat = what
+    countCharBits_0 = countBits(iand(ichar(char),myWhat))
   end function CountCharBits_0
 
   ! --------------------------------------------  CountCharBits_1  -----
-  integer function CountCharBits_1 ( Array )
+  integer function CountCharBits_1 ( Array, What )
     character(len=1), intent(in) :: Array(:)
+    integer, intent(in), optional :: What ! mask of which bits to count
     ! Count the number of nonzero bits in all of the elements of Word.
     integer :: I
     countCharBits_1 = countBits(array(1))
     do i = 2, size(array)
-      countCharBits_1 = countCharBits_1 + countBits(array(i))
+      countCharBits_1 = countCharBits_1 + countBits(array(i),what)
     end do
   end function CountCharBits_1
 
   ! --------------------------------------------  CountCharBits_2  -----
-  integer function CountCharBits_2 ( Array )
+  integer function CountCharBits_2 ( Array, What )
     character, intent(in) :: Array(:,:)
+    integer, intent(in), optional :: What ! mask of which bits to count
     ! Count the number of nonzero bits in all of the elements of Word.
     integer :: I
     countCharBits_2 = countBits(array(:,1))
     do i = 2, size(array,2)
-      countCharBits_2 = countCharBits_2 + countBits(array(:,i))
+      countCharBits_2 = countCharBits_2 + countBits(array(:,i),what)
     end do
   end function CountCharBits_2
 
 end module BitStuff
 
 ! $Log$
+! Revision 2.3  2002/02/13 20:52:26  vsnyder
+! Added a 'what' mask to count_bits_char...
+!
 ! Revision 2.2  2002/02/05 02:39:59  vsnyder
 ! Change mask from 1-bit per to 8-bits per (using character)
 !
