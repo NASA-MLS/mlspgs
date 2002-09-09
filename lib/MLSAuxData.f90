023257faec08a9eae7368d9300f731412990a315
jdone <>
2002-09-09 05:26:15 +0000

    rev 2.2

--- lib/MLSAuxData.f90
+++ lib/MLSAuxData.f90
@@ -12,7 +12,9 @@ module MLSAuxData
        h5pset_chunk_f, h5sclose_f, h5dget_space_f, h5pclose_f, & 
        h5sget_simple_extent_ndims_f, h5sget_simple_extent_dims_f, &
        h5dget_create_plist_f, h5pget_chunk_f, h5dget_type_f, & 
-       h5sselect_hyperslab_f, h5dread_f, h5dwrite_f, h5dextend_f
+       h5sselect_hyperslab_f, h5dread_f, h5dwrite_f, h5dextend_f, &
+       h5acreate_f, h5awrite_f, h5aread_f, h5aclose_f, h5tcopy_f, &
+       h5tset_size_f, h5aopen_name_f, h5aget_type_f, h5aget_space_f
 
   implicit NONE
 
@@ -67,10 +69,7 @@ module MLSAuxData
     real,  dimension(:,:,:), pointer :: RealField => NULL()
     integer,   dimension(:,:,:), pointer :: IntField => NULL()
 
-    integer, dimension(:), pointer :: CounterMAF => NULL() !dimensioned(noMAFs)
-
     ! all the above dimensioned (noAuxInds,maxMIFs,noMAFs)
-
   end type MLSAuxData_T
 
   CHARACTER(len=*), PUBLIC, PARAMETER :: & 
@@ -97,6 +96,8 @@ module MLSAuxData
 
   CHARACTER(len=*), PUBLIC, PARAMETER :: & 
     H5_ERROR_ATT_WRITE  = 'HDF5 Error Writing Attribute '
+  CHARACTER(len=*), PUBLIC, PARAMETER :: & 
+    H5_ERROR_ATT_READ  = 'HDF5 Error Reading Attribute '
   CHARACTER(len=*), PUBLIC, PARAMETER :: & 
     H5_ERROR_ATT_CREATE = 'HDF5 Error Creating Attribute '
   CHARACTER(len=*), PUBLIC, PARAMETER :: & 
@@ -173,9 +174,21 @@ contains ! ============================ MODULE PROCEDURES ====================
 ! check on status.
     endif
 
-    if (associated(MLSAuxData%counterMAF)) then 
-        nullify(MLSAuxData%counterMAF)
-        deallocate(MLSAuxData%counterMAF, stat=status)
+    if (associated(MLSAuxData%FrequencyCoordinates)) then 
+        nullify(MLSAuxData%FrequencyCoordinates)
+        deallocate(MLSAuxData%FrequencyCoordinates, stat=status)
+! check on status.
+    endif
+
+    if (associated(MLSAuxData%VerticalCoordinates)) then 
+        nullify(MLSAuxData%VerticalCoordinates)
+        deallocate(MLSAuxData%VerticalCoordinates, stat=status)
+! check on status.
+    endif
+
+    if (associated(MLSAuxData%HorizontalCoordinates)) then 
+        nullify(MLSAuxData%HorizontalCoordinates)
+        deallocate(MLSAuxData%HorizontalCoordinates, stat=status)
 ! check on status.
     endif
 
@@ -192,56 +205,60 @@ contains ! ============================ MODULE PROCEDURES ====================
 !-----------------------------------------------------------------------
 ! Internal variables
 !
+    character(len=name_len) :: aname
+    real, dimension(3) :: attr_data
     integer(hsize_t), dimension(3) :: chunk_dims, dims, maxdims
-    integer(hsize_t), dimension(1) :: maxdims_MAF, dims_MAF, chunk_dims_MAF 
-    integer(hid_t) :: cparms, dspace_id, dset_id, type_id
-    integer :: i, rank_MAF, rank, h5error
+    integer(hsize_t), dimension(7) :: adims
+    integer(hsize_t), dimension(1) :: adims_create
+   integer(hid_t) :: cparms,dspace_id,dset_id,type_id, &
+         attr_id, atype_id, aspace_id
+    integer :: i, rank_MAF, rank, arank, h5error
 !-----------------------------------------------------------------------
 
     test_type: select case (trim(MLSAuxData%type_name))
     case ('real')
        if (associated(MLSAuxData%RealField)) then 
        rank = size(shape(MLSAuxData%RealField))
+       type_id = H5T_IEEE_F32LE
        do i=1,rank
           dims(i) = size(MLSAuxData%RealField, i)
           chunk_dims(i) = dims(i)
-          type_id = H5T_IEEE_F32LE
           maxdims(i) = H5S_UNLIMITED_F
        end do
        end if
     case ('double')
        if (associated(MLSAuxData%DpField)) then 
        rank = size(shape(MLSAuxData%DpField))
+       type_id = H5T_NATIVE_DOUBLE
        do i=1,rank
           dims(i) = size(MLSAuxData%DpField, i)
           chunk_dims(i) = dims(i)
-          type_id = H5T_NATIVE_DOUBLE
           maxdims(i) = H5S_UNLIMITED_F
        end do
        endif
     case ('integer')
        if (associated(MLSAuxData%IntField)) then 
        rank = size(shape(MLSAuxData%IntField))
+       type_id = H5T_NATIVE_INTEGER
        do i=1,rank
           dims(i) = size(MLSAuxData%IntField, i)
           chunk_dims(i) = dims(i)
-          type_id = H5T_NATIVE_INTEGER
           maxdims(i) = H5S_UNLIMITED_F
        end do
        endif
     case ('character')
        if (associated(MLSAuxData%CharField)) then 
        rank = size(shape(MLSAuxData%CharField))
+       type_id = H5T_NATIVE_CHARACTER
        do i=1, rank
           dims(i) = size(MLSAuxData%CharField, i)
           chunk_dims(i) = dims(i)
-          type_id = H5T_NATIVE_CHARACTER
           maxdims(i) = H5S_UNLIMITED_F
        end do
        endif
     end select test_type
-
-    call h5screate_simple_f(rank, dims(1:rank), dspace_id, h5error, & 
+!--------------------------------------------------------------------
+    call h5screate_simple_f(rank, dims(1:rank),dspace_id,h5error,& 
          maxdims(1:rank))
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
 !       H5_ERROR_DSPACE_CREATE // QuantityName )
@@ -259,6 +276,66 @@ contains ! ============================ MODULE PROCEDURES ====================
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
 !       H5_ERROR_DSET_CREATE // QuantityName ) 
 
+!---------------- write the attributes -----------------------------
+
+    do i = 2, 7
+       adims(i) = 0
+    end do
+
+    if (associated(MLSAuxData%HorizontalCoordinates)) then 
+
+       arank = size(shape(MLSAuxData%HorizontalCoordinates))
+       atype_id = H5T_IEEE_F32LE
+       adims(1) = size(MLSAuxData%HorizontalCoordinates)
+       adims_create(1) = adims(1)
+       do i=1, adims(1)
+          attr_data(i) = MLSAuxData%HorizontalCoordinates(i)
+       end do
+
+    call h5screate_simple_f(arank, adims_create, aspace_id, h5error)
+    aname = "HorizontalCoordinates"  
+    call h5acreate_f(dset_id, trim(aname), atype_id, aspace_id, attr_id, &
+       h5error)
+    call h5awrite_f(attr_id, atype_id, attr_data, adims, h5error)
+    call h5aclose_f(attr_id, h5error)
+    call h5sclose_f(aspace_id, h5error) 
+    endif
+
+    if (associated(MLSAuxData%VerticalCoordinates)) then 
+       arank = size(shape(MLSAuxData%VerticalCoordinates))
+       atype_id = H5T_IEEE_F32LE
+       adims(1) = size(MLSAuxData%VerticalCoordinates)
+       adims_create(1) = adims(1)
+       do i=1,adims(1)
+          attr_data(i) = MLSAuxData%VerticalCoordinates(i)
+       end do
+
+    call h5screate_simple_f(arank, adims_create, aspace_id, h5error)
+    aname = "VerticalCoordinates"   
+    call h5acreate_f(dset_id, trim(aname), atype_id, aspace_id, attr_id, &
+       h5error)
+    call h5awrite_f(attr_id, atype_id, attr_data, adims, h5error)
+    call h5aclose_f(attr_id, h5error)
+    call h5sclose_f(aspace_id, h5error) 
+    endif
+
+    if (associated(MLSAuxData%FrequencyCoordinates)) then 
+       arank = size(shape(MLSAuxData%FrequencyCoordinates))
+       atype_id = H5T_IEEE_F32LE
+       adims(1) = size(MLSAuxData%FrequencyCoordinates)
+       adims_create(1) = adims(1)
+       do i=1,adims(1)
+          attr_data(i) = MLSAuxData%FrequencyCoordinates(i)
+       end do
+
+    call h5screate_simple_f(arank, adims_create, aspace_id, h5error)
+    aname = "FrequencyCoordinates"
+    call h5acreate_f(dset_id, trim(aname), atype_id, aspace_id, attr_id, &
+       h5error)
+    call h5awrite_f(attr_id, atype_id, attr_data, adims, h5error)
+    call h5aclose_f(attr_id, h5error)
+    call h5sclose_f(aspace_id, h5error) 
+    endif
 !---------------- close all structures----------------------------------
 !
     call h5sclose_f( dspace_id, h5error )
@@ -288,11 +365,22 @@ contains ! ============================ MODULE PROCEDURES ====================
 !
 !-------------------------------------------------------------------------
 !
-    integer(hid_t) :: cparms, dset_id, dspace_id, type_id, memspace
-    integer        :: rank, h5error, i, status
-    integer(hsize_t), dimension(3) :: chunk_dims, dims_create, maxdims, start
-    integer(hsize_t), dimension(7) :: dims
+    real, dimension(:,:,:), pointer :: real_buffer, double_buffer
+    integer, dimension(:,:,:), pointer :: integer_buffer
+    character, dimension(:,:,:), pointer :: character_buffer
 
+    integer(hsize_t), dimension(7) :: dims, adims
+    integer(hsize_t), dimension(3) :: chunk_dims, dims_create, maxdims, start
+    integer(hid_t) :: cparms, dset_id, dspace_id, type_id, memspace, & 
+         attr_id, atype_id, aspace_id
+    integer        :: rank, h5error, i, j, k, status
+
+!-- assign name and type for dataset
+
+    MLSAuxData%name = trim(QuantityName)
+    MLSAuxData%type_name = trim(QuantityType)
+
+!---
     call h5dopen_f(file_id,trim(QuantityName),dset_id,h5error)
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
 !       H5_ERROR_DSET_OPEN // trim(QuantityName) )
@@ -310,7 +398,7 @@ contains ! ============================ MODULE PROCEDURES ====================
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
 !       H5_ERROR_DSPACE_GET_DIMS // trim(QuantityName) )
 !
-     call h5dget_create_plist_f(dset_id,cparms,h5error)
+    call h5dget_create_plist_f(dset_id,cparms,h5error)
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
 !       H5_ERROR_DSET_CREATE // trim(QuantityName) )
 
@@ -338,8 +426,6 @@ contains ! ============================ MODULE PROCEDURES ====================
        start(i) = 0
     end do
 
-    if (present (FirstMAF)) start(rank)=FirstMAF-1
-
     call h5sselect_hyperslab_f(memspace, H5S_SELECT_SET_F, &
                                 start(1:rank), dims_create(1:rank), h5error)
 
@@ -361,36 +447,61 @@ contains ! ============================ MODULE PROCEDURES ====================
     call h5dread_f(dset_id,type_id,MLSAuxData%CharField,dims,&
          h5error,memspace,dspace_id)
     end select test_type
-
     else
-    
-    print *,'Does not yet handle optional MAF I/O.'
+     
+    test_type_dims: select case (trim(QuantityType))
+    case ('real')
+    allocate( real_buffer(dims(1),dims(2),dims(3)),stat=status)
+    call h5dread_f(dset_id,type_id,& 
+         real_buffer,dims,h5error, memspace, dspace_id)
+         do k = FirstMAF, LastMAF
+            do j = 1, dims(2)
+               do i = 1, dims(1)
+                  MLSAuxData%RealField(i,j,k) = real_buffer(i,j,k)
+               end do 
+            end do
+         end do
+    case ('double')    
 
-!    test_type_dims: select case (trim(QuantityType))
-!    case ('real')
-!    call h5dread_f(dset_id,type_id,& 
-!         MLSAuxData%RealField(1:dims(1),1:dims(2),FirstMAF:LastMAF),& 
-!         dims,h5error, memspace, dspace_id)
-!    case ('double')    
-!    call h5dread_f(dset_id,type_id,& 
-!         MLSAuxData%DpField(1:dims(1),1:dims(2),FirstMAF:LastMAF),& 
-!         dims,h5error,memspace,dspace_id)
-!    case ('integer')  
-!    call h5dread_f(dset_id,type_id,&
-!         MLSAuxData%IntField(1:dims(1),1:dims(2),FirstMAF:LastMAF),&         
-!         dims,h5error,memspace,dspace_id)
-!    case ('character') 
-!    call h5dread_f(dset_id,type_id,&
-!         MLSAuxData%CharField(1:dims(1),1:dims(2),FirstMAF:LastMAF),&          
-!         dims,h5error,memspace,dspace_id)
-!    end select test_type_dims
+    allocate( double_buffer(dims(1),dims(2),dims(3)),stat=status)
+    call h5dread_f(dset_id,type_id,& 
+         double_buffer,dims,h5error, memspace, dspace_id)
+         do k = FirstMAF, LastMAF
+            do j = 1, dims(2)
+               do i = 1, dims(1)
+                  MLSAuxData%DpField(i,j,k) = double_buffer(i,j,k)
+               end do 
+            end do
+         end do    
+    case ('integer')  
+    allocate( integer_buffer(dims(1),dims(2),dims(3)),stat=status)
+    call h5dread_f(dset_id,type_id,& 
+         integer_buffer,dims,h5error, memspace, dspace_id)
+         do k = FirstMAF, LastMAF
+            do j = 1, dims(2)
+               do i = 1, dims(1)
+                  MLSAuxData%IntField(i,j,k) = integer_buffer(i,j,k)
+               end do 
+            end do
+         end do
+    case ('character') 
+    allocate( character_buffer(dims(1),dims(2),dims(3)),stat=status)
+    call h5dread_f(dset_id,type_id,& 
+         character_buffer,dims,h5error, memspace, dspace_id)
+         do k = FirstMAF, LastMAF
+            do j = 1, dims(2)
+               do i = 1, dims(1)
+                  MLSAuxData%CharField(i,j,k) = character_buffer(i,j,k)
+               end do 
+            end do
+         end do
+    end select test_type_dims
 
     endif
 
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
 !       H5_ERROR_DSET_READ // trim(QuantityName) )
 
-    MLSAuxData%type_name = trim(QuantityType)
 !
 ! Close all identifiers.
 !
@@ -423,10 +534,15 @@ contains ! ============================ MODULE PROCEDURES ====================
 !-------------------------------------------------------------------------
 ! Internal variables.
 !
-    integer(hid_t) :: filespace, memspace, dset_id, dspace_id, type_id
-    integer :: h5error, rank, i
+
     integer(hsize_t), dimension(7) :: dims
     integer(hsize_t), dimension(3) :: dims_create, start
+
+    integer(hid_t) :: filespace, memspace, dset_id, dspace_id, type_id
+
+    integer :: h5error, rank, i
+
+!--------------------------------------------------------------------------
 
     call h5dopen_f(file_id, trim(MLSAuxData%name), dset_id, h5error)
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
@@ -497,7 +613,6 @@ contains ! ============================ MODULE PROCEDURES ====================
     case ('real')
      call h5dwrite_f(dset_id, type_id, MLSAuxData%RealField, dims(1:rank), &
           h5error,memspace,filespace)
-
     case ('double')
      call h5dwrite_f(dset_id, type_id, MLSAuxData%DpField, dims(1:rank), & 
           h5error,memspace,filespace)
@@ -513,29 +628,29 @@ contains ! ============================ MODULE PROCEDURES ====================
 
     print *,'Does not yet handle optional MAF I/O.'
 
-!    test_type_dims: select case (trim(MLSAuxData%type_name))
-!    case ('real')
-!    call h5dwrite_f(dset_id,type_id,& 
-!         MLSAuxData%RealField(1:dims(1),1:dims(2),startMAF:dims(3)),& 
-!         dims,h5error,memspace,filespace)
-!    case ('double')    
-!    call h5dwrite_f(dset_id,type_id,& 
-!         MLSAuxData%DpField(1:dims(1),1:dims(2),startMAF:dims(3)),& 
-!         dims,h5error,memspace,filespace)
-!    case ('integer')  
-!    call h5dwrite_f(dset_id,type_id,&
-!         MLSAuxData%IntField(1:dims(1),1:dims(2),startMAF:dims(3)),&         
-!         dims,h5error,memspace,filespace)
-!    case ('character') 
-!    call h5dwrite_f(dset_id,type_id,&
-!         MLSAuxData%CharField(1:dims(1),1:dims(2),startMAF:dims(3)),&     
-!         dims,h5error,memspace,filespace)
-!    end select test_type_dims    
+    test_type_dims: select case (trim(MLSAuxData%type_name))
+    case ('real')
+    call h5dwrite_f(dset_id,type_id,& 
+         MLSAuxData%RealField(1:dims(1),1:dims(2),startMAF:dims(3)),& 
+         dims,h5error,memspace,filespace)
+    case ('double')    
+    call h5dwrite_f(dset_id,type_id,& 
+         MLSAuxData%DpField(1:dims(1),1:dims(2),startMAF:dims(3)),& 
+         dims,h5error,memspace,filespace)
+    case ('integer')  
+    call h5dwrite_f(dset_id,type_id,&
+         MLSAuxData%IntField(1:dims(1),1:dims(2),startMAF:dims(3)),&         
+         dims,h5error,memspace,filespace)
+    case ('character') 
+    call h5dwrite_f(dset_id,type_id,&
+         MLSAuxData%CharField(1:dims(1),1:dims(2),startMAF:dims(3)),&     
+         dims,h5error,memspace,filespace)
+    end select test_type_dims    
     endif
+
 
 !    if (h5error /= 0) call MLSMessage(MLSMSG_Error, ModuleName, & 
 !       H5_ERROR_DSET_WRITE // trim(QuantityName) )
-
 !
 ! Close all identifiers.
 !
@@ -556,6 +671,6 @@ contains ! ============================ MODULE PROCEDURES ====================
 end module MLSAuxData
 
 ! $Log$
-! Revision 2.1  2002/09/03 04:20:34  jdone
-! add routine to read/write HDF5 files
+! Revision 2.2  2002/09/09 05:26:15  jdone
+! Now reads portion of dataset
 !
