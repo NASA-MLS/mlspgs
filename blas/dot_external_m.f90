2c098bba96de635c46e23257e1d216ab21be2b93
pwagner <>
2002-09-13 18:04:31 +0000

    Added mixed type dot products: a_r4 . b_r8 and a_r8 . b_r4

--- blas/dot_external_m.f90
+++ blas/dot_external_m.f90
@@ -23,11 +23,23 @@ module Dot_M
       integer, intent(in) :: N, INCX, INCY
       double precision, intent(in) :: X, Y
     end function DDOT
+    double precision function SDDOT ( N, X, INCX, Y, INCY )
+      integer, intent(in) :: N, INCX, INCY
+      real, intent(in) :: X
+      double precision, intent(in) :: Y
+    end function SDDOT
+    double precision function DSDOT ( N, X, INCX, Y, INCY )
+      integer, intent(in) :: N, INCX, INCY
+      double precision, intent(in) :: X
+      real, intent(in) :: Y
+    end function DSDOT
   end interface
-
 end module Dot_M
 
 ! $Log$
+! Revision 1.3  2001/11/08 02:37:50  vsnyder
+! Make real argumemnts of ?DOT scalar
+!
 ! Revision 1.2  2001/11/03 00:56:36  pwagner
 ! Added Copyright statement
 !
--- blas/dot_intrinsic.f90
+++ blas/dot_intrinsic.f90
@@ -1,4 +1,4 @@
-! Copyright (c) 2001, California Institute of Technology.  ALL RIGHTS RESERVED.
+! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
 module Dot_M
@@ -15,7 +15,7 @@ module Dot_M
 !---------------------------------------------------------------------------
 
   interface DOT
-    module procedure DDOT, SDOT
+    module procedure DDOT, SDOT, SDDOT, DSDOT
   end interface
 
 contains
@@ -32,6 +32,33 @@ contains
     sdot = dot_product(x(1:1+(n-1)*incx:incx), y(1:1+(n-1)*incy:incy))
   end function SDOT
 
+  double precision function SDDOT ( N, X, INCX, Y, INCY )
+    integer, intent(in) :: N, INCX, INCY
+    real, intent(in) :: X(*)
+    double precision, intent(in) :: Y(*)
+    double precision, dimension(:), pointer :: dbleX
+    if ( n <= 0 ) then
+      sddot = 0.d0
+    elseif ( n == 1 ) then
+      sddot = x(1) * y(1)
+    else
+      allocate(dbleX(n))
+      dbleX = x(1:1+(n-1)*incx:incx)
+      sddot = ddot(n, dbleX, 1, y, incy)
+      deallocate(dbleX)
+    endif
+  end function SDDOT
+
+  double precision function DSDOT ( N, X, INCX, Y, INCY )
+    integer, intent(in) :: N, INCX, INCY
+    double precision, intent(in) :: X(*)
+    real, intent(in) :: Y(*)
+    dsdot = sddot( n, y, incy, x, incx)
+  end function DSDOT
+
 end module Dot_M
 
 ! $Log$
+! Revision 1.2  2001/11/03 00:56:36  pwagner
+! Added Copyright statement
+!
