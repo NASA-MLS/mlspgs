bbcc7056ff391dcc1ff3f5f4a5009a797618e944
livesey <>
2002-09-26 16:27:14 +0000

    rev 2.5

--- lib/Geometry.f90
+++ lib/Geometry.f90
@@ -6,7 +6,7 @@ module Geometry
   ! This module contains some geometry routines common to the forward model and
   ! the scan model.
 
-  use MLSCommon, only: r8
+  use MLSCommon, only: R8, RP
 
   implicit none
   private
@@ -18,24 +18,34 @@ module Geometry
   !---------------------------------------------------------------------------
 
 
-  public :: EarthRadA, EarthRadB, PI, LN10, GeodToGeocLat
+  public :: EarthRadA, EarthRadB, GM, J2, J4, W, PI, LN10, GeodToGeocLat
 
   ! Now parameters etc
 
-  real (r8), parameter :: earthRadA=6378137.0_r8 ! Major axis radius in m
-  real (r8), parameter :: earthRadB=6356752.3141_r8 ! Minor axis radius in m
+  real(r8), parameter :: EarthRadA=6378137.0_r8    ! Major axis radius in m 
+  real(r8), parameter :: EarthRadB=6356752.3141_r8 ! Minor axis radius in m 
 
-  ! Just in case this constant isn't defined everywhere else.
+  ! These are the 1980 reference geoid values.
+
+  real(rp), parameter :: GM = 3.98600436e14_rp ! m^3/sec^2
+  real(rp), parameter :: J2 = 0.0010826256_rp
+  real(rp), parameter :: J4 = -.0000023709122_rp
+
+  ! earth rotational velocity
+
+  real(rp), parameter :: W = 7.292115e-05_rp ! rad/sec
+
+  ! Just in case these constants aren't defined everywhere else.
   
-  real (r8), parameter :: PI=3.1415926535897931159979635_r8
-  real (r8), parameter :: LN10=2.302585124969482421875_r8
+  real(r8), parameter :: PI=3.14159265358979323846264338328_r8   
+  real(r8), parameter :: LN10=2.30258509299404568401799145468_r8 
 
 contains ! ------------------------------- Subroutines and functions ----
 
   ! This function converts a geodetic latitude (IN DEGREES!) into a geocentric
   ! one (IN RADIANS!)
   
-  real(r8) ELEMENTAL function GeodToGeocLat(geodLat)
+  real(r8) elemental function GeodToGeocLat(geodLat)
 
     ! Arguments
     real (r8), intent(IN) :: geodLat
@@ -57,6 +67,9 @@ contains ! ------------------------------- Subroutines and functions ----
 end module Geometry
 
 ! $Log$
+! Revision 2.5  2002/09/26 16:27:14  livesey
+! Changes from Van, new constants etc.
+!
 ! Revision 2.4  2001/03/28 19:50:19  vsnyder
 ! Change constants from d0 to _r8
 !
