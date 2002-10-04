32ebac75449a3b4e396e6428c13109b75d34e79f
vsnyder <>
2002-10-04 01:24:44 +0000

    Move stuff from module scopt to procedure scope, cosmetic changes

--- fwdmdl/d_hunt_m.f90
+++ fwdmdl/d_hunt_m.f90
@@ -2,25 +2,29 @@
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
 module D_HUNT_M
-  use MLSCommon, only: I4, R8
   implicit NONE
   private
   public :: D_HUNT, HUNT
   interface HUNT; module procedure D_HUNT; end interface
 !---------------------------- RCS Ident Info -------------------------------
-  CHARACTER (LEN=256) :: Id = &
-       "$Id$"
-  CHARACTER (LEN=*), PARAMETER :: ModuleName= "$RCSfile$"
+  character (len=*), parameter :: IdParm = &
+    & "$Id$"
+  character (len=len(idParm)) :: Id = idParm
+  character (len=*), parameter :: ModuleName = "$RCSfile$"
 !---------------------------------------------------------------------------
-  integer, private, parameter :: RK = r8
 contains
 ! A binary search routine with a hunt procedure, to start from last known
 ! location (if 0 < JLO < N) or from the begining otherwise.
   subroutine D_HUNT ( ELEMENT, ARRAY, N, JLO, JHI )
+    use MLSCommon, only: I4, R8
+    integer, parameter :: RK = r8
     include 'hunt.f9h'
   end subroutine D_HUNT
 end module D_HUNT_M
 ! $Log$
+! Revision 2.0  2001/09/17 20:26:26  livesey
+! New forward model
+!
 ! Revision 1.5  2001/06/07 23:30:34  pwagner
 ! Added Copyright statement
 !
