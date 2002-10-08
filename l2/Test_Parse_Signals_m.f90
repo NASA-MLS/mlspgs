321605e0977cf694fbd57229e86e8ccb885e77bb
pwagner <>
2002-10-08 17:36:45 +0000

    rev 2.8

--- l2/Test_Parse_Signals_m.f90
+++ l2/Test_Parse_Signals_m.f90
@@ -26,6 +26,7 @@ module Test_Parse_Signals_m
   character (len=len(idParm)) :: Id = idParm
   character (len=*), parameter :: ModuleName= &
     & "$RCSfile$"
+  private :: not_used_here 
   !---------------------------------------------------------------------------
 
 contains
@@ -57,9 +58,16 @@ contains
     end do
 99  return
   end subroutine Test_Parse_Signals
+  logical function not_used_here()
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
+
 end module Test_Parse_Signals_m
 
 ! $Log$
+! Revision 2.8  2002/10/08 17:36:23  pwagner
+! Added idents to survive zealous Lahey optimizer
+!
 ! Revision 2.7  2002/03/12 23:43:26  vsnyder
 ! Added IMPLICIT NONE and PRIVATE statements
 !
