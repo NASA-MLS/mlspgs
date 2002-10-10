10d82be70fd612a969e8825e55b0d03e8bb229e5
vsnyder <>
2002-10-10 19:49:04 +0000

    Move USE statements from module scope to procedure scope.  Remove an unnecessary one.  Get rid of some array temps.  Cosmetic changes.

--- fwdmdl/d_t_script_dtnp_m.f90
+++ fwdmdl/d_t_script_dtnp_m.f90
@@ -1,80 +1,92 @@
 module D_T_SCRIPT_DTNP_M
-  use D_STAT_TEMP_M, only: STAT_TEMP
-  use MLSCommon, only: R8, RP, IP
-  use PHYSICS, only: H_O_K => h_over_k
+
   implicit NONE
   private
   public :: DT_SCRIPT_DT
-!
+
 !---------------------------- RCS Ident Info -------------------------------
-  CHARACTER (LEN=256) :: Id = &
-   "$Id$"
-  CHARACTER (LEN=*), PARAMETER :: ModuleName= &
-   "$RCSfile$"
-  private :: not_used_here 
+  character (len=*), parameter :: IdParm = &
+    &  "$Id$"
+  character (len=len(idParm)) :: Id = idParm
+  character (len=*), parameter :: ModuleName = &
+    & "$RCSfile$"
 !---------------------------------------------------------------------------
 contains
-!----------------------------------------------------------------------
-! This routine builds the derivative of the t_script array w.r.t to Tnp
+!-----------------------------------------------------------------------
+! Build the derivative of the t_script array w.r.t to Tnp
 ! (The 'n' zeta coeff. and the 'p' phi coefficient)
 
-  SUBROUTINE dt_script_dt(t_path,eta_zxp,nu,dt_scr_dt)
-!
+  ! -----------------------------------------------  DT_SCRIPT_DT  -----
+  subroutine DT_SCRIPT_DT ( T_PATH, ETA_ZXP, NU, DT_SCR_DT )
+
 ! Let h_o_k = h / k = (Planck's constant) / (Boltzmann's constant)
 ! Let hxf = h_o_k * f  (f = Frq.)
 ! Let B = stat_temp(T,f) = hxf / (exp(hxf/T) - 1.0)
 ! Then: dB/dT = ((B/T)**2)*exp(hxf/T)
 ! And : dB/dTnp = dB/dT * Etan * Etap
 ! Where: Etan = Eta(n,zeta), Etap = Eta(p,phi)
-!
+
+    use MLSCommon, only: R8, RP, IP
+    use PHYSICS, only: H_O_K => h_over_k
+
 ! inputs
-!
-  REAL(rp), INTENT(in) :: t_path(:) ! path temperatures K
-  REAL(rp), INTENT(in) :: eta_zxp(:,:) ! path eta functions
-!  LOGICAL, INTENT(in) :: not_zero(:,:) ! where eta is not zero
-  REAL(r8) :: nu ! calculation frequency (MHz)
-!
+
+    real(rp), intent(in) :: t_path(:)     ! path temperatures K
+    real(rp), intent(in) :: eta_zxp(:,:)  ! path eta functions
+!    logical, intent(in) :: not_zero(:,:) ! where eta is not zero
+    real(r8) :: nu                        ! calculation frequency (MHz)
+
 ! output
-!
-  REAL(rp), INTENT(out) :: dt_scr_dt(:,:) ! path dt_script temparature
+
+    real(rp), intent(out) :: dt_scr_dt(:,:) ! path dt_script temparature
 !                                           derivative
 ! internals
-!
-  INTEGER(ip) :: n_path, n_sv, sv_i
 
-  REAL(rp), ALLOCATABLE :: dstdt(:)
-  REAL(rp) :: a, b
-!
-! I will do this inefficiently for now because it is quick and easy
-!
-  n_path = SIZE(t_path)
-  n_sv = SIZE(eta_zxp,DIM=2)
-!
-  ALLOCATE(dstdt(1:n_path))
-!
-  dstdt = EXP(h_o_k * nu / t_path)
-  dstdt = dstdt * (h_o_k * nu / (t_path * (dstdt - 1.0_rp)))**2
-!
+    integer(ip) :: n_path, n_sv, sv_i
+
+    real(rp) :: dstdt(1:size(t_path))
+    real(rp) :: a, b
+
+! Do this inefficiently for now because it is quick and easy
+
+    n_path = size(t_path)
+    n_sv = size(eta_zxp,dim=2)
+
+! Avoid some array temps by not using whole-array operations
+
+!{ Compute $\frac{\text{d} B}{\text{d} T} =
+!   \exp \left ( \frac{h \nu}{k T} \right )
+!   \left ( \frac{h \nu}
+!     {k T \left [ \exp \left ( \frac{h \nu}{k T} \right ) - 1 \right ]} \right ) ^2$
+
+    do sv_i = 1, n_path
+      a = h_o_k * nu / t_path(sv_i)        !{ \frac{h \nu}{k T}
+      b = exp(a)                           !{ \exp \left ( \frac{h \nu}{k T} \right )
+      dstdt(sv_i) = b * (a/(b-1.0_rp))**2  !{ \frac{\text{d} B}{\text{d} T}
+    end do
+
 ! This part Zvi will hate
-!
-  DO sv_i = 1 , n_sv
-    a = -dstdt(1) * eta_zxp(1,sv_i)
-    b = -dstdt(n_path) * eta_zxp(n_path,sv_i)
-    dt_scr_dt(:,sv_i) = 0.5_rp * (  &
-        &     EOSHIFT(dstdt * eta_zxp(:,sv_i), 1, b) -   &
-        &     EOSHIFT(dstdt * eta_zxp(:,sv_i),-1, a) )
-  ENDDO
-!
-  DEALLOCATE(dstdt)
-!
-  END SUBROUTINE dt_script_dt
-!
-  logical function not_used_here()
-    not_used_here = (id(1:1) == ModuleName(1:1))
-  end function not_used_here
 
-END module D_T_SCRIPT_DTNP_M
+    do sv_i = 1 , n_sv
+      a = -dstdt(1) * eta_zxp(1,sv_i)
+      b = -dstdt(n_path) * eta_zxp(n_path,sv_i)
+      dt_scr_dt(:,sv_i) = 0.5_rp * (  &
+          &     eoshift(dstdt * eta_zxp(:,sv_i), 1, b) -   &
+          &     eoshift(dstdt * eta_zxp(:,sv_i),-1, a) )
+    end do
+
+  end subroutine DT_SCRIPT_DT
+
+  ! ----------------------------------------------  NOT_USED_HERE  -----
+  logical function NOT_USED_HERE()
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function NOT_USED_HERE
+
+end module D_T_SCRIPT_DTNP_M
 ! $Log$
+! Revision 2.1  2002/10/08 17:08:02  pwagner
+! Added idents to survive zealous Lahey optimizer
+!
 ! Revision 2.0  2001/09/17 20:26:26  livesey
 ! New forward model
 !
