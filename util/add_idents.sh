3e1fcf5fcae57311357c9517ec476718b3b422d6
pwagner <>
2002-10-11 19:37:42 +0000

    Added use(2): print toc/api sections from source files

--- util/add_idents.sh
+++ util/add_idents.sh
@@ -6,6 +6,7 @@
 
 # "$Id$"
 # --------------- add_idents.sh help
+#Use(1) (default)
 #add idents to list of files having $id and $RCS lines
 #List compiled from args on command-line
 #or else automatically computed to match pattern
@@ -20,10 +21,19 @@
 #end module L1BData
 #(where the added lines have been marked with the ">")
 #
+#Use(2)
+#Print toc or api from list of files
+#List compiled from args on command-line
+#or else automatically computed to match pattern
+#  *{$suffix} in supplied directory names
 #Usage:
 #add_idents.sh [opt1 [arg1]] [opt2 [arg2]] .. [optm [argm]] [filelist]
 #
 #    O p t i o n s
+# -api          use(2) printing api
+# -toc          use(2) printing toc
+# -id           with use(2), printing $id line, too
+# -rcs          with use(2), printing $rcs line, too
 # -d path       path of directory if automatic
 # -suf suffix   file name suffix if automatic; e.g. ".f90"
 # -h[elp]       print brief help message; exit
@@ -31,7 +41,57 @@
 #(1)If option -suf present but -d absent, will search current working directory
 #(2)The options -suf and -d may be repeated; e.g.
 #   add_idents -d dir1 -d dir2 .. -suf sufx1 -suf sufx2 ..
+#(3)The default use is Use(1); -api and -toc are mutually exclusive
 # --------------- End add_idents.sh help
+#Another example of my regrettable tendency to bundle a number of
+#different functionalities within a single multi-use file
+#Isn't it cleaner to devote a single file to a single use?
+
+#------------------------------- add_the_lines ------------
+#
+# Function to add the lines needed to assure that after compilation
+# a file's $id and $RCS idents will be found in the executable
+# Relies upon a trick to circumvent Lahey's zealous optimizer
+# usage: add_the_lines file
+
+add_the_lines()
+{
+file="$1"
+test=`grep -i 'not_used_here' $file`                     
+if [ "$test" = "" ]                                      
+then                                                     
+# > >     test1=`grep -i 'END MODULE' $file`             
+# > >     if [ "$test1" != "" ]                          
+# > >     then                                           
+# > >       sed 's/END MODULE/end module/' $file > temp  
+# > >       mv temp $file                                
+# > >     fi                                             
+  test1=`grep -i '^ *contains' $file`                    
+  if [ "$test1" = "" ]                                   
+  then                                                   
+    sed '/\$RCSfile/ a\
+  private \:\: not_used_here ' $file | \
+      sed '/[eE][nN][dD] [mM][oO][dD][uU][lL][eE]/ i\
+contains \
+  logical function not_used_here()\
+    not_used_here = (id(1:1) == ModuleName(1:1))\
+  end function not_used_here\
+'     > temp
+  else
+    sed '/\$RCSfile/ a\
+  private \:\: not_used_here ' $file | \
+      sed '/[eE][nN][dD] [mM][oO][dD][uU][lL][eE]/ i\
+  logical function not_used_here()\
+    not_used_here = (id(1:1) == ModuleName(1:1))\
+  end function not_used_here\
+'     > temp
+  fi                                                
+  mv temp $file                                     
+else                                                
+  echo "$file already has ident added in this way"  
+fi                                                  
+}
+
 #------------------------------- extant_files ------------
 #
 # Function to return only those files among the args
@@ -58,6 +118,26 @@ extant_files()
    fi
 }
 
+#------------------------------- print_the_lines ------------
+#
+# Function to print the lines from a file bookended by e.g. where section="toc"
+#'=== (start of toc) ==='
+# and
+#'=== (end of toc) ==='
+# 
+# usage: print_the_lines section file
+
+print_the_lines()
+{
+section="$1"
+file="$2"
+#echo "Trying to print lines in $file, section $section"
+#echo '/=== (start of '$section') ===/,/=== (end of '$section') ===/ p' $file
+sed -n \
+  '/=== (start of '$section') ===/,/=== (end of '$section') ===/ p' $file \
+           | sed -n 's/^.//p' | sed '1 d; $ d'
+}
+
 #------------------------------- Main Program ------------
 
 #****************************************************************
@@ -69,11 +149,16 @@ extant_files()
 #****************************************************************
 id_list=
 RCS_list=
-echo "Who or what is $@"
+# echo "All the args: $@"
 suffixes=""
 directories=""
 me="$0"
 my_name=add_idents.sh
+# The following takes possible values "add", "api", "toc"
+# the default is "add"
+my_use="add"
+print_id="no"
+print_rcs="no"
 #
 # Get arguments from command line
 #
@@ -90,6 +175,22 @@ while [ "$more_opts" = "yes" ] ; do
 	    suffixes="$suffixes $2"
 	    shift
        shift
+	;;
+	-api )
+	    my_use="api"
+	    shift
+	;;
+	-toc )
+	    my_use="toc"
+	    shift
+	;;
+	-id )
+	    print_id="yes"
+	    shift
+	;;
+	-rcs )
+	    print_rcs="yes"
+	    shift
 	;;
     -h | -help )
        sed -n '/'$my_name' help/,/End '$my_name' help/ p' $me \
@@ -129,42 +230,36 @@ then
   done
 fi
 rm -f temp
-for file in $arglist
+for the_file in $arglist
 do
-  echo file is $file                   
-  test=`grep -i 'not_used_here' $file` 
-  if [ "$test" = "" ]
+  if [ "$my_use" = "add" ]
   then
-# > >     test1=`grep -i 'END MODULE' $file`
-# > >     if [ "$test1" != "" ]
-# > >     then
-# > >       sed 's/END MODULE/end module/' $file > temp
-# > >       mv temp $file
-# > >     fi
-    test1=`grep -i '^ *contains' $file`
-    if [ "$test1" = "" ]
-    then
-      sed '/\$RCSfile/ a\
-  private \:\: not_used_here ' $file | \
-      sed '/[eE][nN][dD] [mM][oO][dD][uU][lL][eE]/ i\
-contains \
-  logical function not_used_here()\
-    not_used_here = (id(1:1) == ModuleName(1:1))\
-  end function not_used_here\
-'     > temp
-    else
-      sed '/\$RCSfile/ a\
-  private \:\: not_used_here ' $file | \
-      sed '/[eE][nN][dD] [mM][oO][dD][uU][lL][eE]/ i\
-  logical function not_used_here()\
-    not_used_here = (id(1:1) == ModuleName(1:1))\
-  end function not_used_here\
-'     > temp
-    fi
-    mv temp $file
+    echo Adding idents to $the_file
   else
-    echo "$file already has ident added in this way"
+    echo "$my_use for $the_file"
   fi
+  if [ "$print_id" = "yes" ]
+  then
+    grep -i '\$id' $the_file
+  fi
+  if [ "$print_rcs" = "yes" ]
+  then
+    grep -i '\$rcs' $the_file
+  fi
+  case "$my_use" in
+    add)
+      add_the_lines $the_file
+      ;;
+    api | toc)
+      print_the_lines "$my_use" $the_file
+      ;;
+    *)
+      echo "Programming error in add_idents.sh"
+      ;;
+  esac
 done
 exit 0
 # $Log$
+# Revision 1.1  2002/10/08 16:57:33  pwagner
+# First commit
+#
