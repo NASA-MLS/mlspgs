0c22faa395800fd33b059711f4334b8b56724c30
jonathan <>
2002-10-30 00:53:36 +0000

    new water vapor formula

--- cloudfwdm/WaterVapor.f90
+++ cloudfwdm/WaterVapor.f90
@@ -27,12 +27,23 @@ contains
 !         t,td ---- K
 !         RH --- %
 !         SD --- g/m3                 
-          REAL(r8) :: es, t, RH, EV
+          REAL(r8) :: es, t, RH, EV, isat, t0
           !... relative to water
           !         ES=6.10779*EXP(17.28*(t-273.15)/(t-36.))
           !... relative to ice
 
-	  es=10**(-2667./t+10.555)
+	t0 = 273.16_r8
+	isat = -9.09718*(t0/t-1.0) + 0.78583503 - 3.56654*ALOG10(t0/t) &
+          &     		+ 0.876793 * (1.0-t/t0)
+
+        es=10**isat
+
+!	  es=10**(-2667./t+10.555)
+
+!        print*, es
+!        print*, ' '
+!        print, 10**(-2667./t+10.555)
+
           if (ES.lt.0._r8) ES=0._r8
           EV=ES*0.01*RH
           
@@ -44,6 +55,9 @@ contains
 end module WaterVapor
 
 ! $Log$
+! Revision 1.3  2002/10/08 17:08:08  pwagner
+! Added idents to survive zealous Lahey optimizer
+!
 ! Revision 1.2  2001/09/21 15:51:38  jonathan
 ! modified F95 version
 !
