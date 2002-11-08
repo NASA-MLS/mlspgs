da8a049d0fb09e44541aad681348c1c4022d5bd4
livesey <>
2002-11-08 19:07:16 +0000

    First version

---
+++ util/killmaster.c
@@ -0,0 +1,16 @@
+#include "/usr/share/pvm3/include/pvm3.h"
+
+/* Compile me with:
+cc -o killmaster killmaster.c -L/usr/share/pvm3/lib/LINUX -lpvm3
+*/
+
+main ( int argc, void *argv[] )
+{
+  int tid;
+  int bufID;
+  int info;
+
+  tid = atoi ( argv[1] );
+  bufID = pvm_initsend ( PvmDataDefault );
+  info = pvm_send ( tid, 999 );
+}
