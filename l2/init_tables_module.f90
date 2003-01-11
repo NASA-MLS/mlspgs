2d94dae9392884c8468bcb93c102a48c2d6c7d62
livesey <>
2003-01-11 01:02:27 +0000

    rev 2.268

--- l2/init_tables_module.f90
+++ l2/init_tables_module.f90
@@ -731,6 +731,8 @@ contains ! =====     Public procedures     =============================
              begin, f+f_height, t+t_numeric_range, n+n_field_type, &
              begin, f+f_ignore, t+t_boolean, n+n_field_type, &
              begin, f+f_mask, t+t_masks, n+n_field_type, &
+             begin, f+f_maxValue, t+t_numeric, n+n_field_type, &
+             begin, f+f_minValue, t+t_numeric, n+n_field_type, &
              begin, f+f_opticalDepth, s+s_vector, f+f_template, f+f_quantities, &
                     n+n_dot, &
              begin, f+f_opticalDepthCutoff, t+t_numeric, n+n_field_type, &
@@ -934,6 +936,9 @@ contains ! =====     Public procedures     =============================
 end module INIT_TABLES_MODULE
 
 ! $Log$
+! Revision 2.268  2003/01/11 01:02:27  livesey
+! Added max and min value in subset
+!
 ! Revision 2.267  2003/01/11 00:01:19  dwu
 ! add flagCloud
 !
