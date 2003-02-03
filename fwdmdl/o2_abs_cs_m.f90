53f1c5136145ac465dca46676d2f8a38075e23a7
vsnyder <>
2003-02-03 22:55:26 +0000

    rev 2.1

---
+++ fwdmdl/o2_abs_cs_m.f90
@@ -0,0 +1,451 @@
+! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
+module O2_Abs_CS_M
+
+  implicit NONE
+  private
+  public :: O2_Abs_CS, Get_QN, Get_QN_By_Frequency
+
+  integer :: O2_in_catalog = -1   ! Index in Spectroscopy catalog of O2 line.
+
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+    &  "$Id$"
+  character (len=len(idParm)) :: Id = idParm
+  character (len=*), parameter :: ModuleName= &
+    &  "$RCSfile$"
+  private :: not_used_here
+!---------------------------------------------------------------------------
+
+contains
+
+! ----------------------------------------------------  O2_Abs_CS  -----
+  subroutine O2_Abs_CS ( freq, qn, h, slabs_0, slabs_nonres, y_nonres, &
+    &                    sigma_p, pi, sigma_m )
+
+! Compute the complex absorption cross section.
+! Modified to use Voigt with interfered lineshape
+
+    use L2PC_PFA_STRUCTURES, only: SLABS_STRUCT
+    use MLSCommon, only: IP, R8, Rk => RP
+    use SLabs_SW_M, only: Simple_Voigt
+    use Units, only: SqrtPi
+
+    real(r8), intent(in) :: Freq              ! Observation frequency
+    integer(ip), intent(in) :: qn(:)          ! Quantum numbers
+    real(rk), intent(in) :: H                 ! Magnetic field
+    type(slabs_struct), intent(in) :: slabs_0 ! contains, among others:
+!    v0s(:)         ! pressure shifted line centers
+!    x1(:)          ! Doppler width
+!    y(:)           ! ratio Pressure to Doppler widths
+!    yi(:)          ! Interference coefficients
+!    slabs1(:)      ! strengths
+    real(rk), intent(in) :: slabs_nonres      ! Nonresonant absorption
+    real(rk), intent(in) :: y_nonres          ! Nonresonant ratio
+    complex(rk), intent(out) :: sigma_p       ! Output Beta values
+    complex(rk), intent(out) :: pi
+    complex(rk), intent(out) :: sigma_m
+
+    integer(ip) :: i, j, no_lines
+
+    real(rk) :: f_o_v0, x, z, u, v, denomm
+
+    complex(rk) :: wing
+
+    no_lines = size(slabs_0%v0s)
+
+! Find index of nearest line that line will be treated as if it were
+! magnetically split regardless of field and pressure
+
+    j = 1
+    x = abs(slabs_0%v0s(1) - freq)
+    do i = 2 , no_lines
+      z = abs(slabs_0%v0s(i) - freq)
+      if ( z < x ) then
+        x = z
+        j = i
+      end if
+    end do
+
+! Do magnetic calculation even if h = 0
+! The nearest line has been found so continue with cross section
+! calculation
+
+    call mag_o2_abs_cs ( qn(j), freq, slabs_0%x1(j), slabs_0%slabs1(j),  &
+      &                  slabs_0%y(j), slabs_0%yi(j), h, slabs_0%v0s(j), &
+      &                  sigma_p, pi, sigma_m )
+
+! Add contribution from nearby lines excluding the already computed
+! magnetic line
+
+! Fill in negative frequency part of VVW lineshape for jth line
+
+    f_o_v0 = freq / slabs_0%v0s(j)
+    z = slabs_0%x1(j) * (slabs_0%v0s(j) + freq)
+    denomm = sqrtPi * (z*z + slabs_0%y(j)*slabs_0%y(j))
+    wing = ( 0.5_rk * slabs_0%slabs1(j) * f_o_v0 ) * cmplx( &
+      & f_o_v0 * (slabs_0%y(j) - slabs_0%yi(j)*z) / denomm, & ! Real part
+      & (z + slabs_0%y(j) * (slabs_0%y(j)/slabs_0%x1(j) - &  ! Imaginary part...
+      &   freq*slabs_0%yi(j)) / slabs_0%v0s(j)) / denomm - &
+      &   2.0_rk/(slabs_0%x1(j) * slabs_0%v0s(j)) &
+      & )
+
+! Now for the other lines (do the same thing on both sides of j).
+
+    do i = 1 , j - 1
+      f_o_v0 = freq / slabs_0%v0s(i)
+      x = slabs_0%x1(i) * (slabs_0%v0s(i) - freq)
+      call simple_voigt ( x, slabs_0%y(i), u, v )
+      z = slabs_0%x1(i) * (slabs_0%v0s(i) + freq)
+      denomm = sqrtPi * (z*z + slabs_0%y(i)*slabs_0%y(i))
+      wing = wing + ( 0.5_rk * slabs_0%slabs1(i) * f_o_v0 ) * cmplx( &
+        & f_o_v0 * (u - slabs_0%yi(i)*v + (slabs_0%y(i) - slabs_0%yi(i)*z) / denomm), & ! Real
+        & u * (slabs_0%y(i)/slabs_0%x1(i) + freq*slabs_0%yi(i)) / slabs_0%v0s(i) + v + & ! Imag...
+        &  (z + slabs_0%y(i) * (slabs_0%y(i) / slabs_0%x1(i) - freq*slabs_0%yi(i)) / &
+        &   slabs_0%v0s(i)) / denomm -                                               &
+        &   2.0_rk / (slabs_0%x1(i) * slabs_0%v0s(i))                                &
+        & )
+    end do
+
+    do i = j + 1 , no_lines
+      f_o_v0 = freq / slabs_0%v0s(i)
+      x = slabs_0%x1(i) * (slabs_0%v0s(i) - freq)
+      call simple_voigt ( x, slabs_0%y(i), u, v )
+      z = slabs_0%x1(i) * (slabs_0%v0s(i) + freq)
+      denomm = sqrtPi * (z*z + slabs_0%y(i)*slabs_0%y(i))
+      wing = wing + ( 0.5_rk * slabs_0%slabs1(i) * f_o_v0 ) * cmplx( &
+        & f_o_v0 * (u - slabs_0%yi(i)*v + (slabs_0%y(i) - slabs_0%yi(i)*z) / denomm), & ! Real
+        & u * (slabs_0%y(i)/slabs_0%x1(i) + freq*slabs_0%yi(i)) / slabs_0%v0s(i) + v + & ! Imag...
+        &  (z + slabs_0%y(i) * (slabs_0%y(i) / slabs_0%x1(i) - freq*slabs_0%yi(i)) / &
+        &   slabs_0%v0s(i)) / denomm -                                               &
+        &   2.0_rk / (slabs_0%x1(i) * slabs_0%v0s(i))                                &
+        & )
+    end do
+
+! Now add non resonant contribution:
+
+    u = freq * freq
+    v = y_nonres * y_nonres
+    wing = cmplx( real(wing) + 0.5_rk * slabs_nonres * u * y_nonres / (u + v), &
+      &          aimag(wing) )
+
+    pi = pi + wing
+    sigma_p = sigma_p + 0.5_rk * wing
+    sigma_m = sigma_m + 0.5_rk * wing
+
+  end subroutine O2_Abs_CS
+
+! ------------------------------------------------  Mag_O2_Abs_CS  -----
+  subroutine Mag_O2_Abs_CS ( n, nu, x1, s, w, y, h, v0, sigma_p, pi, sigma_m )
+
+! Compute the frequency dependent absorption cross section for magnetic o2.
+
+! Other notes:
+! Document refers to "MLS Spectroscopic Data Base" W. G. Read, Version 1.0
+! October 19, 1990.
+
+    use MLSCommon, only: IP, R8, Rk => RP
+    use SLabs_SW_M, only: Simple_Voigt
+
+    integer, intent(in) :: N    ! rotational quantum number, sign indicates delta J
+    real(r8), intent(in) :: Nu  ! transmission frequency in MHz
+    real(rk), intent(in) :: X1  ! Doppler width factor sqrt(ln 2) / D_width
+    real(rk), intent(in) :: S   ! strength factor slabs1 from slabs routine
+    real(rk), intent(in) :: W   ! collision to doppler width ratio in the
+                                ! document corrected for temperature and pressure
+    real(rk), intent(in) :: Y   ! interference coefficient in the document
+                                ! corrected for pressure and temperature
+    real(rk), intent(in) :: H   ! magnetic field in Gauss
+    real(rk), intent(in) :: V0  ! zero magnetic field line position
+
+    complex(rk), intent(out) :: Sigma_P ! absorption coefficient at unity mixing
+                                ! ratio for Delta M = +1
+    complex(rk), intent(out) :: Pi ! absorption coefficient at unity mixing
+                                ! ratio for Delta M = 0
+    complex(rk), intent(out) :: Sigma_M ! absorption coefficient at unity mixing
+                                ! ratio for Delta M = -1
+    integer(ip) :: m
+
+    real(rk) :: nu_offst, del_nu, xi, denom1, denom2, z, f_o_v0, u, v, zr, zi
+
+! Compute the absorption coefficient at unity mixing ratio for N transition
+
+    pi      = 0.0
+    sigma_p = 0.0
+    sigma_m = 0.0
+
+    if ( n == 0 ) return
+
+    f_o_v0 = nu / v0
+    del_nu = v0 - nu
+
+    if ( n > 0 ) then
+
+! Delta J = +1
+
+      denom1 = n * (n + 1)
+      denom2 = (n + 1) * (2 * n +1) * (2 * n + 3)
+
+      do m = -n , n
+
+! pi transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*m*(1-n)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * ((n + 1) * (n + 1) - m * m) / denom2
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w / (x1*nu) + y))
+        pi = pi + cmplx(zr, zi)
+
+! sigma_p transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*(m*(1-n)-n)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n + m + 1) * (n + m + 2) / (4.0_rk * denom2)
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y * v)
+        zi = z * (v + u * f_o_v0 * (w/(x1 * nu) + y))
+        sigma_p = sigma_p + cmplx(zr, zi)
+
+! sigma_m transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*(m*(1-n)+n)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n - m + 1) * (n - m + 2) / (4.0_rk * denom2)
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y * v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        sigma_m = sigma_m + cmplx(zr, zi)
+
+      end do
+
+    else
+
+! Delta J = -1 n is a negative number
+
+      denom1 =  n * (n - 1)
+      denom2 = -n * (4 * n * n - 1)
+
+      do m = n+2 , -(n+2)
+
+! pi transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*m*(2-n)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n * n - m * m) / denom2
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        pi = pi + cmplx(zr, zi)
+
+! sigma_p transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*((m+1)*(2-n)-1)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n + m) * (n + m + 1) / (4.0_rk * denom2)
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        sigma_p = sigma_p + cmplx(zr, zi)
+
+! sigma_m transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*((m-1)*(2-n)+1)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (m - n) * (m - n - 1) / (4.0_rk * denom2)
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        sigma_m = sigma_m + cmplx(zr, zi)
+
+      end do
+
+! Finish off special transition cases for pi and sigmas
+! m = -n
+
+      m = n
+
+! sigma_p transition
+
+      nu_offst = x1*(del_nu + 2.803_rk*((m+1)*(2-n)-1)*h / denom1)
+      call simple_voigt ( nu_offst, w, u, v )
+      xi = 3.0_rk * (n + m) * (n + m + 1) / (4.0_rk * denom2)
+      z  = 0.5_rk * s * xi * f_o_v0
+      zr = z * f_o_v0 * (u - y*v)
+      zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+      sigma_p = sigma_p + cmplx(zr, zi)
+
+! m = n
+
+      m = -n
+
+! sigma_m transition
+
+      nu_offst = x1*(del_nu + 2.803_rk*((m-1)*(2-n)+1)*h / denom1)
+      call simple_voigt ( nu_offst, w, u, v )
+      xi = 3.0_rk * (m - n) * (m - n - 1) / (4.0_rk * denom2)
+      z  =  0.5_rk * s * xi * f_o_v0
+      zr = z * f_o_v0 * (u - y*v)
+      zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+      sigma_m = sigma_m + cmplx(zr, zi)
+
+      if ( n /= -1 ) then
+
+! m = -(n-1)
+
+        m = n + 1
+
+! pi transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*m*(2-n)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n * n - m * m) / denom2
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        pi = pi + cmplx(zr, zi)
+
+! sigma_p transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*((m+1)*(2-n)-1)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n + m) * (n + m + 1) / (4.0_rk * denom2)
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        sigma_p = sigma_p + cmplx(zr, zi)
+
+! m = n - 1
+
+        m = -(n + 1)
+
+! pi transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*m*(2-n)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n * n - m * m) / denom2
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        pi = pi + cmplx(zr, zi)
+
+! sigma_m transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*((m-1)*(2-n)+1)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (m - n) * (m - n - 1) / (4.0_rk * denom2)
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        sigma_m = sigma_m + cmplx(zr, zi)
+
+      else
+
+        m = n + 1
+
+! pi transition
+
+        nu_offst = x1*(del_nu + 2.803_rk*m*(2-n)*h / denom1)
+        call simple_voigt ( nu_offst, w, u, v )
+        xi = 3.0_rk * (n * n - m * m) / denom2
+        z  = 0.5_rk * s * xi * f_o_v0
+        zr = z * f_o_v0 * (u - y*v)
+        zi = z * (v + u * f_o_v0 * (w/(x1*nu) + y))
+        pi = pi + cmplx(zr, zi)
+
+      end if
+
+    end if
+
+  end subroutine Mag_O2_Abs_CS
+
+! ------------------------------------------------------  Find_O2  -----
+  subroutine Find_O2
+  ! Find the O2 in the spectroscopy catalog
+
+    use Molecules, only: SP_O2
+    use SpectroscopyCatalog_m, only: Catalog
+
+    do o2_in_catalog = 1, size(catalog)
+      if ( catalog(o2_in_catalog)%spec_tag == sp_o2 ) return
+    end do
+
+    o2_in_catalog = -1 ! Not found
+  end subroutine Find_O2
+
+! -------------------------------------------------------  Get_QN  -----
+  subroutine Get_QN ( V0, N )
+
+    use MLSCommon, only: IP, R8, Rk => RP
+    use O2_DBase, only: O2_Freq, O2_Data
+
+    real(r8), intent(in) :: V0
+    integer(ip), intent(out) :: N
+
+    integer(ip) :: I, J
+    real(rk) :: Q, R
+
+    j = 1
+    r = abs(v0-o2_freq(i))
+    do i = 2, size(o2_freq)
+      q = abs(v0-o2_freq(i))
+      if ( q < r ) then
+        r = q
+        j = i
+      end if
+    end do
+
+    n = nint(o2_data(1,j))           ! Quantum number
+
+  end subroutine Get_QN
+
+! ------------------------------------------  Get_QN_By_Frequency  -----
+  subroutine Get_QN_By_Frequency ( V0, N )
+
+    ! Get the quantum number for the O2 line that has a center frequency
+    ! nearest to V0.  The quantum number of interest is given by the
+    ! difference between the fourth and second numbers in the QN field
+    ! of the line specification of the spectroscopy database.
+
+    use MLSCommon, only: IP, R8, Rk => RP
+    use SpectroscopyCatalog_M, only: Catalog, Lines
+
+    real(r8), intent(in) :: V0
+    integer(ip), intent(out) :: N
+
+    integer(ip) :: I, J, K
+    real(rk) :: Q, R
+
+    ! Find O2 line the spectroscopy catalog
+    if ( o2_in_catalog < 0 ) call find_o2
+    n = -1
+    ! Find the O2 line that has a center frequency nearest to V0
+    j = catalog(o2_in_catalog)%lines(1)
+    r = abs(v0-lines(j)%v0)
+    do i = 2, size(catalog(o2_in_catalog)%lines)
+      k = catalog(o2_in_catalog)%lines(i)
+      q = abs(v0-lines(k)%v0)
+      if ( q < r ) then
+        r = q
+        j = k
+      end if
+    end do
+    if ( associated(lines(j)%qn) ) then
+      if ( size(lines(j)%qn) >= 4 ) n = lines(j)%qn(4) - lines(j)%qn(2)
+    end if
+  end subroutine Get_QN_By_Frequency
+
+
+! ------------------------------------------------  not_used_here  -----
+  logical function not_used_here()
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
+
+end module O2_Abs_CS_M
+
+! $Log$
+! Revision 2.1  2003/02/03 22:55:26  vsnyder
+! Initial commit
+!
