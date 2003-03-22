4bc5b22f62062feb12bd50c00629dbbf8fad2169
jdone <>
2003-03-22 02:45:34 +0000

    added HDFEOS2/HDFEOS5 functionality; HDFEOS2 is default

--- l3/MLSL3D.f90
+++ l3/MLSL3D.f90
@@ -5,138 +5,152 @@
 !==========================================
 PROGRAM MLSL3D ! MLS Level 3 Daily software
 !==========================================
+  
+  USE L2GPData, ONLY: L2GPData_T, DestroyL2GPDatabase
+  USE L2Interface, ONLY: ReadL2GPProd
+  USE L3CF, ONLY: L3CFDef_T, L3CFProd_T  
+  USE L3DMData, ONLY: L3DMData_T, DestroyL3DMDatabase
+  USE L3SPData, ONLY: L3SPData_T, DestroyL3SPDatabase
+  USE MLSCF, ONLY: Mlscf_T 
+  USE MLSCommon, ONLY: r8
+  USE MLSFiles, ONLY: HDFVERSION_5, HDFVERSION_4
+  USE MLSL3Common, ONLY: DATE_LEN, maxWindow
+  USE MLSMessageModule, ONLY: MLSMSG_Info, MLSMSG_Warning, MLSMessage, & 
+       & MLSMessageExit
+  USE OpenInit, ONLY: PCFData_T, OpenAndInitialize
+  USE OutputClose, ONLY: OutputFlags_T, OutputProd, OutputAndClose
+  USE Synoptic, ONLY: DailyCoreProcessing
+  
+  IMPLICIT NONE
+  
+  !------------------- RCS Ident Info -----------------------
+  CHARACTER(LEN=130) :: Id = &                                                 
+       "$Id$"
+  CHARACTER (LEN=*), PARAMETER :: ModuleName="$RCSfile$"
+  !----------------------------------------------------------
+  
+  ! Brief description of program
+  ! This is the MLS Level 3 Daily program.
+  
+  ! Parameters
+  
+  ! Functions
+  
+  ! Variables
+  
+  TYPE( Mlscf_T ) :: cf
+  TYPE( L3CFDef_T ) :: cfDef
+  TYPE( OutputFlags_T ) :: flags
+  TYPE( PCFData_T ) :: pcf
+  TYPE( L2GPData_T ), POINTER :: l2gp(:), l3r(:), residA(:), residD(:)
+  TYPE( L3CFProd_T ), POINTER :: cfProd(:)
+  TYPE( L3DMData_T ), POINTER :: l3dm(:), dmA(:), dmD(:)
+  TYPE( L3SPData_T ), POINTER :: l3sp(:)
+  
+  CHARACTER (LEN=480) :: msr
+  CHARACTER (LEN=DATE_LEN) :: mis_Days(maxWindow)
+  CHARACTER (LEN=1), POINTER :: anText(:) => NULL()
+  
+  REAL(r8), POINTER :: avgPer(:) => NULL()
+  
+  INTEGER :: i, l2Days, mis_l2Days, hdfVersion
+  
+  INTEGER, PARAMETER :: NORMAL_EXIT_STATUS = 2
+  
+  CALL MLSMessage (MLSMSG_Info, ModuleName, & 
+       & 'EOS MLS Level 3 data processing started')
+  
+  ! Fill structures with input data from the PCF and L3CF.
+  
+  CALL OpenAndInitialize(pcf, cf, cfProd, cfDef, anText, avgPer)
+  
+  ! Determine the necessary version for HDF output
+  
+  if (cfDef%hdfOutputVersionString .EQ. 'hdfeos5' ) then 
+     
+     hdfVersion = HDFVERSION_5
+     
+  else 
+     
+     hdfVersion = HDFVERSION_4
+     
+  endif
+  
+  ! For each product in the DailyMap section of the cf,
+  
+  DO i = 1, SIZE(cfProd)
+     
+     ! Read all the l2gp data which exist in the input window for that product
+     
+     CALL ReadL2GPProd(cfProd(i)%l3prodNameD, cfProd(i)%fileTemplate, &
+          & pcf%l2StartDay, pcf%l2EndDay, l2Days, mis_l2Days, mis_Days, l2gp)
+     
+     ! If no data , go on to the next product
 
-   USE L2GPData, ONLY: L2GPData_T, DestroyL2GPDatabase
-   USE L2Interface
-   USE L3CF
-   USE L3DMData
-   USE L3SPData
-   USE MLSCF
-   USE MLSL3Common
-   USE MLSMessageModule
-   USE OpenInit
-   USE OutputClose
-   USE Synoptic
+     IF (l2Days < cfDef%minDays ) THEN
+        msr = 'Insufficient data found for ' & 
+             & // TRIM(cfProd(i)%l3prodNameD) // &
+             & '.  Skipping CORE processing and moving on to the next product.'
+        CALL MLSMessage (MLSMSG_Warning, ModuleName, msr)
+        CYCLE
+     ELSE
+        msr = 'Input data successfully read for ' //  &
+             & TRIM(cfProd(i)%l3prodNameD) // '; CORE processing started ...'
+        CALL MLSMessage (MLSMSG_Info, ModuleName, msr)
+     ENDIF
+     
+     ! CORE processing
 
-   IMPLICIT NONE
+     CALL DailyCoreProcessing(cfDef, cfProd(i), pcf, l2Days, l2gp, avgPer, & 
+          & l3sp, l3dm, dmA, dmD, l3r, residA, residD, & 
+          & flags)
 
-!------------------- RCS Ident Info -----------------------
-   CHARACTER(LEN=130) :: Id = &                                                    
-   "$Id$"
-   CHARACTER (LEN=*), PARAMETER :: ModuleName="$RCSfile$"
-!----------------------------------------------------------
+     ! Check the output data and place them into the appropriate files. 
+     ! Write the l3dm metadata.  
+     ! Perform any deallocations needed within the product loop.
 
-! Brief description of program
-! This is the MLS Level 3 Daily program.
+     msr = 'CORE processing completed for ' // TRIM(cfProd(i)%l3prodNameD) &
+          & // '; starting Output task ...'
+     CALL MLSMessage (MLSMSG_Info, ModuleName, msr)
+     
+     CALL OutputProd(pcf, cfProd(i), anText, l3sp, l3dm, dmA, dmD, l3r, & 
+          & residA, residD, flags, hdfVersion)
+     
+     ! Deallocate the databases passed between CORE & the I/O shell
+     
+     CALL DestroyL2GPDatabase(l3r)
+     CALL DestroyL2GPDatabase(residA)
+     CALL DestroyL2GPDatabase(residD)
+     CALL DestroyL3DMDatabase(l3dm)
+     CALL DestroyL3DMDatabase(dmA)
+     CALL DestroyL3DMDatabase(dmD)
+     CALL DestroyL3SPDatabase(l3sp)
+     CALL DestroyL2GPDatabase(l2gp)
 
-! Parameters
+  ENDDO
 
-! Functions
+  msr = 'Product loop processing completed; beginning Output/Close task ... '
+  CALL MLSMessage (MLSMSG_Info, ModuleName, msr)
+  
+  ! Perform final Output & Close tasks outside of the product processing loop.
+  
+  CALL OutputAndClose(cf, pcf, cfProd, avgPer, anText)
+  
+  ! Deallocate memory and close
 
-! Variables
-
-   TYPE( Mlscf_T ) :: cf
-   TYPE( L3CFDef_T ) :: cfDef
-   TYPE( OutputFlags_T ) :: flags
-   TYPE( PCFData_T ) :: pcf
-   TYPE( L2GPData_T ), POINTER :: l2gp(:), l3r(:), residA(:), residD(:)
-   TYPE( L3CFProd_T ), POINTER :: cfProd(:)
-   TYPE( L3DMData_T ), POINTER :: l3dm(:), dmA(:), dmD(:)
-   TYPE( L3SPData_T ), POINTER :: l3sp(:)
-
-   CHARACTER (LEN=480) :: msr
-   CHARACTER (LEN=1), POINTER :: anText(:)
-   CHARACTER (LEN=DATE_LEN) :: mis_Days(maxWindow)
-
-   INTEGER :: i, l2Days, error
-   INTEGER :: mis_l2Days
-
-   REAL(r8), POINTER :: avgPer(:)
-   integer, parameter :: NORMAL_EXIT_STATUS = 2
-
-   call h5open_f(error)                              
-   if (error /= 0) then                              
-       call MLSMessage ( MLSMSG_Error, moduleName, & 
-         & "Unable to h5_open_f" )                   
-   endif                                             
-   CALL MLSMessage (MLSMSG_Info, ModuleName, 'EOS MLS Level 3 data processing started')
-
-! Fill structures with input data from the PCF and L3CF.
-
-   CALL OpenAndInitialize(pcf, cf, cfProd, cfDef, anText, avgPer)
-
-! For each product in the DailyMap section of the cf,
-
-   DO i = 1, SIZE(cfProd)
- 
-! Read all the l2gp data which exist in the input window for that product
-
-      CALL ReadL2GPProd(cfProd(i)%l3prodNameD, cfProd(i)%fileTemplate, &
-                        pcf%l2StartDay, pcf%l2EndDay, l2Days, mis_l2Days, mis_Days, l2gp)
-
-! If no data , go on to the next product
-
-      IF (l2Days < cfDef%minDays ) THEN
-         msr = 'Insufficient data found for ' // TRIM(cfProd(i)%l3prodNameD) // &
-               '.  Skipping CORE processing and moving on to the next product.'
-         CALL MLSMessage (MLSMSG_Warning, ModuleName, msr)
-         CYCLE
-      ELSE
-         msr = 'Input data successfully read for ' //  &
-                TRIM(cfProd(i)%l3prodNameD) // '; CORE processing started ...'
-         CALL MLSMessage (MLSMSG_Info, ModuleName, msr)
-      ENDIF
-
-! CORE processing
-
-      CALL DailyCoreProcessing(cfDef, cfProd(i), pcf, l2Days, l2gp, avgPer, l3sp, &
-                               l3dm, dmA, dmD, l3r, residA, residD, mis_l2Days, mis_Days, flags)
-
-! Check the output data and place them into the appropriate files.  Write the
-! l3dm metadata.  Perform any deallocations needed within the product loop.
-
-      msr = 'CORE processing completed for ' // TRIM(cfProd(i)%l3prodNameD) &
-            // '; starting Output task ...'
-      CALL MLSMessage (MLSMSG_Info, ModuleName, msr)
-
-      CALL OutputProd(pcf, cfProd(i), anText, l3sp, l3dm, dmA, dmD, l3r, residA, residD, flags)
-
-! Deallocate the databases passed between CORE & the I/O shell
-
-      CALL DestroyL2GPDatabase(l3r)
-      CALL DestroyL2GPDatabase(residA)
-      CALL DestroyL2GPDatabase(residD)
-      CALL DestroyL3DMDatabase(l3dm)
-      CALL DestroyL3DMDatabase(dmA)
-      CALL DestroyL3DMDatabase(dmD)
-      CALL DestroyL3SPDatabase(l3sp)
-      CALL DestroyL2GPDatabase(l2gp)
-
-
-   ENDDO
-
-   msr = 'Product loop processing completed; beginning Output/Close task ... '
-   CALL MLSMessage (MLSMSG_Info, ModuleName, msr)
-
-! Perform final Output & Close tasks outside of the product processing loop.
-
-
-   CALL OutputAndClose(cf, pcf, cfProd, avgPer, anText)
-
-   CALL MLSMessage (MLSMSG_Info, ModuleName, 'EOS MLS Level 3 data processing successfully completed!')
-   call h5close_f(error)                            
-   if (error /= 0) then                             
-     call MLSMessage ( MLSMSG_Error, moduleName, &  
-      & "Unable to h5_close_f" )                    
-   endif                                            
-   call MLSMessageExit(NORMAL_EXIT_STATUS)
-
-
-! Detailed description of program
-! The program is a prototype for the MLS Level 3 Daily software.
+  CALL MLSMessage (MLSMSG_Info, ModuleName, & 
+       & 'EOS MLS Level 3 data processing successfully completed!')
+  
+  CALL MLSMessageExit(NORMAL_EXIT_STATUS)
+  
+  ! Detailed description of program
+  ! The program is a prototype for the MLS Level 3 Daily software.
 
 !=================
 END PROGRAM MLSL3D
 !=================
 
 ! $Log$
+! Revision 1.11  2002/08/22 23:33:32  pwagner
+! Made ready for hdf5
+!
