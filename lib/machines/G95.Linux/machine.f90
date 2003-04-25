a29f89bdc9d9ac6d657b365815e0aed74b62b077
pwagner <>
2003-04-25 23:00:49 +0000

    FIest commit

---
+++ lib/machines/G95.Linux/machine.f90
@@ -0,0 +1,130 @@
+! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
+! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+
+module MACHINE
+  implicit none
+
+  character(LEN=2) :: END_LINE = ' ' // char(10)
+  character(LEN=1) :: FILSEP = '/'      ! '/' for Unix, '\' for DOS or NT
+  integer, parameter :: HP = 0          ! Offset for first argument for GETARG
+
+   ! According to Sun's own docs, e.g. 806-7987.pdf, Sun supplies
+   ! getarg as an intrinsic library routine
+  public :: GETARG
+  interface
+    subroutine GETARG ( ARGNUM, ARGVAL )
+      integer, intent(in) :: ARGNUM  ! 0 = command name, 1 = first arg, etc.
+      character(len=*), intent(out) :: ARGVAL   ! Blank if argnum out-of-range
+    end subroutine GETARG
+  end interface
+
+  interface IO_ERROR; module procedure IO_ERROR_; end interface
+  private IO_ERROR_
+  public :: SHELL_COMMAND
+  public :: MLS_DISABLE_AUTOGC, MLS_GC_NOW, MLS_HOWMANY_GC, MLS_CONFIG_GC
+
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=256), private :: Id = &
+       "$Id$"
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+!---------------------------------------------------------------------------
+
+contains
+
+  subroutine IO_ERROR_ ( MESSAGE, IOSTAT, FILE )
+  ! Print MESSAGE and FILE, and then do something reasonable with IOSTAT.
+
+    character(len=*), intent(in) :: MESSAGE
+    integer, intent(in) :: IOSTAT
+    character(len=*), intent(in), optional :: FILE
+
+    integer :: L
+!    character(len=127) :: MSG           ! From the Lahey IOSTAT_MSG intrinsic
+
+    write (*,*) message(:len_trim(message))
+    if ( present(file) ) then
+      l = len_trim(file)
+      write (*,*) file(:l)
+    end if
+!    call iostat_msg (iostat, msg)       ! Lahey intrinsic
+!    write (*,*) msg(:len_trim(msg))     ! Print the error message
+    write (*,*) 'Error status code =', iostat
+    return
+  end subroutine IO_ERROR_
+
+  subroutine exit_with_status (istatus)
+    integer,intent(in)::istatus
+
+    call exit(istatus)
+  end subroutine exit_with_status
+
+  subroutine SHELL_COMMAND ( Command, Status, Error )
+  ! Submit a character variable to the system as a shell command.
+  ! Based on Absoft's own documentation :: SupportLibrary.pdf
+  ! Absoft Support Libraries (1991-1998)
+  ! (Based on past adversities, that's no guarantee)
+
+    character(len=*), intent(in) :: Command  ! The command
+    integer, intent(out), optional :: Status ! Its status, if the system
+                                        !  has such a concept, else zero
+    integer, intent(out), optional :: Error  ! Status of the routine to submit
+                                        ! the command, if the system has
+                                        ! such a concept, else zero
+
+    integer :: MyStatus
+
+    interface
+      integer function system ( CH )
+        character(len=*), intent(in) :: CH
+      end function system
+    end interface
+
+    myStatus = system(command)
+    if ( present(error) ) error = 0
+    if ( present(status) ) status = myStatus
+  end subroutine SHELL_COMMAND
+
+  ! ----------------------------------------------
+  ! The following are merely introduced to satisfy 
+  ! NAG call interfaces to f90_gc
+  ! Not yet functional
+  subroutine MLS_CONFIG_GC ( EXPAND, FREQUENCY, RETRIES, SILENT )
+  ! Configures various parameters affecting garbage collection
+   logical dont_EXPAND, SILENT_GC
+   integer full_frequency, max_retries
+    logical, optional, intent(in) :: expand    ! Autoexpand heap?
+    integer, optional, intent(in) :: frequency ! How many incremental colls. betw. fulls
+    integer, optional, intent(in) :: retries   ! How many attempts before giving up
+    logical, optional, intent(in) :: silent    ! Quash report on each collection?
+    if ( present(expand) ) dont_expand = .not. expand
+    if ( present(frequency) ) full_frequency = frequency
+    if ( present(retries) ) max_retries = retries
+    if ( present(silent) ) silent_gc = silent
+  end subroutine MLS_CONFIG_GC
+
+  subroutine MLS_DISABLE_AUTOGC ( Which )
+  ! Turns automatic garbage collection on/off
+    character(len=*), intent(in) :: Which  ! 'On' or 'Off'
+    logical dont_gc
+    if ( Which == 'On' .or. Which == 'ON' &
+      & .or. Which == 'on' ) then
+      DONT_GC = .false.
+    else
+      DONT_GC = .true.
+    endif
+  end subroutine MLS_DISABLE_AUTOGC
+
+  subroutine MLS_GC_NOW
+  ! Manually collects garbage when called
+  !    CALL GCOLLECT
+  end subroutine MLS_GC_NOW
+
+  integer function MLS_HOWMANY_GC()
+  ! Returns how many garbage collections have been performed
+    MLS_HOWMANY_GC = 0 ! NCOLLECTIONS()
+  end function MLS_HOWMANY_GC
+
+end module MACHINE
+
+! $Log$
