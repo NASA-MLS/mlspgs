ac0215bc65602af5400e9d979c4345ca481242ae
vsnyder <>
2003-06-09 20:52:37 +0000

    rev 2.4

--- fwdmdl/Get_D_Deltau_Pol_M.f90
+++ fwdmdl/Get_D_Deltau_Pol_M.f90
@@ -58,11 +58,13 @@ contains
 ! and the {\tt dExDt} routine.
 ! 
 
-  subroutine Get_D_Deltau_Pol_DT ( Frq, H, CT, STCP, STSP, My_Catalog, &
+  SUBROUTINE Get_D_Deltau_Pol_DT ( Frq, mid, H, CT, STCP, STSP, My_Catalog, &
                 & Beta_group, GL_slabs_M, GL_slabs_P, &
-                & T_Path_C, T_Path_M, T_Path_P, Tanh_Path, &
-                & Beta_Path, SPS_Path, Eta_zxp, Del_S, Path_inds, &
-                & Incoptdepth, D_Deltau_Pol_DT )
+                & T_Path_C, T_Path_M, T_Path_P, &
+                & Beta_Path, SPS_Path, Alpha_Path, Eta_zxp, Del_S, Path_inds, &
+                & Incoptdepth, Ref_cor, &
+                & H_path_c, dH_dt_path_c, H_tan, dH_dt_tan, Do_calc_hyd_c, &
+                & D_Delta_dT, D_Deltau_Pol_DT)
 
     use DExdT_m, only: dExdT
     use Get_Beta_Path_m, only: Get_Beta_Path_Polarized
@@ -72,80 +74,111 @@ contains
     use Opacity_m, only: Opacity
     use Physics, only: H_OVER_K
     use SpectroscopyCatalog_m, only: CATALOG_T
+use toggles, only: Switches
 
   ! Arguments
     ! SVE == # of state vector elements
-    real(r8), intent(in) :: Frq          ! frequency in MHz
-    real(rp), intent(in) :: H(:)         ! Magnetic field component in instrument
-                                         ! polarization on the path
-    real(rp), intent(in) :: CT(:)        ! Cos(Theta), where theta
+    real(r8), intent(in) :: Frq             ! frequency in MHz
+    INTEGER, intent(in) :: mid      ! tangent index along the path
+    real(rp), intent(in) :: H(:)            ! Magnetic field component in
+                                            ! instrument polarization on the path
+    real(rp), intent(in) :: CT(:)           ! Cos(Theta), where theta
       ! is the angle between the line of sight and magnetic field vectors.
-    real(rp), intent(in) :: STCP(:)      ! Sin(Theta) Cos(Phi) where
+    real(rp), intent(in) :: STCP(:)         ! Sin(Theta) Cos(Phi) where
       ! theta is as for CT and phi (for this purpose only) is the angle
       ! between the plane defined by the line of sight and the magnetic
       ! field vector, and the "instrument field of view plane polarized"
       ! (IFOVPP) X axis.
-    real(rp), intent(in) :: STSP(:)      ! Sin(Theta) Sin(Phi)
+    real(rp), intent(in) :: STSP(:)         ! Sin(Theta) Sin(Phi)
     type(catalog_t), intent(in) :: My_Catalog(:)
     type (beta_group_T), intent(in) :: Beta_group(:)
     type (slabs_struct), intent(in) :: GL_slabs_m(:,:), GL_slabs_p(:,:) ! for T -/+ del_T
-    real(rp), intent(in) :: T_Path_C(:)  ! path temperatures on coarse grid
+    real(rp), intent(in) :: T_Path_C(:)     ! path temperatures on coarse grid
     real(rp), intent(in) :: T_Path_M(:), T_Path_P(:) ! path temperatures -/+ del_temp
-    real(rp), intent(in) :: Tanh_Path(:) ! tanh ( h \nu / 2 k T_Path_C )
+    !                                         on fine grid -- index with path_inds
     complex(rp), intent(in) :: Beta_Path(-1:,:,:) ! -1:1 x path x sps
-    real(rp), intent(in) :: SPS_Path(:,:) ! species on path, path x sps
-    real(rp), intent(in) :: Eta_zxp(:,:) ! representation basis function
-      !                                    main grid.  path x sve
-    real(rp), intent(in) :: Del_S(:)     !  ! unrefracted path length.
+    real(rp), intent(in) :: SPS_Path(:,:)   ! species on path, path x sps
+    complex(rp), intent(in) :: Alpha_Path(-1:,:) ! -1:1 x path
+    real(rp), intent(in) :: Eta_zxp(:,:)    ! representation basis function
+      !                                       on coarse grid.  path x sve
+    real(rp), intent(in) :: Del_S(:)        ! unrefracted path length.  This
+      !                                       is for the whole coarse path, not
+      !                                       just the part up to the black-out
     integer(ip), intent(in) :: Path_inds(:) ! indicies for reading gl_slabs
-    complex(rp), intent(in) :: Incoptdepth(:,:,:) ! 2 x 2 x path
+    complex(rp), intent(in) :: Incoptdepth(:,:,:) ! negative of incremental
+      !                                       optical depth.  2 x 2 x path
+    real(rp), intent(in) :: Ref_cor(:)      ! refracted to unrefracted path
+    !                                         length ratios.
+
+    ! For hydrostatic
+    real(rp), intent(in) :: H_path_c(:)     ! path heights + req on main grid km.
+    real(rp), intent(in) :: dH_dt_path_c(:,:) ! derivative of path height wrt
+    !                                         temperature(km/K) on main grid.
+    real(rp), intent(in) :: H_tan           ! tangent height + req (km).
+    real(rp), intent(in) :: dH_dt_tan(:)    ! derivative of path height wrt
+    !                                         temperature at the tangent (km/K).
+    logical, intent(in) :: Do_calc_hyd_c(:,:) ! Where is the dh_dt function not
+    !                                         zero on main grid?
+
+    ! From nonpolarized model
+    real(rp), intent(in) :: D_Delta_DT(:,:) ! Incremental opacity derivatives
+    !                                         schlep from drad_tran_dt.  Path x SVE
 
     complex(rp), intent(out) :: D_Deltau_Pol_DT(:,:,:,:) ! 2 x 2 x path x sve
 
   ! Local variables
-    complex(rp) :: Alpha_Path_N(-1:1)    ! alpha_path_n * N
+    complex(rp) :: Alpha_Path_N(-1:1)       ! alpha_path_n * N
     complex(rp) :: Beta_0(-1:1), Beta_M(-1:1), Beta_P(-1:1) ! Single elements of
       ! Beta_Path, Beta_Path_M, Beta_Path_P multiplied by Tanh_Path,  Tanh_M, Tanh_P
-    complex(rp), dimension(-1:1,size(path_inds,1),size(beta_group)) :: &
+    complex(rp), dimension(-1:1,size(path_inds),size(beta_group)) :: &
       & Beta_Path_M, &  ! At T_path_M on coarse path
       & Beta_Path_P     ! At T_path_P on coarse path
-    complex(rp):: D_Alpha_DT(-1:1,size(path_inds,1)) ! n/T * Alpha on the path
-    complex(rp) :: D_Incoptdepth_dT(2,2,size(path_inds,1))
-    real(r8) :: FrqHK                    ! 0.5 * Frq * H_Over_K
-    integer :: I
+    complex(rp):: D_Alpha_DT(-1:1,size(path_inds)) ! n/T * Alpha on the path
+    complex(rp):: D_Alpha_DT_eta(-1:1,size(path_inds)) ! d_Alpha_DT * eta_zxp
+    complex(rp) :: D_Incoptdepth_dT(2,2,size(path_inds))
+    logical :: Do_calc(1:size(path_inds))
+    real(rp) :: Fa, Fb               ! Hydrostatic integrand at ends of
+    !                                  path segment
+    real(r8) :: FrqHK                ! 0.5 * Frq * H_Over_K
+    integer :: H_Stop                ! Stop point for hydrostatic parts
+    integer :: I_stop                ! Stop point, which may be before end
     integer :: J, K
-    real(rp) :: L_TTM, L_TPTM, L_TPT     ! Logarithms of temperature ratios
-    complex(rp) :: N(-1:1)               ! Exponent of (T/T_0) in
+    real(rp) :: L_TTM, L_TPTM, L_TPT ! Logarithms of temperature ratios
+    complex(rp) :: N(-1:1)           ! Exponent of (T/T_0) in
                ! approximation to beta.  One each for Sigma_-, Pi and Sigma_+.
-    integer :: N_Path, N_Sps
+    integer :: N_Sps                 ! Number of species
+    logical :: NeedFA                ! Need FA in hydrostatic calculation
+    integer :: P_i                   ! Index on the path
     complex(rp) :: R0M(-1:1), RPM(-1:1), RP0(-1:1)  ! Beta ratios
-    real(rp) :: Tanh_M, Tanh_P           ! for T -/+ del_T
+    real(rp) :: S_Del_S              ! Sum of Del_S
+    integer :: SV_i                  ! Index of state vector element
+    real(rp) :: Tanh_M, Tanh_P       ! for T -/+ del_T
 
     frqhk = 0.5_r8 * Frq * H_Over_K
-    n_path = size(path_inds)
     n_sps = size(sps_path,2)
+    i_stop = size(path_inds)
 
     call get_beta_path_polarized ( frq, h, my_Catalog, beta_group, gl_slabs_m, &
       & path_inds, beta_path_m )
     call get_beta_path_polarized ( frq, h, my_Catalog, beta_group, gl_slabs_p, &
       & path_inds, beta_path_p )
 
-    do i = 1, n_path
+    do p_i = 1, i_stop
       alpha_path_n = (0.0_rp,0.0_rp)
-      k = path_inds(i)
+      k = path_inds(p_i)
 
-      l_ttm = log(t_path_c(k)/t_path_m(k))
+      l_ttm = log(t_path_c(p_i)/t_path_m(k))
       l_tptm = log(t_path_p(k)/t_path_m(k))
-      l_tpt = log(t_path_p(k)/t_path_c(k))
+      l_tpt = log(t_path_p(k)/t_path_c(p_i))
 
       tanh_m = tanh( frqhk / t_path_m(k) )
       tanh_p = tanh( frqhk / t_path_p(k) )
 
       do j = 1, n_sps
         ! Solve for n
-        beta_0 = beta_path(:,k,j) * tanh_path(k)
-        beta_m = beta_path_m(:,i,j) * tanh_m
-        beta_p = beta_path_p(:,i,j) * tanh_p
+        beta_0 = beta_path(:,p_i,j) ! multiplication by tanh_path(p_i) done by caller
+        beta_m = beta_path_m(:,p_i,j) * tanh_m
+        beta_p = beta_path_p(:,p_i,j) * tanh_p
         where ( beta_m /= 0.0 .and. beta_p /= 0.0 )
           rpm = log(beta_p/beta_m) / l_tptm
           where ( beta_0 /= 0.0 )
@@ -173,33 +206,125 @@ contains
 
         ! not quite D alpha, because we haven't multiplied by tanh or
         ! divided by T.
-        alpha_path_n = alpha_path_n + n * beta_path(:,k,j) * sps_path(k,j)
-      end do ! j
-      ! Still not quite D alpha...
-      alpha_path_n = alpha_path_n * tanh_path(k)
+        alpha_path_n = alpha_path_n + n * beta_0 * sps_path(k,j)
+      end do ! j = 1, n_sps
 
-      ! Now it's more than D alpha, because we've multiplied by del_s, but
-      ! this is OK, because OPACITY is linear.
-      d_alpha_dT(:,i) = alpha_path_n * del_s(k) / t_path_c(k)
-    end do ! i
+      d_alpha_dT(:,p_i) = alpha_path_n * del_s(p_i) / t_path_c(p_i)
+    end do ! p_i = 1, i_stop
 
-    call opacity ( ct, stcp, stsp, d_alpha_dT, d_incoptdepth_dT )
+    do sv_i = 1, size(eta_zxp,2) ! state vector elements
 
-!??? Do we need to have d_incoptdepth_dT for the nonpolarized case, and  ???
-!??? subtract half of it from the diagonal of our d_incoptdepth_dT       ???
+      do p_i = 1, i_stop
+        !{ d\_alpha\_dT\_eta$_i = \frac{\partial \alpha}{\partial T_i} =
+        !                         \frac{\partial \alpha}{\partial T}
+        !                         \frac{\partial T}{\partial T_i} =
+        !                         \frac{\partial \alpha}{\partial T} \eta_i$
+        d_alpha_dT_eta(:,p_i) = d_alpha_dT(:,p_i) * eta_zxp(p_i,sv_i)
+      end do
 
-    do i = 1, n_path            ! along the path
-      k = path_inds(i)
-      do j = 1, size(eta_zxp,2) ! state vector elements
-        if ( eta_zxp(k,j) /= 0.0 ) then
-          call dExdT ( - incoptdepth(:,:,k) * eta_zxp(k,j), &
-                     & - d_incoptdepth_dT(:,:,i) * eta_zxp(k,j), &
-                     & d_deltau_pol_dT(:,:,i,j) ) ! d exp(incoptdepth) / dT
+if ( index(switches,'nhd') /= 0 ) then
+  do_calc=.false.
+  go to 10
+end if
+      ! Now do the hydrostatic part
+      !??? This only goes as far as I_Stop.  We may     ???
+      !??? want to do it this way in DRad_Tran_dT also. ???
+
+      ! First combine boundary flags
+      do_calc = do_calc_hyd_c(:,sv_i)
+      if ( i_stop < mid ) then
+        do_calc(2:i_stop) =   do_calc(2:i_stop) .or. do_calc(1:i_stop-1)
+        h_stop = i_stop
+      else
+        do_calc(2:i_stop) =   do_calc(2:i_stop) .or. do_calc(1:i_stop-1) .or. do_calc(mid)
+        h_stop = mid - 1
+      end if
+      do_calc(1) = .false.
+      s_del_s = sum(del_s(2:mid)) ! Yes, this goes to the midpoint of the coarse path
+      needFA = .true.
+      do p_i = 2 , h_stop
+        if ( do_calc(p_i) ) then
+          if ( needFA ) then
+            fa = (h_path_c(p_i-1) * dh_dt_path_c(p_i-1,sv_i) &
+            &   - h_tan * dh_dt_tan(sv_i)) / s_del_s
+            needFA = .false.
+          end if
+          s_del_s = s_del_s - del_s(p_i)
+          fb = (h_path_c(p_i)*dh_dt_path_c(p_i,sv_i) &
+            & - h_tan * dh_dt_tan(sv_i)) / s_del_s
+          d_alpha_dT_eta(:,p_i) = d_alpha_dT_eta(:,p_i) + alpha_path(:,p_i)*(fa - fb)
+          fa = fb
         else
-          d_deltau_pol_dT(:,:,i,j) = 0.0_rp
+          s_del_s = s_del_s - del_s(p_i)
         end if
-      end do ! j
-    end do ! i
+      end do
+
+      ! special processing at tangent.  fb is zero
+
+      if ( i_stop >= mid ) then
+        if ( do_calc(mid) ) then
+          d_alpha_dT_eta(:,mid) = d_alpha_dT_eta(:,mid) + alpha_path(:,mid) * fa
+        end if
+      end if
+      if ( i_stop > mid ) then
+        if ( do_calc(mid+1) ) then
+          fa = (h_path_c(mid+2) * dh_dt_path_c(mid+2,sv_i) - &
+              & h_tan * dh_dt_tan(sv_i)) / del_s(mid+1)
+          d_alpha_dT_eta(:,mid+1) = d_alpha_dT_eta(:,mid+1) + alpha_path(:,mid+1) * fa
+        else
+          needFA = .true.
+        end if
+
+        do_calc(mid+2:i_stop-1) = do_calc(mid+2:i_stop-1) .or. do_calc(mid+3:i_stop) .or. do_calc(mid+1)
+        if (i_stop == 2*mid) then
+          h_stop = i_stop - 1
+          do_calc(i_stop) = .false.
+        else
+          h_stop = i_stop
+        end if
+
+        s_del_s = del_s(mid+1)
+        do p_i = mid + 2, h_stop
+          if ( do_calc(p_i) ) then
+            if ( needFA ) then
+              fa = (h_path_c(p_i) * dh_dt_path_c(p_i,sv_i) &
+                  & - h_tan * dh_dt_tan(sv_i)) / s_del_s
+              needFA = .false.
+            end if
+            s_del_s = s_del_s + del_s(p_i)
+            fb = (h_path_c(p_i+1)*dh_dt_path_c(p_i+1,sv_i) &
+               - h_tan * dh_dt_tan(sv_i)) / s_del_s
+            d_alpha_dT_eta(:,p_i) = d_alpha_dT_eta(:,p_i) + alpha_path(:,p_i)*(fb - fa)
+            fa = fb
+          else
+            s_del_s = s_del_s + del_s(p_i)
+          end if
+        end do
+      end if
+
+10 continue
+      ! Apply refraction correction
+      ! Then add in contribution from scalar model, 0.25 for +/- sigma,
+      ! 0.5 for pi.
+      do p_i = 1, i_stop             ! along the path
+        d_alpha_dT_eta(:,p_i) = d_alpha_dT_eta(:,p_i) * ref_cor(p_i) + &
+          & 0.25_rp * d_delta_dT(p_i,sv_i)
+        d_alpha_dT_eta(0,p_i) = d_alpha_dT_eta(0,p_i) + &
+          & 0.25_rp * d_delta_dT(p_i,sv_i)
+      end do
+      ! d_alpha_dT_eta is now really \int incremental opacity ds.
+
+      call opacity ( ct, stcp, stsp, d_alpha_dT_eta, d_incoptdepth_dT )
+
+      do p_i = 1, i_stop             ! along the path
+        if ( eta_zxp(p_i,sv_i) /= 0.0 .or. do_calc(p_i) ) then
+          call dExdT ( incoptdepth(:,:,p_i), -d_incoptdepth_dT(:,:,p_i), &
+                     & d_deltau_pol_dT(:,:,p_i,sv_i) ) ! d exp(incoptdepth) / dT
+        else
+          d_deltau_pol_dT(:,:,p_i,sv_i) = 0.0_rp
+        end if
+      end do ! p_i
+    end do ! sv_i
 
   end subroutine Get_D_Deltau_Pol_DT
 
@@ -211,6 +336,9 @@ contains
 end module Get_D_Deltau_Pol_M
 
 ! $Log$
+! Revision 2.4  2003/06/09 20:52:37  vsnyder
+! More work on polarized derivatives
+!
 ! Revision 2.3  2003/05/24 02:26:18  vsnyder
 ! More work on polarized temperature derivatives
 !
