2280dffdbb7709f59e2fbad8fbfd18fdf9014e2b
vsnyder <>
2003-06-20 00:18:23 +0000

    rev 2.3

--- lib/sort.f90
+++ lib/sort.f90
@@ -22,6 +22,14 @@ module Sort_M
     module procedure DSORT, ISORT, SSORT
   end interface
 
+  interface SORTP ! Using a permutation vector
+    module procedure DSORTP, ISORTP, SSORTP
+  end interface
+
+  interface SORTQ ! Using a pre-specified permutation vector
+    module procedure DSORTQ, ISORTQ, SSORTQ
+  end interface
+
 !---------------------------- RCS Ident Info -------------------------------
   character (len=*), private, parameter :: IdParm = &
        "$Id$"
@@ -35,21 +43,74 @@ contains
 
   subroutine DSORT (A, M, N)
     double precision, intent(inout) :: A(*)
-    double precision :: PARTN,TEMP
+    double precision :: PARTN, TEMP
     include "sort.f9h"
   end subroutine DSORT
 
   subroutine ISORT (A, M, N)
     integer, intent(inout) :: A(*)
-    integer :: PARTN,TEMP
+    integer :: PARTN, TEMP
     include "sort.f9h"
   end subroutine ISORT
 
   subroutine SSORT (A, M, N)
     real, intent(inout) :: A(*)
-    real :: PARTN,TEMP
+    real :: PARTN, TEMP
     include "sort.f9h"
   end subroutine SSORT
+
+  subroutine DSORTP (A, M, N, P)
+    double precision, intent(inout) :: A(*)
+    integer, intent(in) :: M, N
+    integer, intent(out) :: P(*)
+    integer :: I
+    do i = m, n
+      p(i) = i
+    end do
+    call SORTQ ( A, M, N, P )
+  end subroutine DSORTP
+
+  subroutine ISORTP (A, M, N, P)
+    integer, intent(inout) :: A(*)
+    integer, intent(in) :: M, N
+    integer, intent(out) :: P(*)
+    integer :: I
+    do i = m, n
+      p(i) = i
+    end do
+    call SORTQ ( A, M, N, P )
+  end subroutine ISORTP
+
+  subroutine SSORTP (A, M, N, P)
+    real, intent(inout) :: A(*)
+    integer, intent(in) :: M, N
+    integer, intent(out) :: P(*)
+    integer :: I
+    do i = m, n
+      p(i) = i
+    end do
+    call SORTQ ( A, M, N, P )
+  end subroutine SSORTP
+
+  subroutine DSORTQ (A, M, N, P)
+    double precision, intent(inout) :: A(*)
+    double precision :: PARTN
+    include "sortq.f9h"
+  end subroutine DSORTQ
+
+  subroutine ISORTQ (A, M, N, P)
+    integer, intent(inout) :: A(*)
+    integer :: PARTN
+    include "sortq.f9h"
+  end subroutine ISORTQ
+
+  subroutine SSORTQ (A, M, N, P)
+    real, intent(inout) :: A(*)
+    real :: PARTN
+    include "sortq.f9h"
+  end subroutine SSORTQ
+
+!---------------------------------------------------------------------------
   logical function not_used_here()
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
@@ -57,6 +118,9 @@ contains
 end module Sort_M
 
 ! $Log$
+! Revision 2.3  2003/06/20 00:18:23  vsnyder
+! Add SORTP and SORTQ
+!
 ! Revision 2.2  2002/10/08 00:09:14  pwagner
 ! Added idents to survive zealous Lahey optimizer
 !
