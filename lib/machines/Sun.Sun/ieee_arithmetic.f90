e4fc90bf94e22e001a6383702b92a8fcc4ce7331
vsnyder <>
2003-07-03 19:11:57 +0000

    Add IEEE_Support_NaN, IEEE_Is_NaN, IEEE_Class_Type. Make IEEE_Quiet_NaN to be of IEEE_Class_Type. Make IEEE_Value more like the IEEE TR and Fortran 2003 standard.

--- lib/machines/Sun.Sun/ieee_arithmetic.f90
+++ lib/machines/Sun.Sun/ieee_arithmetic.f90
@@ -8,45 +8,58 @@ MODULE IEEE_ARITHMETIC              ! Common utilities for the MLSL1 program
 !  use SUNSOWNIEEE, only: ir_isnan, ir_isinf, r_quiet_nan
 
   IMPLICIT NONE
+  private
 
   ! These are the ieee functions and constants needed in mls
   ! that Sun fails to supply.
   ! An improvement might be to introduce function interfaces to allow
   ! both single and double precision versions.
-  public :: ieee_value, ieee_quiet_nan, ieee_is_finite
+  public :: IEEE_Class_Type
+  public :: IEEE_Is_finite, IEEE_Is_NaN, IEEE_Quiet_NaN, IEEE_Support_NaN
+  public :: IEEE_Value
 
-  PRIVATE :: Id, ModuleName
-  !---------------------------- RCS Ident Info -------------------------------
-  CHARACTER (LEN=256) :: Id = &
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), private, parameter :: IdParm = &
        "$Id$"
-  CHARACTER (LEN=*), PARAMETER :: ModuleName= "$RCSFile:$"
-  !---------------------------------------------------------------------------
+  character (len=len(idParm)), private :: Id = idParm
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+!---------------------------------------------------------------------------
 
   ! This module contains glue routines for Sun's own f95 compiler
   ! since it fails to conform to ISO/IEC TR15580:1998(E).
   ! If we should ever obtain one that conforms we may cheerfully
   ! dispose of this crude hack
 
-  private
-
   ! The following is made necessary only because Sun's own f95
   ! fails to comply--this obviously fails if any number ever matches this
 
-  real, parameter :: ieee_quiet_nan = -9.E19
+  type IEEE_Class_Type
+    private
+    integer :: What        ! 1 = IEEE_Quiet_Nam
+                           ! 2 = IEEE_Signaling_Nan
+  end type IEEE_Class_Type
+
+  type(ieee_class_type), parameter :: ieee_quiet_nan = ieee_class_type(1)
+  type(ieee_class_type), parameter :: ieee_signaling_nan = ieee_class_type(2)
+
 !  real, parameter :: ieee_quiet_nan = r_quiet_nan()
+
+  interface IEEE_Is_NaN
+    module procedure IEEE_Is_NaN_D, IEEE_Is_NaN_S
+  end interface
+
+  interface IEEE_Support_NaN
+    module procedure IEEE_Support_NaN_All
+    module procedure IEEE_Support_NaN_D, IEEE_Support_NaN_S
+  end interface
+
+  interface IEEE_Value
+    module procedure IEEE_Value_D, IEEE_Value_S
+  end interface
 
 CONTAINS
 
-  FUNCTION IEEE_VALUE( ARG1, ARG2 ) result ( the_value )
-  ! Formal args
-    real, intent(in) ::          arg1
-    real, intent(in) ::          arg2
-    real ::                      the_value
-    
-  ! Private
-    the_value = arg2
-  END FUNCTION IEEE_VALUE
-  
   LOGICAL FUNCTION IEEE_IS_FINITE( ARG )
   ! Formal args
     real, intent(in) ::          arg
@@ -60,10 +73,67 @@ CONTAINS
     IEEE_IS_FINITE = .TRUE.
   END FUNCTION IEEE_IS_FINITE
   
+  elemental logical function IEEE_Is_NaN_D ( X )
+    double precision, intent(in) :: X
+    IEEE_Is_NaN_D = .not. ( x <= 0.0 .or. x >= 0.0 )
+  end function IEEE_Is_NaN_D
+
+  elemental logical function IEEE_Is_NaN_S ( X )
+    real, intent(in) :: X
+    IEEE_Is_NaN_S = .not. ( x <= 0.0 .or. x >= 0.0 )
+  end function IEEE_Is_NaN_S
+
+  logical function IEEE_Support_NaN_All ( )
+    IEEE_Support_NaN_All = .true.
+  end function IEEE_Support_NaN_All
+
+  logical function IEEE_Support_NaN_D ( X )
+    double precision, intent(in) :: X
+    IEEE_Support_NaN_D = .true.
+  end function IEEE_Support_NaN_D
+
+  logical function IEEE_Support_NaN_S ( X )
+    real, intent(in) :: X
+    IEEE_Support_NaN_S = .true.
+  end function IEEE_Support_NaN_S
+
+  FUNCTION IEEE_VALUE_D ( X, CLASS ) result ( the_value )
+  ! Formal args
+    double precision, intent(in) ::      X
+    type(ieee_class_type), intent(in) :: CLASS
+    double precision ::                  the_value
+    
+  ! Private
+    select case ( class%what )
+    case ( ieee_quiet_nan%what )     ! IEEE_Quiet_NaN
+      the_value = -9.e19
+    case ( ieee_signaling_nan%what ) ! IEEE_Signaling_NaN
+      the_value = 9.e19
+    end select
+  END FUNCTION IEEE_VALUE_D
+
+  FUNCTION IEEE_VALUE_S ( X, CLASS ) result ( the_value )
+  ! Formal args
+    real, intent(in) ::                  X
+    type(ieee_class_type), intent(in) :: CLASS
+    real ::                              the_value
+    
+  ! Private
+    select case ( class%what )
+    case ( ieee_quiet_nan%what )     ! IEEE_Quiet_NaN
+      the_value = -9.e19
+    case ( ieee_signaling_nan%what ) ! IEEE_Signaling_NaN
+      the_value = 9.e19
+    end select
+  END FUNCTION IEEE_VALUE_S
+
 END MODULE IEEE_ARITHMETIC
 
 !
 ! $Log$
+! Revision 1.3  2001/09/18 17:18:12  pwagner
+! Strictly so the darn thing compiles--let others (or Sun) make it work
+!
 ! Revision 1.2  2001/09/14 17:35:33  pwagner
 ! Fixed an obvious blunder; expanded some comments
 !
