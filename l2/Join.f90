6dcaa800a260e3fe50c3cdfa53dc3c407a9f4fd1
livesey <>
2003-09-03 23:05:49 +0000

    rev 2.91

--- l2/Join.f90
+++ l2/Join.f90
@@ -528,8 +528,8 @@ contains ! =====     Public Procedures     =============================
         call get_string ( hdfNameIndex, hdfName, strip=.true. )
         thisDirect%sdNames(source) = hdfName
         if ( precisionVectors(source) /= 0 ) then
-          precQty => GetVectorQtyByTemplateIndex ( vectors(sourceVectors(source)), &
-            & sourceQuantities(source) )
+          precQty => GetVectorQtyByTemplateIndex ( vectors(precisionVectors(source)), &
+            & precisionQuantities(source) )
           ! Check that this is compatible with it's value quantitiy
           if ( qty%template%name /= precQty%template%name ) &
             & call Announce_Error ( son, no_error_code, &
@@ -1351,6 +1351,9 @@ end module Join
 
 !
 ! $Log$
+! Revision 2.91  2003/09/03 23:05:49  livesey
+! More problems with precision in DirectWrite, hope I've got them all now.
+!
 ! Revision 2.90  2003/09/03 00:53:50  livesey
 ! Bug fix on directWrite, it was storing the value field in the precision
 ! too!
