ca9ded97559d42141564dfd2165a88eb449da066
pwagner <>
2003-09-05 23:45:51 +0000

    Made name of log file a variable: LOGFILE; tweaked initial comments

--- util/slavetmplt.sh
+++ util/slavetmplt.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 # slavetmplt.sh
-# run a program specified as the variable MLSPROG
-# assuming that it's in directory MLSBIN
-# Then return an exit status of:
-# 1 if program's exit status is different from
-# the variable specified as NORMAL_STATUS; otherwise
-# 0
+# runs a slave task when mlsl2 is in parallel mode
 #
 # Copyright (c) 2003, California Institute of Technology.  ALL RIGHTS RESERVED.
 # U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
-# usage: slavetmplt.sh [option_1] [option_2] ..
+# usage: not called directly, but as mlsl2.slave after being seded
 
 # Used by mlsl2 master task mlsl2p.sh to launch slave tasks in toolkit
 # environment when running mlsl2 in parallel mode
 # mlsl2p.sh sed's this file to replace ssllaavvee, ppggssbbiinn, etc.
 # as appropriate
-
 
 # The resulting script will be called by mlsl2 master task
 # It will attempt to set some toolkt-savvy environment variables
@@ -38,15 +32,22 @@ PVM_BIN=ppvvmmbbiinn
 PGSMEM_USESHM=NO
 PGS_PC_INFO_FILE=ppccff
 
-if [ ! -w "$PVM_BIN/mlsl2.log" ]
+# The following choice puts the outputs of all the slaves into a single file
+LOGFILE="$PVM_BIN/mlsl2.log"
+
+# The next choice, in contrast, puts each slave's output into its own unique file
+#temp_file_name=`unique_name.sh mlsl2`
+#LOGFILE="$PVM_BIN/$temp_file_name"
+
+if [ ! -w "$LOGFILE" ]
 then
-  echo "#mlsl2.log" > "$PVM_BIN/mlsl2.log"
+  echo "#mlsl2.log" > "$LOGFILE"
 fi
-# echo "Called as $@" >> "$PVM_BIN/mlsl2.log"
-# echo "command is $0" >> "$PVM_BIN/mlsl2.log"
-# echo "1st arg is $1" >> "$PVM_BIN/mlsl2.log"
-# echo "2nd arg is $2" >> "$PVM_BIN/mlsl2.log"
-# echo "3rd arg is $3" >> "$PVM_BIN/mlsl2.log"
+#echo "Called as $@" >> "$LOGFILE"
+#echo "command is $0" >> "$LOGFILE"
+#echo "1st arg is $1" >> "$LOGFILE"
+#echo "2nd arg is $2" >> "$LOGFILE"
+#echo "3rd arg is $3" >> "$LOGFILE"
 
 SLVPROG=mlsl2.ssllaavvee
 # It's possible that $1 is the command name--in which case we
@@ -54,13 +55,17 @@ SLVPROG=mlsl2.ssllaavvee
 if [ "$1" = "$SLVPROG" ]
 then
   shift
-  # echo "Need to shift because 1st arg is command name" >> "$PVM_BIN/mlsl2.log"
+  echo "Need to shift because 1st arg is command name" >> "$LOGFILE"
 fi
 
 more_opts="yes"
 while [ "$more_opts" = "yes" ] ; do
 
     case "$1" in
+    --chunk )
+       shift
+       shift
+       ;;
     --slave )
        masterTid="$2"
        shift
@@ -78,28 +83,28 @@ while [ "$more_opts" = "yes" ] ; do
     esac
 done
 
-echo "PGS_PC_INFO_FILE: $PGS_PC_INFO_FILE" >> "$PVM_BIN/mlsl2.log"
-echo "masterTid: $masterTid" >> "$PVM_BIN/mlsl2.log"
-echo "executable: $PVM_BIN/mlsl2" >> "$PVM_BIN/mlsl2.log"
+echo "PGS_PC_INFO_FILE: $PGS_PC_INFO_FILE" >> "$LOGFILE"
+echo "masterTid: $masterTid" >> "$LOGFILE"
+echo "executable: $PVM_BIN/mlsl2" >> "$LOGFILE"
 
 export PGSMEM_USESHM
 export PGS_PC_INFO_FILE
 
 if [ "$masterTid" = "" ]
 then
-  echo "masterTid undefined" >> "$PVM_BIN/mlsl2.log"
+  echo "masterTid undefined" >> "$LOGFILE"
   exit 1
 elif [ "$PGS_PC_INFO_FILE" = "" ]
 then
-  echo "PCF undefined" >> "$PVM_BIN/mlsl2.log"
+  echo "PCF undefined" >> "$LOGFILE"
   exit 1
 elif [ ! -x "$PVM_BIN/mlsl2" ]
 then
-  echo "$PVM_BIN/mlsl2 not found/executable" >> "$PVM_BIN/mlsl2.log"
+  echo "$PVM_BIN/mlsl2 not found/executable" >> "$LOGFILE"
   exit 1
 fi
 
-$PVM_BIN/mlsl2 --tk --slave $masterTid -g -S'slv,opt1,log,pro,time'  >> "$PVM_BIN/mlsl2.log"
+$PVM_BIN/mlsl2 --tk -m --slave $masterTid -g -S'slv,opt1,log,pro,time' >> "$LOGFILE"
 
 }
       
@@ -121,3 +126,6 @@ do_the_call $all_my_opts
 exit 0
 
 # $Log$
+# Revision 1.1  2003/08/01 16:46:31  pwagner
+# First commit
+#
