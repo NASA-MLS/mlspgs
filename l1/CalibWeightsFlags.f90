6f6f9f7cf4b059285a6b857daefadbf57d770dc8
perun <>
2003-09-15 17:15:58 +0000

    rev 2.2

--- l1/CalibWeightsFlags.f90
+++ l1/CalibWeightsFlags.f90
@@ -33,18 +33,43 @@ CONTAINS
 
     USE MLSL1Config, ONLY: GHz_seq, GHz_seq_use
     USE MLSL1Common, ONLY: L1BFileInfo
-    USE EngTbls, ONLY: EngMAF
+    USE EngTbls, ONLY: EngMAF, EngPkt
     USE L0_sci_tbls, ONLY: SciMAF
     USE SciUtils, ONLY: SwMirPos, GetScAngles
+    USE MLSL1Utils, ONLY: QNan
 
     INTEGER :: i, ios, sci_MAFno, n, sum_S, sum_T
     INTEGER :: EngMAF_unit, SciMAF_unit, MAF_data_unit
     INTEGER :: mindx(0:(MaxMIFs-1))
     INTEGER, PARAMETER :: GHz_sw_indx(0:(MaxMIFs-1)) = (/ (i, i=1,MaxMIFs) /)
     CHARACTER(len=1) :: GHz_sw_pos(0:(MaxMIFs-1))
+    CHARACTER(len=70) :: PLL_DN(0:(MaxMIFs-1)), PLL_dflt_DN(0:(MaxMIFs-1))
     LOGICAL :: recompute
+    REAL :: LLO_EU(16,0:(MaxMIFs-1)), LLO_dflt_EU(16,0:(MaxMIFs-1))
+
+    TYPE Sci_pos_T
+       REAL :: APE(2,0:(MaxMIFs-1))
+       REAL :: ASE(2,0:(MaxMIFs-1))
+       REAL :: GME(2,0:(MaxMIFs-1))
+       REAL :: TSE(2,0:(MaxMIFs-1))
+    END TYPE Sci_pos_T
+
+    TYPE (Sci_pos_T) :: Sci_pos, Sci_dflt_pos
 
     print *, ' Reading MAFdata...'
+
+! Init default positions:
+
+    Sci_dflt_pos%APE = QNan()
+    Sci_dflt_pos%ASE = QNan()
+    Sci_dflt_pos%GME = QNan()
+    Sci_dflt_pos%TSE = QNan()
+
+    DO n = 0, (MaxMIFs - 1)
+       PLL_dflt_DN(n) = CHAR(0)
+    ENDDO
+
+    LLO_dflt_EU = QNan()
 
     MAF_data_unit = L1BFileInfo%MAF_data_unit
     EngMAF_unit = L1BFileInfo%EngMAF_unit
@@ -71,26 +96,50 @@ CONTAINS
 
     outer: DO
 
-       READ (engMAF_unit, iostat=ios) EngMAF
+       READ (engMAF_unit, iostat=ios) EngMAF, EngPkt
        IF (ios /= 0) EXIT outer
+       WRITE (L1BFileInfo%EngId, iostat=ios) EngPkt, EngMAF%Eng%value
 
        READ (sciMAF_unit, iostat=ios) SciMAF
        IF (ios /= 0) EXIT outer
        sci_MAFno = SciMAF(0)%MAFno
+       DO n = 0, (MaxMIFs - 1)
+          Sci_pos%APE(:,n) = SciMAF(n)%APE_pos
+          Sci_pos%ASE(:,n) = SciMAF(n)%ASA_pos
+          Sci_pos%GME(:,n) = SciMAF(n)%GSA_pos
+          Sci_pos%TSE(:,n) = SciMAF(n)%TSSA_pos
+          PLL_DN(n) = SciMAF(n)%PLL_DN
+          LLO_EU(:,n) = SciMAF(n)%LLO_EU
+       ENDDO
 
        DO
-          IF (EngMAF%MAFno == sci_MAFno) EXIT   ! Nothing more to read
+          IF (EngMAF%MAFno == sci_MAFno) THEN
+             WRITE (L1BFileInfo%EngId, iostat=ios) Sci_pos
+             WRITE (L1BFileInfo%EngId, iostat=ios) PLL_DN
+             WRITE (L1BFileInfo%EngId, iostat=ios) LLO_EU
+             EXIT   ! Nothing more to read
+          ENDIF
 
           IF (EngMAF%secTAI <= SciMAF(2)%secTAI) THEN       ! Catch the Sci
 
-             READ (engMAF_unit, iostat=ios) EngMAF
+             WRITE (L1BFileInfo%EngId, iostat=ios) Sci_dflt_pos
+             WRITE (L1BFileInfo%EngId, iostat=ios) PLL_dflt_DN
+             WRITE (L1BFileInfo%EngId, iostat=ios) LLO_dflt_EU
+             READ (engMAF_unit, iostat=ios) EngMAF, EngPkt
              IF (ios /= 0) EXIT outer
+             WRITE (L1BFileInfo%EngId, iostat=ios) EngPkt, EngMAF%Eng%value
 
           ELSE IF (EngMAF%secTAI > SciMAF(2)%secTAI) THEN   ! Catch the Eng
 
              READ (sciMAF_unit, iostat=ios) SciMAF
              IF (ios /= 0) EXIT outer
              sci_MAFno = SciMAF(0)%MAFno
+             DO n = 0, (MaxMIFs - 1)
+                Sci_pos%APE(:,n) = SciMAF(n)%APE_pos
+                Sci_pos%ASE(:,n) = SciMAF(n)%ASA_pos
+                Sci_pos%GME(:,n) = SciMAF(n)%GSA_pos
+                Sci_pos%TSE(:,n) = SciMAF(n)%TSSA_pos
+             ENDDO
 
           ENDIF
        ENDDO
@@ -98,7 +147,7 @@ CONTAINS
 ! Reset GHz SW mirror pos:
 
        DO n = 0, (MaxMIFs - 1)
-          SciMAF(n)%GHz_sw_pos = SwMirPos ("G", SciMAF(n)%GME_pos)
+          SciMAF(n)%GHz_sw_pos = SwMirPos ("G", SciMAF(n)%GSA_pos)
           GHz_sw_pos(n) = SciMAF(n)%GHz_sw_pos
        ENDDO
 
@@ -166,6 +215,9 @@ print *, 'flags: ', WeightsFlags(i)
 END MODULE CalibWeightsFlags
 !=============================================================================
 ! $Log$
+! Revision 2.2  2003/09/15 17:15:53  perun
+! Version 1.3 commit
+!
 ! Revision 2.1  2003/08/15 14:54:55  perun
 ! Version 1.2 commit
 !
--- l1/THzUtils.f90
+++ l1/THzUtils.f90
@@ -1,4 +1,4 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
+! Copyright (c) 2003, California Institute of Technology.  ALL RIGHTS RESERVED.
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
 !=============================================================================
@@ -9,7 +9,7 @@ MODULE THzUtils
 
   PRIVATE
 
-  PUBLIC :: LLO_Bias, Bias_err, MaxBias
+  PUBLIC :: LLO_Bias, Bias_err, MaxBias, LLO_Label, ConvertLLO
 
   !------------------------------- RCS Ident Info ------------------------------
   CHARACTER(LEN=130) :: id = &
@@ -19,6 +19,14 @@ MODULE THzUtils
 
   REAL, PARAMETER :: Bias_err = 10.0
   REAL, PARAMETER :: MaxBias = 2.0
+
+  CHARACTER (LEN=*), PARAMETER, DIMENSION(16) :: LLO_Label = (/ &
+       "Pump MC PZT      ", "Pump CC PZT      ", "FIR IC PZT       ", &
+       "FIR OC PZT       ", "Pump Temperature ", "RF PS Temperature", &
+       "FIR Temperature  ", "Pump Thermopile  ", "Bias Signal      ", &
+       "RF Forward Power ", "Max FIR Power    ", "RFPA AGC         ", &
+       "FIR Thermopile   ", "+5 V Supply      ", "+12 V Supply     ", &
+       "-12 V Supply     " /)
 
 CONTAINS
 
@@ -55,9 +63,65 @@ CONTAINS
 
   END FUNCTION LLO_Bias
 
+!=============================================================================
+  SUBROUTINE ConvertLLO (DN, EU)
+!=============================================================================
+
+    USE MLSL1Utils, ONLY: BigEndianStr, QNan
+
+    CHARACTER (LEN=80), INTENT (IN) :: DN
+    REAL, INTENT (OUT) :: EU(16)
+
+! coefficients:
+
+    REAL, DIMENSION(16), PARAMETER :: c0 = (/ &
+         0.0, 0.0, 0.0, 0.0, -23.2, -19.655, -23.2, -0.1124, &
+         0.0, 0.0, 0.0, -0.1124, -0.02, 0.0, 0.0, -30.0 /)
+    REAL, DIMENSION(16), PARAMETER :: c1 = (/ &
+         0.1, 0.1, 0.1, 0.1, 16.155, 18.414, 16.155, 3.5636, &
+         1.0, 1.0, 1.0, 3.5636, 6.84, 1.0, 2.0, 6.0 /)
+
+    CHARACTER (LEN=2) :: cdat
+    CHARACTER (LEN=5), PARAMETER :: ack7 = CHAR(250)//CHAR(250)//CHAR(3)//&
+         CHAR(0)//CHAR(7)
+
+    INTEGER :: i, n1
+    REAL :: xdn
+
+    EU = QNan()
+    IF (DN(1:5) == ack7) THEN
+
+       DO i = 1, 16
+          n1 = (i - 1) * 2 + 1
+          cdat(1:1) = DN(n1+12:n1+12)
+          cdat(2:2) = DN(n1+11:n1+11)
+          IF (i == 11) THEN
+             EU(i) = BigEndianStr (cdat)
+          ELSE IF (i == 12) THEN
+             EU(i) = BigEndianStr (DN(n1+11:n1+12))
+          ELSE
+             xdn = BigEndianStr (cdat)
+             IF (i /= 11) xdn = xdn / 409.6
+             xdn = c0(i) + c1(i) * xdn
+             IF (i /= 6) THEN
+                EU(i) = xdn
+             ELSE
+                EU(i) = xdn + xdn * xdn * (-5.606) + &
+                     xdn * xdn * xdn * 1.509
+             ENDIF
+          ENDIF
+       ENDDO
+
+    ENDIF
+
+  END SUBROUTINE ConvertLLO
+
 END MODULE THzUtils
 
 ! $Log$
+! Revision 2.2  2003/09/15 17:15:54  perun
+! Version 1.3 commit
+!
 ! Revision 2.1  2003/01/31 18:13:34  perun
 ! Version 1.1 commit
 !
