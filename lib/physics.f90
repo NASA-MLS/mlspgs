f9a6440b7c9a2a01d0a97499ca3fd86c659d232a
vsnyder <>
2004-03-20 04:05:02 +0000

    Add Avogadro and MMM.  Move SpeedOfLight from units.  Define Boltz in terms of Avogadro, MMM, k and ln10 (instead of just typing it in).

--- lib/physics.f90
+++ lib/physics.f90
@@ -3,19 +3,28 @@
 
 module PHYSICS
 ! Physical constants
-  use MLSCommon, only: R8
+  use MLSCommon, only: R8, RP
+  use Units, only: LN10
   implicit NONE
   public
-  Real(r8), Parameter :: H = 6.62606891e-34_r8 ! Physics News, 1998
-  Real(r8), Parameter :: K = 1.3806503e-23_r8  ! NIST, 1998
-  Real(r8), Parameter :: H_OVER_K = H / K * 1.0e6_r8 ! \nu in MHz
-! Real(r8), Parameter :: H_OVER_K = 4.7992377e-5_r8 ! using H and K above
-! Real(r8), Parameter :: H_OVER_K = 4.7992157e-5_r8 ! Zvi's original
+
+  real(r8), parameter :: Avogadro = 6.0221415e23_r8  ! #/mol NIST web site 2004
+  real(rp), parameter :: MMM = 0.028964_rp           ! Mean molecular mass of atmosphere kg/mol
+  real(r8), parameter :: H = 6.62606891e-34_r8       ! J s Physics News, 1998
+  real(r8), parameter :: K = 1.3806503e-23_r8        ! J/K NIST, 1998
+  real(r8), parameter :: H_OVER_K = H / K * 1.0e6_r8 ! \nu in MHz
+! real(r8), parameter :: H_OVER_K = 4.7992377e-5_r8  ! using H and K above
+! real(r8), parameter :: H_OVER_K = 4.7992157e-5_r8  ! Zvi's original
+  real(rp), parameter :: Boltz = Avogadro * k * ln10 / mmm ! m^2/(K s^2),
+! real(rp), parameter :: Boltz = 660.9852945_rp      ! Using above values
+! real(rp), parameter :: Boltz = 660.988_rp          ! Zvi's original
+  real(r8), parameter :: SpeedOfLight = 299792458.3_r8 ! Meters/Second
+
 !---------------------------- RCS Ident Info -------------------------------
-  private :: Id, ModuleName
-  CHARACTER (LEN=256) :: Id = &
-       "$Id$"
-  CHARACTER (LEN=*), PARAMETER :: ModuleName= "$RCSfile$"
+  private :: Id, IdParm, ModuleName
+  character(len=*), parameter :: IdParm = "$Id$"
+  character (len=256) :: Id = idParm
+  character (len=*), parameter :: ModuleName= "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
 contains 
@@ -25,6 +34,9 @@ contains
 
 end module PHYSICS
 ! $Log$
+! Revision 2.1  2003/05/05 23:00:05  livesey
+! Merged in feb03 newfwm branch
+!
 ! Revision 1.1.2.1  2003/04/16 20:11:30  vsnyder
 ! Moved from ../fwdmdl
 !
