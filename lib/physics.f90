2a3c095377ca04ffb5d8f6a83e8fae739e273ba4
vsnyder <>
2004-04-01 23:59:37 +0000

    Get latest values from NIST, add Bohr and G_e

--- lib/physics.f90
+++ lib/physics.f90
@@ -8,17 +8,20 @@ module PHYSICS
   implicit NONE
   public
 
-  real(r8), parameter :: Avogadro = 6.0221415e23_r8  ! #/mol NIST web site 2004
-  real(rp), parameter :: MMM = 0.028964_rp           ! Mean molecular mass of atmosphere kg/mol
-  real(r8), parameter :: H = 6.62606891e-34_r8       ! J s Physics News, 1998
-  real(r8), parameter :: K = 1.3806503e-23_r8        ! J/K NIST, 1998
+  ! NIST as of 2004-04-01: http://physics.nist.gov/cuu/Constants/index.html
+  real(r8), parameter :: Avogadro = 6.0221415e23_r8  ! #/mol ± 10e16 NIST 2004
+  real(r8), parameter :: Bohr = 1.39962458_r8        ! MHz/Gauss, ± 12e-8 NIST 2004
+  real(r8), parameter :: G_E = 2.0023193043718_r8    ! ± 75e-13 NIST 2004
+  real(rp), parameter :: MMM = 0.028964_rp           ! Kg/mol, Mean molecular mass of atmosphere kg/mol
+  real(r8), parameter :: H = 6.62606931e-34_r8       ! J s, ± 11e-41 NIST 2004
+  real(r8), parameter :: K = 1.3806505e-23_r8        ! J/K ± 24e-20, NIST 2004
   real(r8), parameter :: H_OVER_K = H / K * 1.0e6_r8 ! \nu in MHz
 ! real(r8), parameter :: H_OVER_K = 4.7992377e-5_r8  ! using H and K above
 ! real(r8), parameter :: H_OVER_K = 4.7992157e-5_r8  ! Zvi's original
   real(rp), parameter :: Boltz = Avogadro * k * ln10 / mmm ! m^2/(K s^2),
 ! real(rp), parameter :: Boltz = 660.9852945_rp      ! Using above values
 ! real(rp), parameter :: Boltz = 660.988_rp          ! Zvi's original
-  real(r8), parameter :: SpeedOfLight = 299792458.3_r8 ! Meters/Second
+  real(r8), parameter :: SpeedOfLight = 299792458.0_r8 ! M/s, NIST 2004
 
 !---------------------------- RCS Ident Info -------------------------------
   private :: Id, IdParm, ModuleName
@@ -34,6 +37,10 @@ contains
 
 end module PHYSICS
 ! $Log$
+! Revision 2.2  2004/03/20 04:05:02  vsnyder
+! Add Avogadro and MMM.  Move SpeedOfLight from units.  Define Boltz in terms
+! of Avogadro, MMM, k and ln10 (instead of just typing it in).
+!
 ! Revision 2.1  2003/05/05 23:00:05  livesey
 ! Merged in feb03 newfwm branch
 !
