a631a2e1955843b1127c7105a7b11999149cc433
vsnyder <>
2004-06-08 19:29:27 +0000

    rev 2.3

--- l2/PFAData_m.f90
+++ l2/PFAData_m.f90
@@ -14,6 +14,7 @@ module PFAData_m
   public :: PFAData_t, PFAData
   public :: Get_PFAdata_from_l2cf
   public :: Destroy_PFADataBase, Dump_PFADataBase, Dump
+  public :: Write_PFADataBase, Read_PFADataBase
 
   interface Dump
     module procedure Dump_PFADatum
@@ -26,7 +27,7 @@ module PFAData_m
     integer :: SignalIndex                         ! in Signals database
     type(signal_t) :: TheSignal                    ! The signal, with channels
                                                    ! and sidebands added
-    real(r4), pointer :: LnT(:) => NULL()          ! Log temperatures
+    type(vGrid_t), pointer :: TGrid => NULL()      ! Log temperatures
     type(vGrid_t), pointer :: VGrid => NULL()      ! vertical grid
     real(r4) :: VelLin                             ! Velocity linearization, km/s
     real(r4), pointer :: Absorption(:,:) => NULL() ! Ln Absorption data
@@ -49,48 +50,61 @@ module PFAData_m
 contains ! =====     Public Procedures     =============================
 
   ! --------------------------------------  Get_PDAdata_from_l2cf  -----
-  subroutine Get_PFAdata_from_l2cf ( Root, Name, VGrids )
+  subroutine Get_PFAdata_from_l2cf ( Root, Name, VGrids, Error )
   ! Process a PFAdata specification from the l2cf.
 
     use Allocate_Deallocate, only: Allocate_Test, Deallocate_Test
     use Expr_m, only: Expr
     use Init_Tables_Module, only: F_Absorption, F_dAbsDnc, F_dAbsDnu, &
-      & F_dAbsDwc, F_Molecules, F_Signal, F_Temperatures, F_VelLin, F_VGrid
+      & F_dAbsDwc, F_File, F_Molecules, F_Signal, F_Temperatures, F_VelLin, &
+      & F_VGrid, Field_First, Field_Last, L_Zeta
     use Intrinsic, only: PHYQ_Dimensionless, PHYQ_Temperature, PHYQ_Velocity
-    use MLSMessageModule, only: MLSMessage, MLSMSG_Error
+    use IO_Stuff, only: Get_Lun
     use MLSSignals_m, only: Signals
+    use MLSStrings, only: Capitalize
     use MoreTree, only: Get_Field_ID
     use Parse_Signal_m, only: Parse_Signal
     use String_Table, only: Get_String
-    use Tree, only: Decorate, Decoration, NSons, Sub_Rosa, Subtree
+    use Tree, only: Decorate, Decoration, Node_Id, NSons, Sub_Rosa, Subtree
+    use Tree_Types, only: N_String
 
     integer, intent(in) :: Root            ! of the pfaData subtree in the l2cf
     integer, intent(in) :: Name            ! of the pfaData spec, else zero
     type(vGrid_t), intent(in), target :: VGrids(:) ! database of vgrids
+    integer, intent(out) :: Error          ! 0 => OK, else trouble
 
     ! Error codes
-    integer, parameter :: SignalParse = 1
+    integer, parameter :: CannotOpen = 1
+    integer, parameter :: CannotRead = cannotOpen + 1
+    integer, parameter :: NotZeta = cannotRead + 1
+    integer, parameter :: SignalParse = notZeta + 1
     integer, parameter :: TooManyChannels = signalParse + 1
     integer, parameter :: TooManySignals = tooManyChannels + 1
-    integer, parameter :: WrongSize = tooManySignals + 1
+    integer, parameter :: WrongFields = tooManySignals + 1
+    integer, parameter :: WrongSize = wrongFields + 1
     integer, parameter :: WrongUnits = wrongSize + 1
 
     integer :: AbsTree
     logical, pointer :: Channels(:)
     integer :: dAbsDncTree, dAbsDnuTree, dAbsDwcTree, Dim
-    logical :: Error
-    integer :: I, J, NArrays, NPress, NTemps, Sideband
+    integer :: Field, FileIndex ! Where in the tree is the filename?
+    character(255) :: FileName, FileType ! Formatted(default), Unformatted
+    logical :: Got(field_first:field_last)
+    integer :: I, IOStat, J, Lun, NArrays, NPress, NTemps, Sideband
     integer, pointer :: SignalIndices(:)
-    integer :: Son, TemperatureTree, Units(2)
+    integer :: Son, Units(2)
     type(pfaData_t) :: PFADatum
     double precision :: Value(2)
 
-    error = .false.
+    error = 0
+    got = .false.
     nullify ( channels, signalIndices )
     pfaDatum%name = name
     do i = 2, nsons(root)
       son = subtree(i,root)
-      select case ( get_field_id(son) )
+      field = get_field_id(son)
+      got(field) = .true.
+      select case ( field )
       case ( f_absorption )
         absTree = son
       case ( f_dAbsDnc )
@@ -99,14 +113,26 @@ contains ! =====     Public Procedures     =============================
         dAbsDnuTree = son
       case ( f_dAbsDwc )
         dAbsDwcTree = son
+      case ( f_file )
+        fileIndex = subtree(2,son)
+        fileType = 'formatted'
+        if ( node_id(fileIndex) /= n_string ) then
+          call get_string( sub_rosa(subtree(2,fileIndex)), fileType, strip=.true. )
+          fileIndex = subtree(1,fileIndex)
+        end if
+        call get_string ( sub_rosa(fileIndex), fileName, strip=.true. )
       case ( f_molecules )
         call allocate_test ( pfaDatum%molecules, nsons(son)-1, &
           & 'pfaDatum%molecules', moduleName )
         do j = 2, nsons(son)
           pfaDatum%molecules(j-1) = decoration(subtree(j,son))
         end do
+      case ( f_temperatures )
+        pfaDatum%tGrid => vgrids(decoration(decoration(subtree(2,son))))
       case ( f_vGrid )
         pfaDatum%vGrid => vgrids(decoration(decoration(subtree(2,son))))
+        if ( pfaDatum%vGrid%verticalCoordinate /= l_zeta ) &
+          & call announce_error ( subtree(1,son), notZeta )
       case ( f_signal )
         call get_string ( sub_rosa(subtree(2,son)), pfaDatum%signal, strip=.true. )
         call parse_signal ( pfaDatum%signal, signalIndices, &
@@ -122,75 +148,100 @@ contains ! =====     Public Procedures     =============================
         pfaDatum%theSignal%channels => channels
         pfaDatum%theSignal%sideband = sideband
         call deallocate_test ( signalIndices, 'SignalIndices', moduleName )
-      case ( f_temperatures )
-        temperatureTree = son
       case ( f_velLin )
         call expr ( subtree(2,son), units, value )
-        if ( units(1) /= phyq_velocity .and. units(1) /= phyq_dimensionless ) &
+        if ( units(1) /= phyq_velocity ) &
           & call announce_error ( subtree(1,son), wrongUnits, 'Velocity' )
-        pfaDatum%velLin = value(1)
+        pfaDatum%velLin = value(1) / 1000.0 ! fundamental unit is m/s, fwdmdl wants km/s
       end select
     end do
 
-    ! Check sizes
+    ! Check that we have file and not absorption etc, or vice-versa
+    if ( got(f_file) .and. &
+         any( (/ &
+           & got(f_absorption), got(f_dAbsDnc), got(f_dAbsDnu), got(f_dAbsDwc) /) ) &
+    & .or. .not. got(f_file) .and. .not. &
+         all( (/ &
+           & got(f_absorption), got(f_dAbsDnc), got(f_dAbsDnu), got(f_dAbsDwc) /)) ) &
+      call announce_error ( root, wrongFields )
+
     nPress = pfaDatum%vGrid%noSurfs
-    nTemps = nsons(temperatureTree) - 1
+    nTemps = pfaDatum%tGrid%noSurfs
     nArrays = nPress * nTemps + 1
-    if ( nSons(absTree) /= nArrays ) &
-      call announce_error ( subtree(1,absTree), wrongSize, 'Absorption', &
-        & nArrays )
-    if ( nSons(dAbsDncTree) /= nArrays ) &
-      call announce_error ( subtree(1,dAbsDncTree), wrongSize, 'd Abs / d nc', &
-        & nArrays )
-    if ( nSons(dAbsDnuTree) /= nArrays ) &
-      call announce_error ( subtree(1,dAbsDnuTree), wrongSize, 'd Abs / d nu', &
-        & nArrays )
-    if ( nSons(dAbsDwcTree) /= nArrays ) &
-      call announce_error ( subtree(1,dAbsDwcTree), wrongSize, 'd Abs / d wc', &
-        & nArrays )
 
-    ! Store temperatures
-    call allocate_test ( pfaDatum%lnT, nTemps, 'pfaDatum%lnT', moduleName )
-    dim = phyq_dimensionless
-    do j = 2, nTemps + 1
-      call expr ( subtree(j,temperatureTree), units, value )
-      pfaDatum%lnT(j-1) = value(1)
-      if ( units(1) /= phyq_dimensionless ) then
-        if ( units(1) /= phyq_temperature ) then
-          call announce_error ( subtree(j,temperatureTree), wrongUnits, &
-            & 'Temperature' )
+    call allocate_test ( pfaDatum%absorption, nTemps, nPress, 'pfaDatum%absorption', moduleName )
+    call allocate_test ( pfaDatum%dAbsDnc,    nTemps, nPress, 'pfaDatum%dAbsDnc',    moduleName )
+    call allocate_test ( pfaDatum%dAbsDnu,    nTemps, nPress, 'pfaDatum%dAbsDnu',    moduleName )
+    call allocate_test ( pfaDatum%dAbsDwc,    nTemps, nPress, 'pfaDatum%dAbsDwc',    moduleName )
+
+    if ( got(f_file) ) then
+      call get_lun ( lun )
+      fileName = trim(fileName) // pfaDatum%signal
+      open ( unit=lun, file=fileName, form=fileType, status='old', iostat=iostat )
+      if ( iostat /= 0 ) then
+        call announce_error ( fileIndex, cannotOpen, fileName, iostat )
+      else
+        if ( capitalize(fileType) /= 'UNFORMATTED' ) then
+          read ( lun, *, iostat=iostat ) pfaDatum%absorption, pfaDatum%dAbsDnc, &
+            & pfaDatum%dAbsDnu, pfaDatum%dAbsDwc
         else
-          dim = phyq_temperature
+          read ( lun, iostat=iostat ) pfaDatum%absorption, pfaDatum%dAbsDnc, &
+            & pfaDatum%dAbsDnu, pfaDatum%dAbsDwc
         end if
+        if ( iostat /= 0 ) &
+          & call announce_error ( fileIndex, cannotRead, fileName, iostat )
       end if
-    end do
-    if ( dim /= phyq_temperature ) &
-      & call announce_error ( subtree(1,temperatureTree), wrongUnits, &
-        & 'Temperature' )
+    else
+      ! Check sizes
+      if ( nSons(absTree) /= nArrays ) &
+        call announce_error ( subtree(1,absTree), wrongSize, 'Absorption', &
+          & nArrays )
+      if ( nSons(dAbsDncTree) /= nArrays ) &
+        call announce_error ( subtree(1,dAbsDncTree), wrongSize, 'd Abs / d nc', &
+          & nArrays )
+      if ( nSons(dAbsDnuTree) /= nArrays ) &
+        call announce_error ( subtree(1,dAbsDnuTree), wrongSize, 'd Abs / d nu', &
+          & nArrays )
+      if ( nSons(dAbsDwcTree) /= nArrays ) &
+        call announce_error ( subtree(1,dAbsDwcTree), wrongSize, 'd Abs / d wc', &
+          & nArrays )
+      ! Get data from the tree into the data structure
+      call store_2d ( absTree, pfaDatum%absorption )
+      call store_2d ( dAbsDncTree, pfaDatum%dAbsDnc )
+      call store_2d ( dAbsDnuTree, pfaDatum%dAbsDnu )
+      call store_2d ( dAbsDwcTree, pfaDatum%dAbsDwc )
+    end if
 
-    call store_2d ( absTree, pfaDatum%absorption, 'pfaDatum%absorption' )
-    call store_2d ( dAbsDncTree, pfaDatum%dAbsDnc, 'pfaDatum%dAbsDnc' )
-    call store_2d ( dAbsDnuTree, pfaDatum%dAbsDnu, 'pfaDatum%dAbsDnu' )
-    call store_2d ( dAbsDwcTree, pfaDatum%dAbsDwc, 'pfaDatum%dAbsDwc' )
-
-    if ( error ) call MLSMessage ( MLSMSG_Error, ModuleName, &
-      & 'Unable to complete processing of PFAData' )
-
-    call decorate ( root, addPFADatumToDatabase ( pfaData, pfaDatum ) )
+    if ( error == 0 ) &
+      & call decorate ( root, addPFADatumToDatabase ( pfaData, pfaDatum ) )
 
   contains
 
     ! ...........................................  Announce_Error  .....
     subroutine Announce_Error ( Where, What, String, More )
+      use Machine, only: IO_Error
       use MoreTree, only: StartErrorMessage
       use OUTPUT_M, only: OUTPUT
       integer, intent(in) :: Where             ! Tree node index
       integer, intent(in) :: What              ! Error index
       character(len=*), intent(in), optional :: String  ! For more info
       integer, intent(in), optional :: More    ! For more info
-      error = .true.
+      error = 1
       call startErrorMessage ( where )
       select case ( what )
+      case ( cannotOpen )
+        call output ( 'Cannot open file ' )
+        call output ( trim(string) )
+        call output ( more, before='.  IOSTAT = ', after='.', advance='yes' )
+        call io_error ( 'Cannot open file ', more, trim(string) )
+      case ( cannotRead )
+        call output ( 'Cannot read file ' )
+        call output ( trim(string) )
+        call output ( more, before='.  IOSTAT = ', after='.', advance='yes' )
+        call io_error ( 'Cannot read file ', more, trim(string) )
+      case ( notZeta )
+        call output ( 'Vertical coordinate for pressure grid must be Zeta.', &
+          & advance='yes' )
       case ( signalParse )
         call output ( 'Unable to parse signal ' )
         call output ( trim(string), advance='yes' )
@@ -200,6 +251,11 @@ contains ! =====     Public Procedures     =============================
       case ( tooManySignals )
         call output ( string )
         call output ( ' Describes more than one signal.', advance='yes' )
+      case ( wrongFields )
+        call output ( 'Need either file and not absorption, dAbsDnc, dAbsDnu or dAbsDwc,', &
+          advance='yes' )
+        call output ( 'or not file and all of absorption, dAbsDnc, dAbsDnu and dAbsDwc,', &
+          advance='yes' )
       case ( wrongSize )
         call output ( 'Incorrect size for ' )
         call output ( trim(string) )
@@ -211,15 +267,13 @@ contains ! =====     Public Procedures     =============================
     end subroutine Announce_Error
 
     ! .................................................  Store_2d  .....
-    subroutine Store_2d ( Where, What, Name )
+    subroutine Store_2d ( Where, What )
     ! Store data from Where in the L2CF into an nTemps X nPress
     ! array What
       integer, intent(in) :: Where
       real(r4), pointer :: What(:,:)
-      character(len=*), intent(in) :: Name
       integer :: I, J, K
 
-      call allocate_test ( what, nTemps, nPress, name, moduleName )
       j = 0
       k = 1
       do i = 2, nsons(where)
@@ -247,7 +301,6 @@ contains ! =====     Public Procedures     =============================
       call deallocate_test ( pfaData(i)%molecules, 'pfaData%molecules', moduleName )
       call deallocate_test ( pfaData(i)%theSignal%channels, 'pfaData...Channels', &
           & moduleName )
-      call deallocate_test ( pfaData(i)%lnT, 'pfaData%lnT', moduleName )
       call deallocate_test ( pfaData(i)%absorption, 'pfaData%absorption', moduleName )
       call deallocate_test ( pfaData(i)%dAbsDwc, 'pfaData%dAbsDwc', moduleName )
       call deallocate_test ( pfaData(i)%dAbsDnc, 'pfaData%dAbsDnc', moduleName )
@@ -311,9 +364,11 @@ contains ! =====     Public Procedures     =============================
     end if
     call displaySignalName ( pfaDatum%theSignal, advance='yes' )
 
-    call dump ( pfaDatum%lnT, name=' Ln Temperatures' )
 
-    call output ( ' VGrid: ' )
+    call output ( ' TGrid: ' )
+    call display_string ( pfaDatum%tGrid%name )
+
+    call output ( ', VGrid: ' )
     call display_string ( pfaDatum%vGrid%name, advance='yes' )
 
     call output ( pfaDatum%velLin, before=' Velocity Linearization: ', &
@@ -325,6 +380,16 @@ contains ! =====     Public Procedures     =============================
     call dump ( pfaDatum%dAbsDnu, name=' d ln Absorption / d nu' )
 
   end subroutine Dump_PFADatum
+
+  ! ------------------------------------------  Write_PFADatabase  -----
+  subroutine Write_PFADatabase ( FileName )
+    character(len=*), intent(in) :: FileName
+  end subroutine Write_PFADatabase
+
+  ! -------------------------------------------  Read_PFADatabase  -----
+  subroutine Read_PFADatabase ( FileName )
+    character(len=*), intent(in) :: FileName
+  end subroutine Read_PFADatabase
 
 ! =====     Private Procedures     =====================================
 
@@ -355,6 +420,9 @@ contains ! =====     Public Procedures     =============================
 end module PFAData_m
 
 ! $Log$
+! Revision 2.3  2004/06/08 19:29:27  vsnyder
+! Add file field
+!
 ! Revision 2.2  2004/05/29 02:51:40  vsnyder
 ! Allow signal string to denote only one signal
 !
