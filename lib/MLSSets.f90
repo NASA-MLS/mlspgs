908a6c3d81cf67f36358de554e5b762190923d93
pwagner <>
2004-06-11 19:03:35 +0000

    rev 2.3

--- lib/MLSSets.f90
+++ lib/MLSSets.f90
@@ -7,14 +7,18 @@ module MLSSets
 
   implicit NONE
   private
-  public :: FindFirst, FindNext, Intersection, Union
+  public :: FindFirst, FindNext, FindAll, Intersection, Union
 
   interface FindFirst
-    module procedure FindFirstInteger, FindFirstLogical
+    module procedure FindFirstInteger, FindFirstLogical!, FindFirstCharacter
   end interface
 
   interface FindNext
-    module procedure FindNextInteger, FindNextLogical
+    module procedure FindNextInteger, FindNextLogical!, FindNextCharacter
+  end interface
+
+  interface FindAll
+    module procedure FindAllInteger, FindAllLogical!, FindAllCharacter
   end interface
 
 ! === (start of toc) ===                                                 
@@ -236,6 +240,104 @@ contains ! =====     Public Procedures     =============================
 
   end function Union
 
+  ! ------------------------------------------  FindAllInteger  -----
+  subroutine FindAllInteger ( SET, IT, WHICH, HOW_MANY, RE_MAINDER, WHICH_NOT )
+    ! Return which i of set[i] = it
+    ! optionally, may return also:
+    ! how many of them do
+    ! which_not of them (which don't)
+    ! and the re_mainder of the set != it
+    ! e.g. given set = /(4, 3, 1, 2, 1, 3 )/ and it = 1
+    ! produces which = /(3, 5)/, 
+    !      which_not = /(1, 2, 4, 6)/, 
+    !       how_many = 2,
+    !     re_mainder = /(4, 3, 2, 3)/
+    
+    ! Note that which and which_not are arrays of array indices
+    ! while re_mainder is an array of array elements
+    
+    ! This may be useful,
+    ! e.g. in dump for reshaping an array to suppress any dims 
+    ! that are identically 1
+    
+    ! Maybe this should be rewritten to use fraternal functions findfirst
+    ! or findnext
+
+    ! Formal arguments
+    integer, intent(in), dimension(:)  ::           set
+    integer, intent(in)                ::           it
+    integer, intent(out), dimension(:) ::           which
+    integer, intent(out), optional ::               how_many
+    integer, intent(out), dimension(:), optional :: re_mainder
+    integer, intent(out), dimension(:), optional :: which_not
+
+    ! local variables
+    integer :: i, i_which, i_re_mainder
+    
+    if ( size(set) < 1 .or. size(which) < 1 ) then
+      if ( present(how_many) ) how_many = 0
+      if ( present(re_mainder) ) re_mainder = 0
+      return
+    end if
+    i_which = 0
+    i_re_mainder = 0
+    do i=1, size(set)
+      if ( set(i) == it ) then
+        i_which = i_which+1
+        which(min(size(which), i_which)) = i
+      else
+        i_re_mainder = i_re_mainder+1
+        if ( present(which_not) ) &
+          & which_not(min(size(which_not), i_re_mainder)) = i
+        if ( present(re_mainder) ) &
+          & re_mainder(min(size(re_mainder), i_re_mainder)) = set(i)
+      end if
+    end do
+    if ( present(how_many) ) how_many = i_which
+
+  end subroutine FindAllInteger
+
+  ! ------------------------------------------  FindAllLogical  -----
+  subroutine FindAllLogical ( set, WHICH, HOW_MANY, WHICH_NOT )
+    ! Return which i of set[i] = .true.
+    ! optionally, may return also:
+    ! how many of them do
+    ! which_not of them (which don't)
+    ! e.g. given set = /(F, F, T, F, T, F )/
+    ! produces which = /(3, 5)/, 
+    !      which_not = /(1, 2, 4, 6)/, 
+    !       how_many = 2,
+    
+    ! Note that which and which_not are arrays of array indices
+    ! Formal arguments
+    logical, intent(in), dimension(:)  ::           set
+    integer, intent(out), dimension(:) ::           which
+    integer, intent(out), optional ::               how_many
+    integer, intent(out), dimension(:), optional :: which_not
+
+    ! local variables
+    integer :: i, i_which, i_re_mainder
+    
+    if ( size(set) < 1 .or. size(which) < 1 ) then
+      if ( present(how_many) ) how_many = 0
+      return
+    end if
+    i_which = 0
+    i_re_mainder = 0
+    do i=1, size(set)
+      if ( set(i) ) then
+        i_which = i_which+1
+        which(min(size(which), i_which)) = i
+      else
+        i_re_mainder = i_re_mainder+1
+        if ( present(which_not) ) &
+          & which_not(min(size(which_not), i_re_mainder)) = i
+      end if
+    end do
+    if ( present(how_many) ) how_many = i_which
+
+  end subroutine FindAllLogical
+
 ! =====     Private Procedures     =====================================
 
   logical function not_used_here()
@@ -245,6 +347,9 @@ contains ! =====     Public Procedures     =============================
 end module MLSSets
 
 ! $Log$
+! Revision 2.3  2004/06/11 19:03:35  pwagner
+! Added FindAll
+!
 ! Revision 2.2  2004/06/10 20:03:14  vsnyder
 ! Don't use Allocate_Test -- results in USE cycle
 !
