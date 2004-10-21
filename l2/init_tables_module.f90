07cda536d14f8f6f4dd052ade6b8b4062459ce5e
vsnyder <>
2004-10-21 00:45:02 +0000

    rev 2.387

--- l2/init_tables_module.f90
+++ l2/init_tables_module.f90
@@ -206,9 +206,9 @@ module INIT_TABLES_MODULE
         (/OK,   OK,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! Start
           OK,   OK,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! mlsSignals
           OK,   OK,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! spectroscopy
-          OK,   OK,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! globalSettings
-          OK,   OK,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! readApriori
-          OK,   OK,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! mergeGrids
+           0,    0,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! globalSettings
+           0,    0,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! readApriori
+           0,    0,   OK,   OK,   OK,   OK,    0,    0,    0,    0,   OK,    0,  & ! mergeGrids
            0,    0,    0,    0,    0,    0,   OK,   OK,   OK,   OK,   OK,   OK,  & ! chunkDivide
            0,    0,    0,    0,    0,    0,   OK,   OK,   OK,   OK,   OK,   OK,  & ! Construct
            0,    0,    0,    0,    0,    0,   OK,   OK,   OK,   OK,   OK,   OK,  & ! Fill
@@ -1293,6 +1293,9 @@ contains ! =====     Public procedures     =============================
 end module INIT_TABLES_MODULE
 
 ! $Log$
+! Revision 2.387  2004/10/21 00:45:02  vsnyder
+! Require mlsSignals and spectroscopy before globalSettings etc.
+!
 ! Revision 2.386  2004/10/16 17:25:12  livesey
 ! Added asciiFile fill method
 !
