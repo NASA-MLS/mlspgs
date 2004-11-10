6afb3dac0390cc010f8fc6b5385868b724c8c30c
perun <>
2004-11-10 15:36:53 +0000

    rev 2.2

--- l1/FOV.f90
+++ l1/FOV.f90
@@ -1,4 +1,4 @@
-! Copyright (c) 2003, California Institute of Technology.  ALL RIGHTS RESERVED.
+! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
 
 !=============================================================================
@@ -12,7 +12,8 @@ MODULE FOV   ! Field-Of-View routines/data
 
   PRIVATE
 
-  PUBLIC :: InitFOVconsts, CalcMountsToFOV
+  PUBLIC :: InitFOVconsts, CalcMountsToFOV, GroundToFlightMountsGHz, &
+       GroundToFlightMountsTHz, ScToGroundMountsGHz, ScToGroundMountsTHz 
 
   !------------------------------- RCS Ident Info ------------------------------
   CHARACTER(LEN=130) :: id = &
@@ -20,10 +21,27 @@ MODULE FOV   ! Field-Of-View routines/data
   CHARACTER(LEN=*), PARAMETER :: ModuleName="$RCSfile$"
   !-----------------------------------------------------------------------------
 
-  REAL(r8) :: n0(3)
-  REAL(r8) :: M(3,3)
-  REAL(r8) :: S1(3)
-  REAL(r8), PARAMETER :: H1(3) = (/ -0.904784d0, 0.002174d0, -0.425866d0 /)
+  ! Last dimension equals 2 where 1 = GHz and 2 = THz:
+
+  REAL(r8) :: n0(3,2)
+  REAL(r8) :: M(3,3,2)
+  REAL(r8) :: S1(3,2)
+  REAL(r8), PARAMETER :: H1(3,2) = RESHAPE ( &
+       (/ -0.9047748d0,  0.0023903d0, -0.4258837d0, &
+          -0.9047769d0, -0.0017294d0, -0.4258824d0 /), (/ 3, 2 /))
+
+  REAL(r8), PARAMETER :: GroundToFlightMountsGHz(3,3) = RESHAPE ((/ &
+       1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 /), (/ 3, 3 /))
+  REAL(r8), PARAMETER :: GroundToFlightMountsTHz(3,3) = RESHAPE ((/ &
+       1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 /), (/ 3, 3 /))
+  REAL(r8), PARAMETER :: ScToGroundMountsGHz(3,3) = RESHAPE ((/ &
+       1.000000, -0.000401, 0.000428, &
+       0.000401, 1.00000, 0.000586, &
+       -0.000428, -0.000586, 1.0000000 /), (/ 3, 3 /))
+  REAL(r8), PARAMETER :: ScToGroundMountsTHz(3,3) = RESHAPE ((/ &
+       1.000000, -0.000376, -0.000090, &
+       0.000376, 1.00000, 0.000050, &
+       0.000090, -0.000050, 1.0000000 /), (/ 3, 3 /))
 
 CONTAINS
 
@@ -31,18 +49,18 @@ CONTAINS
   SUBROUTINE InitFOVconsts   ! initialize FOV constants
 !=============================================================================
 
-    INTEGER :: i
+    INTEGER :: i, n
 
-! constants for GHz Module
+! constants for each Module
 
-    REAL(r8), PARAMETER :: dc = -0.001181d0
-    REAL(r8), PARAMETER :: thx = 0.000852d0
-    REAL(r8), PARAMETER :: thz = -0.002201d0
+    REAL(r8), PARAMETER :: dc(2) = (/ -0.001217d0, 0.000274d0 /)
+    REAL(r8), PARAMETER :: thx(2) = (/ 0.000903d0, -0.001574d0 /)
+    REAL(r8), PARAMETER :: thz(2) = (/ -0.002263d0, 0.000980d0 /)
 
-! constants for each GHz Rdm
+! constants for each Rdm
 
-    REAL(r8), PARAMETER :: dx = 0.000012d0
-    REAL(r8), PARAMETER :: dz = 0.000399d0
+    REAL(r8), PARAMETER :: dx(2) = (/ 0.000025d0, -0.000348d0 /)
+    REAL(r8), PARAMETER :: dz(2) = (/ 0.000046d0, 0.001144d0 /)
 
 ! other constants
 
@@ -50,23 +68,29 @@ CONTAINS
 
 ! Initialize other constants
 
-    n0 = (/ COS(pi_4 - dc), SIN(pi_4 - dc), 0.0d0 /)
+    DO n = 1, 2    ! GHz/THz
 
-    M = RESHAPE ((/ COS(thz), (COS(thx) * SIN(thz)), (SIN(thx) * SIN(thz)),&
-         -SIN(thz), (COS(thx) * COS(thz)), (SIN(thx) * COS(thz)), &
-         0.0d0, -SIN(thx),  COS(thx) /), (/ 3, 3 /))
+       n0(:,n) = (/ COS(pi_4 - dc(n)), SIN(pi_4 - dc(n)), 0.0d0 /)
 
-    DO i = 1, 3
-       S1(i)  = DOT_PRODUCT (M(i,:), (/ dx, -SQRT (1.0 - dx**2 - dz**2), dz /))
+       M(:,:,n) = RESHAPE ((/ COS(thz(n)), (COS(thx(n)) * SIN(thz(n))), &
+            (SIN(thx(n)) * SIN(thz(n))), -SIN(thz(n)), &
+            (COS(thx(n)) * COS(thz(n))), (SIN(thx(n)) * COS(thz(n))), 0.0d0, &
+            -SIN(thx(n)),  COS(thx(n)) /), (/ 3, 3 /))
+
+       DO i = 1, 3
+          S1(i,n)  = DOT_PRODUCT (M(i,:,n), (/ dx(n), &
+               -SQRT (1.0 - dx(n)**2 - dz(n)**2), dz(n) /))
+       ENDDO
     ENDDO
 
   END SUBROUTINE InitFOVconsts
 
 !=============================================================================
-  SUBROUTINE CalcMountsToFOV (eps_deg, MountsToFOV)
+  SUBROUTINE CalcMountsToFOV (eps_deg, gtindx, MountsToFOV)
 !=============================================================================
 
     REAL, INTENT (IN) :: eps_deg
+    INTEGER, INTENT(IN) :: gtindx   ! 1 = GHz, 2 = THz
     REAL(r8), INTENT (OUT) :: MountsToFOV(3,3)
 
     INTEGER :: i
@@ -80,23 +104,19 @@ CONTAINS
          -SIN(eps), 0.0d0, COS(eps) /), (/ 3, 3 /))
 
     DO i = 1, 3
-       ns(i)  = DOT_PRODUCT (s(i,:), n0)
+       ns(i)  = DOT_PRODUCT (s(i,:), n0(:,gtindx))
     ENDDO
 
     DO i = 1, 3
-       n(i)  = DOT_PRODUCT (M(i,:), ns)
+       n(i)  = DOT_PRODUCT (M(i,:,gtindx), ns)
     ENDDO
 
-    s2 = s1 - 2 * DOT_PRODUCT (n, s1) * n
-    h2 = h1 - 2 * DOT_PRODUCT (n, h1) * n
+    s2 = s1(:,gtindx) - 2 * DOT_PRODUCT (n, s1(:,gtindx)) * n
+    h2 = h1(:,gtindx) - 2 * DOT_PRODUCT (n, h1(:,gtindx)) * n
 
     E2(1) = h2(2) * s2(3) - h2(3) * s2(2)
     E2(2) = h2(3) * s2(1) - h2(1) * s2(3)
     E2(3) = h2(1) * s2(2) - h2(2) * s2(1)
-
-!!$    MountsToFOV(:,1) = E2
-!!$    MountsToFOV(:,2) = H2
-!!$    MountsToFOV(:,3) = S2
 
     MountsToFOV(1,:) = E2
     MountsToFOV(2,:) = H2
@@ -108,6 +128,9 @@ CONTAINS
 END MODULE FOV
 !=============================================================================
 ! $Log$
+! Revision 2.2  2004/11/10 15:36:53  perun
+! Revise calculations per REC
+!
 ! Revision 2.1  2003/08/15 14:25:04  perun
 ! Version 1.2 commit
 !
