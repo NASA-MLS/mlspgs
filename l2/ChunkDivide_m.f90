fcfbd53a147e6c300b668f3f85b46f2fd37b861b
vsnyder <>
2005-06-03 02:02:17 +0000

    rev 2.61

--- l2/ChunkDivide_m.f90
+++ l2/ChunkDivide_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module ChunkDivide_m
 
@@ -30,9 +38,6 @@ module ChunkDivide_m
   public :: DestroyChunkDatabase, ChunkDivide, ReduceChunkDatabase
 
   !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-    "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
   character (len=*), private, parameter :: ModuleName= &
     "$RCSfile$"
   private :: not_used_here 
@@ -2191,12 +2196,20 @@ contains ! ===================================== Public Procedures =====
   end subroutine smoothOutDroppedMAFs
 
   logical function not_used_here()
+  !---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+    "$Id$"
+  character (len=len(idParm)) :: Id = idParm
+  !---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module ChunkDivide_m
 
 ! $Log$
+! Revision 2.61  2005/06/03 02:02:17  vsnyder
+! New copyright notice, move Id to not_used_here to avoid cascades
+!
 ! Revision 2.60  2005/06/01 17:39:26  pwagner
 ! Dont read L1bFile if unassocated
 !
