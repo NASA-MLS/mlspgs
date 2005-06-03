20e9a7ff172268e87613e95b4312d09ee83ae389
vsnyder <>
2005-06-03 02:09:46 +0000

    rev 2.263

--- l2/RetrievalModule.f90
+++ l2/RetrievalModule.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2001, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 module RetrievalModule
@@ -15,9 +23,6 @@ module RetrievalModule
   public :: RETRIEVE
 
 !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
 !---------------------------------------------------------------------------
@@ -2288,12 +2293,20 @@ contains
   end subroutine Retrieve
 
   logical function NOT_USED_HERE()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)) :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function NOT_USED_HERE
 
 end module RetrievalModule
 
 ! $Log$
+! Revision 2.263  2005/06/03 02:09:46  vsnyder
+! New copyright notice, move Id to not_used_here to avoid cascades
+!
 ! Revision 2.262  2005/05/27 23:57:03  vsnyder
 ! Add Flush PFAData
 !
