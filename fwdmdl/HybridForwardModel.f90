c2911ddf68ba265a40d38732af586f4c1f2b8438
vsnyder <>
2005-06-03 01:59:43 +0000

    rev 2.6

--- fwdmdl/HybridForwardModel.f90
+++ fwdmdl/HybridForwardModel.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module HybridForwardModel_m
 
@@ -11,9 +19,6 @@ module HybridForwardModel_m
   public :: HybridForwardModel
 
   !------------------------------- RCS Ident Info ------------------------------
-  character(len=*), parameter :: IdParm = & 
-    "$Id$"
-  character(len=len(idParm)) :: Id = idParm
   character(len=*), parameter :: ModuleName = &
     & "$RCSfile$"
   private :: not_used_here 
@@ -136,12 +141,20 @@ contains ! =====     Public Procedures     =============================
   ! ----------------------------------------------------------------------------
 
   logical function not_used_here()
+  !------------------------------- RCS Ident Info ------------------------------
+  character(len=*), parameter :: IdParm = & 
+    "$Id$"
+  character(len=len(idParm)) :: Id = idParm
+  !-----------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module HybridForwardModel_m
 
 ! $Log$
+! Revision 2.6  2005/06/03 01:59:43  vsnyder
+! New copyright notice, move Id to not_used_here to avoid cascades
+!
 ! Revision 2.5  2003/10/29 00:44:09  livesey
 ! Added forceFoldedOuptut to full forward model call.
 !
