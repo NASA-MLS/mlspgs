99cb4bac4f80556ac418e473ada5e7d15383133a
vsnyder <>
2005-06-03 01:59:25 +0000

    New copyright notice, move Id to not_used_here to avoid cascades

--- fwdmdl/FullForwardModel_m.f90
+++ fwdmdl/FullForwardModel_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module FullForwardModel_m
 
@@ -10,12 +18,10 @@ module FullForwardModel_m
   public :: FullForwardModel
 
 !------------------------------ RCS Ident Info -------------------------------
-  character (len=*), parameter, private :: IdParm = &
-    & "$Id$"
-  character (len=len(idParm)) :: Id = IdParm
   character (len=*), parameter, private :: ModuleName= &
     & "$RCSfile$"
 !------------------------------------------------------------------------------
+
 contains
 
 !---------------------------------------------  FullForwardModel  -------------
@@ -3211,12 +3217,20 @@ contains
 
 ! ------------------------------------------------  not_used_here  -----
   logical function NOT_USED_HERE()
+!------------------------------ RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+    & "$Id$"
+  character (len=len(idParm)) :: Id = IdParm
+!------------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function NOT_USED_HERE
 
 end module FullForwardModel_m
 
 ! $Log$
+! Revision 2.235  2005/04/19 20:16:45  livesey
+! Added a couple of nullifys
+!
 ! Revision 2.234  2005/03/28 20:26:25  vsnyder
 ! Lots of PFA stuff
 !
