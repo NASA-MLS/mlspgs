7733b8acc42848cdb8356ac1f7ded5c136a1a48d
pwagner <>
2005-06-22 18:57:10 +0000

    rev 2.9

--- l2/CloudRetrievalModule.f90
+++ l2/CloudRetrievalModule.f90
@@ -1,3 +1,14 @@
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
+
 !=============================================================================
 module CloudRetrievalModule
 !=============================================================================
@@ -11,12 +22,11 @@ module CloudRetrievalModule
   private
   public :: CloudRetrieval
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
+  private :: not_used_here 
+!---------------------------------------------------------------------------
        
 contains
 
@@ -932,8 +942,20 @@ contains
   end subroutine LOS2Grid    
 
  end subroutine CloudRetrieval
+
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 end module CloudRetrievalModule
 ! $Log$
+! Revision 2.9  2005/06/22 18:57:01  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.8  2004/05/19 19:16:09  vsnyder
 ! Move MLSChunk_t to Chunks_m
 !
--- l2/FGrid.f90
+++ l2/FGrid.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module FGrid                    ! Frequency grid information
 
@@ -22,14 +30,11 @@ module FGrid                    ! Frequency grid information
   public :: FGrid_T, AddFGridToDatabase, CreateFGridFromMLSCFInfo, &
     & DestroyFGridContents, DestroyFGridDatabase, NullifyFgrid, dump
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   ! This module contains code for defining and maintaining 'FGrids'. These
   ! define frequency coordinates for use in quantity templates and thence
@@ -196,12 +201,20 @@ contains ! ===================================== Public procedures =====
   end subroutine NullifyFGrid
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module FGrid
 
 ! $Log$
+! Revision 2.9  2005/06/22 18:57:01  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.8  2004/12/27 23:05:14  vsnyder
 ! Remove unreferenced use names
 !
