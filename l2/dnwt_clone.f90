1b9b4b746905201e41b2aa2a60d8a60184b997c9
pwagner <>
2005-06-22 18:57:01 +0000

    rev 2.4

--- l2/dnwt_clone.f90
+++ l2/dnwt_clone.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module DNWT_CLONE
 
@@ -16,10 +24,7 @@ module DNWT_CLONE
   interface NWTDB; module procedure DNWTDB; end interface
 
     
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -64,12 +69,20 @@ contains
   subroutine DNWTDB
   end subroutine DNWTDB
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module DNWT_CLONE
 
 ! $Log$
+! Revision 2.4  2005/06/22 18:57:01  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.3  2002/10/07 23:43:11  pwagner
 ! Added idents to survive zealous Lahey optimizer
 !
