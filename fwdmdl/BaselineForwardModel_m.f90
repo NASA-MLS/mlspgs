1780cfa5ec4802645ebbf6de80b525a197f16329
pwagner <>
2005-06-22 18:08:18 +0000

    rev 2.24

--- fwdmdl/BaselineForwardModel_m.f90
+++ fwdmdl/BaselineForwardModel_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
  
 module BaselineForwardModel_m
 
@@ -9,14 +17,11 @@ module BaselineForwardModel_m
   private
   public :: BaselineForwardModel
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), parameter, private :: IdParm = &
-    & "$Id$"
-  character (len=len(idParm)) :: Id = IdParm
-  character (len=*), parameter, private :: ModuleName= &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
 contains ! ======================================== BaselineForwardModel ======
 
@@ -485,12 +490,20 @@ contains ! ======================================== BaselineForwardModel ======
   end subroutine BaselineForwardModel
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module BaselineForwardModel_m
   
 ! $Log$
+! Revision 2.24  2005/06/22 18:08:18  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.23  2004/10/16 17:30:23  livesey
 ! Bug fix
 !
