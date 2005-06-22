2e2b803e6c32a518f8d82b2021a95b9ff983cde5
pwagner <>
2005-06-22 17:25:55 +0000

    rev 2.74

--- lib/L2PC_m.f90
+++ lib/L2PC_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 module L2PC_m
@@ -89,10 +97,7 @@ module L2PC_m
   integer, parameter :: DEFAULTSELECTOR_LATITUDE = 1
   integer, parameter :: DEFAULTSELECTOR_FIELDAZIMUTH = 2
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -1724,12 +1729,20 @@ contains ! ============= Public Procedures ==========================
   end subroutine WriteVectorAsHDF5L2PC
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module L2PC_m
 
 ! $Log$
+! Revision 2.74  2005/06/22 17:25:49  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.73  2004/06/10 00:57:47  vsnyder
 ! Move FindFirst, FindNext from MLSCommon to MLSSets
 !
