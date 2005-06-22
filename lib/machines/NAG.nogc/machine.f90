d652265780759455f62b99755a881e3d062f36aa
pwagner <>
2005-06-22 20:35:55 +0000

    Reworded Copyright statement, moved rcs id

--- lib/machines/NAG.nogc/machine.f90
+++ lib/machines/NAG.nogc/machine.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module MACHINE
 !  use F90_GC, only: GCOLLECT, NCOLLECTIONS, &
@@ -23,14 +31,10 @@ module MACHINE
 
   logical, public, save :: NEVERCRASH = .true. ! Change to false for testing
 
-  private :: NOT_USED_HERE
-
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
+  private :: not_used_here 
 !---------------------------------------------------------------------------
 
 contains
@@ -458,12 +462,20 @@ contains
 
   ! ----------------------------------------------  not_used_here  -----
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module MACHINE
 
 ! $Log$
+! Revision 1.2  2004/09/01 01:46:27  vsnyder
+! Added Paul's version of Crash_Burn
+!
 ! Revision 1.1  2002/02/06 00:40:35  pwagner
 ! First commit
 !
