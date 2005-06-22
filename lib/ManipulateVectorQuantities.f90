b958ac5ff033c53d848e55bc93d845d2baf45a64
pwagner <>
2005-06-22 17:25:51 +0000

    rev 2.30

--- lib/ManipulateVectorQuantities.f90
+++ lib/ManipulateVectorQuantities.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module ManipulateVectorQuantities ! Various routines for manipulating vectors
 
@@ -13,13 +21,11 @@ module ManipulateVectorQuantities ! Various routines for manipulating vectors
 
   implicit none
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character (LEN=256), private :: Id = &
-    & "$Id$"
-  character (LEN=*), parameter, private :: ModuleName= &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   private
 
@@ -432,12 +438,20 @@ contains
   end function DoQtysDescribeSameThing
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module ManipulateVectorQuantities
   
 ! $Log$
+! Revision 2.30  2005/06/22 17:25:49  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.29  2004/12/13 20:28:44  vsnyder
 ! Changed DoVGridsMatch to a generic, with a DoVGridsMatch_Vec specific.
 !
