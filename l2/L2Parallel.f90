5f10339bc76ea2609ee4a5a316abe41ee7e86488
pwagner <>
2005-06-22 18:57:27 +0000

    rev 2.74

--- l2/L2Parallel.f90
+++ l2/L2Parallel.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module L2Parallel
   ! This module contains low level routines and stuff for dealing with parallel
@@ -65,14 +73,11 @@ module L2Parallel
   public :: L2MasterTask
   public :: GetChunkInfoFromMaster
   
-  !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-    "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
-    "$RCSfile$"
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   ! Parameters
   integer, parameter :: HDFNAMELEN = 132 ! Max length of name of swath/sd
@@ -1796,6 +1801,11 @@ contains ! ================================ Procedures ======================
   end subroutine ThankL2Q
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
@@ -1803,6 +1813,9 @@ end module L2Parallel
 
 !
 ! $Log$
+! Revision 2.74  2005/06/22 18:57:02  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.73  2005/03/24 21:22:32  pwagner
 ! Tried to fix slowing of masters relying on l2q
 !
