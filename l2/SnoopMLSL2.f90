d60e339893546e7f4f18170b39016f9d2893b851
pwagner <>
2005-06-22 18:57:19 +0000

    rev 2.39

--- l2/SnoopMLSL2.f90
+++ l2/SnoopMLSL2.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module SnoopMLSL2               ! Interface between MLSL2 and IDL snooper via pvm3.
 
@@ -44,10 +52,7 @@ module SnoopMLSL2               ! Interface between MLSL2 and IDL snooper via pv
   implicit none
   private
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -998,12 +1003,20 @@ contains ! ========  Public Procedures =========================================
   end subroutine SnooperRequestedQuantity
   
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module SnoopMLSL2
 
 ! $Log$
+! Revision 2.39  2005/06/22 18:57:02  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.38  2005/03/15 23:50:52  pwagner
 ! PVMERRORMESSAGE now part of MLSMessageModule
 !
