5e0e818b9c5e167c0216a637013050667db69604
pwagner <>
2005-06-22 17:26:23 +0000

    rev 2.12

--- lib/symbol_table.f90
+++ lib/symbol_table.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module SYMBOL_TABLE
 
@@ -24,12 +32,9 @@ module SYMBOL_TABLE
   integer, save, private, allocatable :: SYMBOLS(:)
   integer, save, private :: TYPE_TEXTS(FIRST_TYPE: LAST_TYPE) = 0
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), parameter :: IdParm = &
-    & "$Id$"
-  character (len=len(idParm)) :: Id = idParm
-  character (len=*), parameter :: ModuleName= &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
 
@@ -242,12 +247,20 @@ contains
   end subroutine INCREASE_SYMBOLS
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module SYMBOL_TABLE
 
 ! $Log$
+! Revision 2.12  2005/06/22 17:25:50  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.11  2004/10/30 00:19:56  vsnyder
 ! Revised the CVS stuff
 !
--- lib/trace_m.f90
+++ lib/trace_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module TRACE_M
 
@@ -15,10 +23,7 @@ module TRACE_M
   integer, parameter :: ClockStackMax = 100
   real :: ClockStack(0:clockStackMax) = 0.0
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -89,12 +94,20 @@ contains ! ====     Public Procedures     ==============================
     call output ( '', advance='yes' )
   end subroutine TRACE_END
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module TRACE_M
 
 ! $Log$
+! Revision 2.12  2005/06/22 17:25:51  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.11  2002/10/08 00:09:15  pwagner
 ! Added idents to survive zealous Lahey optimizer
 !
