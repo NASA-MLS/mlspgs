c139df78518fa61cbeabbc4fc3070c39ded60b7f
pwagner <>
2005-06-22 17:25:48 +0000

    rev 2.14

--- lib/Geometry.f90
+++ lib/Geometry.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module Geometry
 
@@ -48,13 +56,11 @@ module Geometry
   ! This is the maximum amount of refraction allowed
   real(r8), parameter :: MaxRefraction = 0.0004 ! Add one to get refractive index
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)) :: Id = idParm
-  character (len=*), parameter :: ModuleName= "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
 contains ! ------------------------------- Subroutines and functions ----
 
@@ -81,12 +87,20 @@ contains ! ------------------------------- Subroutines and functions ----
   end function GeodToGeocLat
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module Geometry
 
 ! $Log$
+! Revision 2.14  2005/06/22 17:25:48  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.13  2003/01/16 23:13:10  livesey
 ! Added maxRefraction
 !
