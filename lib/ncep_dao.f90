69b87d64af46c8f7aba66bf0da4c077d1900e62a
pwagner <>
2005-06-22 17:24:59 +0000

    rev 2.39

--- lib/ncep_dao.f90
+++ lib/ncep_dao.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module ncep_dao ! Collections of subroutines to handle TYPE GriddedData_T
 
@@ -31,13 +39,11 @@ module ncep_dao ! Collections of subroutines to handle TYPE GriddedData_T
   implicit none
   private
 
-  private :: Id,ModuleName
-  !------------------------------- RCS Ident Info ------------------------------
-  character(LEN=130) :: id = & 
-    "$Id$"
-  character(LEN=*), parameter :: ModuleName="$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !-----------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   public:: READ_CLIMATOLOGY
   public:: ReadGriddedData, ReadGloriaFile
@@ -1537,12 +1543,20 @@ contains
   end subroutine announce_error
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module ncep_dao
 
 ! $Log$
+! Revision 2.39  2005/06/22 17:24:59  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.38  2005/06/14 20:37:06  pwagner
 ! Interfaces changed to accept MLSFile_T args
 !
