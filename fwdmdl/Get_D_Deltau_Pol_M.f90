7ac234efbf017dbcdaebbb4d1aebb65718fc46d6
pwagner <>
2005-06-22 18:08:25 +0000

    rev 2.29

--- fwdmdl/Get_D_Deltau_Pol_M.f90
+++ fwdmdl/Get_D_Deltau_Pol_M.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module Get_D_Deltau_Pol_M
 
@@ -7,12 +15,9 @@ module Get_D_Deltau_Pol_M
   private
   public :: Get_D_Deltau_Pol_DF, Get_D_Deltau_Pol_DT
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), parameter :: IdParm = &
-    &  "$Id$"
-  character (len=len(idParm)) :: Id = idParm
-  character (len=*), parameter :: ModuleName= &
-    &  "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
 contains
@@ -491,12 +496,20 @@ contains
 
 !-----------------------------------------------------------------------
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module Get_D_Deltau_Pol_M
 
 ! $Log$
+! Revision 2.29  2005/06/22 18:08:19  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.28  2004/11/01 20:24:32  vsnyder
 ! Reorganization of representation for molecules and beta groups
 !
