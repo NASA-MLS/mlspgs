b1f1ae1de787d4e473e9af094135f96db627ad8a
pwagner <>
2005-06-22 17:26:13 +0000

    rev 2.36

--- lib/PCFHdr.f90
+++ lib/PCFHdr.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !===============================================================================
 MODULE PCFHdr
@@ -34,14 +42,11 @@ MODULE PCFHdr
      & WritePCF2Hdr, WriteInputPointer
    private
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character(len=*), private, parameter :: IdParm = &
-    & "$Id$"
-  character(len=len(idparm)), private :: Id = idParm
-  character(len=*), private, parameter :: ModuleName = &
-       & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
 ! Contents:
 
@@ -1044,6 +1049,11 @@ CONTAINS
 
 !================
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
@@ -1051,6 +1061,9 @@ end module PCFHdr
 !================
 
 !# $Log$
+!# Revision 2.36  2005/06/22 17:25:50  pwagner
+!# Reworded Copyright statement, moved rcs id
+!#
 !# Revision 2.35  2005/06/14 20:35:24  pwagner
 !# Changed interface to mls_io_gen functions
 !#
