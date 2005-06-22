d150a225beede30499c29b79c52552d9f5ed3333
pwagner <>
2005-06-22 17:26:10 +0000

    rev 2.41

--- lib/output_m.f90
+++ lib/output_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module OUTPUT_M
 
@@ -31,9 +39,7 @@ module OUTPUT_M
   character(len=8), save, public :: TIMESTAMPSTYLE = 'post' ! 'pre' or 'post'
   logical, save, private :: ATLINESTART = .true.  ! Used by timeStamp if notpost
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=256), private :: Id = &
-       "$Id$"
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -725,12 +731,20 @@ contains
 
   ! ..............................................  not_used_here  .....
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module OUTPUT_M
 
 ! $Log$
+! Revision 2.41  2005/06/22 17:25:50  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.40  2005/03/19 01:14:41  pwagner
 ! Fixed error in formatting output_date_and_time
 !
