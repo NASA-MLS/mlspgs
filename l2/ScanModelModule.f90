0debbabdb835a27a52714c5aa25f0909f670ecd3
pwagner <>
2005-06-22 18:57:22 +0000

    rev 2.62

--- l2/ScanModelModule.f90
+++ l2/ScanModelModule.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module ScanModelModule          ! Scan model and associated calculations
 
@@ -52,14 +60,11 @@ module ScanModelModule          ! Scan model and associated calculations
   public :: GetBasisGPH, GetHydrostaticTangentPressure, ScanForwardModel, &
     & TwoDScanForwardModel, Get2DHydrostaticTangentPressure, GetGPHPrecision
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), parameter :: IdParm = &
-    & "$Id$"
-  character (len=len(idParm)) :: Id = idParm
-  character (len=*), parameter :: ModuleName = &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   ! Define various constants etc.
   
@@ -2121,12 +2126,20 @@ contains ! =============== Subroutines and functions ==========================
     end function Z_surface
   end subroutine TwoDScanForwardModel
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module ScanModelModule
 
 ! $Log$
+! Revision 2.62  2005/06/22 18:57:02  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.61  2004/03/20 04:05:23  vsnyder
 ! Moved Boltz from units to physics
 !
