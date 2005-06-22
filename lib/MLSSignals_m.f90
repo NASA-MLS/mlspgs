39d004c6bf1bf564c10b442c866a06a62d1f0f31
pwagner <>
2005-06-22 17:26:14 +0000

    rev 2.80

--- lib/MLSSignals_m.f90
+++ lib/MLSSignals_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module MLSSignals_M
 
@@ -183,14 +191,11 @@ module MLSSignals_M
   type(signal_T), public, save, pointer, dimension(:) :: Signals => NULL()
   integer, public, save :: Instrument = l_emls
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-    & "$Id$"
-  character (len=len(idParm)) :: Id = idParm
-  character (len=*), parameter, private :: ModuleName = &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
 contains
 
@@ -1763,12 +1768,20 @@ oc:     do
   end subroutine PVMUnpackSignal
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module MLSSignals_M
 
 ! $Log$
+! Revision 2.80  2005/06/22 17:25:50  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.79  2005/04/06 23:15:31  vsnyder
 ! Add Before, Sideband and Channel arguments for Display_Signal_Name
 !
