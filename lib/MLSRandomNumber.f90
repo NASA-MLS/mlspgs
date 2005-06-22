0339617f019c9291c2b94ef29cec2e278adf6ae6
pwagner <>
2005-06-22 17:26:07 +0000

    rev 2.10

--- lib/MLSRandomNumber.f90
+++ lib/MLSRandomNumber.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 module MLSRandomNumber              ! Some random number-generating things
@@ -17,10 +25,7 @@ module MLSRandomNumber              ! Some random number-generating things
   ! or else via the f95 intrinsic (if false)
   logical, public, save  ::                   MATH77_RAN_PACK = .true.
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -982,6 +987,11 @@ contains
 
 !=============================================================================
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
@@ -990,6 +1000,9 @@ end module MLSRandomNumber
 
 !
 ! $Log$
+! Revision 2.10  2005/06/22 17:25:50  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.9  2002/10/08 00:09:12  pwagner
 ! Added idents to survive zealous Lahey optimizer
 !
