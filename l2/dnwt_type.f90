014041e8527e2c3eaf024721c79f84b0baa43d5c
pwagner <>
2005-06-22 18:57:04 +0000

    rev 2.6

--- l2/dnwt_type.f90
+++ l2/dnwt_type.f90
@@ -1,15 +1,21 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module DNWT_TYPE
 
 ! This is a separate module only because the parameter RK is needed in
 ! the interface body for FANDJ
 
-!---------------------------- RCS Ident Info -------------------------------
-  CHARACTER (LEN=256),private :: Id = &
-       "$Id$"
-  CHARACTER (LEN=*), private, PARAMETER :: ModuleName= &
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
@@ -18,12 +24,20 @@ module DNWT_TYPE
 
 contains 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module DNWT_TYPE
 
 ! $Log$
+! Revision 2.6  2005/06/22 18:57:01  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.5  2002/10/07 23:43:11  pwagner
 ! Added idents to survive zealous Lahey optimizer
 !
