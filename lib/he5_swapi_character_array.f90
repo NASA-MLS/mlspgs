283da91afba6f83db8f8d781e713c3333145ed1d
pwagner <>
2005-06-22 17:26:21 +0000

    rev 2.8

--- lib/he5_swapi_character_array.f90
+++ lib/he5_swapi_character_array.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module HE5_SWAPI_CHARACTER_ARRAY
 
@@ -9,13 +17,11 @@ module HE5_SWAPI_CHARACTER_ARRAY
     & HE5_SWRDATTR_CHARACTER_ARRAY, HE5_SWRDLATTR_CHARACTER_ARRAY
   private
 
-  !------------------------------- RCS Ident Info ----------------------------
-  character(len=256), private :: Id = &
-    & "$Id$"
-  character(len=*), private, parameter :: ModuleName = &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
 contains
 
@@ -130,6 +136,11 @@ contains
   end function HE5_SWRDLATTR_CHARACTER_ARRAY
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
--- lib/tree.f90
+++ lib/tree.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module TREE
 
@@ -56,9 +64,7 @@ module TREE
 
   integer, save, private :: TREE_TEXTS(N_EOF:LAST_TREE_NODE)
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=256), private :: Id = &
-       "$Id$"
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -612,12 +618,20 @@ contains
   end subroutine TREE_ERROR
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module TREE
 
 ! $Log$
+! Revision 2.8  2005/06/22 17:25:51  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.7  2004/05/28 23:14:35  vsnyder
 ! Allow print_subtree to start at the top of the stack with a negative 'root'
 !
