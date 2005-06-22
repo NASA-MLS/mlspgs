f8a3958f4f2a48b2b2b50ebc882bd3c641e1e924
pwagner <>
2005-06-22 18:08:28 +0000

    rev 2.6

--- fwdmdl/Voigt.f90
+++ fwdmdl/Voigt.f90
@@ -1,3 +1,14 @@
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
+
 module Voigt_M
 
   ! Single-Line Absorption Software
@@ -17,12 +28,9 @@ module Voigt_M
 
   real(rp), parameter :: OneOvSPi = 1.0_rp / sqrtPi  ! 1.0/Sqrt(Pi)
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), parameter :: IdParm = &
-    & "$Id$"
-  character ( len=len(idParm)) :: Id = idParm
-  character (len=*), parameter :: ModuleName= &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
 contains
@@ -937,13 +945,20 @@ contains
 !=====================================================================
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, ModuleName(1:1)
   end function not_used_here
 
 end module Voigt_M
 
 ! $Log$
+! Revision 2.6  2005/06/22 18:08:20  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.5  2005/03/29 01:58:17  vsnyder
 ! Make stuff pure
 !
