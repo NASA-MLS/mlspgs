3c6ca8e71173d36dee5ea5f06ddf57cc9297d3da
pwagner <>
2005-06-22 18:57:29 +0000

    rev 2.108

--- l2/OutputAndClose.f90
+++ l2/OutputAndClose.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=======================================================================================
 
@@ -18,14 +26,11 @@ module OutputAndClose ! outputs all data from the Join module to the
   private
   public :: Output_Close, add_metadata
 
-  !------------------------------- RCS Ident Info ------------------------------
-  character(len=*), parameter :: IdParm = &
-    & "$Id$"
-  character(len=len(idParm)) :: Id = idParm
-  character(len=*), parameter :: ModuleName = &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !-----------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   ! -----     Private declarations     ---------------------------------
 
@@ -1030,12 +1035,20 @@ contains ! =====     Public Procedures     =============================
   end subroutine ANNOUNCE_ERROR
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module OutputAndClose
 
 ! $Log$
+! Revision 2.108  2005/06/22 18:57:02  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.107  2005/06/16 18:43:01  pwagner
 ! Should not bomb if catenating split files w/o toolkit
 !
