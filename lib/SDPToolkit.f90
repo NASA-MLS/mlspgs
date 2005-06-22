e698598ad6566a5879d87788400f38f8bb47045f
pwagner <>
2005-06-22 17:26:11 +0000

    rev 2.17

--- lib/SDPToolkit.f90
+++ lib/SDPToolkit.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !===============================================================================
 MODULE SDPToolkit               ! F90 interface to SDP Toolkit.
@@ -7,14 +15,11 @@ MODULE SDPToolkit               ! F90 interface to SDP Toolkit.
    IMPLICIT NONE
    PUBLIC
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character(len=*), private, parameter :: IdParm = &
-    & "$Id$"
-  character(len=len(idparm)), private :: Id = idParm
-  character(len=*), private, parameter :: ModuleName = &
-       & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
 ! Contents:
 
@@ -211,6 +216,11 @@ MODULE SDPToolkit               ! F90 interface to SDP Toolkit.
 !====================
 contains 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
@@ -219,6 +229,9 @@ end module SDPToolkit
 
 !
 ! $Log$
+! Revision 2.17  2005/06/22 17:25:50  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.16  2004/02/05 23:30:24  pwagner
 ! Flipped WARNIFCANTPGSMETREMOVE to FALSE
 !
