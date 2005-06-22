06a282df1ae0a1529e08d34fa952cf1c0020a521
pwagner <>
2005-06-22 17:26:22 +0000

    rev 2.26

--- lib/units.f90
+++ lib/units.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module UNITS
 
@@ -18,11 +26,8 @@ module UNITS
   real(r8), parameter :: Sqrtln2 = 0.8325546111576977563531646448952010476306_r8
   real(r8), parameter :: SqrtPi = 1.772453850905516027298167483341145182798_r8
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
-  character (len=*), private, parameter :: ModuleName = &
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
@@ -122,12 +127,20 @@ contains ! =====     Public procedures     =============================
     end subroutine DECLARE_UNIT
   end subroutine INIT_UNITS
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module UNITS
 
 ! $Log$
+! Revision 2.26  2005/06/22 17:25:51  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.25  2004/06/03 22:58:20  vsnyder
 ! Add ms and kms units
 !
