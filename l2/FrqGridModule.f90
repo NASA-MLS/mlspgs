25a1d45aee433414e65de5f1082bfa6d959fb383
pwagner <>
2005-06-22 18:57:05 +0000

    rev 2.2

--- l2/FrqGridModule.f90
+++ l2/FrqGridModule.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2001, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 module FrqGridModule
@@ -7,10 +15,7 @@ module FrqGridModule
 
   implicit none
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -137,12 +142,20 @@ contains
   end subroutine FrqGrid
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module FrqGridModule
 
 ! $Log$
+! Revision 2.2  2005/06/22 18:57:01  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.1  2003/04/30 00:09:25  vsnyder
 ! Initial commit
 !
