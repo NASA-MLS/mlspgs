a6bf13e9ae1f8912edc5f2593898cd5bb02e209e
pwagner <>
2005-06-22 18:57:25 +0000

    rev 2.138

--- l2/MLSL2.f90
+++ l2/MLSL2.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 program MLSL2
   use Allocate_Deallocate, only: SET_GARBAGE_COLLECTION, TRACKALLOCATES, &
@@ -156,12 +164,13 @@ program MLSL2
   logical :: Timing = .false.      ! -T option is set
   character(len=2048) :: WORD      ! Some text
 
-  !------------------------------- RCS Ident Info ------------------------------
-  character(len=*), parameter :: IdParm = & 
-     "$Id$"
-  character(len=len(idParm)) :: Id = idParm
-  character(len=*), parameter :: ModuleName="$RCSfile$"
-  !-----------------------------------------------------------------------------
+!---------------------------- RCS Ident Info ------------------------------
+  character (len=*), parameter :: ModuleName= &
+       "$RCSfile$"
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)) :: Id = idParm
+!---------------------------------------------------------------------------
 
   type (MLSFile_T), dimension(:), pointer ::     FILEDATABASE
   type (MLSFile_T)                        ::     MLSL2CF
@@ -1051,6 +1060,9 @@ contains
 end program MLSL2
 
 ! $Log$
+! Revision 2.138  2005/06/22 18:57:02  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.137  2005/06/14 20:45:22  pwagner
 ! Many changes to accommodate the new fields in MLSFile_T
 !
