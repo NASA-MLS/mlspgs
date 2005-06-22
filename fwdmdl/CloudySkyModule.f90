8eaa2d0740fe014cbf585341e1d86bc780eb06b3
pwagner <>
2005-06-22 18:08:27 +0000

    rev 2.5

--- fwdmdl/CloudySkyModule.f90
+++ fwdmdl/CloudySkyModule.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module CloudySkyModule
 
@@ -11,14 +19,11 @@ module CloudySkyModule
       Private
       Public :: CLOUDY_SKY, CLOUD_MODEL
 
- !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm =                          &
-    "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
-  character (len=*), private, parameter :: ModuleName=                       &
-    "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
- !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
       
 contains
 
@@ -319,12 +324,20 @@ contains
 
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module CloudySkyModule
 
 ! $Log$
+! Revision 2.5  2005/06/22 18:08:18  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.4  2003/10/09 18:25:24  jonathan
 ! some changes
 !
--- fwdmdl/SizeDistribution.f90
+++ fwdmdl/SizeDistribution.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2003, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module SizeDistribution
 
@@ -23,14 +31,11 @@ USE MLSSpecialFunctions, ONLY: gamma
   integer, public, parameter :: liu_curry_sd = 1100
   integer, public, parameter :: knollenberg_sd = 4000
 
- !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm =                          &
-    "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
-  character (len=*), private, parameter :: ModuleName=                       &
-    "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
- !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
       
 contains
 
@@ -288,12 +293,20 @@ contains
   END SUBROUTINE DRP_SIZE
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module SizeDistribution
 
 ! $Log$
+! Revision 2.5  2005/06/22 18:08:20  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.4  2004/06/03 18:07:28  jonathan
 ! add gama function to water size distribution
 !
