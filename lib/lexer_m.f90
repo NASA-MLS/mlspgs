0f117d5439a2aa6e974701f318b80c24fea13ec1
pwagner <>
2005-06-22 17:26:04 +0000

    rev 2.21

--- lib/lexer_m.f90
+++ lib/lexer_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module LEXER_M
 
@@ -74,10 +82,7 @@ module LEXER_M
 ! x       y       z       {       |       }       ~       DEL
   letter, letter, letter, more,   cont,   more,   more,   more   /) ! 170
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -607,12 +612,20 @@ contains
   end subroutine Lex_Signal
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module LEXER_M
 
 ! $Log$
+! Revision 2.21  2005/06/22 17:25:49  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.20  2004/05/28 23:12:20  vsnyder
 ! Add power (^) operator
 !
--- lib/string_table.f90
+++ lib/string_table.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module STRING_TABLE
 
@@ -59,12 +67,9 @@ module STRING_TABLE
   ! in CHAR_TABLE of the last character of the i'th string
   integer, save :: NSTRING    ! How full, not how big
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), parameter :: IdParm = &
-    & "$Id$"
-  character (len=len(idParm)) :: Id = idParm
-  character (len=*), parameter :: ModuleName= &
-    & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
 
@@ -846,12 +851,20 @@ contains
   end subroutine TEST_STRING
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module STRING_TABLE
 
 ! $Log$
+! Revision 2.21  2005/06/22 17:25:50  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.20  2004/10/30 00:19:56  vsnyder
 ! Revised the CVS stuff
 !
