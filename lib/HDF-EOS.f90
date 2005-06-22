065edaf39d92e8313e086f47aed98224fcddc427
pwagner <>
2005-06-22 17:26:05 +0000

    rev 2.16

--- lib/HDF-EOS.f90
+++ lib/HDF-EOS.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2003, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !===============================================================================
 module HDFEOS               ! F90 interface to HDF-EOS.
@@ -9,14 +17,11 @@ module HDFEOS               ! F90 interface to HDF-EOS.
   public
 
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character(len=*), private, parameter :: IdParm = &
-    & "$Id$"
-  character(len=len(idparm)), private :: Id = idParm
-  character(len=*), private, parameter :: ModuleName = &
-       & "$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   ! Now define f90 interfaces for some HDF-EOS.
 
@@ -179,6 +184,11 @@ module HDFEOS               ! F90 interface to HDF-EOS.
 !====================
 contains 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
@@ -187,6 +197,9 @@ end module HDFEOS
 
 !
 ! $Log$
+! Revision 2.16  2005/06/22 17:25:49  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.15  2003/06/06 22:48:24  pwagner
 ! Added interface for gdcreate
 !
--- lib/PVM.f90
+++ lib/PVM.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module PVM ! Interface to the f77 pvm library.
 
@@ -11,12 +19,11 @@ module PVM ! Interface to the f77 pvm library.
 
   implicit none
 
-  !------------------------------- RCS Ident Info ------------------------------
-  character(LEN=130), private :: Id = & 
-       "$Id$"
-  character(LEN=*), private, parameter :: ModuleName="$RCSfile$"
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
   private :: not_used_here 
-  !-----------------------------------------------------------------------------
+!---------------------------------------------------------------------------
 
   ! First we define constants as enumerated types effectively.  This is
   ! basically taken straight from the fpvm3.h file that comes with pvm, and as
@@ -579,12 +586,20 @@ contains
   end subroutine GetMachineNameFromTid
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module PVM
 
 ! $Log$
+! Revision 2.16  2005/06/22 17:25:50  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.15  2005/03/15 23:46:44  pwagner
 ! Moved PVMERRORMESSAGE to MLSMessageModule
 !
