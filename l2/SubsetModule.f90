8e4c93159950f54fcc0a15cf5f984aed8586b09b
pwagner <>
2005-06-22 18:57:15 +0000

    rev 2.13

--- l2/SubsetModule.f90
+++ l2/SubsetModule.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module SubsetModule
 
@@ -12,14 +20,11 @@ module SubsetModule
 
   public :: RestrictRange, SetupSubset, SetupFlagCloud, UpdateMask
 
-  !---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-    "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
-    "$RCSfile$"
+       "$RCSfile$"
   private :: not_used_here 
-  !---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
   
   ! Local parameters
   character(len=32), private, parameter :: WRONGUNITS = 'The wrong units were supplied'
@@ -1143,12 +1148,20 @@ contains ! ========= Public Procedures ============================
   end function GetMaskBit
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module SubsetModule
  
 ! $Log$
+! Revision 2.13  2005/06/22 18:57:02  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.12  2004/05/28 00:57:50  vsnyder
 ! Move GetIndexFlagsFromList from MoreTree to Expr_m
 !
