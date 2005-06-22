3b2a931c4ced75ca2c5be87f87c2c2cac7a1ddde
pwagner <>
2005-06-22 17:25:58 +0000

    rev 2.105

--- lib/MatrixModule_1.f90
+++ lib/MatrixModule_1.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 module MatrixModule_1          ! Block Matrices in the MLS PGS suite
@@ -257,13 +265,10 @@ module MatrixModule_1          ! Block Matrices in the MLS PGS suite
     type(Matrix_SPD_T), pointer :: SPD => NULL()
   end type Matrix_Database_T
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
-  private not_used_here
+  private :: not_used_here 
 !---------------------------------------------------------------------------
 
 contains ! =====     Public Procedures     =============================
@@ -2545,11 +2550,19 @@ contains ! =====     Public Procedures     =============================
   end function MinDiag_1
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 end module MatrixModule_1
 
 ! $Log$
+! Revision 2.105  2005/06/22 17:25:49  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.104  2005/06/21 23:58:14  livesey
 ! Added forgiveZeros option to UpdateDiagonal
 !
