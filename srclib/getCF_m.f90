b6851a84a6a9d2e18e5e5b1f6bcffe4d9522f07b
pwagner <>
2005-06-22 20:03:56 +0000

    Reworded Copyright statement, moved rcs id

--- srclib/getCF_m.f90
+++ srclib/getCF_m.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !==============================================================================
 module GetCF_M
@@ -18,12 +26,15 @@ module GetCF_M
   use TREE_CHECKER, only: CHECK_TREE
   use TREE, only: ALLOCATE_TREE, PRINT_SUBTREE
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+  implicit none
+  private
+  
+  public :: GetCF, InitGetCF
+
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
+  private :: not_used_here 
 !---------------------------------------------------------------------------
 
 contains ! =====     Public Procedures     =============================
@@ -122,9 +133,21 @@ contains ! =====     Public Procedures     =============================
     end if
     if ( present(status) ) status = error
   end subroutine GetCF
+
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 end module GetCF_M
 
 ! $Log$
+! Revision 1.2  2001/04/26 02:31:25  vsnyder
+! Moved *_indices declarations from init_tables_module to Intrinsic
+!
 ! Revision 1.1  2000/11/02 21:36:45  vsnyder
 ! Initial entry into CVS
 !
--- srclib/tree_checker.f90
+++ srclib/tree_checker.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 ! Copyright (c) 1999, California Institute of Technology.  ALL RIGHTS RESERVED.
 ! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
@@ -75,10 +83,7 @@ module TREE_CHECKER
   logical, private :: GOT(field_first:field_last)
   logical, private :: NO_DUP_FLAG       ! Duplicate named fields prohibited
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
@@ -998,12 +1003,20 @@ m:          do j = 3, nsons(field)
   end subroutine SPEC_ARGS
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module TREE_CHECKER
 
 ! $Log$
+! Revision 1.25  2004/12/31 02:11:34  vsnyder
+! Simplify constructing some error messages
+!
 ! Revision 1.24  2004/11/17 20:23:30  vsnyder
 ! Add checking for scalar field
 !
