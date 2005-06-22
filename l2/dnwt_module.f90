8d76279db7152c69ea92d48c891faf075e6cc6d9
pwagner <>
2005-06-22 18:57:06 +0000

    rev 2.42

--- l2/dnwt_module.f90
+++ l2/dnwt_module.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2002, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 module DNWT_MODULE
 !>> 2000-12-19 W. V. Snyder Removed fwd communication and linear algebra
@@ -233,13 +241,10 @@ module DNWT_MODULE
 
   character(len=*), parameter :: ME = 'DNWT'
 
-!---------------------------- RCS Ident Info -------------------------------
-  character (len=*), private, parameter :: IdParm = &
-       "$Id$"
-  character (len=len(idParm)), private :: Id = idParm
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
-  private :: not_used_here
+  private :: not_used_here 
 !---------------------------------------------------------------------------
 
 contains
@@ -1283,12 +1288,20 @@ contains
   end subroutine FlagName
 
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
 end module DNWT_MODULE
 
 ! $Log$
+! Revision 2.42  2005/06/22 18:57:01  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.41  2003/02/03 23:11:23  vsnyder
 ! Implement Mor\'e and Sorensen calculation of the Lavenberg parameter
 !
