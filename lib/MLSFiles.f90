c4d94b70050f5b6acf73881610f94db6e94d19a2
pwagner <>
2005-06-22 17:25:56 +0000

    rev 2.67

--- lib/MLSFiles.f90
+++ lib/MLSFiles.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contracts NAS7-1407/NAS7-03001 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !===============================================================================
 module MLSFiles               ! Utility file routines
@@ -57,13 +65,11 @@ module MLSFiles               ! Utility file routines
   & RmFileFromDataBase, split_path_name, &
   & unMaskName, unSplitName
 
-  !------------------- RCS Ident Info -----------------------
-  character(LEN=130) :: Id = &
-    "$Id$"
+!---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
-    "$RCSfile$"
+       "$RCSfile$"
   private :: not_used_here 
-  !----------------------------------------------------------
+!---------------------------------------------------------------------------
 
 ! === (start of toc) ===
 !     c o n t e n t s
@@ -2576,6 +2582,11 @@ contains
   end  subroutine GetMLSFilePCFidFromName
 !====================
   logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
   end function not_used_here
 
@@ -2584,6 +2595,9 @@ end module MLSFiles
 
 !
 ! $Log$
+! Revision 2.67  2005/06/22 17:25:49  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.66  2005/06/16 18:41:41  pwagner
 ! Stopped some unnecessary printing
 !
