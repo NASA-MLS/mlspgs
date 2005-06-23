0d402985b88128509eeeaa0ecfca8398f3e30ce6
pwagner <>
2005-06-23 18:41:53 +0000

    rev 2.16

--- l1/OutputL1B.f90
+++ l1/OutputL1B.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 !=============================================================================
 MODULE OutputL1B
 !=============================================================================
@@ -19,11 +27,11 @@ MODULE OutputL1B
        OutputL1B_THz, OutputL1B_rad, OutputL1B_LatBinData, OutputL1B_diags, &
        OutputL1B_DiagsT, OutputL1B_Chi2
 
-  !------------------- RCS Ident Info -----------------------
-  CHARACTER(LEN=130) :: Id = &
-    "$Id$"
-  CHARACTER(LEN=*), PARAMETER :: ModuleName="$RCSfile$"
-  !----------------------------------------------------------
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+  private :: not_used_here 
+!---------------------------------------------------------------------------
 
   REAL(r8), PARAMETER :: TAItoGIRD = 1104537627.0d00
 
@@ -873,10 +881,21 @@ CONTAINS
  END SUBROUTINE OutputL1B_Chi2
 
 !=============================================================================
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 END MODULE OutputL1B
 !=============================================================================
 
 ! $Log$
+! Revision 2.16  2005/06/23 18:41:36  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.15  2004/12/01 17:11:05  perun
 ! Test calibrated DACS data flag in order to output radiances
 !
