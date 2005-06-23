331a3ff0f8841720ed64cdd3030bd42e589922fe
pwagner <>
2005-06-23 18:41:38 +0000

    rev 2.22

--- l1/init_tables_module.f90
+++ l1/init_tables_module.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 MODULE INIT_TABLES_MODULE
 
@@ -23,11 +31,10 @@ MODULE INIT_TABLES_MODULE
   !        names below.
   PRIVATE :: ADD_IDENT, INIT_INTRINSIC, MAKE_TREE
 
-!---------------------------- RCS Ident Info -------------------------------
-  CHARACTER (len=256), PRIVATE :: Id = &
-       "$Id$"
-  CHARACTER (len=*), PRIVATE, PARAMETER :: ModuleName= &
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
+  private :: not_used_here 
 !---------------------------------------------------------------------------
 
 ! Enumeration types:
@@ -334,9 +341,20 @@ CONTAINS ! =====     Public procedures     =============================
   ! --------------------------------------------------  MAKE_TREE  -----
   INCLUDE "make_tree.f9h"
 
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 END MODULE INIT_TABLES_MODULE
   
 ! $Log$
+! Revision 2.22  2005/06/23 18:41:35  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.21  2005/05/02 16:03:27  perun
 ! Added UseAntOffsets field
 !
@@ -362,6 +380,9 @@ END MODULE INIT_TABLES_MODULE
 ! Version 1.2 commit
 !
 ! $Log$
+! Revision 2.22  2005/06/23 18:41:35  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.21  2005/05/02 16:03:27  perun
 ! Added UseAntOffsets field
 !
