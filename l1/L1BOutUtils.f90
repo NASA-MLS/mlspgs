890d0940cb7dd3cb4ca0e6c2b65bd0bff6f2aa0a
pwagner <>
2005-06-23 18:41:45 +0000

    rev 2.13

--- l1/L1BOutUtils.f90
+++ l1/L1BOutUtils.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 MODULE L1BOutUtils
@@ -11,11 +19,11 @@ MODULE L1BOutUtils
 
   PUBLIC :: OutputL1Bdata, OutputL1BOA, WriteHdrAnnots
 
-  !------------------------------- RCS Ident Info ------------------------------
-  CHARACTER(LEN=130) :: id = &
-       "$Id$"
-  CHARACTER(LEN=*), PARAMETER :: ModuleName="$RCSfile$"
-  !-----------------------------------------------------------------------------
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+  private :: not_used_here 
+!---------------------------------------------------------------------------
 
 CONTAINS
 
@@ -115,9 +123,20 @@ CONTAINS
 
   END SUBROUTINE WriteHdrAnnots
 
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 END MODULE L1BOutUtils
 
 ! $Log$
+! Revision 2.13  2005/06/23 18:41:35  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.12  2004/11/10 15:32:44  perun
 ! Output encoder values; deal with gaps compared to L1BOA records
 !
--- l1/Radiances.f90
+++ l1/Radiances.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 MODULE Radiances ! Determine radiances for the GHz module
@@ -21,11 +29,11 @@ MODULE Radiances ! Determine radiances for the GHz module
 
   PUBLIC :: CalcLimbRads
 
-  !------------------------------- RCS Ident Info ------------------------------
-  CHARACTER(LEN=130) :: id = &
-       "$Id$"
-  CHARACTER(LEN=*), PARAMETER :: ModuleName="$RCSfile$"
-  !-----------------------------------------------------------------------------
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+  private :: not_used_here 
+!---------------------------------------------------------------------------
 
 CONTAINS
 
@@ -479,10 +487,21 @@ CONTAINS
   END SUBROUTINE CalcLimbRads
 
 !=============================================================================
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 END MODULE Radiances
 !=============================================================================
 
 ! $Log$
+! Revision 2.13  2005/06/23 18:41:36  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.12  2005/05/02 16:06:38  perun
 ! Use AntOffsetsScale for scaling NonLimbRad
 !
