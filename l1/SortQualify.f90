0195f80f23a9780c9deaf5e7ea9786984af82506
pwagner <>
2005-06-23 18:41:49 +0000

    rev 2.15

--- l1/SortQualify.f90
+++ l1/SortQualify.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2004, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 MODULE SortQualify ! Sort and qualify the L0 data
@@ -18,11 +26,11 @@ MODULE SortQualify ! Sort and qualify the L0 data
 
   PUBLIC :: SortAndQualify
 
-  !------------------------------- RCS Ident Info ------------------------------
-  CHARACTER(LEN=130) :: id = &
-       "$Id$"
-  CHARACTER(LEN=*), PARAMETER :: ModuleName="$RCSfile$"
-  !-----------------------------------------------------------------------------
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+  private :: not_used_here 
+!---------------------------------------------------------------------------
 
   TYPE (MAFdata_T), POINTER :: CurMAFdata
 
@@ -651,10 +659,21 @@ print *, 'DACS, wall: ', bank, CalWin%MAFdata%BankWall%DACS(bank)
   END SUBROUTINE SortAndQualify
 
 !=============================================================================
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 END MODULE SortQualify
 !=============================================================================
 
 ! $Log$
+! Revision 2.15  2005/06/23 18:41:36  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.14  2005/01/28 17:06:18  perun
 ! Get GHz FOV bright object flags
 !
