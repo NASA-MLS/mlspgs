449981cea8e5f7a86f6d35b0633ada8451c2bcf3
pwagner <>
2005-06-23 18:41:36 +0000

    rev 2.19

--- l1/MLSL1Config.f90
+++ l1/MLSL1Config.f90
@@ -1,5 +1,13 @@
-! Copyright (c) 2005, California Institute of Technology.  ALL RIGHTS RESERVED.
-! U.S. Government Sponsorship under NASA Contract NAS7-1407 is acknowledged.
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
 
 !=============================================================================
 MODULE MLSL1Config  ! Level 1 Configuration
@@ -24,11 +32,11 @@ MODULE MLSL1Config  ! Level 1 Configuration
   INTEGER, PARAMETER :: MIFsGHz = 125   ! Length (MIFs) of GHz module data
   INTEGER, PARAMETER :: MIFsTHz = 125   ! Length (MIFs) of THz module data
 
-  !------------------------------- RCS Ident Info ------------------------------
-  CHARACTER(LEN=130) :: id = &
-       "$Id$"
-  CHARACTER(LEN=*), PARAMETER :: ModuleName="$RCSfile$"
-  !-----------------------------------------------------------------------------
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+  private :: not_used_here 
+!---------------------------------------------------------------------------
 
   TYPE Globals_T
      CHARACTER(LEN=80) :: OutputVersionString
@@ -758,9 +766,20 @@ MODULE MLSL1Config  ! Level 1 Configuration
 
     END SUBROUTINE Set_calibration
 
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
 END MODULE MLSL1Config
 
 ! $Log$
+! Revision 2.19  2005/06/23 18:41:35  pwagner
+! Reworded Copyright statement, moved rcs id
+!
 ! Revision 2.18  2005/05/02 16:04:15  perun
 ! Add UseAntOffsets field and AntOffsetsScale factor
 !
