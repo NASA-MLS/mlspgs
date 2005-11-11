cf699c8622140e062e8aff9bd585ffacd0155976
pwagner <>
2005-11-11 21:42:17 +0000

    rev 2.7

--- lib/BitStuff.f90
+++ lib/BitStuff.f90
@@ -13,16 +13,30 @@
 module BitStuff
 !=============================================================================
 
-! This module contains routines for manipulating bits.
+! This module contains routines for interrogating, manipulating, using bits.
 
-  implicit NONE
+  use MLSSets, only: FindAll
+
+  implicit none
   private
   public :: CountBits, CountBits_0, CountBits_1, CountBits_2
   public :: CountCharBits_0, CountCharBits_1, CountCharBits_2
+  public :: isBitSet, IsBitPatternSet, SetBits, WhichBitsAreSet
+  public :: NegativeIfBitSet, NegativeIfBitPatternSet
 
   interface CountBits
     module procedure countBits_0, countBits_1, countBits_2
     module procedure countCharBits_0, countCharBits_1, countCharBits_2
+  end interface
+
+  interface NegativeIfBitSet
+    module procedure NegativeIfBitSet_int, NegativeIfBitSet_sng, &
+      & NegativeIfBitSet_dbl
+  end interface
+
+  interface NegativeIfBitPatternSet
+    module procedure NegativeIfBitPatternSet_int, NegativeIfBitPatternSet_sng, &
+      & NegativeIfBitPatternSet_dbl
   end interface
 
 !---------------------------- RCS Module Info ------------------------------
@@ -30,6 +44,13 @@ module BitStuff
        "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
+
+  ! bitNumber starts with 0 at the smallest
+  ! and goes up from there
+  integer, private :: ibitindx ! A dummy index used in array constructor
+  integer, parameter :: MAXBITNUMBER = 30 ! The 31st is the sign bit
+  integer, parameter, dimension(0:MAXBITNUMBER) :: BITINDX = &
+    & (/ (ibitindx, ibitindx = 0, MAXBITNUMBER) /)
 
 contains
 
@@ -120,6 +141,158 @@ contains
     end do
   end function CountCharBits_2
 
+  ! --------------------------------------------  IsBitPatternSet  -----
+  elemental function IsBitPatternSet ( i, bitPattern ) result(SooDesu)
+    ! Are any of the bitNumbers in bitPattern set in i?
+    ! E.g., if bitPattern=1 bitNumber is 0 and we
+    ! simply test whether i is odd
+    integer, intent(in) :: i
+    integer, intent(in) :: bitPattern
+    logical             :: SooDesu
+    ! Executable
+    SooDesu = ( iand(i, bitPattern) /= 0 )
+  end function IsBitPatternSet
+
+  ! --------------------------------------------  IsBitSet  -----
+  elemental function IsBitSet ( i, bitNumber ) result(SooDesu)
+    ! Is the bitNumber bit of i set?
+    ! if omitted, bitNumber is assumed 0 and we
+    ! simply test whether i is odd
+    integer, intent(in) :: i
+    integer, intent(in), optional :: bitNumber
+    logical             :: SooDesu
+    integer :: myBit
+    ! Executable
+    myBit = 0
+    if ( present(bitNumber) ) myBit = bitNumber
+    SooDesu = btest(i, myBit)
+  end function IsBitSet
+
+  ! --------------------------------------------  NegativeIfBitPatternSet  -----
+  ! This family of functions returns the negative abs. val.
+  ! of value if the bitNumber of i is set
+  ! This may be useful if precisions for certain radiances are to be
+  ! set negative (which means don't use them) when a certain bright object
+  ! is in the field of view
+  elemental function NegativeIfBitPatternSet_int ( value, i, bitPattern ) &
+    & result(res)
+    integer, intent(in) :: value
+    integer, intent(in) :: i
+    integer, intent(in) :: bitPattern
+    integer             :: res
+    ! Executable
+    res = value
+    if ( IsBitPatternSet(i, bitPattern) ) res = - abs(value)
+  end function NegativeIfBitPatternSet_int
+
+  elemental function NegativeIfBitPatternSet_sng ( value, i, bitPattern ) &
+    & result(res)
+    real, intent(in)    :: value
+    integer, intent(in) :: i
+    integer, intent(in) :: bitPattern
+    real                :: res
+    ! Executable
+    res = value
+    if ( IsBitPatternSet(i, bitPattern) ) res = - abs(value)
+  end function NegativeIfBitPatternSet_sng
+
+  elemental function NegativeIfBitPatternSet_dbl ( value, i, bitPattern ) &
+    & result(res)
+    double precision, intent(in) :: value
+    integer, intent(in) :: i
+    integer, intent(in) :: bitPattern
+    double precision    :: res
+    ! Executable
+    res = value
+    if ( IsBitPatternSet(i, bitPattern) ) res = - abs(value)
+  end function NegativeIfBitPatternSet_dbl
+
+  ! --------------------------------------------  NegativeIfBitSet  -----
+  ! This family of functions returns the negative abs. val.
+  ! of value if the bitNumber of i is set
+  ! This may be useful if precisions for certain radiances are to be
+  ! set negative (which means don't use them) when a certain bright object
+  ! is in the field of view
+  elemental function NegativeIfBitSet_int ( value, i, bitNumber ) result(res)
+    integer, intent(in) :: value
+    integer, intent(in) :: i
+    integer, intent(in) :: bitNumber
+    integer             :: res
+    ! Executable
+    res = value
+    if ( isBitSet(i, bitNumber) ) res = - abs(value)
+  end function NegativeIfBitSet_int
+
+  elemental function NegativeIfBitSet_sng ( value, i, bitNumber ) result(res)
+    real, intent(in)    :: value
+    integer, intent(in) :: i
+    integer, intent(in) :: bitNumber
+    real                :: res
+    ! Executable
+    res = value
+    if ( isBitSet(i, bitNumber) ) res = - abs(value)
+  end function NegativeIfBitSet_sng
+
+  elemental function NegativeIfBitSet_dbl ( value, i, bitNumber ) result(res)
+    double precision, intent(in) :: value
+    integer, intent(in) :: i
+    integer, intent(in) :: bitNumber
+    double precision    :: res
+    ! Executable
+    res = value
+    if ( isBitSet(i, bitNumber) ) res = - abs(value)
+  end function NegativeIfBitSet_dbl
+
+  ! --------------------------------------------  SetBits  -----
+  function SetBits ( set ) result(i)
+    ! Return the integer represented by setting the bitNumbers
+    ! in the array set
+    ! E.g., if set = (/ 0, 2, 4 /) result will be 21 (= b'10101')
+    integer, intent(in), dimension(:) :: set
+    integer :: i
+    integer :: indx
+    integer :: iset
+    ! Executable
+    i = 0
+    if ( size(set) < 1 ) return
+    do iset = 1, size(set)
+      indx = set(iset)
+      if ( indx < 0 .or. indx > MAXBITNUMBER ) cycle
+      i = ibset(i, indx)
+    enddo
+  end function SetBits
+
+  ! --------------------------------------------  WhichBitsAreSet  -----
+  subroutine WhichBitsAreSet ( i, set, howmany, unset )
+    ! Which bitNumbers of i are set? Returned in array "set"
+    ! E.g., given 41 (101001 in binary) returns (/ 0,3,5 /)
+    ! If present, unset is filled with bit numbers not set (/ 1,2,4,6,7,.. /)
+    ! Note: because 0 is an actual bit number, we prefill
+    ! arrays with -1. So when returned array has count(set > -1)
+    ! elements you will know how to proceed. (No, how?)
+    integer, intent(in)                          :: i
+    integer, intent(out), dimension(:) :: set
+    integer, intent(out), optional               :: howmany
+    integer, intent(out), dimension(:), optional :: unset
+    integer :: N
+    ! Executable
+    if ( present(unset) ) then
+      N = max( size(set), size(unset) )
+      N = min( N, MAXBITNUMBER+1 )
+      set = -1
+      unset = -1
+      call FindAll( IsBitSet(i, BITINDX(0:N)), set, howmany, unset )
+      ! The first bit number is 0, not 1
+      set = set - 1
+      unset = unset - 1
+    else
+      N = min( size(set), MAXBITNUMBER+1 )
+      set = -1
+      call FindAll( IsBitSet(i, BITINDX(0:N)), set, howmany )
+      set = set - 1
+    endif
+  end subroutine WhichBitsAreSet
+
   logical function not_used_here()
 !---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
@@ -132,6 +305,9 @@ contains
 end module BitStuff
 
 ! $Log$
+! Revision 2.7  2005/11/11 21:42:17  pwagner
+! Added bit setting, retrieval, and conditional sign-changing procedures
+!
 ! Revision 2.6  2005/06/22 17:25:48  pwagner
 ! Reworded Copyright statement, moved rcs id
 !
