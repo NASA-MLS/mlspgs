e8835ceb19430cc3310eb1b1669acb766c9dc42a
vsnyder <>
2005-12-30 01:28:01 +0000

    rev 2.1

---
+++ fwdmdl/Phi_Refractive_Correction_m.f90
@@ -0,0 +1,273 @@
+! Copyright 2005, by the California Institute of Technology. ALL
+! RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+! commercial use must be negotiated with the Office of Technology Transfer
+! at the California Institute of Technology.
+
+! This software may be subject to U.S. export control laws. By accepting this
+! software, the user agrees to comply with all applicable U.S. export laws and
+! regulations. User has the responsibility to obtain export licenses, or other
+! export authority as may be required before exporting such information to
+! foreign countries or providing access to foreign persons.
+
+module Phi_Refractive_Correction_m
+
+  implicit NONE
+
+  private
+  public :: Phi_Refractive_Correction, Phi_Refractive_Correction_Up
+
+  interface Phi_Refractive_Correction
+    module procedure Phi_Refractive_Correction_GL, Phi_Refractive_Correction_T
+  end interface
+
+!---------------------------- RCS Module Info ------------------------------
+  character (len=*), private, parameter :: ModuleName= &
+       "$RCSfile$"
+  private :: not_used_here
+!---------------------------------------------------------------------------
+contains
+!---------------------------------------------------------------------------
+
+  ! -------------------------------  Phi_Refractive_Correction_GL  -----
+  subroutine Phi_Refractive_Correction_GL ( N_path, H_path, dHdz_gw, Z_path, &
+    &                                       Phi_Corr )
+
+  ! Compute the refractive correction for Phi using Gauss-Legendre quadrature,
+  ! in zeta coordinates.
+
+  !{ Write Equation A.6 from Appendix A of [1] as
+  !
+  ! \begin{equation}
+  ! \Delta\phi_i = \int_{H_i}^{H_{i+1}} \frac{\text{d}H}H \left[
+  !  \frac{\mathcal{N}_t H_t}{\sqrt{\mathcal{N}^2 H^2 - \mathcal{N}_t^2 H_t^2}}
+  !  - \frac{H_t}{\sqrt{H^2-H_t^2}} \right ]\,.
+  ! \end{equation}
+  !
+  ! Substitute $a = \frac{H_t}H$,
+  ! $\frac{\text{d}H}{\text{d}a} = -\frac{H_t}{a^2}$, and
+  ! $b = \frac{\mathcal{N}_t}{\mathcal{N}}$, giving
+  !
+  ! \begin{equation}
+  ! \Delta\phi_i = - \int_{a_i}^{a_{i+1}} \text{d}a \left[
+  ! \frac{b}{\sqrt{1-a^2b^2}} - \frac1{\sqrt{1-a^2}} \right]
+  ! \end{equation}
+  !
+  ! Assuming that $\mathcal{N}$ is roughly constant in the range $(a_i,a_{i+1})$,
+  ! and therefore that $b$ is roughly constant in that range, the integral can be
+  ! evaluated analytically, giving
+  !
+  ! \begin{equation}\begin{split}
+  ! \Delta\phi_i \approx\,& \left. - \left[ \sin^{-1} ab - \sin^{-1} a \right]
+  ! \right|_{a=a_i}^{a_{i+1}} =
+  ! \left. \sin^{-1} \frac{H_t}H - \sin^{-1} \frac{\mathcal{N}_tH_t}{\mathcal{N}H}
+  !  \right|_{H=H_i}^{H_{i+1}} \\
+  !  =\,&
+  ! \left. \chi_{\text{eq}} - \chi_{\text{eq}}^{\text{refr}} \right|_{H=H_i}^{H_{i+1}}\,,
+  ! \end{split}\end{equation}
+  !
+  ! Unfortunately, the arcsine method doesn't work; it has serious problems
+  ! near the tangent point.  So 3-point Gauss quadrature in zeta coordinates
+  ! is used instead to compute the integral on the coarse grid, and then
+  ! interpolation is used to estimate it on the fine grid:
+  !
+  ! \begin{equation}
+  ! \Delta\phi_i = \frac1{H_t} \int_{\zeta_i}^{\zeta_{i+1}} \text{d}\zeta
+  !  \frac{\text{d}H}{\text{d}\zeta}
+  !   \left[ \frac{a^2b}{\sqrt{1-a^2b^2}} - \frac{a^2}{\sqrt{1-a^2}} \right]
+  ! \end{equation}
+  !
+  ! \vskip 8pt
+  ! [1] W.G. Read, Z. Shippony, and W.V. Snyder, {\bf Microwave Limb Sounder (MLS)
+  ! Forward Model Algorithm Theoretical Basis Document}, Jet Propulsion Laboratory
+  ! document JPL D-18130, 19 August 2004.
+
+    use GLNP, only: GX, NG
+    use MLSKinds, only: RP, R8
+
+  ! Inputs
+    real(rp), intent(in) :: N_Path(:)  ! Indices of refraction - 1 on the
+                                       ! entire path.  Only the fine-grid (GL)
+                                       ! part is used.
+    real(rp), intent(in) :: H_Path(:)  ! Equivalent circular earth heights on
+                                       ! the entire path.  Only the fine-grid
+                                       ! (GL) part is used.
+    real(rp), intent(in) :: dHdz_gw(:) ! dH/dz times Gauss quadrature weights,
+                                       ! on the entire path.  Only the
+                                       ! fine-grid (GL) part is used.
+    real(rp), intent(in) :: Z_path(:)  ! Zeta on the entire path.  Only the
+                                       ! coarse-grid part is used.
+
+  ! Output
+    real(rp), intent(out) :: Phi_Corr(:) ! Phi refractive corrections on the
+                                       ! entire path.  The ones on the coarse
+                                       ! grid are the indefinite sums of
+                                       ! quadratures, working outward from the
+                                       ! tangent point, of the incremental Phi
+                                       ! refractive corrections for each path
+                                       ! segment.  The ones at the GL points
+                                       ! are interpolated linearly.
+
+    real(rp), parameter :: GX01(NG) = 0.5 * ( 1.0_r8 + gx ) ! GX on 0..1, for
+                           ! interpolating Phi to the fine grid.
+
+    real(rp) :: A          ! a**2 at I
+    real(rp) :: AB         ! A*b = a**2 * b at I
+    real(rp) :: B          ! b at I
+    real(rp) :: HT         ! H at tangent point
+    real(rp) :: II         ! Integrand, then integral, at I
+    real(r8) :: NT         ! N at tangent point
+    integer :: G           ! Gauss-point index, i-mng .. i-m
+    integer :: I, I1, I2   ! Subscript, loop limits
+    integer :: M           ! Direction away from tangent point, +/- 1
+    integer :: MNG         ! M * (NG + 1)
+    integer :: Mid         ! Tangent point index
+
+    mid = size(n_path) / 2
+    ht = h_path(mid)
+    nt = 1.0_r8 + n_path(mid)
+
+    i1 = mid - (ng + 1)
+    i2 = 1
+    phi_corr(mid) = 0.0
+    phi_corr(mid+1) = 0.0 ! Account for zero-thickness tangent layer
+
+    do m = -1, 1, 2
+      mng = m * (ng + 1)
+      do i = i1, i2, mng
+        ! Gauss quadrature on one panel
+        ii = 0.0
+        do g = i-mng+m, i-m, m
+          a = ( ht / h_path(g) ) ** 2     ! a^2
+          b = nt / (1.0_r8 + n_path(g))   ! b
+          ab = a * b                      ! a^2 b
+          ii = ii + dHdz_gw(g) * &
+            & ( ab / sqrt(1.0 - ab*b ) - a / sqrt(1.0 - a) )
+        end do
+        ! The factor of 0.5 is because GW are on -1..1
+        ii = 0.5 * (z_path(i) - z_path(i-mng)) * ii / ht
+        phi_corr(i) = phi_corr(i-mng) + ii
+        ! Linear interpolation from coarse to fine grid
+        phi_corr(i-mng+m:i-m:m) = phi_corr(i-mng) + ii * gx01
+      end do ! i
+      i1 = mid + ng + 2
+      i2 = size(n_path)
+    end do ! m
+
+  end subroutine Phi_Refractive_Correction_GL
+
+  ! --------------------------------  Phi_Refractive_Correction_T  -----
+  subroutine Phi_Refractive_Correction_T ( N_path, H_path, Phi_Corr )
+
+  ! Compute the refractive correction for Phi using trapezoidal quadrature,
+  ! in H coordinates.
+
+    use MLSKinds, only: RP, R8
+
+  ! Inputs
+    real(rp), intent(in) :: N_Path(:)  ! Indices of refraction - 1 on the
+                                       ! entire path.
+    real(rp), intent(in) :: H_Path(:)  ! Equivalent circular earth heights on
+                                       ! the entire path.
+
+  ! Output
+    real(rp), intent(out) :: Phi_Corr(:) ! Phi refractive corrections on the
+                                       ! entire path.  These are indefinite
+                                       ! sums of quadratures, working outward
+                                       ! from the tangent point, of the
+                                       ! incremental Phi refractive corrections
+                                       ! for each path segment.
+
+    real(rp) :: A, AP      ! a I, I-M
+    real(rp) :: B          ! b at I
+    real(rp) :: HT         ! H at tangent point
+    real(rp) :: II, IP     ! Integrand at I, I-M
+    real(r8) :: NT         ! N at tangent point
+    integer :: I, I1, I2   ! Subscript, loop limits
+    integer :: M           ! Direction away from tangent point, +/- 1
+    integer :: Mid         ! Tangent point index
+
+    mid = size(n_path) / 2
+    ht = h_path(mid)
+    nt = 1.0_r8 + n_path(mid)
+
+    i1 = mid - 1
+    i2 = 1
+    phi_corr(mid:mid+1) = 0.0 ! Account for zero-thickness tangent layer
+
+    do m = -1, 1, 2
+      ip = 0.0
+      ap = 1.0
+      do i = i1, i2, m
+        ! trapezoidal quadrature on one panel
+        a = ( ht / h_path(i) )
+        b = nt / (1.0_r8 + n_path(i))
+        ii = 1.0 / sqrt(1.0 - a**2) - b / sqrt(1.0 - (a*b)**2)
+        phi_corr(i) = phi_corr(i-m) + 0.5 * ( a - ap ) * ( ip + ii )
+        ! Get ready for next panel
+        ap = a
+        ip = ii
+      end do ! i
+      i1 = mid + 2
+      i2 = size(n_path)
+    end do ! m
+
+  end subroutine Phi_Refractive_Correction_T
+
+  ! -------------------------------  Phi_Refractive_Correction_Up  -----
+  subroutine Phi_Refractive_Correction_Up ( N_path, H_path, Phi_Corr )
+
+  ! Compute the refractive correction for Phi using trapezoidal quadrature,
+  ! in H coordinates, from the tangent point upward -- not from the zero-
+  ! thickness tangent layer outward, as the other two subroutines do.
+
+    use MLSKinds, only: RP, R8
+
+  ! Inputs
+    real(rp), intent(in) :: N_Path(:)  ! Indices of refraction - 1.
+    real(rp), intent(in) :: H_Path(:)  ! Equivalent circular earth heights.
+
+  ! Output
+    real(rp), intent(out) :: Phi_Corr(:) ! Phi refractive corrections.
+
+    real(rp) :: A, AP      ! a I, I-M
+    real(rp) :: B          ! b at I
+    real(rp) :: HT         ! H at tangent point
+    real(rp) :: II, IP     ! Integrand at I, I-M
+    real(r8) :: NT         ! N at tangent point
+    integer :: I           ! Subscript
+
+    ht = h_path(1)
+    nt = 1.0_r8 + n_path(1)
+
+    phi_corr(1) = 0.0
+
+    ip = 0.0
+    ap = 1.0
+    do i = 2, size(h_path)
+      ! trapezoidal quadrature on one panel
+      a = ( ht / h_path(i) )
+      b = nt / (1.0_r8 + n_path(i))
+      ii = 1.0 / sqrt(1.0 - a**2) - b / sqrt(1.0 - (a*b)**2)
+      phi_corr(i) = phi_corr(i-1) + 0.5 * ( a - ap ) * ( ip + ii )
+      ! Get ready for next panel
+      ap = a
+      ip = ii
+    end do ! i
+
+  end subroutine Phi_Refractive_Correction_Up
+
+  logical function not_used_here()
+!---------------------------- RCS Ident Info -------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)), save :: Id = idParm
+!---------------------------------------------------------------------------
+    not_used_here = (id(1:1) == ModuleName(1:1))
+  end function not_used_here
+
+end module Phi_Refractive_Correction_m
+
+! $Log$
+! Revision 2.1  2005/12/30 01:28:01  vsnyder
+! Initial commit
+!
