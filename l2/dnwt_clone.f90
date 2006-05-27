fde0fb9d9412e9df75c700a3838d93e064754618
vsnyder <>
2006-05-27 02:59:36 +0000

    rev 2.6

--- l2/dnwt_clone.f90
+++ l2/dnwt_clone.f90
@@ -19,16 +19,33 @@ module DNWT_CLONE
     & NF_SOLVE, NF_START, NWT_T, FLAGNAME, NF_GMOVE, NF_BEST, NF_AITKEN,&
     & NF_DX_AITKEN, NF_TOLX, NF_TOLF, NF_TOLX_BEST, NF_TOO_SMALL, NF_FANDJ
 
+  implicit NONE
+
+  private
+
+  public NWT, NWTA, NWTDB, NWTOP
+
   interface NWT; module procedure DNWT; end interface
   interface NWTA; module procedure DNWTA; end interface
   interface NWTDB; module procedure DNWTDB; end interface
+  interface NWTOP; module procedure DNWTOP; end interface
 
-    
 !---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
+
+! *****     Private data     *******************************************
+  save
+
+  ! Some of these aren't used.  They're here so that the DNWTOP from the
+  ! "real" DNWT can be used without modification.
+
+  real(rk) :: AJSCAL, DXMAXI, DXNOIS, RELSF, SPMINI, SPSTRT, TOLXA, TOLXR
+  integer :: K1IT
+
+  character(len=*), parameter :: ME = 'DNWT'
 
 contains
 ! ***************************************************     DNWT     *****
@@ -38,6 +55,8 @@ contains
     real(rk), intent(in) :: XOPT(*)
     integer, intent(in), optional :: NOPT(*)
     nflag = nf_start
+    if ( present(nopt) ) call nwtop ( nopt, xopt )
+    call nwtop ( ) ! default initialization
     return
   end subroutine DNWT
 
@@ -52,11 +71,19 @@ contains
     case ( nf_start )
       nflag = nf_evalf
     case ( nf_evalf )
-      nflag = nf_evalj
+      if ( aj%fnorm < (1.0 + relsf) * aj%fnmin ) then
+        nflag = nf_tolf
+      else
+        nflag = nf_evalj
+      end if
     case ( nf_evalj )
       nflag = nf_solve
     case ( nf_solve )
-      nflag = nf_dx
+      if ( aj%dxn <= tolxa .or. aj%dxn <= tolxr * aj%axmax ) then
+        nflag = nf_tolx
+      else
+        nflag = nf_dx
+      end if
     case ( nf_dx )
       nflag = nf_newx
     case ( nf_newx )
@@ -74,6 +101,95 @@ contains
     ! Present and 0 do CDXDXL, CGDX, DXN, FN, FNMIN, INC, SP, SQ, SQRT(FNXE)
     character(len=*), intent(in), optional :: WHY ! printed if present
   end subroutine DNWTDB
+
+! *************************************************     DNWTOP     *****
+  subroutine DNWTOP ( NOPT, XOPT )
+    ! Process option vector for DNWT.  With no arguments, does default
+    ! initialization.
+    use ERMSG_M, only: ERMSG, ERVN
+    integer, intent(in), optional :: NOPT(*)
+    real(rk), intent(in), optional :: XOPT(*)
+
+    logical, save :: FIRST = .true.
+    integer I, INDIC, K, KA, NACT
+    integer, save :: IOPTS(8) = (/ 0, 0, 0, 0, 0, 0, 0, 150 /)
+    character(len=4), parameter :: LABL(2) = (/ '(I) ', 'NOPT' /)
+    ! ??? Defaults aren't what comments say
+    real(rk),save :: VALUES(9) = (/ 0.1_rk, epsilon(values), epsilon(values), &
+                                    huge(values), 0.01_rk, 0.0_rk, 0.0_rk, &
+                                    0.0_rk, 0.0_rk /)
+    real(rk),save :: VALNOM(9) = (/ 0.1_rk, epsilon(values), epsilon(values), &
+                                    huge(values), 0.01_rk, 0.0_rk, 0.0_rk, &
+                                    0.0_rk, 0.0_rk /)
+    integer :: IVAL(2) ! for error messages
+
+!*************** Start of executable code ******************
+
+    if ( first ) then
+      valnom(7) = sqrt(sqrt(tiny(values))) ** 3
+      valnom(8) = sqrt(sqrt(epsilon(values))) ** 3
+      values(7:8) = valnom(7:8)
+      first = .false.
+    end if
+    if ( present(nopt) ) then
+      if ( .not. present(xopt) ) then
+        call ermsg ( me, 99, 2, 'NOPT present but XOPT absent', '.' )
+        return
+      end if
+      i = 1
+      do while ( nopt(i) /= 0 )
+        k = nopt(i)
+        ka = abs(k)
+        select case ( ka )
+!****************** Change *DATA* values *******************
+        case ( 1, 2 ) ! Set K1IT
+          if ( k > 0) iopts(ka) = nopt(i+1)
+          k1it = max(iopts(1),iopts(2))
+        case ( 3, 4, 7, 8 ) ! Change XSCAL and FSCAL indexes in data
+                            ! or set up bounds option in data
+          if ( k > 0) iopts(ka) = nopt(i+1)
+        case ( 5, 6 ) ! Set flags in data for reverse communi-
+                      ! cation and special matrix operations
+          if ( k > 0) iopts(ka) = 1
+          i = i - 1
+        case ( 9, 10 ) ! Set to default values
+          iopts = 0
+          iopts(8) = 150
+          values = valnom
+          i = i - 1
+        case ( 11:19 ) ! Set in data SPSTRT, SPMINI, AJSCAL,
+                       ! DXMAXI, RELSF, and DXNOIS
+          values(ka-10) = valnom(ka-10) ! Reset to nominal value
+          if ( k > 0) & ! If indicated, set to user input value
+            values(ka-10) = xopt(nopt(i+1))
+        case default
+          indic = 1
+          nact = 2
+          call ermsg ( me, indic, nact, 'INVALID NOPT', ',' )
+          ival(1) = i
+          ival(2) = nopt(i)
+          call ervn ( labl, ival, '.' )
+          return
+        end select
+        i = i + 2
+      end do
+    else
+      k1it = max(iopts(1),iopts(2))
+    end if
+!                     Reset every time one of them changes
+    spmini = values(2)
+    ajscal = values(3)
+    dxmaxi = values(4)
+    relsf = max(values(5),epsilon(relsf))
+    tolxa = values(7)
+    tolxr = values(8)
+    if ( present(nopt) ) return
+!                     Only set during initialization
+    spstrt = values(1)
+    dxnois = values(6)
+    return
+  end subroutine DNWTOP
+
   logical function not_used_here()
 !---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
@@ -86,6 +202,9 @@ contains
 end module DNWT_CLONE
 
 ! $Log$
+! Revision 2.6  2006/05/27 02:59:36  vsnyder
+! Add convergence test
+!
 ! Revision 2.5  2006/05/20 02:17:23  vsnyder
 ! Make DNWTDB compatible with dnwt_module
 !
