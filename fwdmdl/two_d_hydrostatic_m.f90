1b160177ddd8bab5f8d5cfdba1dad7664a9dedd4
vsnyder <>
2006-09-28 21:00:47 +0000

    Improved computation of csq again

--- fwdmdl/two_d_hydrostatic_m.f90
+++ fwdmdl/two_d_hydrostatic_m.f90
@@ -29,7 +29,7 @@ module Two_D_Hydrostatic_m
 
 ! Compute the 2 dimensional hydrostatic stuff
 
-  use Geometry, only: earthRadA, earthRadB
+  use Geometry, only: earthRadA, earthRadB, Earth_Axis_Ratio_Squared_m1
   use Hydrostatic_m, only: hydrostatic
   use Load_sps_data_m, ONLY: Grids_T
   use MLSCommon, only: RP, IP
@@ -38,29 +38,31 @@ module Two_D_Hydrostatic_m
 ! Inputs:
 
   type (Grids_T), intent(in) :: Grids_tmp   ! All Temperature's coordinates
-  real(rp), intent(in) :: z_refs(:)  !reference pressures
-  real(rp), intent(in) :: h_refs(:)  !reference geopotential heights at z_refs
-!                                 the horizontal bases for these is aligned
-!                                 with p_basis.
-  real(rp), intent(in) :: z_grid(:)!pressures for which heights/temps are
-!                                   needed
-  real(rp), intent(in) :: beta   ! spacecraft beta angle (Radians)
+  real(rp), intent(in) :: z_refs(:)  ! Reference pressures
+  real(rp), intent(in) :: h_refs(:)  ! Reference geopotential heights (km) at
+!                                 z_refs. The horizontal basis for these is
+!                                 aligned with Grids_tmp%phi_basis.
+  real(rp), intent(in) :: z_grid(:)  ! pressures for which heights/temps are
+!                                 needed
+  real(rp), intent(in) :: beta       ! spacecraft beta angle (Radians)
 
 ! Outputs:
 
-  real(rp), intent(out):: t_grid(:,:)!computed temperatures
-  real(rp), intent(out):: h_grid(:,:)!computed heights
-  real(rp), intent(out):: dhidzij(:,:)!derivative of height wrt zeta
-  real(rp), intent(out):: dhidtlm(:,:,:) !derivative of height wrt temps
-!                                     on outputted phi grid
+  real(rp), intent(out):: t_grid(:,:)    ! Computed temperatures
+  real(rp), intent(out):: h_grid(:,:)    ! Computed heights
+  real(rp), intent(out):: dhidzij(:,:)   ! Derivative of height wrt zeta
+  real(rp), intent(out):: dhidtlm(:,:,:) ! Derivative of height wrt temps
+!                                     on output phi grid
   real(rp), optional, intent(out):: ddhdhdtl0(:,:,:)!second order derivative
 !                             at the tangent only---used for antenna affects
 ! Internal stuff
 
-  integer(ip) :: Z_coeffs, P_coeffs, I, J1, J2
+  integer(ip) :: I, J1, J2
+  integer(ip) :: P_coeffs ! Size of interesting part of Grids_tmp%phi_basis
+  integer(ip) :: Z_coeffs ! Size of interesting part of Grids_tmp%zet_basis
 
   real(rp) :: CSQ ! C**2
-  real(rp) :: Lat, Red_phi_t, SinBeta, SinPhi, SinPhiSQ
+  real(rp) :: Lat, SinBeta, SinPhi, SinPhiSQ
 
   real(rp), parameter :: PId2=0.5_rp*pi, PI2=2.0_rp*pi, PI3d2=1.5_rp*pi
 
@@ -70,26 +72,16 @@ module Two_D_Hydrostatic_m
   p_coeffs = Grids_tmp%l_p(1) ! - Grids_tmp%l_p(0), which is always zero
 
 !{ Compute the orbit-plane projected minor axis $c$, where
-!  $c^2 = \frac{a^2\,b^2}{a^2 \sin^2 \beta + b^2 \cos^2 \beta}$
+!  $c^2 = \frac{a^2\,b^2}{a^2 \sin^2 \beta + b^2 \cos^2 \beta} =
+!         \frac{a^2}{\left(\frac{a^2}{b^2}-1\right) \sin^2 \beta + 1}$
 
   sinBeta = sin(beta)
-  csq = (earthrada * earthradb)**2 / &
-        & ((earthrada**2-earthradb**2)*sinBeta**2 + earthradb**2)
-
+  csq = earthrada**2 / (Earth_Axis_Ratio_Squared_m1 * sinBeta**2 + 1)
 
 ! compute the 2 d hydrostatic
 
   j2 = 0
   do i = 1, p_coeffs
-
-! rephase the phi
-
-    red_phi_t = modulo(Grids_tmp%phi_basis(i),PI2)
-    if ( PiD2 < red_phi_t .and. red_phi_t <= Pi3D2 ) then
-      red_phi_t = Pi - red_phi_t
-    else if ( red_phi_t > Pi3D2 ) then
-      red_phi_t = red_phi_t - Pi2
-    end if
 
 !{ Compute the geocentric latitude $\lambda\, = \, 
 !  \sin^{-1} ( \sin \gamma \, \sin \beta )$, where
@@ -97,7 +89,7 @@ module Two_D_Hydrostatic_m
 !  and $\gamma$ is the geocentric angle in the orbit plane ellipse between
 !  $\mathbf{R}^{\oplus}$ and the $x$ axis.
 
-    sinPhi = sin(red_phi_t)
+    sinPhi = sin(Grids_tmp%phi_basis(i))
     sinPhiSQ = sinPhi**2
     lat = asin(csq * sinPhi * sinBeta &
       & / sqrt(earthrada**4*(1.0_rp-sinPhiSQ) + csq**2*sinPhiSQ))
@@ -129,6 +121,9 @@ module Two_D_Hydrostatic_m
 end module Two_D_Hydrostatic_m
 !---------------------------------------------------
 ! $Log$
+! Revision 2.13  2005/12/22 20:59:18  vsnyder
+! Improved computation of csq
+!
 ! Revision 2.12  2005/06/22 18:08:20  pwagner
 ! Reworded Copyright statement, moved rcs id
 !
