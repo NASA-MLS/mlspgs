eb4ff3d64512316b2180d43f7380b6705e4fec81
pwagner <>
2006-11-01 20:51:40 +0000

    Comments to subtly urge user to switch to newmlsl2path.sh

--- scripts/getmlsl2path.sh
+++ scripts/getmlsl2path.sh
@@ -1,6 +1,23 @@
 #! /bin/bash
+# Copyright 2005, by the California Institute of Technology. ALL
+# RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+# commercial use must be negotiated with the Office of Technology Transfer
+# at the California Institute of Technology.
+
+# This software may be subject to U.S. export control laws. By accepting this
+# software, the user agrees to comply with all applicable U.S. export laws and
+# regulations. User has the responsibility to obtain export licenses, or other
+# export authority as may be required before exporting such information to
+# foreign countries or providing access to foreign persons.
+
 # $Id$
-# This shell script prints the name of the desired directory for a given file type
+# This shell script prints the name of the desired directory 
+# for a given file type supplied as an arg
+
+# Used when expanding l2cf files
+# see also newmlsl2path.sh and setupcalfiles.l2cf
+
+# This script represents the old way
 # First work out where we are.
 if [ -f ~/machine.id ]
 then
@@ -189,6 +206,9 @@ while [ -n "$(echo $1)" ]; do
 done
 
 # $Log$
+# Revision 1.45  2005/01/18 05:23:20  livesey
+# Another bug fix in sidsrad
+#
 # Revision 1.44  2005/01/17 20:23:59  livesey
 # More fixes to sidsrad
 #
