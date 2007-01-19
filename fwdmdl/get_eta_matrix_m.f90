a9bede267f963f2b2bd65294cc390ee76d0b75b8
vsnyder <>
2007-01-19 02:38:29 +0000

    Separate paths for sorted and unsorted grid

--- fwdmdl/get_eta_matrix_m.f90
+++ fwdmdl/get_eta_matrix_m.f90
@@ -16,19 +16,34 @@ module Get_Eta_Matrix_m
   implicit NONE
 
   private
+  public :: Eta_2D_T
   public :: Eta_Func, Eta_Func_1d, Eta_Func_2d
+  public :: Eta_Mat_Mat, Eta_Mat_Vec, Eta_Mult
   public :: Get_Eta_Sparse
   public :: Get_Eta_Sparse_1d, Get_Eta_Sparse_1d_fl, Get_Eta_Sparse_1d_nz
-  public ::  Get_Eta_Sparse_2d, Get_Eta_Sparse_2d_nz
+  public :: Get_Eta_Sparse_2d, Get_Eta_Sparse_2d_nz
+  public :: Get_Eta_Sparse_Eta
 
   interface Eta_Func
     module procedure Eta_Func_1d, Eta_Func_2d
   end interface Eta_Func
 
+  interface Eta_Mult
+    module procedure Eta_Mat_Mat, Eta_Mat_Vec
+  end interface Eta_Mult
+
   interface Get_Eta_Sparse
     module procedure Get_Eta_Sparse_1d, Get_Eta_Sparse_1d_fl, Get_Eta_Sparse_1d_nz
     module procedure Get_Eta_Sparse_2d, Get_Eta_Sparse_2d_nz
   end interface
+
+  type Eta_2D_T
+    ! Represent 2-d interpolation coefficient matrices
+    real(rp), pointer :: Eta(:,:) => Null()     ! interpolation coefficients
+                                                ! allocate (n,2)
+    integer, pointer :: FirstCol(:) => NULL()   ! first nonzero column in Eta
+                                                ! allocate (n)
+  end type Eta_2D_T
 
 !---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
@@ -344,6 +359,15 @@ contains
     real(rp) :: Del_basis
     logical :: MySorted
 
+! The first coefficient is one for all values of grid below basis(1)
+! until grid = basis(1),then it ramps down in the usual triangular sense,
+! then the last coefficient is one for all values of grid above basis(n_coeffs).
+! I is the independent variable grid index and J is the coefficient index
+
+    n_coeffs = size(basis)
+    eta = 0.0_rp
+    ! eta(:,:n_coeffs) = 0.0_rp
+
 ! Things below go more efficiently if Grid is sorted
 
     mySorted = .false.
@@ -351,48 +375,68 @@ contains
 
     n_grid = size(grid)
     if ( mySorted ) then
-      do i = 1, n_grid
-        p(i) = i
-      end do
-    else
-      call sortp ( grid, 1, n_grid, p ) ! grid(p(:)) are now sorted
-    end if
-
-! The first coefficient is one for all values of grid below basis(1)
-! until grid = basis(1),then it ramps down in the usual triangular sense.
-! I is the independent variable grid index and J is the coefficient index
-
-    n_coeffs = size(basis)
-    eta = 0.0_rp
-    ! eta(:,:n_coeffs) = 0.0_rp
 
 ! first basis calculation
 
-    do i = 1, n_grid
-      if ( grid(p(i)) > basis(1) ) exit
-      eta(p(i),1) = 1.0_rp
-    end do
+      do i = 1, n_grid
+        if ( grid(i) > basis(1) ) exit
+        eta(i,1) = 1.0_rp
+      end do
 
 ! Normal triangular function for j=2 to j=n_coeffs-1
 
-    do j = 2 , n_coeffs
-      del_basis = 1.0 / (basis(j) - basis(j-1))
-      do while ( i <= n_grid )
-        if ( grid(p(i)) > basis(j) ) exit
-        if ( basis(j-1) <= grid(p(i)) ) then
-          eta(p(i),j-1) = (basis(j) - grid(p(i))) * del_basis
-          eta(p(i),j) =   (grid(p(i)) - basis(j-1)) * del_basis
-        end if
-        i = i + 1
+      do j = 2 , n_coeffs
+        del_basis = 1.0 / (basis(j) - basis(j-1))
+        do while ( i <= n_grid )
+          if ( grid(i) > basis(j) ) exit
+          if ( basis(j-1) <= grid(i) ) then
+            eta(i,j-1) = (basis(j) - grid(i)) * del_basis
+            eta(i,j) =   (grid(i) - basis(j-1)) * del_basis
+          end if
+          i = i + 1
+        end do
       end do
-    end do
 
 ! last basis calculation
 
-    do i = i, n_grid
-      if ( basis(n_coeffs) >= grid(p(i)) ) exit
-      eta(p(i),n_coeffs) = 1.0_rp
-    end do
+      do i = i, n_grid
+        if ( basis(n_coeffs) >= grid(i) ) exit
+        eta(i,n_coeffs) = 1.0_rp
+      end do
+
+    else
+
+      call sortp ( grid, 1, n_grid, p ) ! grid(p(:)) are now sorted
+
+! first basis calculation
+
+      do i = 1, n_grid
+        if ( grid(p(i)) > basis(1) ) exit
+        eta(p(i),1) = 1.0_rp
+      end do
+
+! Normal triangular function for j=2 to j=n_coeffs-1
+
+      do j = 2 , n_coeffs
+        del_basis = 1.0 / (basis(j) - basis(j-1))
+        do while ( i <= n_grid )
+          if ( grid(p(i)) > basis(j) ) exit
+          if ( basis(j-1) <= grid(p(i)) ) then
+            eta(p(i),j-1) = (basis(j) - grid(p(i))) * del_basis
+            eta(p(i),j) =   (grid(p(i)) - basis(j-1)) * del_basis
+          end if
+          i = i + 1
+        end do
+      end do
+
+! last basis calculation
+
+      do i = i, n_grid
+        if ( basis(n_coeffs) >= grid(p(i)) ) exit
+        eta(p(i),n_coeffs) = 1.0_rp
+      end do
+
+    end if
 
   end subroutine Get_Eta_Sparse_2d
 
@@ -421,6 +465,125 @@ contains
     real(rp) :: Del_basis
     logical :: MySorted
 
+! The first coefficient is one for all values of grid below basis(1)
+! until grid = basis(1),then it ramps down in the usual triangular sense,
+! then the last coefficient is one for all values of grid above basis(n_coeffs).
+! I is the independent variable grid index and J is the coefficient index
+
+    n_coeffs = size(basis)
+    eta = 0.0_rp
+    not_zero = .false.
+
+! Things below go more efficiently if Grid is sorted
+
+    mySorted = .false.
+    if ( present(sorted) ) mySorted = sorted
+
+    n_grid = size(grid)
+    if ( mySorted ) then
+
+! first basis calculation
+
+      do i = 1, n_grid
+        if ( grid(i) > basis(1) ) exit
+        eta(i,1) = 1.0_rp
+        not_zero(i,1) = .true.
+      end do
+
+! Normal triangular function for j=2 to j=n_coeffs-1.  Both Basis and
+! Grid(p(:)) are sorted, so we don't need to start from i=1.
+
+      do j = 2, n_coeffs
+        del_basis = 1.0_rp / ( basis(j) - basis(j-1) )
+        do while ( i <= n_grid )
+          pi = i
+          if ( grid(pi) > basis(j) ) exit
+          if ( basis(j-1) <= grid(pi) ) then
+            eta(pi,j-1) = (basis(j) - grid(pi)) * del_basis
+            eta(pi,j  ) = (grid(pi) - basis(j-1)) * del_basis
+            not_zero(pi,j-1) = .true.
+            not_zero(pi,j) = .true.
+          end if
+          i = i + 1
+        end do
+      end do
+
+! last basis calculation
+
+      do i = i, n_grid
+        if ( basis(n_coeffs) >= grid(i) ) exit
+        eta(i,n_coeffs) = 1.0_rp
+        not_zero(i,n_coeffs) = .true.
+      end do
+
+    else
+
+      call sortp ( grid, 1, n_grid, p ) ! grid(p(:)) are now sorted
+
+! first basis calculation
+
+      do i = 1, n_grid
+        if ( grid(p(i)) > basis(1) ) exit
+        eta(p(i),1) = 1.0_rp
+        not_zero(p(i),1) = .true.
+      end do
+
+! Normal triangular function for j=2 to j=n_coeffs-1.  Both Basis and
+! Grid(p(:)) are sorted, so we don't need to start from i=1.
+
+      do j = 2, n_coeffs
+        del_basis = 1.0_rp / ( basis(j) - basis(j-1) )
+        do while ( i <= n_grid )
+          pi = p(i)
+          if ( grid(pi) > basis(j) ) exit
+          if ( basis(j-1) <= grid(pi) ) then
+            eta(pi,j-1) = (basis(j) - grid(pi)) * del_basis
+            eta(pi,j  ) = (grid(pi) - basis(j-1)) * del_basis
+            not_zero(pi,j-1) = .true.
+            not_zero(pi,j) = .true.
+          end if
+          i = i + 1
+        end do
+      end do
+
+! last basis calculation
+
+      do i = i, n_grid
+        if ( basis(n_coeffs) >= grid(p(i)) ) exit
+        eta(p(i),n_coeffs) = 1.0_rp
+        not_zero(p(i),n_coeffs) = .true.
+      end do
+
+    end if
+
+  end subroutine Get_Eta_Sparse_2d_nz
+
+!--------------------------------------------  Get_Eta_Sparse_Eta  -----
+  subroutine Get_Eta_Sparse_Eta ( Basis, Grid, Eta, Sorted )
+
+! Compute the eta matrix.  Basis is assumed to be sorted.  Grid need not
+! be sorted, but if it is, Sorted can be set .true. to suppress sorting it
+! here.
+
+    use Sort_m, only: SortP
+! Inputs
+
+    real(rp), intent(in) :: Basis(:) ! basis break points
+    real(rp), intent(in) :: Grid(:)  ! grid values
+    logical, optional, intent(in) :: Sorted ! "Grid is sorted"
+
+! Outputs
+
+    type(Eta_2d_t), intent(inout) :: Eta ! representation basis function.
+                                     ! inout so as not to clobber pointers,
+                                     ! which it assumes are associated
+
+! Internals
+
+    integer(ip) :: I, J, N_coeffs, N_Grid, P(size(grid))
+    real(rp) :: Del_basis
+    logical :: MySorted
+
 ! Things below go more efficiently if Grid is sorted
 
     mySorted = .false.
@@ -440,30 +603,26 @@ contains
 ! I is the independent variable grid index and J is the coefficient index
 
     n_coeffs = size(basis)
-    eta = 0.0_rp
-    not_zero = .false.
 
 ! first basis calculation
 
     do i = 1, n_grid
       if ( grid(p(i)) > basis(1) ) exit
-      eta(p(i),1) = 1.0_rp
-      not_zero(p(i),1) = .true.
+      eta%eta(p(i),1) = 1.0_rp
+      eta%eta(p(i),2) = 0.0_rp
+      eta%firstCol(p(i)) = 1
     end do
 
-! Normal triangular function for j=2 to j=n_coeffs-1.  Both Basis and
-! Grid(p(:)) are sorted, so we don't need to start from i=1.
+! Normal triangular function for j=2 to j=n_coeffs-1
 
-    do j = 2, n_coeffs
-      del_basis = 1.0_rp / ( basis(j) - basis(j-1) )
+    do j = 2 , n_coeffs
+      del_basis = 1.0 / (basis(j) - basis(j-1))
       do while ( i <= n_grid )
-        pi = p(i)
-        if ( grid(pi) > basis(j) ) exit
-        if ( basis(j-1) <= grid(pi) ) then
-          eta(pi,j-1) = (basis(j) - grid(pi)) * del_basis
-          eta(pi,j  ) = (grid(pi) - basis(j-1)) * del_basis
-          not_zero(pi,j-1) = .true.
-          not_zero(pi,j) = .true.
+        if ( grid(p(i)) > basis(j) ) exit
+        if ( basis(j-1) <= grid(p(i)) ) then
+          eta%eta(p(i),1) = (basis(j) - grid(p(i))) * del_basis
+          eta%eta(p(i),2) =   (grid(p(i)) - basis(j-1)) * del_basis
+          eta%firstCol(p(i)) = j-1
         end if
         i = i + 1
       end do
@@ -473,11 +632,46 @@ contains
 
     do i = i, n_grid
       if ( basis(n_coeffs) >= grid(p(i)) ) exit
-      eta(p(i),n_coeffs) = 1.0_rp
-      not_zero(p(i),n_coeffs) = .true.
+      eta%eta(p(i),1) = 0.0_rp
+      eta%eta(p(i),2) = 1.0_rp
+      eta%firstCol(p(i)) = n_coeffs - 1
     end do
 
-  end subroutine Get_Eta_Sparse_2d_nz
+  end subroutine Get_Eta_Sparse_Eta
+
+!---------------------------------------------------  Eta_Mat_Mat  -----
+  subroutine Eta_Mat_Mat ( Eta, Mat_in, Mat_out )
+    ! Multiply Eta by Mat_in giving Mat_out
+    type(eta_2d_t), intent(in) :: Eta
+    real(rp), intent(in) :: Mat_in(:,:)
+    real(rp), intent(out) :: Mat_out(:,:)
+
+    integer :: I, J
+
+    do i = 1, size(eta%eta,1)
+      do j = 1, size(eta%eta,2)
+        mat_out(i,j) = dot_product(eta%eta(i,:), &
+                                   mat_in(eta%firstCol(i):eta%firstCol(i)+1,j) )
+      end do
+    end do
+
+  end subroutine Eta_Mat_Mat
+
+!---------------------------------------------------  Eta_Mat_Vec  -----
+  subroutine Eta_Mat_Vec ( Eta, Vec_in, Vec_out )
+    ! Multiply Eta by Vec_in giving Vec_out
+    type(eta_2d_t), intent(in) :: Eta
+    real(rp), intent(in) :: Vec_in(:)
+    real(rp), intent(out) :: Vec_out(:)
+
+    integer :: i
+
+    do i = 1, size(eta%eta,1)
+      vec_out(i) = dot_product(eta%eta(i,:), &
+                               vec_in(eta%firstCol(i):eta%firstCol(i)+1) )
+    end do
+
+  end subroutine Eta_Mat_Vec
 
   logical function not_used_here()
 !---------------------------- RCS Ident Info -------------------------------
@@ -491,6 +685,9 @@ contains
 end module Get_Eta_Matrix_m
 !---------------------------------------------------
 ! $Log$
+! Revision 2.13  2006/12/13 21:53:11  vsnyder
+! Delete declaration of unused variable
+!
 ! Revision 2.12  2006/12/09 02:23:45  vsnyder
 ! Use generic instead of optional, add First, Last
 !
