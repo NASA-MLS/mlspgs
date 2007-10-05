14d67799e08d88a64c8d4161aa872d6f19ee8805
vsnyder <>
2007-10-05 23:59:54 +0000

    rev 2.4

--- fwdmdl/RefractiveIndex.f90
+++ fwdmdl/RefractiveIndex.f90
@@ -12,116 +12,94 @@
 module RefractiveIndex
 
 !==================================================================
-! COMPUTE COMPLEX REFRACTIVE INDEX OF WATER DROPS AND ICE-PARTICLES       
+! COMPUTE COMPLEX REFRACTIVE INDEX OF WATER DROPS AND ICE PARTICLES
 !
-!* WI ---- 'W' OR 'I' ( WATER CASE OR ICE CASE )      ++ INPUT    *  
-!* T  ---- TEMPERATURE IN  K                          ++ INPUT    *  
+!* T  ---- TEMPERATURE IN  K                          ++ INPUT    *
 !* F  ---- FREQUENCY  IN  GHZ                         ++ INPUT    *
-!* E  ---- REFRACTIVE INDEX (E=M**2)  ++ OUTPUT                   *  
+!* E  ---- REFRACTIVE INDEX (E=M**2)                  ++ OUTPUT   *
 !==================================================================
 
-         use MLSCommon, only: r8
-         implicit none
+  implicit none
 
-         Private :: COMX
-         Public :: UKSUB, UKISUB
+  public :: UKSUB  ! For Water
+  public :: UKISUB ! For Ice
 
 !---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
-  private :: not_used_here 
+  private :: not_used_here
 !---------------------------------------------------------------------------
-      
+
 contains
 
-         SUBROUTINE UKSUB(F,T,M)
-         !-----------------------
-         real(r8) :: F, T
-         COMPLEX(r8) :: E,M
-         CALL COMX('W',T,F,E)
-         M=SQRT(E)
- 
-         END SUBROUTINE UKSUB
+  subroutine UKSUB ( F, T, M )  ! For Water
+    !-----------------------
+    use MLSKinds, only: r8
+    real(r8), intent(in) :: F, T
+    complex(r8), intent(out) :: M
+    real(r8) :: th, fp, fs, e0, x, y     ! Liebe
+    real(r8), parameter :: e1=5.48_r8    ! Liebe
+    real(r8), parameter :: e2=3.51_r8    ! Liebe
+!    real(r8) :: A, BV, C, E1_Lu, O0, O1, O2, R, S, Wl, X1, X2, XS, XC ! Lu
 
-         SUBROUTINE UKISUB(F,T,M)
-         ! ----------------------
-         real(r8) :: F, T
-         COMPLEX(r8) :: E, M
-         CALL COMX('I',T,F,E)
-         M=SQRT(E)
+    th = 300.0/t - 1.0
 
-         END SUBROUTINE UKISUB
-
-          subroutine comx(wi,t,f,e) 
-
-          complex(r8) :: e,c
-          character(1) wi
-          real(r8) :: t, f, th, fp, fs, e0, e1, e2, x, y
-          real(r8) :: a, b
-  
-          th=300./t
-!          wl=30./f
-          if ( wi .eq. 'W') then                                          
-            goto 100                                                  
-          else                                        
-            goto 200                                                  
-          end if                                                           
-                                                         
 !... from Liebe (1989)
 
- 100    if (t .lt. 233.15 ) then
-           t = 233.15
-        endif
+    fp = 20.09 - (142.4 - 294*th)*th
+    fs = 590.0 - 1500.0*th
+    e0 = 77.66 + 103.3*th
+    x = (e0-e1)/(1+(f/fp)**2) + (e1-e2)/(1+(f/fs)**2) + e2
+    y = -( (e0-e1)*f/(fp*(1+(f/fp)**2)) + (e1-e2)*f/(fs*(1+(f/fs)**2)) )
 
-        fp=20.09-142.4*(th-1.)+294*(th-1.)**2
-        fs=590.-1500.*(th-1.)
-        e0=77.66+103.3*(th-1.)
-        e1=5.48
-        e2=3.51
-        x=(e0-e1)/(1+(f/fp)**2)+(e1-e2)/(1+(f/fs)**2)+e2
-        y=(e0-e1)*f/fp/(1+(f/fp)**2)+(e1-e2)*f/fs/(1+(f/fs)**2)
+!... Lu
+!    wl=30./f
+!    C=max(max(T,233.15)-273.16,-50.0)
+!    E0=5.27137+(0.0216474-0.00131198*C)*C
+!    A=-16.8129/(C+273.)+0.0609265
+!    S=12.5664E+08
+!    R=0.00033836*EXP(2513.98/(C+273.))
+!    e1_Lu=78.54*(1.0-4.579E-3*(C-25.)+1.19E-5*(C-25.)**2-2.8E-8*(C-25.)**3)
+!    X1=(R/WL)**(1.0-A)
+!    XX=X1*X1
+!    X2=A*PAI/2
+!    XS=ASIN(X2)
+!    XC=ACOS(X2)
+!    BV=18.8496E+10
+!    O1=(e1_Lu-E0)*(1.0+X1*XS)
+!    O2=(e1_Lu-E0)*X1*XC
+!    O0=1.0+2.0*X1*XS+XX
+!    X=E0+O1/O0
+!    Y=-( S*WL/BV+O2/O0 )
+    m=sqrt(cmplx(x,y))
 
-!..Lu          C=T-273.16 
-!          IF(C.LT.-50.) C=-50.
-!  100     E0=5.27137+0.0216474*C-0.00131198*C*C                            
-!          A=-16.8129/(C+273.)+0.0609265                                    
-!          S=12.5664E+08                                                   
-!          R=0.00033836*EXP(2513.98/(C+273.))                               
-!          E1=78.54*(1.0-4.579E-3*(C-25.)+
-!     1       1.19E-5*(C-25.)**2-2.8E-8*(C-25.)**3)   
-!          X1=(R/WL)**(1.0-A)                                                 
-!          XX=X1*X1                                                         
-!          X2=A*PAI/2                                                       
-!          XS=ASIN(X2)                                                       
-!          XC=ACOS(X2)                                                       
-!          BV=18.8496E+10                                                   
-!          O1=(E1-E0)*(1.0+X1*XS)                                             
-!          O2=(E1-E0)*X1*XC                                                 
-!          O0=1.0+2.0*X1*XS+XX                                                
-!          X=E0+O1/O0                                                       
-!          Y=S*WL/BV+O2/O0  
+  end subroutine UKSUB
 
-        e=x*(1.0,0.0)+y*(0.0,-1.0)
+  subroutine UKISUB ( F, T, M )  ! For Ice
+    ! ----------------------
+    use MLSKinds, only: r8
+    real(r8), intent(in) :: F, T
+    complex(r8), intent(out) :: M
+    real(r8) :: a, b, th, x, y           ! Hufford
+    real(r8), parameter :: c=1.16e-11_r8 ! Mishima
 
-        return              !jj
+    th = 300.0/t
 
 !... from Hufford (1991) model
 
-  200   a=(50.4+62.*(th-1))*1.e-4*exp(-22.1*(th-1.))
-        b=(0.633/th-0.131)*1e-4+(7.36e-4*th/(th-0.9927))**2
+    a = (50.4 + 62.*(th-1)) * 1.0e-4*exp(-22.1*(th-1.))
+    b = (0.633/th-0.131)*1e-4 + (7.36e-4*th/(th-0.9927))**2
+    x = 3.15
+!    y = -( a/f+b*f )
 
 !... additional term from Mishima
-        c=1.16e-11_r8
 
-        x=3.15
-!        y=a/f+b*f
-        y=a/f+b*f+c*f**3
-!        write(*,*)y,f,t
+    y = -( a/f + b*f + c*f**3 )
+!    write(*,*)y,f,t
 
-        e=x*(1.0,0.0)+y*(0.0,-1.0)
-        return
+    m=sqrt(cmplx(x,y))
 
-        END subroutine comx
+  end subroutine UKISUB
 
   logical function not_used_here()
 !---------------------------- RCS Ident Info -------------------------------
@@ -135,6 +113,10 @@ contains
 end module RefractiveIndex
 
 ! $Log$
+! Revision 2.4  2007/10/05 23:59:54  vsnyder
+! Move water part of private COMX into UKSUB, ice part into UKISUB. then
+! delete COMX.  Polish up formatting.
+!
 ! Revision 2.3  2007/06/21 00:52:54  vsnyder
 ! Remove tabs, which are not part of the Fortran standard
 !
