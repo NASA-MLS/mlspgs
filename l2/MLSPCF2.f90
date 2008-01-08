83400ff66d3d44fa595f7616aea34d277ac99bf4
pwagner <>
2008-01-08 00:18:48 +0000

    rev 2.21

--- l2/MLSPCF2.f90
+++ l2/MLSPCF2.f90
@@ -33,8 +33,8 @@ MODULE MLSPCF2
    INTEGER, PARAMETER :: mlspcf_pcf_start = 900
    INTEGER, PARAMETER :: mlspcf_pcf_end = 900
    !
-   INTEGER, PARAMETER :: mlspcf_l2cf_start = 901
-   INTEGER, PARAMETER :: mlspcf_l2cf_end = 905
+   INTEGER, PARAMETER :: mlspcf_mobile_l2cf_start = 901
+   INTEGER, PARAMETER :: mlspcf_mobile_l2cf_end = 905
    !
    INTEGER, PARAMETER :: mlspcf_nomen_start = 20000
    INTEGER, PARAMETER :: mlspcf_nomen_end = 20000
@@ -66,11 +66,11 @@ MODULE MLSPCF2
    INTEGER, PARAMETER :: mlspcf_l2apriori_start = 21000
    INTEGER, PARAMETER :: mlspcf_l2apriori_end = 21049
    !
-   INTEGER, PARAMETER :: mlspcf_l1b_rad_start = 21050
-   INTEGER, PARAMETER :: mlspcf_l1b_rad_end = 21109
+   INTEGER, PARAMETER :: mlspcf_mobile_l1b_rad_start = 21050
+   INTEGER, PARAMETER :: mlspcf_mobile_l1b_rad_end = 21109
    !
-   INTEGER, PARAMETER :: mlspcf_l1b_oa_start = 21110
-   INTEGER, PARAMETER :: mlspcf_l1b_oa_end = 21139
+   INTEGER, PARAMETER :: mlspcf_mobile_l1b_oa_start = 21110
+   INTEGER, PARAMETER :: mlspcf_mobile_l1b_oa_end = 21139
    !
    INTEGER, PARAMETER :: mlspcf_l2ncep_start = 21900
    INTEGER, PARAMETER :: mlspcf_l2ncep_end = 21999
@@ -84,33 +84,34 @@ MODULE MLSPCF2
    INTEGER, PARAMETER :: mlspcf_l2clim_start = 22200
    INTEGER, PARAMETER :: mlspcf_l2clim_end = 22299
    !
-   INTEGER, PARAMETER :: mlspcf_l2gp_start = 30000
-   INTEGER, PARAMETER :: mlspcf_l2gp_end = 30569
+   INTEGER, PARAMETER :: mlspcf_mobile_l2gp_start = 30000
+   INTEGER, PARAMETER :: mlspcf_mobile_l2gp_end = 30569
    !
-   INTEGER, PARAMETER :: mlspcf_l2dgg_start = 30570
-   INTEGER, PARAMETER :: mlspcf_l2dgg_end = 30599
+   INTEGER, PARAMETER :: mlspcf_mobile_l2dgg_start = 30570
+   INTEGER, PARAMETER :: mlspcf_mobile_l2dgg_end = 30599
    !
-   INTEGER, PARAMETER :: mlspcf_l2dgm_start = 30600
-   INTEGER, PARAMETER :: mlspcf_l2dgm_end = 30629
+   INTEGER, PARAMETER :: mlspcf_mobile_l2dgm_start = 30600
+   INTEGER, PARAMETER :: mlspcf_mobile_l2dgm_end = 30629
    !
-   INTEGER, PARAMETER :: mlspcf_l2fwm_full_start = 30630
-   INTEGER, PARAMETER :: mlspcf_l2fwm_full_end = 30659
+   INTEGER, PARAMETER :: mlspcf_mobile_l2fwm_full_start = 30630
+   INTEGER, PARAMETER :: mlspcf_mobile_l2fwm_full_end = 30659
    !
-   INTEGER, PARAMETER :: mlspcf_l2parsf_start = 30660
-   INTEGER, PARAMETER :: mlspcf_l2parsf_end = 30660
+   INTEGER, PARAMETER :: mlspcf_mobile_l2parsf_start = 30660
+   INTEGER, PARAMETER :: mlspcf_mobile_l2parsf_end = 30660
    !
-   INTEGER, PARAMETER :: mlspcf_mcf_l2log_start = 4000
-   INTEGER, PARAMETER :: mlspcf_mcf_l2log_end = 4000
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2log_start = 4000
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2log_end = 4000
    !
-   INTEGER, PARAMETER :: mlspcf_mcf_l2gp_start = 4001
-   INTEGER, PARAMETER :: mlspcf_mcf_l2gp_end = 4021
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2gp_start = 4001
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2gp_end = 4021
    !
-   INTEGER, PARAMETER :: mlspcf_mcf_l2dgg_start = 4022
-   INTEGER, PARAMETER :: mlspcf_mcf_l2dgg_end = 4022
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2dgg_start = 4022
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2dgg_end = 4022
    !
-   INTEGER, PARAMETER :: mlspcf_mcf_l2dgm_start = 4023
-   INTEGER, PARAMETER :: mlspcf_mcf_l2dgm_end = 4023
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2dgm_start = 4023
+   INTEGER, PARAMETER :: mlspcf_mobile_mcf_l2dgm_end = 4023
    !
+   include 'pcf2mob.f9h'
 contains 
   logical function not_used_here()
 !---------------------------- RCS Ident Info -------------------------------
@@ -124,6 +125,9 @@ contains
 END MODULE MLSPCF2
 
 ! $Log$
+! Revision 2.21  2008/01/08 00:18:48  pwagner
+! Levels 1 and 2 can use same shared PCF now
+!
 ! Revision 2.20  2007/01/10 00:02:54  pwagner
 ! Made PCF entries for spectroscopy and surfaceHeight files
 !
