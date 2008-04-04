17c253dd7bb0e75f8bcbf61a0d6b6e038335eabb
pwagner <>
2008-04-04 20:51:22 +0000

    first commit

---
+++ util/slavetmpltntk.sh
@@ -0,0 +1,370 @@
+#!/bin/sh
+# slavetmpltntk.sh
+# runs a slave task when mlsl2 is in parallel mode
+#
+# Copyright 2008, by the California Institute of Technology. ALL
+# RIGHTS RESERVED. United States Government Sponsorship acknowledged. Any
+# commercial use must be negotiated with the Office of Technology Transfer
+# at the California Institute of Technology.
+
+# This software may be subject to U.S. export control laws. By accepting this
+# software, the user agrees to comply with all applicable U.S. export laws and
+# regulations. User has the responsibility to obtain export licenses, or other
+# export authority as may be required before exporting such information to
+# foreign countries or providing access to foreign persons.
+
+# usage: not called directly, but as mlsl2.slave after being seded
+
+# Used by mlsl2 master task mlsl2p.sh to launch slave tasks in toolkit
+# environment when running mlsl2 in parallel mode
+# mlsl2p.sh sed's this file to replace ssllaavvee, ppggssbbiinn, etc.
+# as appropriate
+
+# The resulting script will be called by mlsl2 master task
+# It will attempt to set some toolkt-savvy environment variables
+# and then launch the regular mlsl2 binary
+
+#---------------------------- add_option
+# To prevent adding the same option if it's already part
+# usage:
+# instead of: otheropts="--skipRetrieval $otheropts"
+# try: otheropts=`add_option "$otheropts" --skipRetrieval`
+# By default the new options get added on at the end, each separated by a space
+# To add them on at the front, supply a third arg
+# e.g.: otheropts=`add_option "$otheropts" --skipRetrieval to_front`
+add_option()
+{
+   # How many args?
+      if [ $# -gt 1 ]
+      then
+      # Does the option to be added duplicate one already present?
+        mayday=`echo "$1" | grep -e "$2"`
+        if [  "$1" = "" ]
+        then
+          echo $2
+        elif [  "$mayday" != "" ]
+        then
+          echo $1
+        elif [ $# -gt 2 ]
+        then
+          echo "$2 $1"
+        else
+          echo "$1 $2"
+        fi
+      fi
+}
+
+#---------------------------- get_unique_name
+get_unique_name()
+{
+
+      me="$0"
+      my_name=unique_name.sh
+   # How many args?
+      if [ $# -gt 1 ]
+      then
+        pt="$2"
+        temp="$1"
+      elif [ $# -gt 0 ]
+      then
+        mayday=`echo "$1" | grep '\-h'`
+        if [ "$mayday" != "" ]
+        then
+          sed -n '/'$my_name' help/,/End '$my_name' help/ p' $me \
+              | sed -n 's/^.//p' | sed '1 d; $ d'
+          exit
+        fi
+        pt="."
+        temp="$1"
+      else
+        pt="."
+        temp="temp"
+      fi
+   # Was second arg "-reverse?" If so,
+   # that's a reverse part oder in name flag, not a redef of pt
+   if [ "$pt" = "-reverse" ] 
+   then
+     reverse="yes"
+     pt="."
+   else
+     reverse="no"
+   fi
+   # Is $HOST defined?
+   if [ "$HOST" != "" ]
+   then
+      our_host_name="$HOST"
+   elif [ "$HOSTNAME" != "" ]
+   then
+      our_host_name="$HOSTNAME"
+   else
+      our_host_name="host"
+   fi
+   # if in form host.moon.planet.star.. extract host
+   our_host_name=`echo $our_host_name | sed 's/\./,/g'`
+   our_host_name=`perl -e '@parts=split(",","$ARGV[0]"); print $parts[0]' $our_host_name`
+   if [ "$reverse" = "yes" ]
+   then
+     echo $our_host_name.$$.$temp
+   else
+     echo $temp${pt}$our_host_name${pt}$$
+   fi
+}
+      
+#---------------------------- do_the_call
+#
+# Put after a trip through the main program simply to
+# separate the various options which may have been conglomerated
+# before being passed as args to this script.
+# It's possible that "--slave {task ID}" can come through as one
+# argument containing a space, rather than as 2 arguments.
+# The same thing will happen with --idents {masterIdent}.
+
+do_the_call()
+{
+
+# Not sure why this isn't done automatically
+if [ -r "$HOME/.bashrc" ]
+then
+. $HOME/.bashrc
+fi
+
+#This only works at the scf--change it when copying to the sips
+LASTDITCHPGSBIN="/software/toolkit/LF6.1/toolkit/bin/linux"
+#LOGFILE=${HOME}/slave.log
+PGE_ROOT=ppggeerroott
+#echo $PGE_ROOT > $LOGFILE
+if [ -r "${HOME}/mlspgs/util/tkreset.sh"  ]
+then
+#echo ${HOME}/mlspgs/util/tkreset.sh >> $LOGFILE
+#echo ". ${HOME}/mlspgs/util/tkreset.sh"  >> $LOGFILE
+#. ${HOME}/mlspgs/util/tkreset.sh  >> $LOGFILE
+. ${HOME}/mlspgs/util/tkreset.sh
+fi
+#env >> $LOGFILE
+PGSBIN=ppggssbbiinn
+#echo $PGSBIN >> $LOGFILE
+if [ -r "$PGE_ROOT/science_env.sh"  ]
+then
+. ${PGE_ROOT}/science_env.sh
+elif [ -r "$PGSBIN/pgs-env.ksh" ]
+then
+. $PGSBIN/pgs-env.ksh
+else
+#Last ditch--if this doesn't work we're outta here
+. $LASTDITCHPGSBIN/pgs-env.ksh
+fi
+export
+PGSMEM_USESHM=ppggssmmeemmuusseesshhmm
+SLVPROG=ssllaavveessccrriipptt
+OTHEROPTS=ootthheerrooppttss
+PGE_BINARY=ppggeebbiinnaarryy
+JOBDIR=jjoobbddiirr
+export PGSMEM_USESHM
+export FLIB_DVT_BUFFER=0
+
+# The following choice puts the outputs of all the slaves into a single file
+#LOGFILE="${JOBDIR}/pvmlog/mlsl2.log"
+
+# The next choice, in contrast, puts each slave's output into its own unique file
+temp_file_name=`get_unique_name log -reverse`
+#OLDLOGFILE=$LOGFILE
+LOGFILE="${JOBDIR}/pvmlog/$temp_file_name"
+UNBUFFERED="${LOGFILE}.u"
+#ENVSETTINGS="${LOGFILE}.env"
+#echo $LOGFILE >> $OLDLOGFILE
+
+#env  2>&1 | tee "$ENVSETTINGS"
+
+if [ ! -w "$LOGFILE" ]
+then
+  echo "#$LOGFILE mlsl2.log" 2>&1 | tee "$LOGFILE"
+fi
+echo "Slave task slavetmpltntk.sh started with arguments: $@" >> $LOGFILE
+
+# It's possible that $1 is the command name--in which case we
+# need to do a shift to get the actual args
+if [ "$1" = "$SLVPROG" ]
+then
+  shift
+  echo "Need to shift because 1st arg is command name" 2>&1 | tee -a "$LOGFILE"
+fi
+
+masterIdent="none"
+otheropts="-g"
+switches="-S'slv,opt1,log,pro,time,glob1'"
+# otheropts="-g -S'slv,opt1,log,pro,time,glob1'"
+# otheropts="$OTHEROPTS"
+echo "otheropts starting as $otheropts" 2>&1 | tee -a "$LOGFILE"
+more_opts="yes"
+while [ "$more_opts" = "yes" ] ; do
+    echo "Considering argument: $1" >> $LOGFILE
+    case "$1" in
+    --chunk )
+       echo "Skipping chunk-setting arguments: $1 $2" >> $LOGFILE
+       shift
+       shift
+       ;;
+    --crash* )
+       otheropts=`add_option "$otheropts" $1`
+       echo "Adding argument to crash on any error: $1" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       ;;
+    --delay )
+       echo "Skipping argument to change master delay: $1 $2" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       shift
+       ;;
+    --idents )
+       masterIdent="$2"
+       shift
+       shift
+       ;;
+    --skipR* )
+       otheropts=`add_option "$otheropts" --skipRetrieval`
+       echo "Adding argument to skip retrieval: $1" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       ;;
+    --skipS* )
+       otheropts=`add_option "$otheropts" --skipSections`
+       otheropts=`add_option "$otheropts" $2`
+       echo "Adding argument to skip certain sections: $1 $2" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       shift
+       ;;
+    --slave* )
+	   masterTid="$2"
+	   shift
+	   shift
+       ;;
+    --state* )
+       otheropts=`add_option "$otheropts" $1`
+       otheropts=`add_option "$otheropts" $2`
+       echo "Adding argument to fill skipped retrievals: $2" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       shift
+       ;;
+    --stdout )
+       otheropts=`add_option "$otheropts" $1`
+       # Note that we can't have all the slaves and masters directing
+       # unbuffered stdout to $2
+       otheropts=`add_option "$otheropts" $UNBUFFERED`
+       echo "Adding argument to buffer stdout to $UNBUFFERED" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       shift
+       ;;
+    --share* )
+       otheropts=`add_option "$otheropts" --sharedPCF`
+       shift
+       ;;
+    --ntk )
+       otheropts=`add_option "$otheropts" --ntk`
+       shift
+       ;;
+    --cat )
+       otheropts=`add_option "$otheropts" --cat`
+       echo "Adding argument to catenate split dgg/dgm files: $1" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       ;;
+    --wall )
+       otheropts=`add_option "$otheropts" --wall`
+       echo "Adding argument to use wall clock: $1" >> $LOGFILE
+       echo "$otheropts" >> $LOGFILE
+       shift
+       ;;
+    --* )
+       echo "Skipping unknown argument: $1" >> $LOGFILE
+       shift
+       ;;
+#    -S* )
+#       echo "Skipping switch-setting argument: $1" >> $LOGFILE
+#       shift
+#       ;;
+    -S* )
+       switches="$switches $1"
+       echo "Appending switch-setting argument: $1" >> $LOGFILE
+       shift
+       ;;
+    -f* )
+       otheropts=`add_option "$otheropts" $1`
+       echo "Adding argument to trace forward model: $1" >> $LOGFILE
+       shift
+       ;;
+    -g* )
+       echo "Skipping superfluous argument to trace execution: $1" >> $LOGFILE
+       shift
+       ;;
+    -* )
+       echo "Skipping unknown argument: $1" >> $LOGFILE
+       shift
+       ;;
+    * )
+       echo "Finished considering arguments" >> $LOGFILE
+       more_opts="no"
+       ;;
+    esac
+done
+
+otheropts="$otheropts $switches"
+
+echo "(running without toolkit panoply)" 2>&1 | tee -a "$LOGFILE"
+echo "masterTid: $masterTid" 2>&1 | tee -a "$LOGFILE"
+echo "masterIdent file: $masterIdent" 2>&1 | tee -a "$LOGFILE"
+echo "executable: $PGE_BINARY" 2>&1 | tee -a "$LOGFILE"
+echo "otheropts $otheropts" 2>&1 | tee -a "$LOGFILE"
+
+if [ "$masterTid" = "" ]
+then
+  echo "masterTid undefined" 2>&1 | tee -a "$LOGFILE"
+  exit 1
+elif [ ! -x "$PGE_BINARY" ]
+then
+  echo "$PGE_BINARY not found/executable" 2>&1 | tee -a "$LOGFILE"
+  exit 1
+fi
+
+# Compare master task's ident with this slave's
+# If different, quit with error message
+if [ -f "$masterIdent" ]
+then
+  the_diff=`ident "$PGE_BINARY" | diff - "$masterIdent"`
+  if [ ! "$the_diff" = "" ]
+  then
+     echo "master task ident differs from slave"
+     echo "Possibly an error in paths; please check PVM_BIN, PVM_EP, ~/bin"
+     exit 1
+  fi
+fi
+
+#env | sort > "$ENVSETTINGS"
+ulimit -s unlimited
+#ulimit -a >> "$ENVSETTINGS"
+
+echo $PGE_BINARY --ntk -m --slave $masterTid $otheropts $@ 2>&1 | tee -a "$LOGFILE"
+$PGE_BINARY --ntk -m --slave $masterTid $otheropts $@ 2>&1 | tee -a "$LOGFILE"
+
+}
+      
+#------------------------------- Main Program ------------
+
+#****************************************************************
+#                                                               *
+#                  * * * Main Program  * * *                    *
+#                                                               *
+#                                                               *
+#	The entry point where control is given to the script         *
+#****************************************************************
+
+# This just separates any args that have suffered conglomeration
+# e.g., "arg1 arg2" being passed as a single space-containing arg
+all_my_opts=$@
+do_the_call $all_my_opts
+exit 0
+
+# $Log$
