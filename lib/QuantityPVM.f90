c144d2d3420ac71e44a556e0bd18e2acedd57e32
pwagner <>
2009-06-23 18:26:06 +0000

    rev 2.23

--- lib/QuantityPVM.f90
+++ lib/QuantityPVM.f90
@@ -420,18 +420,22 @@ contains ! ================================== Module procedures ============
 
   end subroutine PVMReceiveQuantity
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module QuantityPVM
 
 ! $Log$
+! Revision 2.23  2009/06/23 18:25:42  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.22  2006/11/29 03:05:19  vsnyder
 ! Remove unused USE names
 !
