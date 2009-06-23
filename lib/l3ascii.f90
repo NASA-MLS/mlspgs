938e276dc356a620217ff7663930c297d97a75c5
pwagner <>
2009-06-23 18:25:59 +0000

    rev 2.35

--- lib/l3ascii.f90
+++ lib/l3ascii.f90
@@ -847,21 +847,24 @@ itemsloop:do
 !===========================
 
 !=============================================================================
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module L3ascii
 !=============================================================================
 
 !
 ! $Log$
+! Revision 2.35  2009/06/23 18:25:43  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.34  2009/06/16 17:19:22  pwagner
 ! Made default access private
 !
