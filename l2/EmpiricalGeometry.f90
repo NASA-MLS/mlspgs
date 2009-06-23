94aa6313791b0182a7202139c523be71c409b225
pwagner <>
2009-06-23 18:46:30 +0000

    rev 2.14

--- l2/EmpiricalGeometry.f90
+++ l2/EmpiricalGeometry.f90
@@ -216,19 +216,22 @@ contains ! ========================= Public Procedures ====================
     if (NormalizeLongitude > 180.0) NormalizeLongitude = NormalizeLongitude - 360.0
   end function NormalizeLongitude
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module EmpiricalGeometry
 
 ! $Log$
+! Revision 2.14  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.13  2009/05/13 20:41:55  vsnyder
 ! Get constants from Constants, kinds from MLSKinds
 !
