7376c53d013504f3dac1daae9d20d1f4e0f122c5
pwagner <>
2009-06-23 18:46:50 +0000

    rev 2.93

--- l2/L2Parallel.f90
+++ l2/L2Parallel.f90
@@ -1996,20 +1996,23 @@ contains
   end subroutine ThankL2Q
 
   ! -------------------------------------- Never used here --------------
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module L2Parallel
 
 !
 ! $Log$
+! Revision 2.93  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.92  2009/06/16 17:41:19  pwagner
 ! Changed api for dump, diff routines; now rely on options for most optional behavior
 !
