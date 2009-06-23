f46c333e7e92604e6eca12af349727b2c7aa37db
pwagner <>
2009-06-23 18:46:56 +0000

    rev 2.8

--- l2/TestCholesky_m.f90
+++ l2/TestCholesky_m.f90
@@ -71,18 +71,22 @@ contains
     deallocate ( bigCholesky, bigNeq, stat=ierr )
   end subroutine TestCholesky 
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module TestCholesky_M
 
 ! $Log$
+! Revision 2.8  2009/06/23 18:46:19  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.7  2005/06/22 18:57:02  pwagner
 ! Reworded Copyright statement, moved rcs id
 !
