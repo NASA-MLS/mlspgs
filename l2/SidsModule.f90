db4162ae9a8bdc0ec2dc78496545c319d61c613b
pwagner <>
2009-06-23 18:46:32 +0000

    rev 2.53

--- l2/SidsModule.f90
+++ l2/SidsModule.f90
@@ -364,18 +364,22 @@ contains
 
   end subroutine SIDS
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module SidsModule
 
 ! $Log$
+! Revision 2.53  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.52  2008/06/06 01:59:28  vsnyder
 ! Set DoTScat flag in configs, some cannonball polishing
 !
