d344df5e8f9a727877ced160e49cc0a2ad597ae1
pwagner <>
2009-06-23 18:46:42 +0000

    rev 2.97

--- l2/open_init.f90
+++ l2/open_init.f90
@@ -643,14 +643,15 @@ contains ! =====     Public Procedures     =============================
   end subroutine Announce_Error
 !===========================
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module Open_Init
 
@@ -658,6 +659,9 @@ end module Open_Init
 
 !
 ! $Log$
+! Revision 2.97  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.96  2007/10/04 20:43:12  vsnyder
 ! Remove unused symbols
 !
