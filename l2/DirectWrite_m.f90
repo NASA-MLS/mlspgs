8124a256dee21e45121959fd0485a55184e8cc33
pwagner <>
2009-06-23 18:46:38 +0000

    rev 2.44

--- l2/DirectWrite_m.f90
+++ l2/DirectWrite_m.f90
@@ -1081,19 +1081,22 @@ contains ! ======================= Public Procedures =========================
     end  if
   end subroutine ANNOUNCE_ERROR
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module DirectWrite_m
 
 ! $Log$
+! Revision 2.44  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.43  2009/04/23 23:02:52  pwagner
 ! May specify upperOverlap or lowerOverlap in DirectWrites
 !
