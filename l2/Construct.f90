dec57228e7dfc5e9347718042ba8dd878442f67e
pwagner <>
2009-06-23 18:46:48 +0000

    rev 2.63

--- l2/Construct.f90
+++ l2/Construct.f90
@@ -273,20 +273,24 @@ contains ! =====     Public Procedures     =============================
   end subroutine MLSL2DeConstruct
 
 !=============================================================================
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-  !------------------------------- RCS Ident Info ------------------------------
-  character(len=*), parameter :: IdParm = &
-    & "$Id$"
-  character(len=len(idParm)), save :: Id = idParm
-  !-----------------------------------------------------------------------------
+  character (len=*), parameter :: IdParm = &
+       "$Id$"
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 END MODULE Construct
 !=============================================================================
 
 !
 ! $Log$
+! Revision 2.63  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.62  2007/12/07 01:13:18  pwagner
 ! Lets us catch warnings and assign to runtime Booleans
 !
