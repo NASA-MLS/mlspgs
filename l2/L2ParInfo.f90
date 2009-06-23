747b3e0f74e9cb48dea7cbbb490bffe340678aae
pwagner <>
2009-06-23 18:46:36 +0000

    rev 2.52

--- l2/L2ParInfo.f90
+++ l2/L2ParInfo.f90
@@ -817,18 +817,22 @@ contains ! ==================================================================
     endif
   end function what_options
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module L2ParInfo
 
 ! $Log$
+! Revision 2.52  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.51  2009/06/16 17:41:37  pwagner
 ! Changed api for dump, diff routines; now rely on options for most optional behavior
 !
