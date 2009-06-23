42c00a07fd62015757df915728275892b6fd01c4
pwagner <>
2009-06-23 18:46:41 +0000

    rev 2.139

--- l2/Join.f90
+++ l2/Join.f90
@@ -2134,20 +2134,23 @@ contains ! =====     Public Procedures     =============================
     end if
   end subroutine ANNOUNCE_ERROR
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module Join
 
 !
 ! $Log$
+! Revision 2.139  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.138  2009/06/02 17:53:15  cvuu
 ! Add NRT Lat and Lon bounding to metadata
 !
