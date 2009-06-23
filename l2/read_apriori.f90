29c8ac3ee9177c3a19bbf5d1547a6d98c9a2b29b
pwagner <>
2009-06-23 18:46:46 +0000

    rev 2.77

--- l2/read_apriori.f90
+++ l2/read_apriori.f90
@@ -931,15 +931,15 @@ contains ! =====     Public Procedures     =============================
   end subroutine Announce_error
 !===========================
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module ReadAPriori
 
@@ -947,6 +947,9 @@ end module ReadAPriori
 
 !
 ! $Log$
+! Revision 2.77  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.76  2008/12/02 23:12:47  pwagner
 ! mls_io_gen_[openF,closeF] functions now private; use MLSFile_T interfaces instead
 !
