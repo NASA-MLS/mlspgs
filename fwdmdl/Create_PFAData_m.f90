4cefceb62b55a2ac2ff62a71cbbdf5abcc8baac6
pwagner <>
2009-06-23 18:26:22 +0000

    rev 2.24

--- fwdmdl/Create_PFAData_m.f90
+++ fwdmdl/Create_PFAData_m.f90
@@ -424,19 +424,22 @@ contains ! =====     Public Procedures     =============================
 
 ! =====     Private Procedures     =====================================
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
   character (len=len(idParm)) :: Id = idParm
-!---------------------------------------------------------------------------
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module Create_PFAData_m
 
 ! $Log$
+! Revision 2.24  2009/06/23 18:26:10  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.23  2008/10/03 16:26:32  livesey
 ! Pushed lo down to support EXTINCTIONV2
 !
