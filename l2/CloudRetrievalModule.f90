d0c99f4e2cb9bdb9561e11dcad2ed8dbd8278885
pwagner <>
2009-06-23 18:46:26 +0000

    rev 2.13

--- l2/CloudRetrievalModule.f90
+++ l2/CloudRetrievalModule.f90
@@ -942,17 +942,20 @@ contains
 
  end subroutine CloudRetrieval
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 end module CloudRetrievalModule
 ! $Log$
+! Revision 2.13  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.12  2009/05/13 20:41:55  vsnyder
 ! Get constants from Constants, kinds from MLSKinds
 !
