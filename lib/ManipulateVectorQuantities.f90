8e7092f9c2de8cd2831f7f33952bea312dab0c70
pwagner <>
2009-06-23 18:25:46 +0000

    rev 2.34

--- lib/ManipulateVectorQuantities.f90
+++ lib/ManipulateVectorQuantities.f90
@@ -496,18 +496,22 @@ contains
 
   end function DoQtysDescribeSameThing
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module ManipulateVectorQuantities
   
 ! $Log$
+! Revision 2.34  2009/06/23 18:25:42  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.33  2008/06/06 22:52:21  pwagner
 ! EssentiallyEqual moved to MLSFillValues
 !
