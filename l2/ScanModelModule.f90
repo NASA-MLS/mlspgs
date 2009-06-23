0b89a3a5069460cdb8fdced5df6f4b2348515e3c
pwagner <>
2009-06-23 18:46:47 +0000

    rev 2.72

--- l2/ScanModelModule.f90
+++ l2/ScanModelModule.f90
@@ -2173,19 +2173,22 @@ contains ! =============== Subroutines and functions ==========================
       if (Timing) call output( iter, advance='yes')
     end function Z_surface
   end subroutine TwoDScanForwardModel
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module ScanModelModule
 
 ! $Log$
+! Revision 2.72  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.71  2009/05/13 20:41:55  vsnyder
 ! Get constants from Constants, kinds from MLSKinds
 !
