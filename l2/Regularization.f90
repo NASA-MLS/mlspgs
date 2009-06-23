5d1e19b2afbeac3b12ac762ccebdba1b37a62b15
pwagner <>
2009-06-23 18:46:39 +0000

    rev 2.40

--- l2/Regularization.f90
+++ l2/Regularization.f90
@@ -686,18 +686,22 @@ o:          do while ( c2 <= ni )
 
   end subroutine Regularize
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module Regularization
 
 ! $Log$
+! Revision 2.40  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.39  2006/11/29 01:07:31  vsnyder
 ! Fix up some comments
 !
