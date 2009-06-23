f20d55a52ff80cbd2be1e1f0c369cc19188b350b
pwagner <>
2009-06-23 18:46:31 +0000

    rev 2.49

--- l2/dnwt_module.f90
+++ l2/dnwt_module.f90
@@ -1361,18 +1361,22 @@ contains
     end select
   end subroutine FlagName
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module DNWT_MODULE
 
 ! $Log$
+! Revision 2.49  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.48  2007/09/22 00:25:16  vsnyder
 ! Initialize DXMAXI to 0.1*huge() so as not to get overflow
 !
