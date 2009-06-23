08c514ede9d83bbc4f47be5f8940ba6fc4aee441
pwagner <>
2009-06-23 18:26:21 +0000

    rev 2.31

--- fwdmdl/BaselineForwardModel_m.f90
+++ fwdmdl/BaselineForwardModel_m.f90
@@ -515,18 +515,22 @@ contains ! ======================================== BaselineForwardModel ======
 
   end subroutine BaselineForwardModel
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module BaselineForwardModel_m
   
 ! $Log$
+! Revision 2.31  2009/06/23 18:26:10  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.30  2009/04/20 18:46:55  pwagner
 ! Needed changes to allow radiance adjustments by means of a baseline fwdmdl
 !
