dd93883ad3a6b70bb93e895b007042c9a34d299f
pwagner <>
2009-06-23 18:46:28 +0000

    rev 2.25

--- l2/L2FWMParallel.f90
+++ l2/L2FWMParallel.f90
@@ -760,18 +760,22 @@ contains
     end do listenLoop
   end subroutine IntelligentPVMFRecv
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module L2FWMParallel
 
 ! $Log$
+! Revision 2.25  2009/06/23 18:46:18  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.24  2008/05/02 00:34:35  vsnyder
 ! Delete unused symbol
 !
