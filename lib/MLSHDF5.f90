e0bd2839962b029ebb6400697cf9c4a5de6d906c
pwagner <>
2009-06-23 18:25:56 +0000

    rev 2.92

--- lib/MLSHDF5.f90
+++ lib/MLSHDF5.f90
@@ -4955,19 +4955,22 @@ contains ! ======================= Public Procedures =========================
     end if
   end function WhatTypeAmI
 
+!--------------------------- end bloc --------------------------------------
   logical function not_used_here()
-!---------------------------- RCS Ident Info -------------------------------
   character (len=*), parameter :: IdParm = &
        "$Id$"
-  character (len=len(idParm)), save :: Id = idParm
-!---------------------------------------------------------------------------
+  character (len=len(idParm)) :: Id = idParm
     not_used_here = (id(1:1) == ModuleName(1:1))
-    print *, not_used_here ! .mod files sometimes change if PRINT is added
+    print *, Id ! .mod files sometimes change if PRINT is added
   end function not_used_here
+!---------------------------------------------------------------------------
 
 end module MLSHDF5
 
 ! $Log$
+! Revision 2.92  2009/06/23 18:25:42  pwagner
+! Prevent Intel from optimizing ident string away
+!
 ! Revision 2.91  2009/06/16 17:14:24  pwagner
 ! Changed api for dump, diff routines; now rely on options for most optional behavior
 !
