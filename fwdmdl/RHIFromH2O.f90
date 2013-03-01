22820208bad49b0d0e87e9800d4598378f90ea73
pwagner <>
2013-03-01 01:06:57 +0000

    rev 2.6

--- fwdmdl/RHIFromH2O.f90
+++ fwdmdl/RHIFromH2O.f90
@@ -17,16 +17,16 @@ module RHIFromH2O                     ! H2O <-> RHI Conversions
   ! between H2O  concentration and RHI.
   ! Also their respective precisions
 
-  use MLSCommon, only: R8
+  use MLSCOMMON, only: DEFAULTUNDEFINEDVALUE
+  use MLSKINDS, only: R8
   implicit none
   private
-  public :: RHIFromH2O_Factor, RHIPrecFromH2O, H2OPrecFromRHi
+  public :: RHIFROMH2O_FACTOR, RHIPRECFROMH2O, H2OPRECFROMRHI
 !---------------------------- RCS Module Info ------------------------------
   character (len=*), private, parameter :: ModuleName= &
        "$RCSfile$"
   private :: not_used_here 
 !---------------------------------------------------------------------------
-  real, parameter ::    UNDEFINED_VALUE = -999.99 ! Same as %template%badvalue
 
 contains ! =====     Public Procedures     =============================
 
@@ -186,7 +186,7 @@ contains ! =====     Public Procedures     =============================
       real(r8), parameter    :: a0 = -1.2141649d0
       real(r8), parameter    :: a1 = 9.09718d0
       real(r8), parameter    :: a2 = 0.876793d0
-      real, parameter        :: ILLEGALTEMP = UNDEFINED_VALUE
+      real, parameter        :: ILLEGALTEMP = DEFAULTUNDEFINEDVALUE
       !
       if ( T > 0.d0 ) then
         C = a0 - a1*(273.16/T -1.0d0) + a2*(1.0d0 - T/273.16)
@@ -203,7 +203,7 @@ contains ! =====     Public Procedures     =============================
       real(r8), parameter    :: a0 = -1.2141649d0
       real(r8), parameter    :: a1 = 9.09718d0
       real(r8), parameter    :: a2 = 0.876793d0
-      real, parameter        :: ILLEGALTEMP = UNDEFINED_VALUE
+      real, parameter        :: ILLEGALTEMP = DEFAULTUNDEFINEDVALUE
       !
       if ( T > 0.d0 ) then
         dC_dT = a1*(273.16/T**2) - a2/273.16
@@ -227,6 +227,9 @@ end module RHIFromH2O
 
 !
 ! $Log$
+! Revision 2.6  2013/03/01 01:06:57  pwagner
+! Get R8 from MLSKinds; DEFAULTUNDEFINEDVALUE from MLSCommon
+!
 ! Revision 2.5  2009/08/24 20:08:55  pwagner
 ! May Fill H2O precision from RHI precision
 !
