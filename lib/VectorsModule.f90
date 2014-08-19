0513bd068a8e5b74cb620e1002c9c5d9dc63472c
vsnyder <>
2014-08-19 00:29:59 +0000

    Add VectorMemoryInUse, VectorsMemoryInUse

--- lib/VectorsModule.f90
+++ lib/VectorsModule.f90
@@ -105,6 +105,8 @@ module VectorsModule            ! Vectors in the MLS PGS suite
 ! SubtractFromVector           x = x - y
 ! SubtractVectors              Returns z = x - y
 ! ValidateVectorQuantity       Test vector quantity for matching components
+! VectorMemoryInUse            Returns number of elements in all quantities
+! VectorsMemoryInUse           Returns number of elements in all vectors
 ! === (end of toc) ===
 
   ! --------------------------------------------------------------------------
@@ -168,6 +170,7 @@ module VectorsModule            ! Vectors in the MLS PGS suite
   public :: RESHAPEVECTORVALUE, RMVECTORFROMDATABASE
   public :: SCALEVECTOR, SETMASK, SUBTRACTFROMVECTOR
   public :: SUBTRACTVECTORS, VALIDATEVECTORQUANTITY
+  public :: VectorMemoryInUse, VectorsMemoryInUse
   ! Types
   public :: VECTORTEMPLATE_T, VECTORVALUE_T, VECTOR_T
   ! Parameters
@@ -900,26 +903,71 @@ contains ! =====     Public Procedures     =============================
     character(len=*), optional, intent(in) :: OPTIONS
     ! Internal variables
     character(len=8) :: MYOPTIONS
+integer :: I, J
+character, pointer :: C1, C2, C3
+print '(a)', 'CloneVectorQuantity 1'
 
     ! Executable statements:
     myOptions = ' '
     if ( present(options) ) myOptions = options
     call NullifyVectorValue ( z )
+print '(a)', 'CloneVectorQuantity 2'
     if ( index( myOptions, 'd' ) > 0 ) then
       call CopyQuantityTemplate ( z%template, x%template )
+print '(a)', 'CloneVectorQuantity 3'
     else
       z%template = x%template
+print '(a)', 'CloneVectorQuantity 4'
     endif
     z%index = x%index
     if ( associated(x%values) ) then
       call createVectorValue ( z, 'z%values' )
+print '(a)', 'CloneVectorQuantity 5'
       z%values = x%values
+print '(a)', 'CloneVectorQuantity 6'
     end if
     if ( associated(x%mask) ) then
       call createMask ( z )
+print '(a)', 'CloneVectorQuantity 7'
+call dump ( x, details=2 )
+print '(a)', 'CloneVectorQuantity 7.1'
+if ( .not. associated(z%mask) ) then
+  print '(a)', 'Why is z%mask not associated?'
+else
+  if ( .not. associated(z%mask1) ) print '(a)', 'Why is z%mask1 not associated?'
+  if ( .not. associated(z%mask3) ) print '(a)', 'Why is z%mask3 not associated?'
+  if ( associated(z%mask1) .and. associated(z%mask3) ) then
+    c1 => z%mask1(1)
+    c2 => z%mask(1,1)
+    c3 => z%mask3(1,1,1)
+    if ( .not. associated(c1,c2) ) print '(a)', 'Why are C1 and C2 not associated?'
+    if ( .not. associated(c3,c2) ) print '(a)', 'Why are C3 and C2 not associated?'
+  end if
+  print '(a)', 'CloneVectorQuantity 7.2'
+  call dump ( z, details=2, name='ZZZZ' )
+print '(a)', 'CloneVectorQuantity 7.3'
+  if ( size(z%mask) /= size(x%mask) ) then
+  print '(a)', 'Why do x%mask and z%mask have different sizes?'
+  end if
+print '(a)', 'CloneVectorQuantity 7.3.1'
+  if ( any(shape(z%mask) /= shape(x%mask)) ) then
+  print '(a)', 'Why do x%mask and z%mask have different shapes?'
+  end if
+end if
+print '(a)', 'CloneVectorQuantity 7.4'
+print '(a,i0)', 'Count(ichar(x%mask)/=0) = ', Count(ichar(x%mask)/=0)
+print '(a,i0)', 'Count(ichar(z%mask)/=0) = ', Count(ichar(z%mask)/=0)
+print '(2(a,2i5))', 'Shape(x%mask) = ', shape(x%mask), ' Shape(z%mask) = ', shape(z%mask)
+do i = 1, size(x%mask,1)
+do j = 1, size(x%mask,2)
+print '(5(a,i0))', 'Doing z%mask(',i,',',j,') = x%mask(',i,',',j,')'
+z%mask(i,j) = x%mask(i,j)
+end do; end do
       z%mask = x%mask
+print '(a)', 'CloneVectorQuantity 8'
     end if
     z%label = x%label
+print '(a)', 'CloneVectorQuantity 9'
   end subroutine CloneVectorQuantity
 
   ! --------------------------------------------  ConstantXVector  -----
@@ -3230,6 +3278,34 @@ contains ! =====     Public Procedures     =============================
 
   end function ValidateVectorQuantity
 
+  ! ------------------------------------------ VectorMemoryInUse -------
+  integer function VectorMemoryInUse ( V )
+  ! Report the total number of real(rv) elements in all quantities in vector V.
+    type (Vector_T), intent(in) :: V
+    integer :: I
+
+    vectorMemoryInUse = 0
+    do i = 1, size(v%quantities)
+      if ( associated(v%quantities(i)%value1) ) &
+        & vectorMemoryInUse = vectorMemoryInUse + size(v%quantities(i)%value1)
+    end do
+
+  end function VectorMemoryInUse
+
+  ! ----------------------------------------- VectorsMemoryInUse -------
+  integer function VectorsMemoryInUse ( D )
+  ! Report the total number of real(rv) elements in all vectors in the
+  ! database.
+    type (Vector_T),  dimension(:) :: D
+    integer :: I
+
+    vectorsMemoryInUse = 0
+    do i = 1, size(d)
+      vectorsMemoryInUse = vectorsMemoryInUse + vectorMemoryInUse ( d(i) )
+    end do
+
+  end function VectorsMemoryInUse
+
 ! =====     Private Procedures     =====================================
   subroutine CreateValues ( Vector, highBound, lowBound )
   ! Allocate space for the values of a vector.
@@ -3283,6 +3359,9 @@ end module VectorsModule
 
 !
 ! $Log$
+! Revision 2.190  2014/07/18 23:13:24  pwagner
+! Aimed for consistency in names passed to allocate_test; added allocationName field to datatype
+!
 ! Revision 2.189  2014/05/20 23:53:19  vsnyder
 ! Add the POINTER attribute to the SourceValues argument to ReshapeVectorValue
 ! to work around bugs in compilers that have not properly implemented the
