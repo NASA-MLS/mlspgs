7d9f5fc39ddc8876aad2aef3c33222f694c5945d
vsnyder <>
2016-11-17 01:47:40 +0000

    rev 2.3

--- fwdmdl/More_Metrics_3D_m.f90
+++ fwdmdl/More_Metrics_3D_m.f90
@@ -32,12 +32,13 @@ contains
           ! Outputs
           & T_Path, dHitdZi, &
           ! Optional inputs
-          & QTM_Tree, ddHidHidTl0, dHidTlm, T_Deriv_Flag, Z_Basis, Z_Ref, &
+          & QTM_Tree, ddHidHidTl0, dHidTlm, T_Sv, Z_Ref, &
           ! Optional outputs
-          & ddHtdHtdTl0, dHitdTlm, dHtdTl0, dHtdZt, Eta_zQT, Tan_T )
+          & ddHtdHtdTl0, dHitdTlm, dHtdTl0, dHtdZt, Eta_zQT, &
+          & Do_Calc_T, Do_Calc_Hyd, Tan_T )
 
     use Generate_QTM_m, only: QTM_Tree_t
-    use Geolocation_0, only: RG
+    use Load_Sps_Data_m, only: Grids_t
     use MLSKinds, only: RP
     use QTM_Interpolation_Weights_3D_m, only: QTM_Interpolation_Weights, &
       & S_QTM_t, Weight_ZQ_t
@@ -46,28 +47,29 @@ contains
 
     type(S_QTM_t), intent(in) :: S(:)    ! Intersections of path with constant-zeta
     integer, intent(in) :: N_Tan         ! Tangent index in S
-    real(rp), intent(in) :: T_Ref(:,:)   ! temperatures at Z_Ref X hGrid
-    real(rp), intent(in) :: dHidZij(:,:) ! vertical derivative at Z_Ref X hGrid
+    real(rp), intent(in) :: T_Ref(:,:)   ! Temperatures at Z_Ref X hGrid
+                                         ! adjacent to the path
+    real(rp), intent(in) :: dHidZij(:,:) ! Vertical derivative at Z_Ref X hGrid
+                                         ! adjacent to the path
 
     ! Outputs
 
-    real(rp), intent(out) :: T_Path(:)   ! computed temperatures
-    real(rp), intent(out) :: dHitdZi(:)  ! derivative of height wrt zeta
-                                       ! --may be useful in future computations
+    real(rp), intent(out) :: T_Path(:)   ! Computed temperatures
+    real(rp), intent(out) :: dHitdZi(:)  ! Derivative of height wrt zeta --
+                                         ! may be useful in future computations
 
-    ! Optional inputs
+    ! Optional inputs for temperature derivatives
 
     type(QTM_tree_t), optional, intent(in) :: QTM_Tree
     real(rp), optional, intent(in) :: ddHidHidTl0(:,:,:) ! second order
-    !          derivatives of height w.r.t T_Ref. This is (height, QTM,
-    !          zeta_basis). Needed only if present(dHidTlm).
+    !          derivatives of height w.r.t T_Ref. This is an extract of
+    !          (height, zeta_basis, QTM) adjacent to the path.
+    !          Needed only if present(dHidTlm).
     real(rp), optional, intent(inout) :: dHidTlm(:,:,:) ! reference temperature
     !          derivatives. This gets adjusted so that at ref_h(1,@tan)) is
-    !          0.0 for all temperature coefficients.
-    !          This is height X zeta_basis X QTM
-    logical, optional, intent(in) :: T_Deriv_Flag(:)  ! User's deriv. flags for
-    !          Temperature.  Needed only if present(dHidTlm).
-    real(rg), optional, intent(in) :: Z_Basis(:)   ! Vertical temperature basis.
+    !          0.0 for all temperature coefficients. This is an extract of
+    !          height X zeta_basis X QTM adjacent to the path.
+    type(grids_t), optional, intent(in) :: T_Sv    ! State vector temperature
     !          Needed only if present(dHidTlm).
     real(rp), optional, intent(in) :: Z_Ref(:)     ! -log pressures (zetas) for
     !          which derivatives are needed.  Only the parts from the tangent
@@ -78,8 +80,9 @@ contains
     real(rp), optional, intent(out), target :: ddHtdHtdTl0(:)  ! Second
     !          order derivatives of height w.r.t T_Ref at the tangent only --
     !          used for antenna affects. Computed if present(dHidTlm).
-    real(rp), optional, intent(out) :: dHitdTlm(:,:)   ! Derivative of path
-    !          position wrt temperature state vector (z_basis X QTM)
+    real(rp), optional, intent(out), target :: dHitdTlm(:,:,:)   ! Derivative of
+    !          path position wrt temperature state vector
+    !          (S X t_sv%zet_basis X # vertices adjacent to path)
     real(rp), optional, intent(out), target :: dHtdTl0(:)      ! First order
     !          derivatives of height w.r.t T_Ref at the tangent only.  Computed
     !          if present(dHidTlm).
@@ -87,6 +90,10 @@ contains
     !          pressure at the tangent.  Computed if present(dHidTlm).
     type(weight_ZQ_t), optional, intent(out) :: Eta_zQT(:) ! Interpolation
     !          coefficients from QTM to path for temperature.
+    logical, optional, intent(out) :: Do_Calc_T(:,:,:)     ! Eta_zQT /= 0 on
+    !          path Z X temperature Z X temperature QTM grid
+    logical, optional, intent(out) :: Do_Calc_Hyd(:,:,:)   ! dHitdTlm /= 0 on
+    !          path Z X temperature Z X temperature QTM grid
     real(rp), optional, intent(out) :: Tan_T ! temperature at the tangent
 
     integer :: I
@@ -98,38 +105,42 @@ contains
 
     ! Path quantities.  These involve only horizontal interpolation because
     ! temperature and dHidZij were interpolated onto the GL grid by
-    ! Two_D_Hydrostatic. The interpolation coefficients were computed and
-    ! put into S by Metrics_3D.
+    ! Two_D_Hydrostatic. The horizontal interpolation coefficients from
+    ! vertices adjacent to the path onto the path, were computed and put into
+    ! S%Coeff by Metrics_3D.
     do i = 1, n_path
       nc = s(i)%n_coeff
       iz = s(i)%h_ind
-      t_path(i) =  dot_product ( t_ref(iz,s(i)%ser(:nc)),   s(i)%coeff(:nc) )
-      dHitdZi(i) = dot_product ( dHidZij(iz,s(i)%ser(:nc)), s(i)%coeff(:nc) )
+      t_path(i) =  dot_product ( t_ref(iz,s(i)%pv(:nc)),   s(i)%coeff(:nc) )
+      dHitdZi(i) = dot_product ( dHidZij(iz,s(i)%pv(:nc)), s(i)%coeff(:nc) )
     end do
 
     ! Optional tangent quantities.
     if ( n_tan <= n_path ) then
       if ( present(tan_t) ) tan_t = t_path(n_tan)
       if ( present(dHtdZt) ) dHtdZt = dHitdZi(n_tan)
-
-      if ( present(dHidTlm) ) &
-        & call Temperature_Derivatives ( s(n_tan), size(z_basis) )
+      if ( present(dHidTlm) ) call Temperature_Derivatives ( s(n_tan) )
     end if
 
   contains
 
-    subroutine Temperature_Derivatives ( TP, Z_Coeffs )
-      ! This is a subroutine instead of inline so that the references to the
-      ! size of Z_Basis in the dimensions of automatic variables are only
-      ! attempted if Z_Basis is present.
-      type(S_QTM_t), intent(in) :: TP  ! Tangent point
-      integer, intent(in) :: Z_Coeffs  ! size(z_basis) = size(T_ref,1)
+    subroutine Temperature_Derivatives ( TP )
 
-      real(rp), pointer :: ddH2(:,:) ! Two-d (V x H) pointer for ddHtdHtdTl0
-      real(rp), pointer :: dH2(:,:)  ! Two-d (V x H) pointer for dHtdTl0
-      integer :: I
+      type(S_QTM_t), intent(in) :: TP  ! Tangent point
+
+      real(rp), pointer :: ddH2(:,:) ! Two-d (Z x H) pointer for ddHtdHtdTl0
+      logical, pointer :: dF2(:,:)   ! Two-d (Z x H) pointer for t_sv%deriv_flags
+      real(rp), pointer :: dH2(:,:)  ! Two-d (Z x H) pointer for dHtdTl0
+      integer :: IC     ! Index of a coefficient in S, 1...S(ip)%nc
+      integer :: IP     ! Index of vertex adjacent to path
+      integer :: IS     ! Index of point on path
       integer :: IZ     ! First (vertical) subscript at tangent
       integer :: NC     ! Number of interpolating coefficients at the tangent
+      integer :: P_Coeffs   ! t_sv%l_p(1)
+      integer :: Sv_Z       ! Zeta subscript for t_sv%deriv_flags
+      real(rp) :: W         ! Horizontal interpolating coefficient from one
+                            ! profile to one path point
+      integer :: Z_Coeffs   ! t_sv%l_z(1) = size(T_ref,1)
 
       ! Adjust the 2d hydrostatic temperature derivative relative to the
       ! surface. Even though this is updated on every invocation, that is,
@@ -139,10 +150,13 @@ contains
       ! the same as starting from the original dHidTlm and updating with the
       ! latest S.  The algebra is horrible, but Maple has verified this.
 
+      p_coeffs = t_sv%l_p(1)
+      z_coeffs = t_sv%l_z(1)
+
       nc = tp%n_coeff
-      do i = 1, z_coeffs
-        dHidTlm(:,i,:) = dHidTlm(:,i,:) - &
-          & dot_product ( dHidTlm(1,i,tp%ser(:nc)), tp%coeff(:nc) )
+      do sv_z = 1, z_coeffs
+        dHidTlm(:,sv_z,:) = dHidTlm(:,sv_z,:) - &
+          & dot_product ( dHidTlm(1,sv_z,tp%pv(:nc)), tp%coeff(:nc) )
       end do
 
       iz = tp%h_ind
@@ -150,13 +164,40 @@ contains
       ddHtdHtdTl0 = 0
       dH2(1:z_coeffs,1:size(t_ref,2)) => dHtdTl0
       ddH2(1:z_coeffs,1:size(t_ref,2)) => ddHtdHtdTl0
-      do i = 1, z_coeffs
-        dH2(i,tp%ser(:nc)) = dHidTlm(iz,i,tp%ser(:nc)) * tp%coeff(:nc)
-        ddH2(i,tp%ser(:nc)) = ddHidHidTl0(iz,i,tp%ser(:nc)) * tp%coeff(:nc)
+      do sv_z = 1, z_coeffs
+        dH2(sv_z,tp%pv(:nc)) = dHidTlm(iz,sv_z,tp%pv(:nc)) * tp%coeff(:nc)
+        ddH2(sv_z,tp%pv(:nc)) = ddHidHidTl0(iz,sv_z,tp%pv(:nc)) * tp%coeff(:nc)
       end do
 
-      ! Get coefficients to interpolate to temperature (zeta,QTM) basis
-      call QTM_Interpolation_Weights ( QTM_Tree, z_basis, s, z_ref, eta_zQT )
+      dF2(1:z_coeffs,1:p_coeffs) => t_sv%deriv_flags
+      dHitdTlm = 0          ! Sparse representation might be desirable some day
+      do_calc_hyd = .false. ! Sparse representation might be desirable some day
+      do is = 1, size(s)         ! Path length
+        do ic = 1, s(ip)%n_coeff ! # horizontal interpolation coefficients
+          ip = s(ip)%pv(ic)      ! Index among adjacent profiles
+          w = s(is)%coeff(ic)    ! Horizontal interpolation coefficient from
+                                 ! profile IP to path point IS
+          do sv_z = 1, z_coeffs  ! Zeta levels of profiles
+            if ( dF2(sv_z,s(is)%ser(ic)) ) then ! Derivative flag for temperature
+              dHitdTlm(is,sv_z,ip) = &
+                & max(dHidTlm(s(is)%h_ind,sv_z,ip),0.0_rp) * w
+              do_calc_hyd(is,sv_z,ip) = dHitdTlm(is,sv_z,ip) /= 0
+            end if
+          end do
+        end do
+      end do
+
+      ! Get coefficients to interpolate from temperature (zeta, adjacent-to-path)
+      ! basis to the path, noting where nonzeros are relative to the QTM
+      call QTM_Interpolation_Weights ( QTM_Tree, t_sv%zet_basis, s, z_ref, eta_zQT )
+      do_calc_t = .false.
+      do is = 1, size(s)         ! Path length
+        do ic = 1, eta_zQT(is)%n ! Number of nonzero coefficients
+          !         path Z      temperature Z        QTM serial #
+          do_calc_t(s(is)%h_ind,eta_zQT(is)%w(ic)%nz,eta_zQT(is)%w(ic)%n) = &
+            & .true. ! There are only nonzero coefficients in eta_zQT
+        end do
+      end do
 
     end subroutine Temperature_Derivatives
 
@@ -175,6 +216,9 @@ contains
 end module More_Metrics_3D_m
 
 ! $Log$
+! Revision 2.3  2016/11/17 01:47:40  vsnyder
+! Use the Grids_t structure for temperature instead of pieces of it
+!
 ! Revision 2.2  2016/11/03 19:11:47  vsnyder
 ! Inching toward 3D forward model
 !
