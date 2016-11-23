88458d3587fb71a5c204e448f1ceec076773d9ca
vsnyder <>
2016-11-23 21:32:25 +0000

    rev 2.2

--- lib/Get_Eta_List_m.f90
+++ lib/Get_Eta_List_m.f90
@@ -11,17 +11,24 @@
 
 module Get_Eta_List_m
 
-  use Indexed_Values_m, only: Value_1D_List_t, Value_1D_t
+  use Indexed_Values_m, only: Value_1D_List_t, Value_1D_t, &
+    & Value_2D_List_t, Value_3D_List_t, &
+    & Value_QTM_1D_List_t, Value_QTM_2D_List_t, Value_QTM_3D_List_t
 
   implicit NONE
 
   private
 
-  public :: Eta_List_1D
+  public :: Eta_List_1D, Eta_List_1D_1D_2D, Eta_List_1D_2D_3D
+  public :: Eta_List_1D_QTM_1D_2D, Eta_List_1D_QTM_2D_3D
 
   ! Compute Eta to interpolate from Basis to Grid
   interface Eta_List_1D
     module procedure Eta_List_1D_D, Eta_List_1D_S
+    module procedure Eta_List_1D_1D_2D!_d, Eta_List_1D_1D_2D_s
+    module procedure Eta_List_1D_2D_3D!_d, Eta_List_1D_2D_3D_s
+    module procedure Eta_List_1D_QTM_1D_2D!_d, Eta_List_1D_QTM_1D_2D_s
+    module procedure Eta_List_1D_QTM_2D_3D!_d, Eta_List_1D_QTM_2D_3D_s
   end interface
 
 !---------------------------- RCS Module Info ------------------------------
@@ -44,6 +51,34 @@ contains
     include "Eta_List_1D.f9h"
   end subroutine Eta_List_1D_S
 
+  ! Compute Eta to interpolate from 2-D basis to 1-D grid.  The matrix C
+  ! is the outer product of interpoation matrices A and B, all represented
+  ! as sparse lists.
+  subroutine Eta_List_1D_1D_2D ( A, B, C )
+    include "Eta_List_1D_1D_2D.f9h"
+  end subroutine Eta_List_1D_1D_2D
+
+  ! Compute Eta to interpolate from 3-D basis to 1-D grid.  The matrix C
+  ! is the outer product of interpoation matrices A and B, all represented
+  ! as sparse lists.
+  subroutine Eta_List_1D_2D_3D ( A, B, C )
+    include "Eta_List_1D_2D_3D.f9h"
+  end subroutine Eta_List_1D_2D_3D
+
+  ! Compute Eta to interpolate from a 2-D QTM basis and a 1-D vertical
+  ! basis to a 1-D grid.  The matrix C is the outer product of
+  ! interpoation matrices A and B, all represented as sparse lists.
+  subroutine Eta_List_1D_QTM_1D_2D ( A, B, C )
+    include "Eta_List_1D_QTM_1D_2D.f9h"
+  end subroutine Eta_List_1D_QTM_1D_2D
+
+  ! Compute Eta to interpolate from a 2-D QTM basis, a 1-D vertical, and a
+  ! 1-D frequency  basis to a 1-D grid.  The matrix C is the outer product
+  ! of interpoation matrices A and B, all represented as sparse lists.
+  subroutine Eta_List_1D_QTM_2D_3D ( A, B, C )
+    include "Eta_List_1D_QTM_2D_3D.f9h"
+  end subroutine Eta_List_1D_QTM_2D_3D
+
 !=========================================================================
 
 !--------------------------- end bloc --------------------------------------
@@ -59,6 +94,10 @@ contains
 end module Get_Eta_List_m
 !---------------------------------------------------
 ! $Log$
+! Revision 2.2  2016/11/23 21:32:25  vsnyder
+! Add Eta_List_1D_... routines to compute outer products of lower-
+! dimensionality lists.
+!
 ! Revision 2.1  2016/11/23 00:05:47  vsnyder
 ! Initial commit
 !
