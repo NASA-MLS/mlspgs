e9dbe7f79b6065b879c8b313de139a5a135cecb6
pwagner <>
2021-02-19 00:29:46 +0000

    repaired many bugs; still unsatisfactory imo

--- lib/NeuralNetworkUtils_m.f90
+++ lib/NeuralNetworkUtils_m.f90
@@ -19,6 +19,7 @@ MODULE NeuralNetUtils_M
 
   USE Dump_0, only: Dump
   USE HighOutput, only: OutputNamedValue
+  USE MLSHDF5, only: SaveAsHDF5DS
   USE MLSKinds, only: R8
   USE MLSMessageModule, ONLY: MLSMessage, MLSMSG_Error
   IMPLICIT NONE
@@ -28,33 +29,42 @@ MODULE NeuralNetUtils_M
      !INTEGER :: NumMAFS ! <----- it's only ever going to be one MAF, right?
      INTEGER :: NumChannels
      INTEGER :: NumMIFs
-     REAL(R8), DIMENSION(:,:),ALLOCATABLE :: values
+     REAL(R8), dimension(:,:), allocatable :: values
   END TYPE radiance_T
 
-  ! According to Frank, the 
   TYPE NeuralNetCoeffs_T
      ! Will be allocated in the caller. 
      ! [3]  Number of Bands
      character(len=32), dimension(:), allocatable :: Bands
+     
+     ! The first index is M, the max num of channels used (probably 51)
+     ! The second index is the number of Bands (currently 3)
+     ! We need this info because the Neural Net doesn't currently use
+     ! every channel in the DACS 
+     ! (and someday may even skip some in the non-DACS)
+     ! So (M,3)
+     Integer,  dimension(:,:), allocatable :: Channels_In_Each_Band
 
      ! these are the dimensions for each part (assuming that we keep
      ! with Frank set up for Temperature retrieval)
 
      ! [42], number of pressure levels
-     REAL(R8), DIMENSION(:),allocatable :: Intercepts_Hidden_Labels_Layer
+     REAL(R8), dimension(:), allocatable :: Intercepts_Hidden_Labels_Layer
+     REAL(R8), dimension(:), allocatable :: Output_Pressure_Levels
+     Integer,  dimension(:), allocatable :: Output_Pressure_Levels_Indices
 
      ! [5078]. Number of Neurons in hidden layers
-     REAL(R8), DIMENSION(:),allocatable :: Intercepts_Hidden_Layer_1
-     REAL(R8), DIMENSION(:),allocatable :: Intercepts_Hidden_Layer_2
+     REAL(R8), dimension(:), allocatable :: Intercepts_Hidden_Layer_1
+     REAL(R8), dimension(:), allocatable :: Intercepts_Hidden_Layer_2
 
-     ! [5078, 41] = [nNeurons, nLevels]
-     REAL(R8), DIMENSION(:,:),allocatable :: Weights_Hidden_Labels_Layer
-     REAL(R8), DIMENSION(:,:),allocatable :: Weights_Hidden_Layer_1
-     REAL(R8), DIMENSION(:,:),allocatable :: Weights_Hidden_Layer_2
+     ! [5078, 42] = [nNeurons, nLevels]
+     REAL(R8), dimension(:,:), allocatable :: Weights_Hidden_Labels_Layer
+     REAL(R8), dimension(:,:), allocatable :: Weights_Hidden_Layer_1
+     REAL(R8), dimension(:,:), allocatable :: Weights_Hidden_Layer_2
 
-     ! [41]. Number of levels
-     REAL(R8), DIMENSION(:),allocatable :: Normalization_Labels_Max
-     REAL(R8), DIMENSION(:),allocatable :: Normalization_Labels_Min
+     ! [42]. Number of levels
+     REAL(R8), dimension(:), allocatable :: Normalization_Labels_Max
+     REAL(R8), dimension(:), allocatable :: Normalization_Labels_Min
      
      ! [75]. Number of MIFs
      integer, dimension(:), allocatable :: MIFs
@@ -62,8 +72,8 @@ MODULE NeuralNetUtils_M
      ! [7575] number of 'variables', i.e. 2*25*75 + 51*75
      ! This is band 1[channel X MIF] + band 8 [channel X MIF ]  and 
      ! band 22 [channel X MIF]
-     REAL(R8), DIMENSION(:),allocatable :: Standardization_Brightness_Temperature_Mean
-     REAL(r8), DIMENSION(:),allocatable :: Standardization_Brightness_Temperature_Std
+     REAL(R8), dimension(:), allocatable :: Standardization_Brightness_Temperature_Mean
+     REAL(r8), dimension(:), allocatable :: Standardization_Brightness_Temperature_Std
 
   END TYPE NeuralNetCoeffs_T
 
@@ -113,15 +123,20 @@ CONTAINS
     if ( present(Details) ) myDetails = Details
     
     call outputNamedValue ( 'Number of Bands', size(Coeffs%Bands) )
-    if ( myDetails > 0 ) &
+    if ( myDetails > 0 ) then
     call dump ( Coeffs%Bands, 'Bands' )
+    call dump ( Coeffs%Channels_In_Each_Band, 'Channels' )
+    endif
 
     call outputNamedValue ( 'Number of MIFs', size(Coeffs%MIFs) )
     if ( myDetails > 0 ) &
     call dump ( Coeffs%MIFs, 'MIFs' )
     
-    call outputNamedValue ( 'Number of pressure levels', size(Coeffs%Intercepts_Hidden_Labels_Layer) )
+    call outputNamedValue ( 'Number of pressure levels', &
+         & SIZE(Coeffs%Intercepts_Hidden_Labels_Layer) )
     if ( myDetails > 0 ) then
+    call dump ( Coeffs%Output_Pressure_Levels, 'Output_Pressure_Levels' )
+    call dump ( Coeffs%Output_Pressure_Levels_Indices, 'Output_Pressure_Indices' )
     call dump ( Coeffs%Intercepts_Hidden_Labels_Layer, 'Intercepts layer' )
     call dump ( Coeffs%Normalization_Labels_Max, 'Normalization Labels Max' )
     call dump ( Coeffs%Normalization_Labels_Min, 'Normalization Labels Max' )
@@ -151,7 +166,8 @@ CONTAINS
     call dump ( Coeffs%Standardization_Brightness_Temperature_Std , 'Std_Bright_Temp_Std ' )
   end subroutine Dump_Coeffs
 
-  FUNCTION NeuralNetFit(nnInputData, nnCoeffs, nHL,TYPE) RESULT(prediction)
+  FUNCTION NeuralNetFit(nnInputData, nnCoeffs, nHL, TYPE, &
+    & FileID ) RESULT(prediction)
 
     ! Fortran version of the IDL code in `example_idl.pro' from Frank
 
@@ -159,27 +175,41 @@ CONTAINS
     TYPE (NeuralNetCoeffs_T), INTENT(IN)  :: nnCoeffs
     INTEGER,intent(in) :: nHL ! number of hidden layers. Will probably always be 2
     CHARACTER(len=*),INTENT(IN) :: TYPE ! type  may be either 'tanh' or 'sigmoid'
-    REAL(r8), DIMENSION(:), ALLOCATABLE :: prediction
+    REAL(r8), dimension(:), ALLOCATABLE :: prediction
+    integer, optional, intent(in) :: FileID ! For optionally saving Datasets
 
 
 
     ! Local variables
     LOGICAL,SAVE :: first = .TRUE.
     INTEGER, SAVE:: nMIFs
-    INTEGER, SAVE, DIMENSION(2) :: nChans
+    INTEGER, SAVE, dimension(2) :: nChans
     INTEGER, SAVE :: nSurfs, nVars, nNeurons
 
     ! Various working space arrays
-    REAL(r8),DIMENSION(:),ALLOCATABLE :: working_space
-    REAL(r8), DIMENSION(:), ALLOCATABLE  :: NeuronValues
+    REAL(r8),dimension(:), allocatable :: working_space
+    REAL(r8), dimension(:), ALLOCATABLE  :: NeuronValues, NeuronValues2
 
     real(r8) :: y_pred
 
-    INTEGER :: c,n,m,jj,s ! various counters
+    INTEGER :: c,n,m,jj,s,v! various counters
     INTEGER, dimension(2) :: dims2
 
 
     ! ----------- executable statements -----------------
+
+    ! make sure TYPE is correct.
+    IF ( TRIM(TYPE) .NE. 'tanh' .AND. &
+         & TRIM(TYPE) .NE. 'sigmoid' ) THEN 
+       CALL MLSMessage(MLSMSG_error, ModuleName, &
+           & "input var TYPE must equal either 'tanh' or 'sigmoid'")
+    ENDIF
+    IF (NHL /=  1 .AND. NHL /= 2) THEN 
+       CALL MLSMessage(MLSMSG_error, ModuleName, &
+           & "NHL must equal either 1 or 2")
+     ENDIF
+
+
     call Dump ( nnCoeffs )
 
     ! These are fixed for the run
@@ -195,15 +225,20 @@ CONTAINS
       first=.FALSE.
     ENDIF
 
-    ! allocate space                                   
-    ALLOCATE( working_space(nVars )) ! nVars = 2*Chanels(bands 1&8) *  nMIFs + nChans(22)*nMIFs
+    ! allocate space
+    ! nMIFs=75 for all bands (so far)
+    ! nVars = 2*Chanels(bands 1&8) *  nMIFs + nChans(22)*nMIFs
+    ALLOCATE( working_space(nVars )) 
     ALLOCATE(neuronValues(nNeurons))
+    ALLOCATE(neuronValues2(nNeurons))
     ALLOCATE(prediction(nSurfs))
 
     neuronValues = 0.0
     prediction = 0.0
 
-    ! load the data into the working_space
+    ! load the data into the working_space. The data is arranged as
+    ! the radiances in MIF, then channel order for band 1, band 8 and
+    ! then band 22
     jj=1
 
     ! Load band1
@@ -225,7 +260,7 @@ CONTAINS
     ! load band 22
     DO c=1,nChans(2)
       DO m=1,nMIFs
-        working_space( jj ) = nnInputData%Band_8_Radiances%Values(c,m)
+        working_space( jj ) = nnInputData%Band_22_Radiances%Values(c,m)
         jj = jj + 1
       END DO
     END DO
@@ -233,58 +268,86 @@ CONTAINS
 
     call OutputNamedValue ( 'nVars', nVars )
     call OutputNamedValue ( 'shape(WHL1)', shape(nnCoeffs%Weights_Hidden_Layer_1) )
-      
-    DO n=1,nNeurons
-      neuronValues(n) = DOT( working_space, nnCoeffs%Weights_Hidden_Layer_1(n,:))
+    
+    ! Optionally save this dataset for comparison with
+    ! PyThon or iDl or whaTeVer you guys use these daYs
+    if ( present(FileID) ) &
+      & call SaveAsHDF5DS ( FileID, 'Radiances', working_space(1:jj-1) )
+
+    ! unlike the IDL code, this routine will only ever see 1 MAF
+    ! (sample) at a time, so we don't need the IDL loop over `samples'
+    ! working_space is [nVars]
+    ! weights is [nVars,nNeurons]
+    ! neuronValues is [nNeurons]
+    DO v=1,nVars
+      neuronValues = neuronValues + &
+           & SUM( working_space(v) * nnCoeffs%Weights_Hidden_Layer_1(v,:))
     END DO
+
+
+    IF (TRIM(TYPE) .EQ. 'tanh') THEN 
+      neuronValues=TANH(neuronValues + & 
+           & nnCoeffs%Intercepts_Hidden_Layer_1)
+    ELSE IF (TRIM(TYPE) .EQ. 'sigmoid') THEN 
+      CALL SIGMOID(neuronValues + & 
+           & nnCoeffs%Intercepts_Hidden_Layer_1, neuronValues)
+
+    ENDIF
     
     call OutputNamedValue ( 'num neurons', nNeurons )
     call OutputNamedValue ( 'shape(WHLL)', shape(nnCoeffs%Weights_Hidden_Labels_Layer) )
-      
-    DO s=1,nSurfs 
 
+
+    neuronValues2=0.0
+    ! Loop over surfaces
+    DO s=1,nSurfs 
       ! neuron values 'activation' 
       IF (nHL .EQ. 1) THEN 
 
         IF (TRIM(TYPE) .EQ. 'tanh') THEN
-          neuronValues = TANH(neuronValues + nnCoeffs%Intercepts_Hidden_Layer_1)
+          neuronValues = TANH(neuronValues + &
+               & nnCoeffs%Weights_Hidden_Labels_Layer(:,s)) + &
+               & nnCoeffs%Intercepts_Hidden_labels_layer(s)
         ELSE IF(trim(TYPE) .EQ. 'sigmoid') THEN 
-          call SIGMOID(neuronValues + nnCoeffs%Intercepts_Hidden_Layer_2,neuronValues)
+          CALL SIGMOID(neuronValues + &
+               & nnCoeffs%Weights_Hidden_Labels_Layer(:,s), neuronValues)
+               neuronValues = neuronValues + nnCoeffs%Intercepts_Hidden_Labels_Layer(s)
         ENDIF
 
       ELSEIF (nHL .EQ. 2) THEN 
 
+        DO n=1,nNeurons
+          neuronValues2 = neuronValues2 + &
+               & SUM( neuronValues(n) * nnCoeffs%Weights_Hidden_Layer_2(n,:))
+        END DO
+
+
         IF (TRIM(TYPE) .EQ. 'tanh') THEN
-          neuronValues = TANH(neuronValues + nnCoeffs%Intercepts_Hidden_Layer_2)
+          neuronValues2 = TANH(neuronValues2 + nnCoeffs%Intercepts_Hidden_Layer_2)
         ELSE IF(trim(TYPE) .EQ. 'sigmoid') THEN 
-          call SIGMOID(neuronValues + nnCoeffs%Intercepts_Hidden_Layer_2,neuronValues)
+          call SIGMOID(neuronValues2 + nnCoeffs%Intercepts_Hidden_Layer_2,neuronValues)
         ENDIF
-
-      ELSE 
-
-        CALL MLSMessage(MLSMSG_error, ModuleName, &
-             & "input var TYPE must equal either 'tanh' or 'sigmoid'")
-
       ENDIF
 
-      y_pred = DOT( neuronValues,nnCoeffs%Weights_Hidden_Labels_Layer(:,s)) + &
-           nnCoeffs%Intercepts_Hidden_Labels_Layer(s)
-      
+      y_pred = DOT( neuronValues2, &
+           & nnCoeffs%Weights_Hidden_Labels_Layer(:,s) ) + &
+           &   nnCoeffs%Intercepts_Hidden_Labels_Layer(s)
 
       ! Calculate the final product
-       prediction(s) = y_pred * &
-            & ( nnCoeffs%Normalization_Labels_Max(s) - &
-            &   nnCoeffs%Normalization_Labels_Min(s)) + &
-            &   nnCoeffs%Normalization_Labels_Min(s)
+      prediction(s) = y_pred * &
+           & ( nnCoeffs%Normalization_Labels_Max(s) - &
+           &   nnCoeffs%Normalization_Labels_Min(s)) + &
+           &   nnCoeffs%Normalization_Labels_Min(s)
 
     ENDDO ! loop over pressure surfaces
+
 
   END function NeuralNetFit
 
 
   ! Dot product.
   REAL(r8) FUNCTION dot(X,Y)
-    REAL(r8), DIMENSION(:),INTENT(IN):: x,y
+    REAL(r8), dimension(:),INTENT(IN):: x,y
     INTEGER :: i,nn
     dot=0
     nn=size(x)
@@ -298,10 +361,10 @@ CONTAINS
     END DO
   END FUNCTION dot
 
-  ! Sigmoid function 
+  ! Sigmoid subroutine
   SUBROUTINE sigmoid(x,s) 
-    REAL(r8),DIMENSION(:),INTENT(in) :: x
-    REAL(r8),DIMENSION(:) :: s
+    REAL(r8),dimension(:),INTENT(in) :: x
+    REAL(r8),dimension(:) :: s
     s = 1.0/(1.0+EXP(-x))
   END SUBROUTINE sigmoid
 
@@ -317,3 +380,6 @@ CONTAINS
 
 END MODULE NeuralNetUtils_M
 ! $Log$
+! Revision 2.1  2021/02/05 05:14:40  pwagner
+! First commit
+!
